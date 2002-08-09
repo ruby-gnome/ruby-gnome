0b970bee78eafff1408d2dd053ec4464ff9ba748
Masahiro Sakai <sakai@tom.sfc.keio.ac.jp>
2002-08-09 14:32:55 +0000

    optimize

--- glib/src/utils_int64.c
+++ glib/src/utils_int64.c
@@ -4,7 +4,7 @@
   utils_int64.c -
 
   $Author: sakai $
-  $Date: 2002/08/09 13:49:18 $
+  $Date: 2002/08/09 14:32:55 $
 
   Copyright (C) 2002  Masahiro Sakai
                       Kenichi Komiya
@@ -14,7 +14,6 @@
 #include <glib/gtypes.h>
 #include "ruby.h"
 
-#if 0
 static ID id_and;
 static ID id_rshift;
 static ID id_lshift;
@@ -22,14 +21,12 @@ static ID id_lt;
 static ID id_plus;
 static ID id_uminus;
 static ID id_abs;
-#endif
+static VALUE max_PRUint32;
 
 typedef guint64 PRUint64;
 typedef gint64 PRInt64;
 
-#define max_PRUint32 G_MAXUINT
-
-#define LL_ZERO 0
+#define LL_ZERO G_GINT64_CONSTANT(0)
 #define LL_UI2L(lhs,rhs) ((lhs)=(rhs))
 #define LL_L2UI(lhs,rhs) ((lhs)=(guint32)(rhs))
 #define LL_SHL(lhs,v1,v2) ((lhs)=(v1)<<(v2))
@@ -38,17 +35,19 @@ typedef gint64 PRInt64;
 #define LL_NEG(lhs,rhs) ((lhs)=-(rhs))
 #define LL_CMP(v1,op,v2) ((v1) op (v2))
 
-//////////////////////////////////////////////////////////////////////////////
-// following is ripped from rbXPCOM
-// http://www.ruby-lang.org/en/raa-list.rhtml?name=rbXPCOM
-// Copyright (C) 2001 Kenichi Komiya
+/**********************************************************************/
+/*
+   following is ripped from rbXPCOM-0.0.3
+   http://www.ruby-lang.org/en/raa-list.rhtml?name=rbXPCOM
+   Copyright (C) 2001 Kenichi Komiya <kom@mail1.accsnet.ne.jp>
+*/ 
 
 static PRUint64
 RubyTo64BitInt(VALUE aRuby)
 {
-    VALUE bitMask = UINT2NUM(max_PRUint32);
-    VALUE lo = rb_funcall(aRuby, rb_intern("&"), 1, bitMask);
-    VALUE hi = rb_funcall(aRuby, rb_intern(">>"), 1, INT2FIX(32));
+    VALUE bitMask = max_PRUint32;
+    VALUE lo = rb_funcall(aRuby, id_and, 1, bitMask);
+    VALUE hi = rb_funcall(aRuby, id_rshift, 1, INT2FIX(32));
     PRUint64 result, hi64, lo64;
     LL_UI2L(hi64, NUM2UINT(hi));
     LL_UI2L(lo64, NUM2UINT(lo));
@@ -68,9 +67,9 @@ RubyToPRUint64(VALUE aRuby)
 static PRInt64
 RubyToPRInt64(VALUE aRuby) 
 {
-    if(RTEST(rb_funcall(aRuby, rb_intern("<"), 1, INT2FIX(0))))
+    if(RTEST(rb_funcall(aRuby, id_lt, 1, INT2FIX(0))))
     {
-        VALUE absRuby = rb_funcall(aRuby, rb_intern("abs"), 0);
+        VALUE absRuby = rb_funcall(aRuby, id_abs, 0);
         PRInt64 result;
         LL_NEG(result, RubyTo64BitInt(absRuby));
         return result;
@@ -88,8 +87,8 @@ RubyFrom64BitInt(PRUint64 aNative)
     {
     VALUE lo = UINT2NUM(lo64);
     VALUE hi = UINT2NUM(hi64);
-    VALUE hiRuby = rb_funcall(hi, rb_intern("<<"), 1, INT2FIX(32));
-    return rb_funcall(hiRuby, rb_intern("+"), 1, lo);
+    VALUE hiRuby = rb_funcall(hi, id_lshift, 1, INT2FIX(32));
+    return rb_funcall(hiRuby, id_plus, 1, lo);
     }
 }
 
@@ -106,14 +105,14 @@ PRInt64ToRuby(PRInt64 aNative)
     {        
         PRUint64 abs64;
         LL_NEG(abs64, aNative);
-        return rb_funcall(RubyFrom64BitInt(abs64), rb_intern("-@"), 0);
+        return rb_funcall(RubyFrom64BitInt(abs64), id_uminus, 0);
     }
     else
         return RubyFrom64BitInt((PRUint64)aNative);
 }
 
-// end of ripping
-//////////////////////////////////////////////////////////////////////////////
+/* end of ripping */
+/**********************************************************************/
 
 VALUE
 rbgobj_int64_to_num(guint64 val)
@@ -139,18 +138,18 @@ rbgobj_num_to_uint64(VALUE val)
     return RubyToPRUint64(val);
 }
 
-//////////////////////////////////////////////////////////////////////////////
+/**********************************************************************/
 
 void
 Init_utils_int64()
 {
-#if 0
     id_and    = rb_intern("&");
     id_rshift = rb_intern(">>");
-    id_lshift = rb_intern(">>");
+    id_lshift = rb_intern("<<");
     id_lt     = rb_intern("<");
     id_plus   = rb_intern("+");
     id_uminus = rb_intern("-@");
     id_abs    = rb_intern("abs");
-#endif
+
+    max_PRUint32 = UINT2NUM(G_MAXUINT);
 }
