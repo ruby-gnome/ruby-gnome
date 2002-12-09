64a5310efcc69a4abbc1a5a87c84e43f44a46e66
Masao Mutoh <mutoh@highway.ne.jp>
2002-12-09 16:32:07 +0000

    * Restore previous version.

--- gnome/extconf.rb
+++ gnome/extconf.rb
@@ -22,7 +22,7 @@ begin
   config_cmd = with_config("pkg-config", "pkg-config libgnomeui-2.0")
   config_libs = "--libs"
   config_cflags = "--cflags"
-  config_library = "gnomeui gtkxmhtml"
+  config_library = ""
   version = `#{config_cmd} --version`
   if not version.chomp.empty? then
     $libs += ' ' + `#{config_cmd} #{config_libs} #{config_library}`.chomp
