8692995466c17e5dc22078df2059c8285495cf3d
Masao Mutoh <mutoh@highway.ne.jp>
2002-12-25 13:18:07 +0000

    * src/rbgtkviewport.c: Change behavior of Gtk::Viewport.new to have to    set hadj, vadj.

--- gtk/src/rbgtkviewport.c
+++ gtk/src/rbgtkviewport.c
@@ -4,7 +4,7 @@
   rbgtkviewport.c -
 
   $Author: mutoh $
-  $Date: 2002/10/23 18:02:19 $
+  $Date: 2002/12/25 13:18:07 $
 
   Copyright (C) 1998-2000 Yukihiro Matsumoto,
                           Daisuke Kanda,
@@ -14,18 +14,11 @@
 #include "global.h"
 
 static VALUE
-vport_initialize(argc, argv, self)
-    int argc;
-    VALUE *argv;
-    VALUE self;
+vport_initialize(self, hadj, vadj)
+    VALUE self, hadj, vadj;
 {
-    VALUE arg1, arg2;
-    GtkAdjustment *h_adj = NULL;
-    GtkAdjustment *v_adj = NULL;
-
-    rb_scan_args(argc, argv, "02", &arg1, &arg2);
-    if (!NIL_P(arg1)) h_adj = GTK_ADJUSTMENT(RVAL2GOBJ(arg1));
-    if (!NIL_P(arg2)) v_adj = GTK_ADJUSTMENT(RVAL2GOBJ(arg2));
+    GtkAdjustment *h_adj = GTK_ADJUSTMENT(RVAL2GOBJ(hadj));
+    GtkAdjustment *v_adj = GTK_ADJUSTMENT(RVAL2GOBJ(vadj));
 
     RBGTK_INITIALIZE(self, gtk_viewport_new(h_adj, v_adj));
     return Qnil;
@@ -35,5 +28,5 @@ void
 Init_gtk_viewport()
 {
     VALUE gViewport = G_DEF_CLASS(GTK_TYPE_VIEWPORT, "Viewport", mGtk);
-    rb_define_method(gViewport, "initialize", vport_initialize, -1);
+    rb_define_method(gViewport, "initialize", vport_initialize, 2);
 }
