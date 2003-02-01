750685b26a37c4693fcadcffa29ad49051e96aaf
Masao Mutoh <mutoh@highway.ne.jp>
2003-02-01 15:28:05 +0000

    * src/rbpangofontmap.c: Comment out test code.

--- pango/src/rbpangofontmap.c
+++ pango/src/rbpangofontmap.c
@@ -4,18 +4,18 @@
   rbpangofontmap.c -
 
   $Author: mutoh $
-  $Date: 2003/02/01 15:24:26 $
+  $Date: 2003/02/01 15:28:05 $
 
   Copyright (C) 2002,2003 Masao Mutoh <mutoh@highway.ne.jp>
 ************************************************/
 
 #include "rbpango.h"
-#include "pango/pangoft2.h"
 
 #define _SELF(self) (PANGO_FONT_MAP(RVAL2GOBJ(self)))
 #define RVAL2DESC(d) ((PangoFontDescription*)RVAL2BOXED(d, PANGO_TYPE_FONT_DESCRIPTION))
 #define RVAL2LANG(l) ((PangoLanguage*)RVAL2BOXED(l, PANGO_TYPE_LANGUAGE))
 
+/*
 static VALUE
 font_map_initialize(self)
     VALUE self;
@@ -23,6 +23,7 @@ font_map_initialize(self)
     G_INITIALIZE(self, pango_ft2_font_map_new());
     return Qnil;
 }
+*/
 
 static VALUE
 font_map_load_font(self, context, desc)
@@ -54,7 +55,9 @@ Init_pango_font_map()
 {
     VALUE pMap = G_DEF_CLASS(PANGO_TYPE_FONT_MAP, "FontMap", mPango);
     
+	 /*
     rb_define_method(pMap, "initialize", font_map_initialize, 0);
+	 */
     rb_define_method(pMap, "load_font", font_map_load_font, 2);
     rb_define_method(pMap, "load_fontset", font_map_load_fontset, 3);
 }
