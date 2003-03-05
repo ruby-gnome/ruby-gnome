6c433983cea1f494498fad609a0f1fa1406670a2
Masao Mutoh <mutoh@highway.ne.jp>
2003-03-05 17:21:56 +0000

    * src/rbgtkcombo.c: Remove Gtk::Combo#list, #button, #popup, #popwin, 	#set_item_string. 	Gtk::Combo.new accept an argument as string array.

--- gtk/src/rbgtkcombo.c
+++ gtk/src/rbgtkcombo.c
@@ -4,7 +4,7 @@
   rbgtkcombo.c -
 
   $Author: mutoh $
-  $Date: 2003/02/01 16:46:23 $
+  $Date: 2003/03/05 17:21:56 $
 
   Copyright (C) 2002,2003 Ruby-GNOME2 Project Team
   Copyright (C) 1998-2000 Yukihiro Matsumoto,
@@ -14,13 +14,6 @@
 
 #include "global.h"
 
-static VALUE
-combo_initialize(self)
-    VALUE self;
-{
-    RBGTK_INITIALIZE(self, gtk_combo_new());
-    return Qnil;
-}
 
 static VALUE
 combo_set_popdown_strings(self, ary)
@@ -40,6 +33,27 @@ combo_set_popdown_strings(self, ary)
 
     gtk_combo_set_popdown_strings(GTK_COMBO(RVAL2GOBJ(self)), glist);
     return self;
+}
+
+static VALUE
+combo_initialize(argc, argv, self)
+    int argc;
+    VALUE *argv;
+    VALUE self;
+{
+    VALUE ary;
+    GtkWidget* widget = NULL; 
+
+    rb_scan_args(argc, argv, "01", &ary);
+    widget = gtk_combo_new();
+
+    RBGTK_INITIALIZE(self, widget);
+
+    if (! NIL_P(ary)){
+        combo_set_popdown_strings(self, ary);
+    }
+
+    return Qnil;
 }
 
 static VALUE
@@ -70,16 +84,6 @@ combo_set_use_arrows_always(self, val)
 }
 
 static VALUE
-combo_set_item_string(self, item, val)
-    VALUE self, item, val;
-{
-    gtk_combo_set_item_string(GTK_COMBO(RVAL2GOBJ(self)),
-			      GTK_ITEM(RVAL2GOBJ(item)),
-			      NIL_P(val)?NULL:RVAL2CSTR(val));
-    return self;
-}
-
-static VALUE
 combo_disable_activate(self)
     VALUE self;
 {
@@ -94,52 +98,19 @@ combo_entry(self)
     return GOBJ2RVAL(GTK_COMBO(RVAL2GOBJ(self))->entry);
 }
 
-static VALUE
-combo_button(self)
-    VALUE self;
-{
-    return GOBJ2RVAL(GTK_COMBO(RVAL2GOBJ(self))->button);
-}
-
-static VALUE
-combo_popup(self)
-    VALUE self;
-{
-    return GOBJ2RVAL(GTK_COMBO(RVAL2GOBJ(self))->popup);
-}
-
-static VALUE
-combo_popwin(self)
-    VALUE self;
-{
-    return GOBJ2RVAL(GTK_COMBO(RVAL2GOBJ(self))->popwin);
-}
-
-static VALUE
-combo_list(self)
-    VALUE self;
-{
-    return GOBJ2RVAL(GTK_COMBO(RVAL2GOBJ(self))->list);
-}
-
 void 
 Init_gtk_combo()
 {
     VALUE gCombo = G_DEF_CLASS(GTK_TYPE_COMBO, "Combo", mGtk);
 
-    rb_define_method(gCombo, "initialize", combo_initialize, 0);
+    rb_define_method(gCombo, "initialize", combo_initialize, -1);
     rb_define_method(gCombo, "set_value_in_list", combo_set_val_in_list, 2);
     rb_define_method(gCombo, "set_use_arrows", combo_set_use_arrows, 1);
     rb_define_method(gCombo, "set_use_arrows_always", combo_set_use_arrows_always, 1);
-    rb_define_method(gCombo, "set_item_string", combo_set_item_string, 2);
     rb_define_method(gCombo, "set_popdown_strings", combo_set_popdown_strings, 1);
     rb_define_method(gCombo, "disable_activate", combo_disable_activate, 0);
 
     rb_define_method(gCombo, "entry", combo_entry, 0);
-    rb_define_method(gCombo, "button", combo_button, 0);
-    rb_define_method(gCombo, "popup", combo_popup, 0);
-    rb_define_method(gCombo, "popwin", combo_popwin, 0);
-    rb_define_method(gCombo, "list", combo_list, 0);
 
     G_DEF_SETTERS(gCombo);
 }
