a52f2c54123a4afb954496e24ec7361b54de429c
Masao Mutoh <mutoh@highway.ne.jp>
2003-05-24 03:27:02 +0000

    Add a file.

---
+++ gtk/src/rbgtkaccessible.c
@@ -0,0 +1,29 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
+/************************************************
+
+  rbgtkaccessible.c -
+
+  $Author: mutoh $
+  $Date: 2003/05/24 03:27:02 $
+
+  Copyright (C) 2003 Masao Mutoh
+************************************************/
+
+#include "global.h"
+
+static VALUE
+access_connect_widget_destroyed(self)
+     VALUE self;
+{
+    gtk_accessible_connect_widget_destroyed(GTK_ACCESSIBLE(RVAL2GOBJ(self)));
+    return self;
+}
+
+void
+Init_gtk_accessible()
+{
+    VALUE gAccess = G_DEF_CLASS(GTK_TYPE_ACCESSIBLE, "Accessible", mGtk);
+
+    rb_define_method(gAccess, "connect_widget_destroyed", access_connect_widget_destroyed, 0);
+}
+
