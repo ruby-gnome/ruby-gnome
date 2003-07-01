70a92c6883691b5493c336344c9e55aa224bd560
Masao Mutoh <mutoh@highway.ne.jp>
2003-07-01 14:45:18 +0000

    Added.

---
+++ gtk/src/rbgdkdisplay.c
@@ -0,0 +1,181 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
+/************************************************
+
+  rbgdkdisplay.c -
+
+  $Author: mutoh $
+  $Date: 2003/07/01 14:45:18 $
+
+  Copyright (C) 2002,2003 Ruby-GNOME2 Project Team
+************************************************/
+
+#include "global.h"
+
+#if GTK_MINOR_VERSION >= 2
+#define _DISPLAY(i) GDK_DISPLAY_OBJECT(RVAL2GOBJ(i))
+
+static VALUE
+gdkdisplay_initialize(argc, argv, self)
+    int argc;
+    VALUE self, *argv;
+{
+    if (argc == 0)
+    {
+      G_INITIALIZE(self, gdk_display_get_default());
+    }
+    else
+    {
+      G_INITIALIZE(self, gdk_display_open(RVAL2CSTR(argv[0])));
+    }
+    return Qnil;
+}
+
+static VALUE
+gdkdisplay_default(self)
+    VALUE self;
+{
+    return GOBJ2RVAL(gdk_display_get_default());
+}
+
+
+static VALUE
+gdkdisplay_name(self)
+    VALUE self;
+{
+    return rb_str_new2(gdk_display_get_name(_DISPLAY(self)));
+}
+
+static VALUE
+gdkdisplay_n_screens(self)
+    VALUE self;
+{
+    return INT2NUM(gdk_display_get_n_screens(_DISPLAY(self)));
+}
+
+static VALUE
+gdkdisplay_get_screen(self, num)
+    VALUE self, num;
+{
+    return GOBJ2RVAL(gdk_display_get_screen(_DISPLAY(self), NUM2INT(num)));
+}
+
+static VALUE
+gdkdisplay_default_screen(self)
+    VALUE self;
+{
+    return GOBJ2RVAL(gdk_display_get_default_screen(_DISPLAY(self)));
+}
+
+static VALUE
+gdkdisplay_pointer_ungrab(self,time)
+    VALUE self,time;
+{
+    gdk_display_pointer_ungrab(_DISPLAY(self),NUM2ULONG(time));
+    return self;
+}
+
+static VALUE
+gdkdisplay_keyboard_ungrab(self,time)
+    VALUE self,time;
+{
+    gdk_display_keyboard_ungrab(_DISPLAY(self),NUM2ULONG(time));
+    return self;
+}
+
+static VALUE
+gdkdisplay_pointer_grabbed(self)
+    VALUE self;
+{
+    return gdk_display_pointer_is_grabbed(_DISPLAY(self)) ? Qtrue : Qfalse;
+}
+
+static VALUE
+gdkdisplay_beep(self)
+    VALUE self;
+{
+    gdk_display_beep(_DISPLAY(self));
+    return self;
+}
+
+static VALUE
+gdkdisplay_sync(self)
+    VALUE self;
+{
+    gdk_display_sync(_DISPLAY(self));
+    return self;
+}
+
+static VALUE
+gdkdisplay_close(self)
+    VALUE self;
+{
+    gdk_display_close(_DISPLAY(self));
+    return self;
+}
+
+static VALUE
+gdkdisplay_list_devices(self)
+    VALUE self;
+{
+    return rbgutil_glist2ary(gdk_display_list_devices(_DISPLAY(self)));
+}
+
+static VALUE
+gdkdisplay_get_pointer(self)
+    VALUE self;
+{
+    GdkScreen *screen;
+    int x,y;
+    GdkModifierType mask;
+    
+    gdk_display_get_pointer(_DISPLAY(self), &screen, &x, &y, &mask);
+    
+    return rb_ary_new3(4, GOBJ2RVAL(screen), INT2NUM(x), INT2NUM(y), INT2NUM(mask));
+}
+static VALUE
+gdkdisplay_get_window_at_pointer(self)
+    VALUE self;
+{
+    GdkWindow *window;
+    int x,y;
+    
+    window = gdk_display_get_window_at_pointer(_DISPLAY(self), &x, &y);
+    
+    return rb_ary_new3(3, GOBJ2RVAL(window), INT2NUM(x), INT2NUM(y));
+}
+
+void 
+Init_gtk_gdk_display()
+{
+    VALUE gdkDisplay = G_DEF_CLASS(GDK_TYPE_DISPLAY, "Display", mGdk);
+
+    rb_define_method(gdkDisplay, "initialize", gdkdisplay_initialize, -1);
+    rb_define_singleton_method(gdkDisplay, "default", gdkdisplay_default, 0);
+    rb_define_method(gdkDisplay, "name", gdkdisplay_name, 0);
+    rb_define_method(gdkDisplay, "n_screens", gdkdisplay_n_screens, 0);
+    rb_define_method(gdkDisplay, "get_screen", gdkdisplay_get_screen, 1);
+    rb_define_alias(gdkDisplay, "[]", "get_screen");
+    rb_define_alias(gdkDisplay, "screen", "get_screen");
+    rb_define_method(gdkDisplay, "default_screen", gdkdisplay_default_screen, 0);
+    
+    rb_define_method(gdkDisplay, "pointer_ungrab", gdkdisplay_pointer_ungrab, 1);
+    rb_define_method(gdkDisplay, "keyboard_ungrab", gdkdisplay_keyboard_ungrab, 1);
+    rb_define_method(gdkDisplay, "pointer_grabbed?", gdkdisplay_pointer_grabbed, 0);
+    
+    rb_define_method(gdkDisplay, "beep", gdkdisplay_beep, 0);
+    rb_define_method(gdkDisplay, "sync", gdkdisplay_sync, 0);
+    rb_define_method(gdkDisplay, "close", gdkdisplay_close, 0);
+    
+    rb_define_method(gdkDisplay, "devices", gdkdisplay_list_devices, 0);
+    rb_define_method(gdkDisplay, "pointer", gdkdisplay_get_pointer, 0);
+    rb_define_method(gdkDisplay, "window_at_pointer", gdkdisplay_get_window_at_pointer, 0);
+}
+#else
+
+void 
+Init_gtk_gdk_display()
+{
+}
+
+#endif
+
---
+++ gtk/src/rbgdkscreen.c
@@ -0,0 +1,128 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
+/************************************************
+
+  rbgdkdisplay.c -
+
+  $Author: mutoh $
+  $Date: 2003/07/01 14:45:18 $
+
+  Copyright (C) 2003 Geoff Youngs
+************************************************/
+
+#include "global.h"
+
+#if GTK_MINOR_VERSION >= 2
+#define _SCREEN(i) GDK_SCREEN(RVAL2GOBJ(i))
+
+static VALUE
+gdkscreen_initialize(self)
+    VALUE self;
+{
+      G_INITIALIZE(self, gdk_screen_get_default());
+      return Qnil;
+}
+
+static VALUE
+gdkscreen_default(self)
+    VALUE self;
+{
+      return GOBJ2RVAL(gdk_screen_get_default());
+}
+
+static VALUE
+gdkscreen_number(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_number(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_width(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_width(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_height(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_height(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_width_mm(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_width_mm(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_height_mm(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_height_mm(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_n_monitors(self)
+    VALUE self;
+{
+      return INT2NUM(gdk_screen_get_n_monitors(_SCREEN(self)));
+}
+static VALUE
+gdkscreen_monitor_geometry(self, num)
+    VALUE self, num;
+{
+      GdkRectangle rect;
+      gdk_screen_get_monitor_geometry(_SCREEN(self), NUM2INT(num), &rect);
+      return BOXED2RVAL(&rect, GDK_TYPE_RECTANGLE);
+}
+static VALUE
+gdkscreen_monitor_at_point(self,x,y)
+    VALUE self,x,y;
+{
+      return INT2NUM(gdk_screen_get_monitor_at_point(_SCREEN(self),NUM2INT(x),NUM2INT(y)));
+}
+static VALUE
+gdkscreen_monitor_at_window(self,win)
+    VALUE self,win;
+{
+      return INT2NUM(gdk_screen_get_monitor_at_window(_SCREEN(self),GDK_WINDOW(RVAL2GOBJ(win))));
+}
+
+static VALUE
+gdkscreen_get_setting(self,name)
+    VALUE self,name;
+{
+      GValue val;
+      if (gdk_screen_get_setting(_SCREEN(self), RVAL2CSTR(name), &val))
+      {
+      	  return GVAL2RVAL(&val);
+      }
+      return Qnil;
+}
+
+
+void 
+Init_gtk_gdk_screen()
+{
+    VALUE gdkScreen = G_DEF_CLASS(GDK_TYPE_SCREEN, "Screen", mGdk);
+
+    rb_define_method(gdkScreen, "initialize", gdkscreen_initialize, 0);
+    rb_define_singleton_method(gdkScreen, "default", gdkscreen_default, 0);
+    rb_define_method(gdkScreen, "number", gdkscreen_number, 0);
+    rb_define_method(gdkScreen, "width", gdkscreen_width, 0);
+    rb_define_method(gdkScreen, "height", gdkscreen_height, 0);
+    rb_define_method(gdkScreen, "width_mm", gdkscreen_width_mm, 0);
+    rb_define_method(gdkScreen, "height_mm", gdkscreen_height_mm, 0);
+    rb_define_method(gdkScreen, "n_monitors", gdkscreen_n_monitors, 0);
+    rb_define_method(gdkScreen, "monitor_geometry", gdkscreen_monitor_geometry, 1);
+    rb_define_method(gdkScreen, "monitor_at_point", gdkscreen_monitor_at_point, 2);
+    rb_define_method(gdkScreen, "monitor_at_window", gdkscreen_monitor_at_window, 1);
+    rb_define_method(gdkScreen, "setting", gdkscreen_get_setting, 1);
+}
+#else
+
+void 
+Init_gtk_gdk_screen()
+{
+}
+
+#endif
+
