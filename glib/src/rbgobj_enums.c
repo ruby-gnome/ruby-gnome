c74ad1f92bcc03d02edd03b93d6e45a04765dfc7
Geoff Youngs <git@intersect-uk.co.uk>
2003-07-28 08:51:24 +0000

    * src/rbgobj_enums.c: add rbgobj_constant_* to allow renaming auto detected constant names which aren't valid for ruby

--- glib/src/rbgobj_enums.c
+++ glib/src/rbgobj_enums.c
@@ -3,8 +3,8 @@
 
   rbgobj_enums.c -
 
-  $Author: sakai $
-  $Date: 2003/07/22 04:02:22 $
+  $Author: geoff_youngs $
+  $Date: 2003/07/28 08:51:24 $
   created at: Sat Jul 27 16:56:01 JST 2002
 
   Copyright (C) 2002,2003  Masahiro Sakai
@@ -12,6 +12,79 @@
 **********************************************************************/
 
 #include "global.h"
+
+typedef struct {
+	char *original;
+	char *replacement;
+} constant_map;
+
+static GSList *rbgobj_cmap = NULL;
+
+static gint
+rbgobj_constant_find(constant_map *a, char *name)
+{
+      return strcmp(a->original, name);
+}
+
+void 
+rbgobj_constant_remap(const char *original, 
+			const char *replacement)
+{
+      constant_map *map = g_new(constant_map,1);
+      
+      map -> original     = g_strdup(original);
+      map -> replacement  = g_strdup(replacement);
+      
+      rbgobj_cmap = g_slist_append(rbgobj_cmap, map);
+}
+
+static char *
+rbgobj_constant_lookup(const char *name)
+{
+      GSList *p = rbgobj_cmap;
+      
+      p = g_slist_find_custom(rbgobj_cmap, name, 
+      			(GCompareFunc)rbgobj_constant_find);
+      if (p)
+      {
+          char *replacement;
+	  constant_map *map;
+	  
+	  map         = (constant_map*) p -> data;
+	  rbgobj_cmap = g_slist_delete_link(rbgobj_cmap, p);
+	  replacement = map -> replacement;
+	  
+	  g_free(map -> original);
+	  g_free(map);
+	  
+	  return replacement;
+      }
+      return NULL;
+}
+
+static void 
+rbgobj_define_const(VALUE mod, const char *name,
+			VALUE value)
+{
+     if (name[0] >= 'A' && name[0] <= 'Z')
+     {
+         rb_define_const(mod, name, value);
+     }
+     else
+     {
+         char *new_name = rbgobj_constant_lookup(name);
+	 
+	 if (new_name)
+	 {
+	     rb_define_const(mod, new_name, value);
+	     g_free(new_name);
+	 }
+	 else
+	 {
+	     rb_warn("Invalid constant name '%s' - skipped", name);
+	 }
+     }
+}
 
 static void
 rbgobj_enum_add_constants(VALUE mod, GType enum_type,
@@ -31,7 +104,7 @@ rbgobj_enum_add_constants(VALUE mod, GType enum_type,
                       value->value_name, strip_prefix);
         } else {
             const char* name = value->value_name + prefix_len;
-            rb_define_const(mod, name, INT2NUM(value->value));
+            rbgobj_define_const(mod, name, INT2NUM(value->value));
         }
     }
 
@@ -56,7 +129,7 @@ rbgobj_flags_add_constants(VALUE mod, GType flags_type,
                       value->value_name, strip_prefix);
         } else {
             const char* name = value->value_name + prefix_len;
-            rb_define_const(mod, name, UINT2NUM(value->value));
+            rbgobj_define_const(mod, name, UINT2NUM(value->value));
         }
     }
 
