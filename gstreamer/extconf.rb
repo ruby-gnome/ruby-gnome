1e305b1e1da1328bc00f0e7644d4df1246c9e78d
Laurent Sansonetti <lrz@gnome.org>
2003-07-29 08:28:41 +0000

    check if the gstmedia-info library is installed

--- gstreamer/extconf.rb
+++ gstreamer/extconf.rb
@@ -17,6 +17,12 @@ $CFLAGS += " " + ['glib/src'].map{|d|
 srcdir = File.dirname($0) == "." ? "." :
   File.expand_path(File.dirname($0) + "/src")
 
+have_header("assert.h")
+
+if have_library("gstmedia-info-0.6", "gst_media_info_read")
+	$CFLAGS += " -DHAVE_MEDIA_INFO"
+end
+
 Dir.mkdir('src') unless File.exist? 'src'
 Dir.chdir "src"
 begin
@@ -24,7 +30,5 @@ begin
 ensure
   Dir.chdir('..')
 end
-
-have_header("assert.h")
 
 create_top_makefile
