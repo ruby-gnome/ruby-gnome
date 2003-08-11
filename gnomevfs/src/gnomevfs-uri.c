7e3611b0074155a6e3cf757a0926206dd986a016
Nikolai Weibull <now@bitwi.se>
2003-08-11 11:11:10 +0000

    Updated file headers

--- gnomevfs/src/gnomevfs-uri.c
+++ gnomevfs/src/gnomevfs-uri.c
@@ -18,9 +18,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  *
- * Author: Nikolai :: lone-star :: Weibull <lone-star@home.se>
+ * $Author: pcppopper $
  *
- * Latest Revision: 2003-08-04
+ * $Date: 2003/08/11 11:11:10 $
  *
  *****************************************************************************/
 
