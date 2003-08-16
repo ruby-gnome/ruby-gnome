b6d3dee9e66908ba89acaea443066a32f49da8b5
Masahiro Sakai <sakai@tom.sfc.keio.ac.jp>
2003-08-16 19:02:24 +0000

    * src/lib/glib2.rb: add new file.

---
+++ glib/src/lib/glib2.rb
@@ -0,0 +1,16 @@
+require 'glib2.so'
+
+module GLib
+
+  class Type
+    def decendants
+      [self] + children.map{|t| t.decendants }.flatten
+    end
+
+    def ancestors
+      #  ([self] + interfaces + (parent ? parent.ancestors : [])).reverse.uniq.reverse
+      [self] + (parent ? parent.ancestors : [])
+    end
+  end
+
+end
