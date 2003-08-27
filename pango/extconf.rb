7fea6dc6f8e6d5d4e26f40c5dd3ff7ba670982ca
Masao Mutoh <mutoh@highway.ne.jp>
2003-08-27 17:35:12 +0000

    Revised.

--- pango/extconf.rb
+++ pango/extconf.rb
@@ -3,90 +3,42 @@ extconf.rb for Ruby/Pango extention library
 =end
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../glib/src/lib')
-
 require 'mkmf-gnome2'
 
-#
-# detect configurations
-#
-
-PKGConfig.have_package('pango') or exit
+PKGConfig.have_package('pango') or exit -1
 check_win32
-
-STDOUT.print("checking for new allocation framework... ") # for ruby-1.7
-if Object.respond_to? :allocate
-  STDOUT.print "yes\n"
-  $defs << "-DHAVE_OBJECT_ALLOCATE"
-else
-  STDOUT.print "no\n"
-end
 
 top = File.expand_path(File.dirname(__FILE__) + '/..') # XXX
 $CFLAGS += " " + ['glib/src'].map{|d|
   "-I" + File.join(top, d)
 }.join(" ")
 
-have_func("rb_define_alloc_func") # for ruby-1.8
-
-
-set_output_lib('libruby-pango.a')
 if /cygwin|mingw/ =~ RUBY_PLATFORM
   top = "../.."
   [
     ["glib/src", "ruby-glib2"],
   ].each{|d,l|
-    $LDFLAGS << " -L#{top}/#{d}"
     $libs << " -l#{l}"
+    $LDFLAGS << " -L#{top}/#{d}"
   }
 end
 
-
 begin
-  srcdir = File.dirname($0) == "." ? "." :
-    File.expand_path(File.dirname($0) + "/src")
+  File.delete("src/rbpangoinits.c") if FileTest.exist?("src/rbpangoinits.c")
 
-  Dir.mkdir "src" unless File.exist? "src"
   Dir.chdir "src"
-
-  begin
-    File.delete("rbpangoinits.c") if FileTest.exist?("rbpangoinits.c")
-
-    obj_ext = ".#{$OBJEXT}"
-
-    $libs = $libs.split(/\s/).uniq.join(' ')
-    $source_files = Dir.glob("#{srcdir}/*.c").map{|fname|
-      fname[0, srcdir.length+1] = ''
-      fname
-    }
-    $objs = $source_files.collect do |item|
-      item.gsub(/\.c$/, obj_ext)
-    end
-    $objs << "rbpangoinits.o"
-
-    #
-    # create Makefiles
-    #
-    $defs << "-DRUBY_PANGO_COMPILATION"
-    create_makefile("pango", srcdir)
-    raise Interrupt if not FileTest.exist? "Makefile"
-
-    mfile = File.open("Makefile", "a")
-    $source_files.each do |e|
-      mfile.print "#{e.gsub(/\.c$/, obj_ext)}: #{e} rbpango.h \n"
-    end
-    mfile.print "\
-rbpangoinits.c:;    $(RUBY) $(srcdir)/makeinits.rb $(srcdir)/*.c > $@
-allclean: clean
-	rm -rf *.a
-"
-    mfile.close
-  ensure
-    Dir.chdir ".."
+  system("ruby makeinits.rb *.c > rbpangoinits.c")
+  $objs = []
+  Dir.glob("*.c") do |f|
+    f = File.basename(f)
+    f.sub!(/.c$/, ".o")
+    $objs.push f
   end
-
-  create_top_makefile()
-rescue Interrupt
-  print "  [error] " + $!.to_s + "\n"
+  $defs << "-DRUBY_PANGO_COMPILATION"
+  create_makefile("pango", File.expand_path(File.dirname($0) + "/src"))
+ensure
+  Dir.chdir('..')
 end
 
+create_top_makefile
 
