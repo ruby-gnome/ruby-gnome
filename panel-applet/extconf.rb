3c02b24ffae5887e536dff9749e200694aaa7b9e
Masao Mutoh <mutoh@highway.ne.jp>
2003-08-27 17:32:18 +0000

    * rbpanel-applet.c: Rewritten. * extconf.rb: Rewritten.

--- panel-applet/extconf.rb
+++ panel-applet/extconf.rb
@@ -1,33 +1,27 @@
 =begin
-extconf.rb for Ruby/PanelApplet extention library
+extconf.rb for Ruby/GdkPixbuf extention library
 =end
 
 $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../glib/src/lib')
-
 require 'mkmf-gnome2'
 
-gnome_config = with_config('gnome-config', 'gnome-config')
-rbgtk_dir = with_config("ruby-gtk", File.dirname(__FILE__)+"/../gtk")
-
-unless FileTest.exist?(rbgtk_dir)
-  raise "directry #{rbgtk_dir} not found.  Please specify Ruby-GTK source dir."
-end
-
-rbgnome_dir = with_config("ruby-gnome", File.dirname(__FILE__)+"/../gnome")
-unless FileTest.exist?(rbgnome_dir)
-  raise "directry #{rbgnome_dir} not found.  Please specify Ruby-GNOME source dir."
-end
-
-PKGConfig.have_package("libpanelapplet-2.0") or exit
+PKGConfig.have_package('libpanelapplet-2.0') or exit 1
 check_win32
 
-$CFLAGS += " -I#{rbgtk_dir}/src -I#{rbgnome_dir}/src "
+top = File.expand_path(File.dirname(__FILE__) + '/..') # XXX
+$CFLAGS += " " + ['glib/src', 'gtk/src'].map{|d|
+  "-I" + File.join(top, d)
+}.join(" ")
 
-have_library("X11", "XOpenDisplay") &&
-have_library("Xi", "XOpenDevice") &&
-have_library("Xext", "XextFindDisplay") &&
-have_library("Xmu", "XmuInternAtom") &&
-have_func("g_print") &&
-have_func("gtk_init") &&
-have_func("applet_widget_init") &&
-create_makefile('panel_applet')
+if /cygwin|mingw/ =~ RUBY_PLATFORM
+  top = '..'
+  [
+    ["glib/src", "ruby-glib2"],
+    ["gtk/src", "ruby-gtk2"],
+  ].each{|d,l|
+    $LDFLAGS << " -L#{top}/#{d}"
+    $libs << " -l#{l}"
+  }
+end
+
+create_makefile('panelapplet2')
--- panel-applet/rbpanel-applet.c
+++ panel-applet/rbpanel-applet.c
@@ -1,183 +1,134 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /************************************************
 
-  rbapplet.c: Wrapper for AppletWidget in libpanel
+  rbpanelapplet.c
 
   $Author: mutoh $
-  $Date: 2002/05/19 12:39:31 $
+  $Date: 2003/08/27 17:32:18 $
 
-  Copyright (C) 2001 Hiroshi Igarashi
-  Copyright (C) 2002 Neil Conway
-
+  Copyright (C) 2003 Masao Mutoh
 ************************************************/
 
-#include <rbgnome.h>
-#include <applet-widget.h>
+#include "rbgobject.h"
+#include "rbgtk.h"
+#include <panel-applet.h>
 
-static VALUE cAppletWidget;
-static VALUE mGnomePanel;
+static ID id_call;
+
+/*
+gboolean    (*PanelAppletFactoryCallback)   (PanelApplet *applet,
+                                             const gchar *iid,
+                                             gpointer user_data);
+GtkWidget*  panel_applet_new                (void);
+PanelAppletOrient panel_applet_get_orient   (PanelApplet *applet);
+guint       panel_applet_get_size           (PanelApplet *applet);
+PanelAppletBackgroundType panel_applet_get_background
+                                            (PanelApplet *applet,
+                                             GdkColor *color,
+                                             GdkPixmap **pixmap);
+gchar*      panel_applet_get_preferences_key
+                                            (PanelApplet *applet);
+void        panel_applet_add_preferences    (PanelApplet *applet,
+                                             const gchar *schema_dir,
+                                             GError **opt_error);
+PanelAppletFlags panel_applet_get_flags     (PanelApplet *applet);
+void        panel_applet_set_flags          (PanelApplet *applet,
+                                             PanelAppletFlags flags);
+void        panel_applet_set_size_hints     (PanelApplet *applet,
+                                             const int *size_hints,
+                                             int n_elements,
+                                             int base_size);
+BonoboControl* panel_applet_get_control     (PanelApplet *applet);
+BonoboUIComponent* panel_applet_get_popup_component
+                                            (PanelApplet *applet);
+void        panel_applet_setup_menu         (PanelApplet *applet,
+                                             const gchar *xml,
+                                             const BonoboUIVerb *verb_list,
+                                             gpointer user_data);
+void        panel_applet_setup_menu_from_file
+                                            (PanelApplet *applet,
+                                             const gchar *opt_datadir,
+                                             const gchar *file,
+                                             const gchar *opt_app_name,
+                                             const BonoboUIVerb *verb_list,
+                                             gpointer user_data);
+int         panel_applet_factory_main       (const gchar *iid,
+                                             GType applet_type,
+                                             PanelAppletFactoryCallback callback,
+                                             gpointer data);
+*/
+
+static gboolean
+rbpanel_cb(applet, iid, data)
+    PanelApplet* applet;
+    const gchar* iid;
+    gpointer data;
+{
+    VALUE ret = rb_yield(rb_ary_new3(2, GOBJ2RVAL(applet), CSTR2RVAL(iid)));
+    printf("aaa\n");
+    g_object_unref(applet);
+    return RTEST(ret);
+}
+   
 
 static VALUE
-rb_s_applet_widget_gtk_main(self)
+rbpanel_s_main(argc, argv, self)
+    int argc;
+    VALUE* argv;
     VALUE self;
 {
-    applet_widget_gtk_main();
-    return Qnil;
+    VALUE iid, type, name, version;
+    GType gtype;
+    int ret;
+
+    if (argc > 3){
+        rb_scan_args(argc, argv, "40", &iid, &type, &name, &version);
+        gtype = NUM2INT(type);
+    } else {
+        rb_scan_args(argc, argv, "30", &iid, &name, &version);
+        gtype = PANEL_TYPE_APPLET;
+    }
+
+    ret = INT2NUM(panel_applet_factory_main(STR2CSTR(iid), gtype, 
+                                            (PanelAppletFactoryCallback)rbpanel_cb, 
+                                            NULL));
+    printf("bbb\n");
+    return ret;
 }
 
-static VALUE
-rb_s_applet_widget_gtk_main_quit(self)
-    VALUE self;
-{
-    applet_widget_gtk_main_quit();
-    return Qnil;
-}
+/*
+int         panel_applet_factory_main_closure
+                                            (const gchar *iid,
+                                             GType applet_type,
+                                             GClosure *closure);
 
-static VALUE
-rb_s_applet_widget_get_applet_count(self)
-    VALUE self;
-{
-    return NUM2INT(applet_widget_get_applet_count());
-}
-
-static VALUE
-rb_s_applet_widget_panel_quit(self)
-    VALUE self;
-{
-    applet_widget_panel_quit();
-    return Qnil;
-}
-
-static VALUE
-rb_applet_widget_initialize(self, goad_id)
-    VALUE self, goad_id;
-{
-    GtkWidget *applet = applet_widget_new(STR2CSTR(goad_id));
-    set_widget(self, applet);
-
-    return Qnil;
-}
-
-static VALUE
-rb_applet_widget_add(self, widget)
-    VALUE self, widget;
-{
-    applet_widget_add(APPLET_WIDGET(get_widget(self)), get_widget(widget));
-    return self;
-}
-
-static VALUE
-rb_applet_widget_panel_orient(self)
-    VALUE self;
-{
-    return INT2NUM(
-            applet_widget_get_panel_orient(APPLET_WIDGET(get_widget(self))));
-}
-
-static VALUE
-rb_applet_widget_panel_pixel_size(self)
-    VALUE self;
-{
-    return INT2NUM(
-            applet_widget_get_panel_pixel_size(APPLET_WIDGET(get_widget(self))));
-}
-
-static VALUE
-rb_applet_widget_get_free_space(self)
-    VALUE self;
-{
-    return INT2NUM(applet_widget_get_free_space(APPLET_WIDGET(get_widget(self))));
-}
-
-static VALUE
-rb_applet_widget_send_position(self, enable)
-    VALUE self, enable;
-{
-    applet_widget_send_position(APPLET_WIDGET(get_widget(self)),
-                                RTEST(enable));
-    return Qnil;
-}
-
-static VALUE
-rb_applet_widget_send_draw(self, enable)
-    VALUE self, enable;
-{
-    applet_widget_send_draw(APPLET_WIDGET(get_widget(self)),
-                            RTEST(enable));
-    return Qnil;
-}
-
-static VALUE
-rb_applet_widget_sync_config(self)
-    VALUE self;
-{
-    applet_widget_sync_config(APPLET_WIDGET(get_widget(self)));
-    return Qnil;
-}
-
-static VALUE
-rb_applet_widget_queue_resize(self)
-    VALUE self;
-{
-    applet_widget_queue_resize(APPLET_WIDGET(get_widget(self)));
-    return Qnil;
-}
+Bonobo_Unknown panel_applet_shlib_factory   (const char *iid,
+                                             GType applet_type,
+                                             PortableServer_POA poa,
+                                             gpointer impl_ptr,
+                                             PanelAppletFactoryCallback callback,
+                                             gpointer user_data,
+                                             CORBA_Environment *ev);
+Bonobo_Unknown panel_applet_shlib_factory_closure
+                                            (const char *iid,
+                                             GType applet_type,
+                                             PortableServer_POA poa,
+                                             gpointer impl_ptr,
+                                             GClosure *closure,
+                                             CORBA_Environment *ev);
+#define     PANEL_APPLET_BONOBO_SHLIB_FACTORY(iid, type, descr, callback, data)
+*/
 
 void
-Init_panel_applet()
+Init_panelapplet2()
 {
-    int argc, i;
-    char **argv;
+    VALUE mGnome = rb_define_module("Gnome");
+    VALUE cApplet = rb_define_class_under(mGnome, "PanelApplet", GTYPE2CLASS(PANEL_TYPE_APPLET));
 
-    /* setup args */
-    argc = RARRAY(rb_argv)->len;
-    argv = ALLOCA_N(char*,argc+1);
-    argv[0] = STR2CSTR(rb_argv0);
-    for (i = 0; i < argc; ++i) {
-        if (TYPE(RARRAY(rb_argv)->ptr[i]) == T_STRING) {
-            argv[i+1] = RSTRING(RARRAY(rb_argv)->ptr[i])->ptr;
-        } else {
-            argv[i+1] = "";
-        }
-    }
-    ++argc;
+    id_call = rb_intern("call");
 
-    applet_widget_init(argv[0], "0.0.0", argc, argv, NULL, 0, NULL);
+    rb_define_singleton_method(cApplet, "main", rbpanel_s_main, -1);
 
-    /* sync args */
-    for (i = 1; i < argc; ++i) {
-        RARRAY(rb_argv)->ptr[i-1] = rb_obj_taint(rb_str_new2(argv[i]));
-    }
-    RARRAY(rb_argv)->len = argc-1;
+//    rb_define_method(cAppletWidget, "queue_resize", rb_applet_widget_queue_resize, 0);
 
-    /* define classes and methods */
-    cAppletWidget = rb_define_class_under(mGnome, "AppletWidget", gPlug);
-    rb_define_singleton_method(cAppletWidget, "main", rb_s_applet_widget_gtk_main, 0);
-    rb_define_singleton_method(cAppletWidget, "main_quit", rb_s_applet_widget_gtk_main_quit, 0);
-    rb_define_singleton_method(cAppletWidget, "applet_count", rb_s_applet_widget_get_applet_count, 0);
-    rb_define_singleton_method(cAppletWidget, "panel_quit", rb_s_applet_widget_panel_quit, 0);
-    rb_define_method(cAppletWidget, "initialize", rb_applet_widget_initialize, 1);
-    rb_define_method(cAppletWidget, "add", rb_applet_widget_add, 1);
-    rb_define_method(cAppletWidget, "panel_orient", rb_applet_widget_panel_orient, 0);
-    rb_define_method(cAppletWidget, "panel_pixel_size", rb_applet_widget_panel_pixel_size, 0);
-    rb_define_method(cAppletWidget, "free_space", rb_applet_widget_get_free_space, 0);
-    rb_define_method(cAppletWidget, "send_position", rb_applet_widget_send_position, 1);
-    rb_define_method(cAppletWidget, "send_draw", rb_applet_widget_send_draw, 1);
-    rb_define_method(cAppletWidget, "sync_config", rb_applet_widget_sync_config, 0);
-    rb_define_method(cAppletWidget, "queue_resize", rb_applet_widget_queue_resize, 0);
-
-    /* signals */
-    rb_define_const(cAppletWidget, "SIGNAL_CHANGE_ORIENT", rb_str_new2("change_orient"));
-    rb_define_const(cAppletWidget, "SIGNAL_CHANGE_PIXEL_SIZE", rb_str_new2("change_pixel_size"));
-    rb_define_const(cAppletWidget, "SIGNAL_SAVE_SESSION", rb_str_new2("save_session"));
-    rb_define_const(cAppletWidget, "SIGNAL_BACK_CHANGE", rb_str_new2("back_change"));
-    rb_define_const(cAppletWidget, "SIGNAL_DO_DRAW", rb_str_new2("do_draw"));
-    rb_define_const(cAppletWidget, "SIGNAL_TOOLTIP_STATE", rb_str_new2("tooltip_state"));
-    rb_define_const(cAppletWidget, "SIGNAL_CHANGE_POSITION", rb_str_new2("change_position"));
-
-    /* constants */
-    mGnomePanel = rb_define_module_under(mGnome, "Panel");
-    rb_define_const(mGnomePanel, "ORIENT_UP", INT2NUM(GNOME_Panel_ORIENT_UP));
-    rb_define_const(mGnomePanel, "ORIENT_DOWN", INT2NUM(GNOME_Panel_ORIENT_DOWN));
-    rb_define_const(mGnomePanel, "ORIENT_LEFT", INT2NUM(GNOME_Panel_ORIENT_LEFT));
-    rb_define_const(mGnomePanel, "ORIENT_RIGHT", INT2NUM(GNOME_Panel_ORIENT_RIGHT));
 }
