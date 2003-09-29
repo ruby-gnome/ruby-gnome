af5b6f27d745674edf9f0cb6d82a1bb9f9813e87
Laurent Sansonetti <lrz@gnome.org>
2003-09-29 17:33:38 +0000

    * src/rbgdaclient.c: Added a few methods (#close_all_connections,   #begin_transaction, #commit_transaction, #rollback_transaction,   #notify_event, #notify_error_event, #notify_connection_opened_event,   #notify_transaction_started_event, #notify_transaction_committed_event,   #notify_transaction_cancelled_event) and constants (EVENT_ERROR,   EVENT_CONNECTION_OPENED, EVENT_CONNECTION_CLOSED, EVENT_TRANSACTION_STARTED,   EVENT_TRANSACTION_COMMITTED, EVENT_TRANSACTION_CANCELLED) to complete the   class.

--- libgda/src/rbgdaclient.c
+++ libgda/src/rbgdaclient.c
@@ -95,6 +95,91 @@ static VALUE rb_gda_client_find_connection(self, dsn, username, password)
         : Qnil;
 }
 
+static VALUE rb_gda_client_close_all_connections(self)
+    VALUE self;
+{
+    gda_client_close_all_connections(RGDA_CLIENT(self));
+    return self;
+}
+
+static VALUE rb_gda_client_begin_transaction(self, tr)
+    VALUE self, tr;
+{
+    gda_client_begin_transaction(RGDA_CLIENT(self),
+                                 RGDA_TRANSACTION(tr));
+    return self;
+}
+
+static VALUE rb_gda_client_commit_transaction(self, tr)
+    VALUE self, tr;
+{
+    gda_client_commit_transaction(RGDA_CLIENT(self),
+                                  RGDA_TRANSACTION(tr));
+    return self;
+}
+
+static VALUE rb_gda_client_rollback_transaction(self, tr)
+    VALUE self, tr;
+{
+    gda_client_rollback_transaction(RGDA_CLIENT(self),
+                                    RGDA_TRANSACTION(tr));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_event(self, conn, event, params)
+    VALUE self, conn, event, params;
+{
+    gda_client_notify_event(RGDA_CLIENT(self),
+                            RGDA_CONNECTION(conn),
+                            FIX2INT(event),
+                            NIL_P(params) ? NULL : RGDA_PARAMETER_LIST(params));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_error_event(self, conn, error)
+    VALUE self, conn, error;
+{
+    gda_client_notify_error_event(RGDA_CLIENT(self),
+                                  RGDA_CONNECTION(conn),
+                                  RGDA_ERROR(error));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_connection_opened_event(self, conn)
+    VALUE self, conn;
+{
+    gda_client_notify_connection_opened_event(RGDA_CLIENT(self),
+                                              RGDA_CONNECTION(conn));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_transaction_started_event(self, conn, tr)
+    VALUE self, conn, tr;
+{
+    gda_client_notify_transaction_started_event(RGDA_CLIENT(self),
+                                                RGDA_CONNECTION(conn),
+                                                RGDA_TRANSACTION(tr));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_transaction_committed_event(self, conn, tr)
+    VALUE self, conn, tr;
+{
+    gda_client_notify_transaction_committed_event(RGDA_CLIENT(self),
+                                                  RGDA_CONNECTION(conn),
+                                                  RGDA_TRANSACTION(tr));
+    return self;
+}
+
+static VALUE rb_gda_client_notify_transaction_cancelled_event(self, conn, tr)
+    VALUE self, conn, tr;
+{
+    gda_client_notify_transaction_cancelled_event(RGDA_CLIENT(self),
+                                                  RGDA_CONNECTION(conn),
+                                                  RGDA_TRANSACTION(tr));
+    return self;
+}
+
 void Init_gda_client(void) {
     VALUE c = G_DEF_CLASS(GDA_TYPE_CLIENT, "Client", mGda);
 
@@ -104,5 +189,35 @@ void Init_gda_client(void) {
     rb_define_method(c, "connections",     rb_gda_client_get_connections,  0);
     rb_define_method(c, "each_connection", rb_gda_client_each_connection,  0);
     rb_define_method(c, "find_connection", rb_gda_client_find_connection,  3);
+
+    rb_define_method(c, "close_all_connections", rb_gda_client_close_all_connections, 0);   
+ 
+    rb_define_method(c, "begin_transaction",    rb_gda_client_begin_transaction,    1);
+    rb_define_method(c, "commit_transaction",   rb_gda_client_commit_transaction,   1);
+    rb_define_method(c, "rollback_transaction", rb_gda_client_rollback_transaction, 1);
+
+    rb_define_method(c, "notify_event",       rb_gda_client_notify_event,       3);
+    rb_define_method(c, "notify_error_event", rb_gda_client_notify_error_event, 2);
+    rb_define_method(c, "notify_connection_opened_event", 
+                     rb_gda_client_notify_connection_opened_event, 1);
+    rb_define_method(c, "notify_transaction_started_event", 
+                     rb_gda_client_notify_transaction_started_event, 2);
+    rb_define_method(c, "notify_transaction_committed_event", 
+                     rb_gda_client_notify_transaction_committed_event, 2);
+    rb_define_method(c, "notify_transaction_cancelled_event", 
+                     rb_gda_client_notify_transaction_cancelled_event, 2);
+
+    rb_define_const(c, "EVENT_ERROR", 
+                    INT2FIX(GDA_CLIENT_EVENT_ERROR));
+    rb_define_const(c, "EVENT_CONNECTION_OPENED", 
+                    INT2FIX(GDA_CLIENT_EVENT_CONNECTION_OPENED));
+    rb_define_const(c, "EVENT_CONNECTION_CLOSED", 
+                    INT2FIX(GDA_CLIENT_EVENT_CONNECTION_CLOSED));
+    rb_define_const(c, "EVENT_TRANSACTION_STARTED", 
+                    INT2FIX(GDA_CLIENT_EVENT_TRANSACTION_STARTED));
+    rb_define_const(c, "EVENT_TRANSACTION_COMMITTED", 
+                    INT2FIX(GDA_CLIENT_EVENT_TRANSACTION_COMMITTED));
+    rb_define_const(c, "EVENT_TRANSACTION_CANCELLED", 
+                    INT2FIX(GDA_CLIENT_EVENT_TRANSACTION_CANCELLED));
 }
 
