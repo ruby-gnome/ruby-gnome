26ebdcf5ffc6494ddefc397b798c9fa4b9e0a629
Masao Mutoh <mutoh@highway.ne.jp>
2003-11-21 16:06:03 +0000

    src/rbgtkconst.c: Update version information.

--- gtk/src/rbgtkconst.c
+++ gtk/src/rbgtkconst.c
@@ -4,7 +4,7 @@
   rbgtkconst.c -
 
   $Author: mutoh $
-  $Date: 2003/11/08 19:18:24 $
+  $Date: 2003/11/21 16:06:03 $
 
   Copyright (C) 2002,2003 Ruby-GNOME2 Project Team
   Copyright (C) 1998-2000 Yukihiro Matsumoto,
@@ -16,7 +16,7 @@
 
 #define RBGTK_MAJOR_VERSION 0
 #define RBGTK_MINOR_VERSION 8
-#define RBGTK_MICRO_VERSION 0
+#define RBGTK_MICRO_VERSION 1
 
 /*
  * constants
