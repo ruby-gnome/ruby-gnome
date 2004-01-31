012ade646f6ba45718f11c8f77ea0bc122867324
Masao Mutoh <mutoh@highway.ne.jp>
2004-01-31 18:50:30 +0000

    * src/rbgtksettings.c: Add Gtk::Setting.rc_property_parse_*. * sample/misc/settings.rb: Added.

---
+++ gtk/sample/misc/settings.rb
@@ -0,0 +1,43 @@
+=begin
+  setting.rb - Gtk::Settings sample script.
+
+  Copyright (C) 2004 Masao Mutoh <mutoh@highway.ne.jp>
+  This program is licenced under the same licence as Ruby-GNOME2.
+
+  $Id: settings.rb,v 1.1 2004/01/31 18:50:30 mutoh Exp $
+=end
+
+require 'gtk2'
+
+Gtk.init
+
+spec = GLib::Param::Int.new("integer",         # name
+			    "Integer",         # nick
+			    "Integer integer", # blurb
+			    0,                 # min
+			    10000,             # max
+			    0,                 # default
+			    GLib::Param::READABLE|
+			    GLib::Param::WRITABLE)
+
+enum = GLib::Param::Enum.new("enum",                      # name
+			     "Enum",                      # nick
+			     "Enum enum",                 # blurb
+			     GLib::Type["GdkCursorType"], #type
+			     Gdk::Cursor::ARROW,          #default
+			     GLib::Param::READABLE|
+			     GLib::Param::WRITABLE)
+
+flags = GLib::Param::Flags.new("flags",                       # name
+			       "Flags",                       # nick
+			       "Flags flags",                 # blurb
+			       GLib::Type["GdkEventMask"],    #type
+			       Gdk::Event::LEAVE_NOTIFY_MASK, #default
+			       GLib::Param::READABLE|
+			       GLib::Param::WRITABLE)
+
+p Gtk::Settings.rc_property_parse_color(spec, "{2222,44444,65535}").to_a
+p Gtk::Settings.rc_property_parse_enum(enum, "watch")
+p Gtk::Settings.rc_property_parse_flags(flags, "(scroll-mask|structure-mask)")
+p Gtk::Settings.rc_property_parse_requisition(spec, "{100, 200}")
+p Gtk::Settings.rc_property_parse_border(spec, "{100, 200, 300, 400}")
--- gtk/src/rbgtksettings.c
+++ gtk/src/rbgtksettings.c
@@ -4,12 +4,14 @@
   rbgtksettings.c -
 
   $Author: mutoh $
-  $Date: 2003/06/26 15:15:32 $
+  $Date: 2004/01/31 18:50:30 $
 
   Copyright (C) 2003 Masao Mutoh
 ************************************************/
 
 #include "global.h"
+
+static VALUE prop_func_table;
 
 static VALUE
 settings_s_get_default(self)
@@ -18,49 +20,128 @@ settings_s_get_default(self)
     return GOBJ2RVAL(gtk_settings_get_default());
 }
 
+#if GTK_CHECK_VERSION(2,2,0)
 static VALUE
-settings_s_install_property(self, pspec)
-    VALUE self, pspec;
+settings_s_get_for_screen(self, screen)
+    VALUE self, screen;
 {
-    gtk_settings_install_property(G_PARAM_SPEC(RVAL2GOBJ(pspec)));
-    return pspec;
+    return GOBJ2RVAL(gtk_settings_get_for_screen(GDK_SCREEN(RVAL2GOBJ(screen))));
+}
+#endif
+
+/*
+ * Gtk::Settings.install_property(...) do |spec, str|
+ *   #parse str
+ *   new_val = ....   # parse value from string.
+ *   new_val          # return new_val or nil if you can't convert the value.
+ * end
+ */
+static gboolean
+rc_property_parser(pspec, rc_string, property_value)
+    const GParamSpec *pspec;
+    const GString *rc_string;
+    GValue *property_value;
+{
+    VALUE spec = GOBJ2RVAL((gpointer)pspec);
+    VALUE func = rb_hash_aref(prop_func_table, spec);
+    VALUE ret = rb_funcall(func, id_call, 2, spec, CSTR2RVAL(rc_string->str));
+    if (NIL_P(ret)) {
+        return FALSE;
+    } else if (RTEST(ret)){
+        rbgobj_rvalue_to_gvalue(ret, property_value);
+        return TRUE;
+    } else {
+        rb_raise(rb_eArgError, "Gtk::Settings#install_property() block should return new value or nil");
+    }
 }
 
-/* How can I implement them?
-gboolean
-_rc_property_parser(pspec, rc_string, property_value)
-   const GParamSpec *pspec;
-   const GString *rc_string;
-   GValue *property_value;
+static VALUE
+settings_s_install_property(self, spec)
+    VALUE self, spec;
 {
-   
+    GParamSpec* pspec = G_PARAM_SPEC(RVAL2GOBJ(spec));
+    if (rb_block_given_p()){
+        VALUE func = G_BLOCK_PROC();
+        rb_hash_aset(prop_func_table, spec, func);
+        gtk_settings_install_property_parser(pspec, rc_property_parser);
+    } else {
+        gtk_settings_install_property(pspec);
+    }
+    return self;
 }
 
 static VALUE
-settings_s_install_property_parser(pspec, parser)
-   VALUE pspec, parser;
+settings_rc_property_parse_color(self, spec, str)
+    VALUE self, spec, str;
 {
-   volatile VALUE func = G_BLOCK_PROC();
-   gtk_settings_install_property_parser(G_PARAM_SPEC(RVAL2GOBJ(pspec)), 
-   (GtkRcPropertyParser)_rc_property_parser);
+    gboolean ret; 
+    GValue prop = {0,};
+    g_value_init(&prop, GDK_TYPE_COLOR);
+    ret = gtk_rc_property_parse_color(G_PARAM_SPEC(RVAL2GOBJ(spec)), 
+                                      g_string_new(RVAL2CSTR(str)), &prop);
+    return ret ? GVAL2RVAL(&prop) : Qnil;
 }
-gboolean    gtk_rc_property_parse_color     (const GParamSpec *pspec,
-                                             const GString *gstring,
-                                             GValue *property_value);
-gboolean    gtk_rc_property_parse_enum      (const GParamSpec *pspec,
-                                             const GString *gstring,
-                                             GValue *property_value);
-gboolean    gtk_rc_property_parse_flags     (const GParamSpec *pspec,
-                                             const GString *gstring,
-                                             GValue *property_value);
-gboolean    gtk_rc_property_parse_requisition
-                                            (const GParamSpec *pspec,
-                                             const GString *gstring,
-                                             GValue *property_value);
-gboolean    gtk_rc_property_parse_border    (const GParamSpec *pspec,
-                                             const GString *gstring,
-                                             GValue *property_value);
-*/
+
+static VALUE
+settings_rc_property_parse_enum(self, spec, str)
+    VALUE self, spec, str;
+{
+    gboolean ret; 
+    GValue prop = {0,};
+    GParamSpec* gspec = G_PARAM_SPEC(RVAL2GOBJ(spec));
+    g_value_init(&prop, gspec->value_type);
+    ret = gtk_rc_property_parse_enum(gspec, g_string_new(RVAL2CSTR(str)), &prop);
+    return ret ? GVAL2RVAL(&prop) : Qnil;
+}
+
+static VALUE
+settings_rc_property_parse_flags(self, spec, str)
+    VALUE self, spec, str;
+{
+    gboolean ret; 
+    GValue prop = {0,};
+    GParamSpec* gspec = G_PARAM_SPEC(RVAL2GOBJ(spec));
+    g_value_init(&prop, gspec->value_type);
+    ret = gtk_rc_property_parse_flags(gspec, g_string_new(RVAL2CSTR(str)), &prop);
+    return ret ? GVAL2RVAL(&prop) : Qnil;
+}
+
+static VALUE
+settings_rc_property_parse_requisition(self, spec, str)
+    VALUE self, spec, str;
+{
+    gboolean ret; 
+    GValue prop = {0,};
+    g_value_init(&prop, GTK_TYPE_REQUISITION);
+    ret = gtk_rc_property_parse_requisition(G_PARAM_SPEC(RVAL2GOBJ(spec)), 
+                                            g_string_new(RVAL2CSTR(str)), &prop);
+    if (ret){
+        GtkRequisition* req = g_value_get_boxed(&prop);
+        return rb_assoc_new(INT2NUM(req->width), INT2NUM(req->height));
+    } else {
+        return Qnil;
+    }
+}
+
+static VALUE
+settings_rc_property_parse_border(self, spec, str)
+    VALUE self, spec, str;
+{
+    gboolean ret; 
+    GValue prop = {0,};
+    g_value_init(&prop, GTK_TYPE_BORDER);
+    ret = gtk_rc_property_parse_border(G_PARAM_SPEC(RVAL2GOBJ(spec)), 
+                                      g_string_new(RVAL2CSTR(str)), &prop);
+    if (ret){
+        GtkBorder* border = g_value_get_boxed(&prop);
+        return rb_ary_new3(4, INT2NUM(border->left), INT2NUM(border->right), 
+                           INT2NUM(border->top), INT2NUM(border->bottom));
+    } else {
+        return Qnil;
+    }
+
+    return ret ? GVAL2RVAL(&prop) : Qnil;
+}
 
 static VALUE
 settings_set_property_value(self, name, value, origin)
@@ -100,8 +181,20 @@ Init_gtk_settings()
 {
     VALUE gSettings = G_DEF_CLASS(GTK_TYPE_SETTINGS, "Settings", mGtk);
 
+    rb_global_variable(&prop_func_table);
+    prop_func_table = rb_hash_new();
+
     rb_define_singleton_method(gSettings, "default", settings_s_get_default, 0);
+#if GTK_CHECK_VERSION(2,2,0)
+    rb_define_singleton_method(gSettings, "get_for_screen", settings_s_get_for_screen, 1);
+#endif
     rb_define_singleton_method(gSettings, "install_property", settings_s_install_property, 1);
+
+    rb_define_singleton_method(gSettings, "rc_property_parse_color", settings_rc_property_parse_color, 2);
+    rb_define_singleton_method(gSettings, "rc_property_parse_enum", settings_rc_property_parse_enum, 2);
+    rb_define_singleton_method(gSettings, "rc_property_parse_flags", settings_rc_property_parse_flags, 2);
+    rb_define_singleton_method(gSettings, "rc_property_parse_requisition", settings_rc_property_parse_requisition, 2);
+    rb_define_singleton_method(gSettings, "rc_property_parse_border", settings_rc_property_parse_border, 2);
 
     rb_define_method(gSettings, "set_property_value", settings_set_property_value, 3);
 }
