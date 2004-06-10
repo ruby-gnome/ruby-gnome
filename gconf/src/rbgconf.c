fa869ac913f08abaa17e7cecc819dbf166565f56
Masao Mutoh <mutoh@highway.ne.jp>
2004-06-10 16:37:45 +0000

    * src/rbgconf.c: Fix a wrong definition of g_id_call.    by Giovanni Corriga

--- gconf/src/rbgconf.c
+++ gconf/src/rbgconf.c
@@ -18,9 +18,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  *
- * $Author: pcppopper $
+ * $Author: mutoh $
  *
- * $Date: 2003/04/12 23:35:57 $
+ * $Date: 2004/06/10 16:37:45 $
  *
  *****************************************************************************/
 
@@ -35,6 +35,8 @@
 /* Function Declarations *****************************************************/
 
 /* Global Variables **********************************************************/
+
+ID g_id_call;
 
 /* Function Implementations **************************************************/
 
