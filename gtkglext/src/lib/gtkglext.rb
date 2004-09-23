879bda7870aee94b2b5e150e63e7e1213ee9b8f9
Vincent Isambart <vincent.isambart@laposte.net>
2004-09-23 13:44:26 +0000

    2004-09-23 Vincent Isambart <isambart@netcourrier.com>
    	* src/lib/gtkglext.rb: Improved log handling.
    	Defined Gtk::GL::LOG_DOMAIN and Gdk::GL::LOG_DOMAIN.
    	(Thanks to Masao because he did 99.9% of this ;o))

--- gtkglext/src/lib/gtkglext.rb
+++ gtkglext/src/lib/gtkglext.rb
@@ -1,3 +1,18 @@
 require 'gtk2'
 require 'gtkglext.so'
 require 'opengl'
+
+module Gdk
+  module GL
+    LOG_DOMAIN="GdkGLExt"
+  end
+end
+
+module Gtk
+  module GL
+    LOG_DOMAIN="GtkGLExt"
+  end
+end
+
+GLib::Log.set_log_domain(Gdk::GL::LOG_DOMAIN)
+GLib::Log.set_log_domain(Gtk::GL::LOG_DOMAIN)
