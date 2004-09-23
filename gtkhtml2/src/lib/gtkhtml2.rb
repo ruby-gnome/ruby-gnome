4359c3b555a1e231ce2c6554e5d8cb60d0bd0b2f
Masao Mutoh <mutoh@highway.ne.jp>
2004-09-23 01:07:54 +0000

    * src/lib/gtkhtml2.rb: Support GLib::Log.set_log_domain. 	Define Gtk::Html::LOG_DOMAIN,Gtk::HtmlView::LOG_DOMAIN, 	Gtk::HtmlDocument::LOG_DOMAIN.

--- gtkhtml2/src/lib/gtkhtml2.rb
+++ gtkhtml2/src/lib/gtkhtml2.rb
@@ -1,2 +1,28 @@
 require 'gtk2'
 require 'gtkhtml2.so'
+
+module Gtk
+  module Html
+    LOG_DOMAIN = "GtkHtml"
+  end
+
+  class HtmlDocument
+    LOG_DOMAIN = "HtmlDocument"
+  end
+
+  class HtmlView
+    LOG_DOMAIN = "HtmlView"
+  end
+    
+end
+
+GLib::Log.set_log_domain(Gtk::Html::LOG_DOMAIN)
+GLib::Log.set_log_domain(Gtk::HtmlDocument::LOG_DOMAIN)
+GLib::Log.set_log_domain(Gtk::HtmlView::LOG_DOMAIN)
+
+GLib::Log.set_log_domain("HtmlA11y")
+GLib::Log.set_log_domain("HtmlCss")
+GLib::Log.set_log_domain("DomViews")
+GLib::Log.set_log_domain("HtmlGraphics")
+GLib::Log.set_log_domain("HtmlLayout")
+GLib::Log.set_log_domain("HtmlUtil")
