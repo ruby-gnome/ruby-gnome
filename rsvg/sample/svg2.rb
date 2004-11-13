0956660e15d304fca2f3b85d8aad0bd26f117485
Kouhei Sutou <kou@cozmixng.org>
2004-11-13 09:32:06 +0000

    * forgot to add sample/.

---
+++ rsvg/sample/svg2.rb
@@ -0,0 +1,17 @@
+require "rsvg2"
+
+if ARGV.size < 2
+  puts "usage: #{$0} input.svg output [scale_ratio]"
+  exit(-1)
+end
+
+input, output, ratio = ARGV
+ratio ||= 1.0
+ratio = ratio.to_f
+
+output_type = File.extname(output)[1..-1].downcase
+output_type = "jpeg" if /jpg/ =~ output_type
+
+pixbuf = RSVG.pixbuf_from_file_at_zoom(input, ratio, ratio)
+puts "saving to #{output}(#{pixbuf.width}x#{pixbuf.height})..."
+pixbuf.save(output, output_type)
