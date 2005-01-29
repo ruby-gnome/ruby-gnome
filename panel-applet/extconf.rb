af0941c2774015c510468cabf4dfe97b587bab91
Masao Mutoh <mutoh@highway.ne.jp>
2005-01-29 18:56:53 +0000

    * extconf.rb: Follow mkmf-gnome2.rb changes.

--- panel-applet/extconf.rb
+++ panel-applet/extconf.rb
@@ -2,26 +2,20 @@
 extconf.rb for Ruby/GdkPixbuf extention library
 =end
 
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../glib/src/lib')
+PACKAGE_NAME = "panelapplet2"
+
+TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
+MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
+SRCDIR = TOPDIR + '/panel-applet'
+
+$LOAD_PATH.unshift MKMF_GNOME2_DIR
+
 require 'mkmf-gnome2'
 
 PKGConfig.have_package('libpanelapplet-2.0') or exit 1
-check_win32
+setup_win32(PACKAGE_NAME)
 
-top = File.expand_path(File.dirname(__FILE__) + '/..') # XXX
-$CFLAGS += " " + ['glib/src', 'gtk/src'].map{|d|
-  "-I" + File.join(top, d)
-}.join(" ")
-
-if /cygwin|mingw/ =~ RUBY_PLATFORM
-  top = '..'
-  [
-    ["glib/src", "ruby-glib2"],
-    ["gtk/src", "ruby-gtk2"],
-  ].each{|d,l|
-    $LDFLAGS << " -L#{top}/#{d}"
-    $libs << " -l#{l}"
-  }
-end
+add_depend_package("glib2", "glib/src", TOPDIR)
+add_depend_package("gtk2", "gtk/src", TOPDIR)
 
 create_makefile('panelapplet2')
