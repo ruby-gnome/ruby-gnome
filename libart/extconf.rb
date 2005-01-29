ec54e4c10618f33c4a070df4f187cb0bd31e5bfc
Masao Mutoh <mutoh@highway.ne.jp>
2005-01-29 17:37:20 +0000

    * extconf.rb: Follow mkmf-gnome2.rb changes.

--- libart/extconf.rb
+++ libart/extconf.rb
@@ -2,7 +2,13 @@
 extconf.rb for Ruby/Libart extention library
 =end
 
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../glib/src/lib')
+PACKAGE_NAME = "libart2"
+
+TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
+MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
+SRCDIR = TOPDIR + '/libart/src'
+
+$LOAD_PATH.unshift MKMF_GNOME2_DIR
 
 require 'mkmf-gnome2'
 
@@ -30,64 +36,10 @@ unless have_header("jpeglib.h")
 end
 have_library("png", "png_create_write_struct")
 have_library("jpeg", "jpeg_set_defaults")
+have_library("art_lgpl_2", "art_affine_identity") or exit 1
 
-check_win32
+setup_win32(PACKAGE_NAME)
 
-set_output_lib('libruby-libart2.a')
+create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_LIBART2_COMPILATION")
+create_top_makefile
 
-#
-# create Makefiles
-#
-begin
-  srcdir = File.dirname($0) == "." ? "." :
-    File.expand_path(File.dirname($0) + "/src")
-
-  Dir.mkdir "src" unless File.exist? "src"
-  Dir.chdir "src"
-
-  begin
-    lib_ary = [ ["art_lgpl_2", "art_affine_identity"] ]
-
-    lib_ary.each do |ary|
-      if not have_library(ary[0], ary[1])
-        msg = format("cannot found %s in %s.", ary[1], ary[0])
-        if ary[0] == "art_lgpl_2"
-          msg += " (or maybe `libart2-config --lib' is incorrect...)"
-        end
-        raise Interrupt, msg
-      end
-    end
-
-    obj_ext = ".#{$OBJEXT}"
-
-    $libs = $libs.split(/\s/).uniq.join(' ')
-    $source_files = Dir.entries(srcdir).select{|fname| /\.c$/ =~ fname }
-    $objs = $source_files.collect do |item|
-      item.gsub(/\.c$/, obj_ext)
-    end
-
-    #
-    # create Makefiles
-    #
-    create_makefile("libart2", srcdir)
-    raise Interrupt if not FileTest.exist? "Makefile"
-
-    mfile = File.open("Makefile", "a")
-    $source_files.each do |e|
-      mfile.print "#{e.gsub(/\.c$/, obj_ext)}: #{e} rbart.h\n"
-    end
-    mfile.print "\
-allclean: clean
-	rm -rf *.a
-"
-    mfile.close
-  ensure
-    Dir.chdir ".."
-  end
-
-  create_top_makefile
-
-rescue Interrupt
-  print "  [error] " + $!.to_s + "\n"
-  exit 1
-end
