69a39be4cce93f07f9054a6559b8bdfd6e0b1934
Masao Mutoh <mutoh@highway.ne.jp>
2005-01-29 17:28:24 +0000

    * extconf.rb: Follow mkmf-gnome2.rb changes. And make it simple.

--- gnome/extconf.rb
+++ gnome/extconf.rb
@@ -2,88 +2,22 @@
 extconf.rb for Ruby/GNOME extention library
 =end
 
-$LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__) + '/../glib/src/lib')
+PACKAGE_NAME = "gnome2"
+
+TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
+MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
+SRCDIR = TOPDIR + '/gnome/src'
+
+$LOAD_PATH.unshift MKMF_GNOME2_DIR
 
 require 'mkmf-gnome2'
 
-#
-# detect Gnome configurations
-#
-
 PKGConfig.have_package('libgnomeui-2.0') or exit 1
-check_win32
+setup_win32(PACKAGE_NAME)
 
-top = File.expand_path(File.dirname(__FILE__) + '/..') # XXX
-$CFLAGS += " " + ['glib/src', 'gtk/src'].map{|d|
-  "-I" + File.join(top, d)
-}.join(" ")
+add_depend_package("glib2", "glib/src", TOPDIR)
+add_depend_package("gtk2", "gtk/src", TOPDIR)
 
+create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOME2_COMPILATION")
 
-begin
-  srcdir = File.dirname($0) == "." ? "." :
-    File.expand_path(File.dirname($0) + "/src")
-
-  Dir.mkdir "src" unless File.exist? "src"
-  Dir.chdir "src"
-
-  begin
-    lib_ary = [
-      ["X11", "XOpenDisplay"],
-      ["gnome-2", "gnome_program_init"],
-      ["popt", "poptGetArgs"],
-    ]
-
-    lib_ary.each do |ary|
-
-      if not have_library(ary[0], ary[1])
-	msg = format("cannot found %s in %s.", ary[1], ary[0])
-	if ary[0] == "X11"
-	  msg += " (or maybe `pkg-config libgnomeui-2.0 --libs' is incorrect...)"
-	end
-	raise Interrupt, msg
-      end
-    end
-
-    if /cygwin|mingw/ =~ RUBY_PLATFORM
-      top = "../.."
-      [
-    	["glib/src", "ruby-glib2"],
-    	["gtk/src", "ruby-gtk2"],
-      ].each{|d,l|
-    	$libs << " -l#{l}"
-    	$LDFLAGS << " -L#{top}/#{d}"
-      }
-    end
-
-    obj_ext = ".#{$OBJEXT}"
-
-    $libs = $libs.split(/\s/).uniq.join(' ')
-    $source_files = Dir.entries(srcdir).select{|fname| /\.c$/ =~ fname }
-    $objs = $source_files.collect do |item|
-      item.gsub(/\.c$/, obj_ext)
-    end
-
-    #
-    # create Makefiles
-    #
-    create_makefile("gnome2", srcdir)
-    raise Interrupt if not FileTest.exist? "Makefile"
-
-    mfile = File.open("Makefile", "a")
-    $source_files.each do |e|
-      mfile.print "#{e.gsub(/\.c$/, obj_ext)}: #{e} rbgnome.h\n"
-    end
-    mfile.print "\
-allclean: clean
-	rm -rf *.a
-"
-    mfile.close
-  ensure
-    Dir.chdir ".."
-  end
-
-  create_top_makefile
-
-rescue Interrupt
-  print "  [error] " + $!.to_s + "\n"
-end
+create_top_makefile
