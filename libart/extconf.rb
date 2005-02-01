477db6c06c7102b17f77d3ab926c80a17443200f
Masao Mutoh <mutoh@highway.ne.jp>
2005-02-01 11:48:44 +0000

    * extconf.rb: Follow mkmf-gnome2.rb changes. Support MSVC++.   compile to set GTK_BASEPATH as the environment variable. * sample/rose.rb, testlibart2.rb: Work on mswin32/mingw.

--- libart/extconf.rb
+++ libart/extconf.rb
@@ -15,12 +15,6 @@ require 'glib2'
 
 PKGConfig.have_package('libart-2.0') or exit 1
 
-include_path = nil
-if ENV['GTK_INCLUDE_PATH']
-  inlucde_path = ENV['GTK_INCLUDE_PATH'].gsub("\\", "/") 
-  $hdrdir = include_path
-end
-
 $libs += " -lz "
 have_header("png.h") or exit 1
 have_header("jpeglib.h") or exit 1
@@ -30,8 +24,6 @@ have_library("jpeg", "jpeg_set_defaults")
 have_library("art_lgpl_2", "art_affine_identity") or exit 1
 
 setup_win32(PACKAGE_NAME)
-
-$CFLAGS += " -I#{$include_path} " if include_path
 
 create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_LIBART2_COMPILATION")
 create_top_makefile
