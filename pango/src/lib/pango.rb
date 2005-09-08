f9dc0df6bd5934cd008f98515899b04eed35cfbe
Kouhei Sutou <kou@cozmixng.org>
2005-09-08 05:23:19 +0000

    * src/lib/pango.rb (Pango): Add convenient aliases.   - Pango::Attribute::*    -> Pango::ATTRIBUTE_*   - Pango::Coverage::*     -> Pango::COVERAGE_*   - Pango::AttrXXX::TYPE_* -> Pango::XXX_*   - Pango::AttrXXX::*      -> Pango::XXX_*   - Pango::XXX::*          -> Pango::*

--- pango/src/lib/pango.rb
+++ pango/src/lib/pango.rb
@@ -17,7 +17,31 @@ module Pango
      end while iter.next!
     end
   end
+
+  constants.each do |name|
+    const = const_get(name)
+    if const.is_a?(Module)
+      case name
+      when /^Attr([A-Z].*)/
+        prefix = $1.gsub(/[A-Z][a-z]*/) {|x| "#{x.upcase}_"}
+      when "Attribute", "Coverage"
+        prefix = "#{name.upcase}_"
+      else
+        prefix = ""
+      end
+      
+      const.constants.each do |sub_name|
+        if sub_name == sub_name.upcase and sub_name != "LOG_DOMAIN"
+          if /^TYPE_(.*)/ =~ sub_name
+            new_name = "#{prefix}#{$1}"
+          else
+            new_name = "#{prefix}#{sub_name}"
+          end
+          const_set(new_name, const.const_get(sub_name))
+        end
+      end
+    end
+  end
 end
 
 GLib::Log.set_log_domain(Pango::LOG_DOMAIN)
-
