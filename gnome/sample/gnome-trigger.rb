6dbac725c2f757f277e993971c3364ffd19a3d86
Masao Mutoh <mutoh@highway.ne.jp>
2005-09-24 18:22:13 +0000

    * Code cleanup.

--- gnome/sample/gnome-trigger.rb
+++ gnome/sample/gnome-trigger.rb
@@ -4,7 +4,7 @@
   Copyright (C) 2005 Masao Mutoh
   This program is licenced under the same licence as Ruby-GNOME2.
 
-  $Id: gnome-trigger.rb,v 1.1 2005/09/24 18:02:43 mutoh Exp $
+  $Id: gnome-trigger.rb,v 1.2 2005/09/24 18:22:13 mutoh Exp $
 =end
 
 require 'gnome2'
@@ -16,9 +16,6 @@ Gnome::Trigger.set_action_func {|msg, level, supinfo|
 }
 
 trig = Gnome::Trigger.new(Gnome::Trigger::FUNCTION, "level1")
-
 trig.add_trigger("gnome", "logout")
 
 Gnome::Trigger.do("Trigger message", "level1", "gnome", "logout")
-# Gnome::Trigger.new(Gnome::Trigger::COMMAND, command, level = nil)
-# Gnome::Trigger.new(Gnome::Trigger::MEDIAPLAY, file, cache_id, level = nil)
