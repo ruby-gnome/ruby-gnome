30579d6f1543ad244103170d5262517ad7d39353
Masao Mutoh <mutoh@highway.ne.jp>
2005-09-28 17:38:25 +0000

    * src/rbatk.c: Renamed Atk::VERSION to Atk::BUILD_VERSION. 	Removed Atk::(MAJOR|MINOR|MICRO)_VERSION.

--- atk/src/rbatk.c
+++ atk/src/rbatk.c
@@ -4,7 +4,7 @@
   rbatk.c -
 
   $Author: mutoh $
-  $Date: 2005/09/15 17:41:18 $
+  $Date: 2005/09/28 17:38:25 $
 
   Copyright (C) 2003-2005 Masao Mutoh
 ************************************************/
@@ -18,14 +18,10 @@ Init_atk()
 {
     mAtk = rb_define_module("Atk");
 
-    rb_define_const(mAtk, "VERSION", 
+    rb_define_const(mAtk, "BUILD_VERSION", 
                     rb_ary_new3(3, 
                                 INT2FIX(ATK_MAJOR_VERSION), 
                                 INT2FIX(ATK_MINOR_VERSION), 
                                 INT2FIX(ATK_MICRO_VERSION)));
-    rb_define_const(mAtk, "MAJOR_VERSION", INT2FIX(ATK_MAJOR_VERSION));
-    rb_define_const(mAtk, "MINOR_VERSION", INT2FIX(ATK_MINOR_VERSION));
-    rb_define_const(mAtk, "MICRO_VERSION", INT2FIX(ATK_MICRO_VERSION));
-
     Init_atk_inits();
 }
