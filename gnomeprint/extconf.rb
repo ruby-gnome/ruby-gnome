0477668577810340d890b77dcad50fab316eec9d
Kouhei Sutou <kou@cozmixng.org>
2005-10-10 01:59:48 +0000

    * extconf.rb: Use make_version_header. * src: Revised copyright information. * src/rbgp-gnome-print-pango.h: Removed.

--- gnomeprint/extconf.rb
+++ gnomeprint/extconf.rb
@@ -11,6 +11,7 @@ $LOAD_PATH.unshift MKMF_GNOME2_DIR
 
 require 'mkmf-gnome2'
 modname = "libgnomeprint-2.2"
+app_name = "LIBGNOMEPRINT"
 
 PKGConfig.have_package(modname, 2, 8) or exit 1
 setup_win32(PACKAGE_NAME)
@@ -18,27 +19,7 @@ setup_win32(PACKAGE_NAME)
 add_depend_package("glib2", "glib/src", TOPDIR)
 add_depend_package("libart2", "libart/src", TOPDIR)
 
-add_distcleanfile("rbgp-version.h")
-version = PKGConfig.modversion(modname).split(/\./)
-File.open(File.join("src", "rbgp-version.h"), "w") do |f|
-  f.print <<-EOH
-#ifndef RBGP_VERSION_H
-#define RBGP_VERSION_H
-
-#define LIBGNOMEPRINT_MAJOR_VERSION (#{version[0]})
-#define LIBGNOMEPRINT_MINOR_VERSION (#{version[1]})
-#define LIBGNOMEPRINT_MICRO_VERSION (#{version[2]})
-
-#define LIBGNOMEPRINT_CHECK_VERSION(major, minor, micro) \\
-  (LIBGNOMEPRINT_MAJOR_VERSION > (major) ||              \\
-    (LIBGNOMEPRINT_MAJOR_VERSION == (major) &&           \\
-      LIBGNOMEPRINT_MINOR_VERSION > (minor)) ||          \\
-    (LIBGNOMEPRINT_MAJOR_VERSION == (major) &&           \\
-      LIBGNOMEPRINT_MINOR_VERSION == (minor) &&          \\
-      LIBGNOMEPRINT_MICRO_VERSION >= (micro)))
-#endif
-EOH
-end
+make_version_header(app_name, modname)
 
 create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOMEPRINT2_COMPILATION")
 create_top_makefile
