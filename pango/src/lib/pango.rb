2de6ab4ca00d9c1b15084ac5dba88cccd2d78132
Masao Mutoh <mutoh@highway.ne.jp>
2005-10-15 17:26:31 +0000

    * src/lib/pango.rb: Rewrited a code which creates convinient constants         to Pango module to avoid to make unexpect values and works Ruby-1.6.

--- pango/src/lib/pango.rb
+++ pango/src/lib/pango.rb
@@ -1,10 +1,17 @@
+#
+# pango.rb
+# Copyright(C) 2005 Ruby-GNOME2 Project.
+#
+# This program is licenced under the same
+# license of Ruby-GNOME2.
+#
+
 require 'glib2'
 begin
   require 'cairo'
 rescue LoadError
 end
 require 'pango.so'
-
 
 module Pango
   LOG_DOMAIN = "Pango"
@@ -29,15 +36,20 @@ module Pango
   end
 
   # [[klass, prefix], ....]]
-  [[Layout::Alignment, "ALIGN_"], [AttrScale, "SCALE_"], [Script, "SCRIPT_"],
+  targets = [[Layout::Alignment, "ALIGN_"], [AttrScale, "SCALE_"], 
     [Coverage::Level, "COVERAGE_"], [Context::Direction, "DIRECTION_"],
-    [Layout::EllipsizeMode, "ELLIPSIZE_"], [Layout::WrapMode, "WRAP_"],
-    [FontDescription::FontMask, "FONT_MASK_"], 
+    [Layout::WrapMode, "WRAP_"], [FontDescription::FontMask, "FONT_MASK_"], 
     [FontDescription::Stretch, "STRETCH_"], [FontDescription::Style, "STYLE_"],
     [FontDescription::Variant, "VARIANT_"], [FontDescription::Weight, "WEIGHT_"], 
-    [Renderer::Part, "PART_"], [TabArray::TabAlign, "TAB_"],
-    [AttrUnderline::Underline, "UNDERLINE_"]] .each do |klass, prefix|
-    klass.constants.each do |name|
+    [TabArray::TabAlign, "TAB_"],
+    [AttrUnderline::Underline, "UNDERLINE_"]]
+
+  targets << [Script, "SCRIPT_"] if defined? Script
+  targets << [Layout::EllipsizeMode, "ELLIPSIZE_"] if defined? Layout::EllipsizeMode
+  targets << [Renderer::Part, "PART_"] if defined? Renderer::Part
+
+  targets.each do |klass, prefix|
+    (klass.constants - klass.superclass.constants).each do |name|
       unless klass.const_get(name).is_a? Class
 	const_set(prefix + name, klass.const_get(name))
       end
