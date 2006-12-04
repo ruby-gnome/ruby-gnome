3bb19f7e85b5ff30e0dce91a7469d6a8aaf2666e
Kouhei Sutou <kou@cozmixng.org>
2006-12-04 13:50:45 +0000

    * tests/runner.rb: setup $LOAD_PATH for test environment.

--- glib/tests/runner.rb
+++ glib/tests/runner.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
+
+base = File.expand_path(File.dirname(__FILE__))
+top = File.expand_path(File.join(base, ".."))
+
+$LOAD_PATH.unshift(File.join(top, "src"))
+$LOAD_PATH.unshift(File.join(top, "src", "lib"))
+$LOAD_PATH.unshift(base)
+
 require 'test/unit'
+
 require 'test-glib2.rb'
 require 'test-iochannel.rb'
 
