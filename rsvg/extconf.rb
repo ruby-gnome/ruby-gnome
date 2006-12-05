72d0be27da11b5e4c142e87fea1bbf3d637bda27
Kouhei Sutou <kou@cozmixng.org>
2006-12-05 14:41:12 +0000

    * extconf.rb: used glib_mkenums.

--- rsvg/extconf.rb
+++ rsvg/extconf.rb
@@ -2,92 +2,6 @@
 extconf.rb for Ruby/RSVG extention library
 =end
 
-def mkenums(output, config, files)
-  makefile_created = $makefile_created
-  $makefile_created = true
-  args = []
-  %w(fhead fprod ftail eprod vhead vprod vtail comments template).each do |param|
-    param = param.intern
-    args += ["--#{param}", config[param]] if config.has_key?(param)
-  end
-  args += files
-  for_read, for_write = IO.pipe
-  pid = fork do
-    STDOUT.reopen(for_write)
-    for_write.close
-    system('glib-mkenums', *args)
-  end
-  for_write.close
-  File.open(output, "w") do |out|
-    out.print(for_read.read)
-  end
-  Process.waitpid(pid)
-ensure
-  $makefile_created = makefile_created
-end
-
-def mkenums_h(prefix, files)
-  header = prefix + ".h"
-  const = "__#{header.upcase.gsub(/-|\./, '_')}__"
-  config = {
-    :fhead => <<-FHEAD,
-#ifndef #{const}
-#define #{const}
-
-#include <glib-object.h>
-
-G_BEGIN_DECLS
-FHEAD
-    :fprod => <<-FPROD,
-/* enumerations from "@filename@" */
-FPROD
-    :vhead => <<-VHEAD,
-GType @enum_name@_get_type (void);
-#define RSVG_TYPE_@ENUMSHORT@ (@enum_name@_get_type())
-VHEAD
-    :ftail => <<-FTAIL,
-G_END_DECLS
-
-#endif /* #{const}*/
-FTAIL
-  }
-  mkenums(header, config, files)
-end
-
-def mkenums_c(prefix, files)
-  header = prefix + ".h"
-  c = prefix + ".c"
-  config = {
-    :fhead => <<-FHEAD,
-#include "#{header}"
-#include <librsvg/rsvg.h>
-FHEAD
-    :fprod => <<-FPROD,
-/* enumerations from "@filename@" */
-FPROD
-    :vhead => <<-VHEAD,
-GType
-@enum_name@_get_type (void)
-{
-  static GType etype = 0;
-  if (etype == 0) {
-    static const G@Type@Value values[] = {
-VHEAD
-    :vprod => <<-VPROD,
-      { @VALUENAME@, "@VALUENAME@", "@valuenick@" },
-VPROD
-    :vtail => <<-VTAIL,
-      { 0, NULL, NULL }
-    };
-    etype = g_@type@_register_static ("@EnumName@", values);
-  }
-  return etype;
-}
-VTAIL
-  }
-  mkenums(c, config, files)
-end
-
 PACKAGE_NAME = "rsvg2"
 
 TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
@@ -125,7 +39,7 @@ end
 
 add_depend_package("glib2", "glib/src", TOPDIR)
 
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_RSVG2_COMPILATION") {
+create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_RSVG2_COMPILATION") do
   enum_type_prefix = "librsvg-enum-types"
   unless have_header("librsvg/#{enum_type_prefix}.h")
     include_paths = PKGConfig.cflags_only_I("librsvg-2.0")
@@ -133,12 +47,8 @@ create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_RSVG2_COMPILATION") {
       /librsvg/.match(x)
     end.sub(/^-I/, "")
     headers = Dir.glob(File.join(include_path, "librsvg", "*.h"))
-    mkenums_h(enum_type_prefix, headers)
-    mkenums_c(enum_type_prefix, headers)
+    glib_mkenums(enum_type_prefix, headers, "RSVG_TYPE_", ["librsvg/rsvg.h"])
   end
-
-  add_distcleanfile(enum_type_prefix + ".c")
-  add_distcleanfile(enum_type_prefix + ".h")
-}
+end
 
 create_top_makefile
