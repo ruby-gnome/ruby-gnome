50019fca66954d1596e9be246a70978c5b0dfc66
Kouhei Sutou <kou@cozmixng.org>
2006-12-17 08:30:26 +0000

    * src/rbgdkcairo.c (gdkdraw_cairo_set_source_pixbuf): moved to Ruby/GdkPixbuf2.

--- gtk/src/rbgdkcairo.c
+++ gtk/src/rbgdkcairo.c
@@ -4,7 +4,7 @@
   rbgdkcairo.c -
 
   $Author: ktou $
-  $Date: 2006/12/17 07:55:05 $
+  $Date: 2006/12/17 08:30:26 $
 
   Copyright (C) 2005 Kouhei Sutou
 ************************************************/
@@ -22,28 +22,19 @@ static VALUE
 gdkdraw_cairo_set_source_color(self, color)
     VALUE self, color;
 {
-    gdk_cairo_set_source_color(_SELF(self), 
+    gdk_cairo_set_source_color(_SELF(self),
                                (GdkColor*)RVAL2BOXED(color, GDK_TYPE_COLOR));
     rb_cairo_check_status(cairo_status(_SELF(self)));
     return self;
 }
 
-static VALUE
-gdkdraw_cairo_set_source_pixbuf(argc, argv, self)
-    int argc;
-    VALUE *argv;
-    VALUE self;
-{
-    VALUE pixbuf, pixbuf_x, pixbuf_y;
+/* Move to gdkpixbuf/rbgdk-pixbuf.c
+void gdk_cairo_set_source_pixbuf (cairo_t      *cr,
+				  GdkPixbuf    *pixbuf,
+				  double        pixbuf_x,
+				  double        pixbuf_y);
+*/
 
-    rb_scan_args(argc, argv, "12", &pixbuf, &pixbuf_x, &pixbuf_y);
-
-    gdk_cairo_set_source_pixbuf(_SELF(self), GDK_PIXBUF(RVAL2GOBJ(pixbuf)),
-                                NIL_P(pixbuf_x) ? 0 : NUM2DBL(pixbuf_x),
-                                NIL_P(pixbuf_y) ? 0 : NUM2DBL(pixbuf_y));
-    rb_cairo_check_status(cairo_status(_SELF(self)));
-    return self;
-}
 
 #if GTK_CHECK_VERSION(2,10,0)
 static VALUE
@@ -84,7 +75,6 @@ Init_gtk_gdk_cairo()
 #if GTK_CHECK_VERSION(2,8,0)
 #  ifdef HAVE_RB_CAIRO_H
     rb_define_method(rb_cCairo_Context, "set_source_color", gdkdraw_cairo_set_source_color, 1);
-    rb_define_method(rb_cCairo_Context, "set_source_pixbuf", gdkdraw_cairo_set_source_pixbuf, -1);
 #if GTK_CHECK_VERSION(2,10,0)
     rb_define_method(rb_cCairo_Context, "set_source_pixmap", gdkdraw_cairo_set_source_pixmap, 3);
 #endif
