5bbfd03614de1d7b493bc24d41c2afe35a34a592
Kouhei Sutou <kou@cozmixng.org>
2007-11-11 08:55:14 +0000

    * src/rbgcompat.h: fixed a typo.

--- glib/src/rbgcompat.h
+++ glib/src/rbgcompat.h
@@ -12,7 +12,7 @@ G_BEGIN_DECLS
 
 typedef RGConvertTable RGFundamental;
 
-#define G_DEF_FUNDAMENTAL(f) RG_CONVERSION(f)
+#define G_DEF_FUNDAMENTAL(f) RG_DEF_CONVERSION(f)
 #define rbgobj_fund_define_fundamental(f) rbgobj_convert_define(f)
 
 G_END_DECLS
