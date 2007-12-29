13f2ec1662d76be59c3df9f6d04781bf9e3ec64a
Kouhei Sutou <kou@cozmixng.org>
2007-12-29 05:44:07 +0000

    * src/rbglib_mainloop.c: moved poll related functions to ... * src/rbglib_maincontext.c: ... here.

--- glib/src/rbglib_maincontext.c
+++ glib/src/rbglib_maincontext.c
@@ -11,6 +11,14 @@
 
 #include "rbgprivate.h"
 
+#ifndef HAVE_RB_THREAD_BLOCKING_REGION
+#  include <rubysig.h>
+#  include <node.h>
+#  ifdef HAVE_CURR_THREAD
+#    define rb_curr_thread curr_thread
+#  endif
+#endif
+
 /*
 static ID id_poll_func;
 */
@@ -26,6 +34,235 @@ typedef struct _callback_info_t
     VALUE callback;
     guint id;
 } callback_info_t;
+
+/*****************************************/
+static GPollFunc default_poll_func;
+
+#ifdef HAVE_RB_THREAD_BLOCKING_REGION
+typedef struct _PollInfo
+{
+    GPollFD *ufds;
+    guint nfsd;
+    gint timeout;
+    gint result;
+} PollInfo;
+
+static VALUE
+rg_poll_in_blocking(void *data)
+{
+    PollInfo *info = data;
+
+    info->result = default_poll_func(info->ufds, info->nfsd, info->timeout);
+
+    return Qnil;
+}
+
+static gint
+rg_poll(GPollFD *ufds, guint nfsd, gint timeout)
+{
+    PollInfo info;
+
+    info.ufds = ufds;
+    info.nfsd = nfsd;
+    info.timeout = timeout;
+    info.result = 0;
+
+    rb_thread_blocking_region(rg_poll_in_blocking, &info, RB_UBF_DFL, NULL);
+
+    return info.result;
+}
+#else
+static gint
+rg_poll(GPollFD *ufds, guint nfsd, gint timeout)
+{
+    gint result;
+    int thread_critical;
+
+    thread_critical = rb_thread_critical;
+    rb_thread_critical = 1;
+    TRAP_BEG;
+    result = default_poll_func(ufds, nfsd, timeout);
+    rb_thread_critical = thread_critical;
+    TRAP_END;
+
+    return result;
+}
+#endif
+
+static void
+restore_poll_func(VALUE data)
+{
+    if (g_main_context_get_poll_func(NULL) == (GPollFunc)rg_poll) {
+        g_main_context_set_poll_func(NULL, default_poll_func);
+    }
+}
+
+#ifndef HAVE_RB_THREAD_BLOCKING_REGION
+/* from eval.c */
+#define WAIT_FD		(1<<0)
+#define WAIT_SELECT	(1<<1)
+#define WAIT_TIME	(1<<2)
+#define WAIT_JOIN	(1<<3)
+#define WAIT_PID	(1<<4)
+
+#define DELAY_INFTY 1E30
+
+static double
+timeofday()
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (double)tv.tv_sec + (double)tv.tv_usec * 1e-6;
+}
+
+/*****************************************/
+typedef struct _RGSource
+{
+    GSource source;
+
+    GList *poll_fds;
+    gboolean ready;
+} RGSource;
+
+static void
+source_free_poll_fds(GSource *source, gboolean source_is_destroyed)
+{
+    RGSource *rg_source = (RGSource *)source;
+    GList *node;
+
+    for (node = rg_source->poll_fds; node; node = g_list_next(node)) {
+        GPollFD *poll_fd = node->data;
+        if (!source_is_destroyed)
+            g_source_remove_poll(source, poll_fd);
+        g_free(poll_fd);
+    }
+    g_list_free(rg_source->poll_fds);
+    rg_source->poll_fds = NULL;
+}
+
+static inline void
+source_prepare_add_poll_fd(GSource *source, rb_thread_t thread)
+{
+    RGSource *rg_source = (RGSource *)source;
+    GPollFD *poll_fd;
+
+    poll_fd = g_new0(GPollFD, 1);
+    poll_fd->fd = thread->fd;
+    poll_fd->events = G_IO_IN;
+    if (FD_ISSET(thread->fd, &thread->readfds))
+        poll_fd->events |= G_IO_IN;
+    if (FD_ISSET(thread->fd, &thread->writefds))
+        poll_fd->events |= G_IO_OUT;
+    if (FD_ISSET(thread->fd, &thread->exceptfds))
+        poll_fd->events |= G_IO_PRI | G_IO_ERR | G_IO_HUP;
+
+    g_source_add_poll(source, poll_fd);
+    rg_source->poll_fds = g_list_prepend(rg_source->poll_fds, poll_fd);
+}
+
+static inline gboolean
+source_prepare_setup_poll_fd(GSource *source, gint *timeout)
+{
+    rb_thread_t thread;
+    gdouble now;
+
+    now = timeofday();
+    thread = rb_curr_thread;
+    do {
+        thread = thread->next;
+
+        if ((thread->wait_for == 0 && thread->status == THREAD_RUNNABLE &&
+             thread != rb_curr_thread) ||
+            (thread->wait_for & WAIT_JOIN &&
+             thread->join->status == THREAD_KILLED))
+            return TRUE;
+
+        if (thread->wait_for & WAIT_TIME && thread->delay != DELAY_INFTY) {
+            gint delay;
+
+            delay = (thread->delay - now) * 1000;
+            if (delay <= 0)
+                return TRUE;
+            if (*timeout == -1 || delay < *timeout)
+                *timeout = delay;
+        }
+
+        if (thread->wait_for & WAIT_FD)
+            source_prepare_add_poll_fd(source, thread);
+    } while (thread != rb_curr_thread);
+
+    return FALSE;
+}
+
+static gboolean
+source_prepare(GSource *source, gint *timeout)
+{
+    RGSource *rg_source = (RGSource *)source;
+
+    *timeout = -1;
+    source_free_poll_fds(source, FALSE);
+    rg_source->ready = source_prepare_setup_poll_fd(source, timeout);
+
+    return rg_source->ready;
+}
+
+static gboolean
+source_check(GSource *source)
+{
+    RGSource *rg_source = (RGSource *)source;
+    GList *node;
+
+    if (rg_source->ready)
+        return TRUE;
+
+    for (node = rg_source->poll_fds; node; node = g_list_next(node)) {
+        GPollFD *poll_fd = node->data;
+
+        if (poll_fd->revents)
+            return TRUE;
+    }
+
+    return FALSE;
+}
+
+static gboolean
+source_dispatch(GSource *source, GSourceFunc callback, gpointer user_data)
+{
+    TRAP_BEG;
+    rb_thread_schedule();
+    TRAP_END;
+
+    return TRUE;
+}
+
+static void
+source_finalize(GSource *source)
+{
+    source_free_poll_fds(source, TRUE);
+}
+
+static GSourceFuncs source_funcs = {
+    source_prepare,
+    source_check,
+    source_dispatch,
+    source_finalize
+};
+
+static GSource *
+ruby_source_new(void)
+{
+    GSource *source;
+    RGSource *rg_source;
+
+    source = g_source_new(&source_funcs, sizeof(RGSource));
+    g_source_set_can_recurse(source, TRUE);
+
+    rg_source = (RGSource *)source;
+    rg_source->poll_fds = NULL;
+
+    return source;
+}
+#endif
 
 static VALUE
 source_remove(self, tag)
@@ -80,7 +317,21 @@ static VALUE
 mc_initialize(self)
     VALUE self;
 {
-    G_INITIALIZE(self, g_main_context_new());
+    GMainContext *context;
+
+    context = g_main_context_new();
+
+    g_main_context_set_poll_func(context, rg_poll);
+#ifndef HAVE_RB_THREAD_BLOCKING_REGION
+    {
+        GSource *source;
+        source = ruby_source_new();
+        g_source_attach(source, context);
+        g_source_unref(source);
+    }
+#endif
+
+    G_INITIALIZE(self, context);
     return Qnil;
 }
 
@@ -501,5 +752,22 @@ Init_glib_main_context()
 #if GLIB_CHECK_VERSION(2,4,0)
     rb_define_module_function(child_watch, "source_new", child_watch_source_new, 1);
     rb_define_module_function(child_watch, "add", child_watch_add, 1);
+#endif
+
+
+    default_poll_func = g_main_context_get_poll_func(NULL);
+    g_main_context_set_poll_func(NULL, rg_poll);
+    rb_set_end_proc(restore_poll_func, Qnil);
+
+#ifndef HAVE_RB_THREAD_BLOCKING_REGION
+    {
+        GSource *source;
+        guint tag;
+
+        source = ruby_source_new();
+        tag = g_source_attach(source, NULL);
+        g_source_unref(source);
+        rb_set_end_proc((void (*)(VALUE))g_source_remove, (VALUE)tag);
+    }
 #endif
 }
