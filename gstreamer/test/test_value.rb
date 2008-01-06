34a4cc19d27b2bc4b08f11c801d2fe69c23f7200
Kouhei Sutou <kou@cozmixng.org>
2008-01-06 03:52:15 +0000

    * test/test_value.rb: supported GstIntRange.

--- gstreamer/test/test_value.rb
+++ gstreamer/test/test_value.rb
@@ -3,6 +3,24 @@ class TestValue < Test::Unit::TestCase
     assert_equal([1234, 4321], audio_convert_caps[0]["endianness"])
   end
 
+  def test_int_range
+    range = Gst::IntRange.new(10, 100)
+    assert_equal(10, range.min)
+    assert_equal(100, range.max)
+    assert_equal("[10,100]", range.to_s)
+
+    range.min = 1
+    range.max = 500
+    assert_equal(1, range.min)
+    assert_equal(500, range.max)
+    assert_equal("[1,500]", range.to_s)
+
+    range.set(100, 9999)
+    assert_equal(100, range.min)
+    assert_equal(9999, range.max)
+    assert_equal("[100,9999]", range.to_s)
+  end
+
   private
   def audio_convert_caps
     convert = Gst::ElementFactory.find("audioconvert")
