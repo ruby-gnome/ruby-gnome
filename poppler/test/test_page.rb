3f5b3c0e9bc31852eff999f62519016c7e57e4ab
Kouhei Sutou <kou@cozmixng.org>
2008-03-21 13:32:32 +0000

    * test/test_page.rb (TestPage#test_render_to_pixbuf): added.

--- poppler/test/test_page.rb
+++ poppler/test/test_page.rb
@@ -10,6 +10,18 @@ class TestPage < Test::Unit::TestCase
     end
   end
 
+  def test_render_to_pixbuf
+    document = Poppler::Document.new(image_pdf)
+    page = document[0]
+    width, height = page.size
+    pixbuf = Gdk::Pixbuf.new(Gdk::Pixbuf::COLORSPACE_RGB, true, 8,
+                             width / 2, height / 2)
+    pixbuf.fill!(0)
+    assert_equal("\0" * 10, pixbuf.pixels[0, 10])
+    page.render(0, 0, width, height, 0.5, 0, pixbuf)
+    assert_not_equal("\0" * 10, pixbuf.pixels[0, 10])
+  end
+
   private
   def find_first_image_mapping(document)
     document.each do |page|
