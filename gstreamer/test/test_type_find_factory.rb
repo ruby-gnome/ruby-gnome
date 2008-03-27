37af96edf2c8c26b4256eb4d0ccf4e09516cccc6
Kouhei Sutou <kou@cozmixng.org>
2008-03-27 11:54:41 +0000

    * test/test_type_find_factory.rb   (TestTypeFindFactory#test_extensions): normalized result.

--- gstreamer/test/test_type_find_factory.rb
+++ gstreamer/test/test_type_find_factory.rb
@@ -3,6 +3,6 @@ class TestTypeFindFactory < Test::Unit::TestCase
     factories = Gst::TypeFindFactory.list
     html_factory = factories.find {|f| f.name == "text/html"}
     assert_not_nil(html_factory)
-    assert_equal(["htm", "html"], html_factory.extensions)
+    assert_equal(["htm", "html"].sort, html_factory.extensions.sort)
   end
 end
