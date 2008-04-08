c5f16defe371d26a2f2dc9b5a34c5d09e5cf611c
Kouhei Sutou <kou@cozmixng.org>
2008-04-08 15:44:29 +0000

    * test/test_gdk_display.rb: add.

---
+++ gtk/test/test_gdk_display.rb
@@ -0,0 +1,18 @@
+class TestGdkDisplay < Test::Unit::TestCase
+  def test_supports_composite?
+    display = Gdk::Display.default
+    if Gtk.check_version?(2, 12, 0)
+      assert_boolean(display.supports_composite?)
+    else
+      assert_respond_to(display, :supports_composite?)
+    end
+  end
+
+  private
+  def assert_boolean(expected)
+    assert([true, false].include?(expected),
+           build_message(nil,
+                         "<true or false> expected but was\n<?>",
+                         expected))
+  end
+end
