41249a700823f842a9658ef5b787dcfc47b1dbde
Kouhei Sutou <kou@cozmixng.org>
2008-04-10 14:17:16 +0000

    * src/rbgobject.h: G_DEF_CLASS2 -> G_DEF_CLASS_WITH_GC_FUNC.

--- glib/src/rbgobject.h
+++ glib/src/rbgobject.h
@@ -30,8 +30,10 @@ extern "C" {
 
 #define G_DEF_CLASS(gtype, name, module)\
     (rbgobj_define_class(gtype, name, module, 0, 0, Qnil))
-#define G_DEF_CLASS2(gtype, name, module, mark, free)\
+#define G_DEF_CLASS_WITH_GC_FUNC(gtype, name, module, mark, free)	\
     (rbgobj_define_class(gtype, name, module, mark, free, Qnil))
+#define G_DEF_CLASS2(gtype, name, module, mark, free)			\
+    G_DEF_CLASS_WITH_GC_FUNC(gtype, name, module, mark, free)
 #define G_DEF_CLASS3(gtype_name, name, module)\
     (rbgobj_define_class_dynamic(gtype_name, name, module, 0, 0))
 #define G_DEF_CLASS4(gtype, name, module, parent) \
