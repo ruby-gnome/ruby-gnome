deab33bb4d9f20be14819ed99e7834f8abb38868
Kouhei Sutou <kou@cozmixng.org>
2008-04-15 12:47:09 +0000

    * src/rbgst-x-overlay.c: fix style.

--- gstreamer/src/rbgst-x-overlay.c
+++ gstreamer/src/rbgst-x-overlay.c
@@ -19,20 +19,20 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
  */
 
-#if defined(HAVE_GST_OVERLAY)
+#ifdef HAVE_GST_OVERLAY
 
 #include "rbgst.h"
 #include <gst/interfaces/xoverlay.h>
 
-#define RGST_X_OVERLAY(o)           (GST_X_OVERLAY(RVAL2GOBJ(o)))
+#define SELF(o)           (GST_X_OVERLAY(RVAL2GOBJ(o)))
 
-static GQuark xoverlay_xid_data_quark ;
+static GQuark xoverlay_xid_data_quark;
 
 struct xid_callback_data
 {
-  GObject *bus;
-  gulong cb_id;
-  gulong xid;
+    GObject *bus;
+    gulong cb_id;
+    gulong xid;
 };
 
 static void
@@ -41,7 +41,7 @@ bus_sync_func_cb(GstBus *bus, GstMessage *message, gpointer user_data)
     struct xid_callback_data *xid_cb_data;
     GstXOverlay *sink = GST_X_OVERLAY(user_data);
     xid_cb_data = (struct xid_callback_data *)
-       g_object_get_qdata(G_OBJECT(sink), xoverlay_xid_data_quark);
+        g_object_get_qdata(G_OBJECT(sink), xoverlay_xid_data_quark);
 
     if (xid_cb_data == NULL) {
         return;
@@ -82,51 +82,51 @@ rb_gst_x_overlay_set_xwindow_id_with_buswatch(VALUE self, VALUE bus,
 
     sink = RVAL2GOBJ(self);
     g_object_set_qdata_full(sink, xoverlay_xid_data_quark,
-                          xid_cb_data, xid_callback_data_destroy_cb);
+                            xid_cb_data, xid_callback_data_destroy_cb);
 
     gst_bus_enable_sync_message_emission(GST_BUS(xid_cb_data->bus));
     xid_cb_data->cb_id =
-      g_signal_connect_object(xid_cb_data->bus, "sync-message",
-                              G_CALLBACK(bus_sync_func_cb), sink, 0);
+        g_signal_connect_object(xid_cb_data->bus, "sync-message",
+                                G_CALLBACK(bus_sync_func_cb), sink, 0);
     return self;
 }
 
 static VALUE
 rb_gst_x_overlay_set_xwindow_id (VALUE self, VALUE x_window_id)
 {
-    gst_x_overlay_set_xwindow_id (RGST_X_OVERLAY (self), NUM2ULONG(x_window_id));
+    gst_x_overlay_set_xwindow_id(SELF(self), NUM2ULONG(x_window_id));
     return self;
 }
 
 static VALUE
 rb_gst_x_overlay_expose (VALUE self)
 {
-    gst_x_overlay_expose (RGST_X_OVERLAY (self));
+    gst_x_overlay_expose(SELF(self));
     return self;
 }
 
 static VALUE
 rb_gst_x_overlay_got_xwindow_id (VALUE self, VALUE x_window_id)
 {
-    gst_x_overlay_got_xwindow_id (RGST_X_OVERLAY (self), NUM2ULL (x_window_id));
+    gst_x_overlay_got_xwindow_id(SELF(self), NUM2ULL(x_window_id));
     return self;
 }
 
 void
 Init_gst_x_overlay (void)
 {
-    VALUE i = G_DEF_INTERFACE (GST_TYPE_X_OVERLAY, "XOverlay", mGst);
+    VALUE i = G_DEF_INTERFACE(GST_TYPE_X_OVERLAY, "XOverlay", mGst);
     
-    xoverlay_xid_data_quark = 
+    xoverlay_xid_data_quark =
       g_quark_from_static_string("__rbgst_xoverlay_xid_data_quark__");
 
-    rb_define_method (i, "set_xwindow_id", rb_gst_x_overlay_set_xwindow_id, 1);
-    rb_define_method (i, "expose", rb_gst_x_overlay_expose, 0);
+    rb_define_method(i, "set_xwindow_id", rb_gst_x_overlay_set_xwindow_id, 1);
+    rb_define_method(i, "expose", rb_gst_x_overlay_expose, 0);
    
-    rb_define_method (i, "got_xwindow_id", rb_gst_x_overlay_got_xwindow_id, 1);
-    rb_define_method (i, "set_xwindow_id_with_buswatch", rb_gst_x_overlay_set_xwindow_id_with_buswatch, 2);
+    rb_define_method(i, "got_xwindow_id", rb_gst_x_overlay_got_xwindow_id, 1);
+    rb_define_method(i, "set_xwindow_id_with_buswatch", rb_gst_x_overlay_set_xwindow_id_with_buswatch, 2);
     
-    G_DEF_SETTERS (i);
+    G_DEF_SETTERS(i);
 }
 
 #endif /* HAVE_GST_OVERLAY */
