eec54a28c5a3d62865b5e16ec26eaa2f8ff072cc
Kouhei Sutou <kou@cozmixng.org>
2008-05-15 00:03:45 +0000

    * run_tests.rb: set include path.

--- run_tests.rb
+++ run_tests.rb
@@ -5,6 +5,7 @@
 
 require "rbconfig"
 require "open3"
+require "pathname"
 
 ruby = File.join(Config::CONFIG['bindir'],
                  Config::CONFIG['RUBY_INSTALL_NAME'] +Config::CONFIG['EXEEXT'])
@@ -14,23 +15,38 @@ def separator
   "-" * 80
 end
 
+targets = []
+includes = []
+
+base_dir = Pathname(File.dirname(__FILE__))
+Pathname.glob((base_dir + "*").to_s) do |dir|
+  next unless dir.directory?
+  dir = dir.expand_path
+  src_dir = dir + "src"
+  if src_dir.directory?
+    includes.concat(["-I", (src_dir + "lib").to_s, "-I", src_dir.to_s])
+  else
+    includes.concat(["-I", (dir + "lib").to_s, "-I", dir.to_s])
+  end
+  next unless (dir + "test").directory?
+  targets << dir
+end
+
 puts "starting tests..."
 File.open("tests.log", "w") do |log|
-  Dir.glob("*").each do |dir|
-    next unless File.directory?(dir)
-    Dir.chdir(dir) do
-      if File.exists?("test")
-        log.puts "#{Time.now} running test for #{dir}"
-        log.puts separator
+  targets.each do |target|
+    Dir.chdir(target.to_s) do
+      log.puts "#{Time.now} running test for #{target}"
+      log.puts separator
 
-        stdin, stdout, stderr = Open3.popen3(ruby, "test/run-test.rb")
+      args = includes + ["test/run-test.rb"]
+      stdin, stdout, stderr = Open3.popen3(ruby, *args)
 
-        log.puts stdout.readlines
-        log.puts stderr.readlines
-        log.puts
-        log.puts
-        log.puts separator
-      end
+      log.puts stdout.readlines
+      log.puts stderr.readlines
+      log.puts
+      log.puts
+      log.puts separator
     end
   end
 end
