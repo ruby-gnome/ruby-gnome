0375505cf537b0195a0e9248b0e7ee8205a73413
Kouhei Sutou <kou@cozmixng.org>
2008-05-21 08:08:07 +0000

    * test/test_thread_handling.rb   (TestThreadHandling#test_state_changes): fix indentation.

--- gstreamer/test/test_thread_handling.rb
+++ gstreamer/test/test_thread_handling.rb
@@ -28,27 +28,27 @@ class TestThreadHandling < Test::Unit::TestCase
 
   def test_state_changes
     only_gst_version(0, 10, 16)
-     pipeline = Gst::Pipeline.new
-     buffers_seen = 0
+    pipeline = Gst::Pipeline.new
+    buffers_seen = 0
 
-     src = Gst::ElementFactory.make("videotestsrc")
-     identity = Gst::ElementFactory.make("identity")
-     sink = Gst::ElementFactory.make("fakesink")
+    src = Gst::ElementFactory.make("videotestsrc")
+    identity = Gst::ElementFactory.make("identity")
+    sink = Gst::ElementFactory.make("fakesink")
 
-     identity.signal_handoffs = true
+    identity.signal_handoffs = true
 
-     identity.signal_connect("handoff") { | element, buffer |
+    identity.signal_connect("handoff") do |element, buffer|
        buffers_seen += 1
-     }
+    end
 
-     pipeline.add(src, identity, sink)
-     src >> identity >> sink
-     loop = GLib::MainLoop.new(nil, false)
+    pipeline.add(src, identity, sink)
+    src >> identity >> sink
+    loop = GLib::MainLoop.new(nil, false)
 
-     10.times do
-       pipeline.play
-       sleep 1
-       pipeline.stop
-     end
+    10.times do
+      pipeline.play
+      sleep 1
+      pipeline.stop
+    end
   end
 end
