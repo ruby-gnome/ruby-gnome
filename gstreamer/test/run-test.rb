3469151681cac49cca34f7b4f7fdf4c17a3bcd99
Kouhei Sutou <kou@cozmixng.org>
2008-06-14 11:37:38 +0000

    * test/run-test.rb: use glib/test/glib-test-init.rb.

--- gstreamer/test/run-test.rb
+++ gstreamer/test/run-test.rb
@@ -2,8 +2,6 @@
 
 # ENV["GST_DEBUG"] = "GST_BUFFER:5"
 
-$VERBOSE = true
-
 base_dir = File.expand_path(File.join(File.dirname(__FILE__), ".."))
 
 if system("which make > /dev/null")
@@ -11,7 +9,8 @@ if system("which make > /dev/null")
 end
 
 glib_dir = File.expand_path(File.join(base_dir, "..", "glib"))
-test_unit_ext_dir = File.join(glib_dir, "test-unit-ext", "lib")
+$LOAD_PATH.unshift(glib_dir)
+require 'test/glib-test-init'
 
 $LOAD_PATH.unshift(File.join(glib_dir, "src"))
 $LOAD_PATH.unshift(File.join(glib_dir, "src", "lib"))
@@ -21,35 +20,6 @@ $LOAD_PATH.unshift(File.join(base_dir, "src", "lib"))
 require "gst"
 
 $LOAD_PATH.unshift(File.join(base_dir, "test"))
-$LOAD_PATH.unshift(test_unit_ext_dir)
-require 'test-unit-ext'
-
 require 'gst-test-utils'
-test_file = File.join(base_dir, "test", "test_*.rb")
-case ARGV.first
-when /\A--\z/
-  ARGV.shift
-when /\A-/, nil;
-else
-  test_file = ARGV.shift
-end
 
-Dir.glob(test_file) do |file|
-  require file
-end
-
-argv = ARGV.dup
-unless argv.find {|opt| /\A(?:-v|--verbose=)/ =~ opt}
-  argv << "--verbose=n"
-end
-unless argv.find {|opt| /\A--(?:no-)priority/ =~ opt}
-  argv << "--priority"
-end
-
-args = [File.dirname($0), argv.dup]
-if Test::Unit::AutoRunner.respond_to?(:standalone?)
-  args.unshift(false)
-else
-  args.unshift($0)
-end
-Test::Unit::AutoRunner.run(*args)
+exit Test::Unit::AutoRunner.run(true)
