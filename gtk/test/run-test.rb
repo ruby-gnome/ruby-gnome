65f458b2a9b369657f8c9bb401c1acbdd027e9b0
Kouhei Sutou <kou@cozmixng.org>
2008-06-14 11:36:00 +0000

    * test/run-test.rb: use glib/test/glib-test-init.rb.

--- gtk/test/run-test.rb
+++ gtk/test/run-test.rb
@@ -8,7 +8,9 @@ atk_base = File.join(ruby_gnome2_base, "atk")
 pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdkpixbuf")
 gtk_base = File.join(ruby_gnome2_base, "gtk")
-test_unit_ext_dir = File.join(glib_base, "test-unit-ext", "lib")
+
+$LOAD_PATH.unshift(glib_base)
+require 'test/glib-test-init'
 
 [glib_base, atk_base, pango_base, gdk_pixbuf_base, gtk_base].each do |target|
   if system("which make > /dev/null")
@@ -20,15 +22,9 @@ test_unit_ext_dir = File.join(glib_base, "test-unit-ext", "lib")
   $LOAD_PATH.unshift(File.join(target, "lib"))
 end
 
-$LOAD_PATH.unshift(test_unit_ext_dir)
-require 'test-unit-ext'
-
 $LOAD_PATH.unshift(File.join(gtk_base, "test"))
 require 'gtk-test-utils'
 
-require 'pp'
 require 'gtk2'
 
-Dir.glob(File.join(gtk_base, "test", "test_*.rb")) do |file|
-  require file.gsub(/\.rb$/, '')
-end
+exit Test::Unit::AutoRunner.run(true)
