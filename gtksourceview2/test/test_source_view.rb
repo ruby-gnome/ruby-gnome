25831a90142b187d1e3a48eaeba9a3d63ac7c12f
Kouhei Sutou <kou@cozmixng.org>
2008-09-30 12:58:30 +0000

    * test/test_source_view.rb: add a test.

--- gtksourceview2/test/test_source_view.rb
+++ gtksourceview2/test/test_source_view.rb
@@ -7,7 +7,11 @@ class TestSourceView < Test::Unit::TestCase
     red_rgb = [65535, 0, 0]
     red_color = Gdk::Color.parse("red")
     assert_not_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
+
     view.set_mark_category_background("XXX", red_color)
     assert_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
+
+    view.set_mark_category_background("XXX", nil)
+    assert_not_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
   end
 end
