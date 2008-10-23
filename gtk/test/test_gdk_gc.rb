2065b0880e51c28e0274736393a1ccfcf1d07520
Kouhei Sutou <kou@cozmixng.org>
2008-10-23 12:25:45 +0000

    * test/test_gtk_unix_print.rb, test/test_gdk_gc.rb: use   assert_const_defined and assert_const_not_defined.

--- gtk/test/test_gdk_gc.rb
+++ gtk/test/test_gdk_gc.rb
@@ -2,8 +2,6 @@ class TestGdkGC < Test::Unit::TestCase
   include GtkTestUtils
 
   def test_constant
-    assert_nothing_raised do
-      foo = Gdk::GC::LINE_ON_OFF_DASH
-    end
+    assert_const_defined(Gdk, :GC)
   end
 end
--- gtk/test/test_gtk_unix_print.rb
+++ gtk/test/test_gtk_unix_print.rb
@@ -3,20 +3,12 @@ class TestGtkUnixPrint < Test::Unit::TestCase
     if GLib.os_unix?
       assertion = :assert_const_defined
     else
-      assertion = :assert_not_const_defined
+      assertion = :assert_const_not_defined
     end
-    send(assertion, :PageSetupUnixDialog)
-    send(assertion, :Printer)
-    send(assertion, :PrintCapabilities)
-    send(assertion, :PrintJob)
-    send(assertion, :PrintUnixDialog)
-  end
-
-  def assert_const_defined(name)
-    assert_operator(Gtk, :const_defined?, name)
-  end
-
-  def assert_not_const_defined(name)
-    assert(Gtk.const_defined?(name), "!Gtk.const_defined?(#{name.inspect})")
+    send(assertion, Gtk, :PageSetupUnixDialog)
+    send(assertion, Gtk, :Printer)
+    send(assertion, Gtk, :PrintCapabilities)
+    send(assertion, Gtk, :PrintJob)
+    send(assertion, Gtk, :PrintUnixDialog)
   end
 end
