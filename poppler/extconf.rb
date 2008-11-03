24537e845c266c53ec1b28a392b136bdb04c0097
Kouhei Sutou <kou@cozmixng.org>
2008-11-03 01:58:08 +0000

    * extconf.rb: remove needless checks.

--- poppler/extconf.rb
+++ poppler/extconf.rb
@@ -16,10 +16,6 @@ require 'mkmf-gnome2'
 PKGConfig.have_package(PACKAGE_ID, 0, 8, 0) or exit 1
 setup_win32(PACKAGE_NAME)
 
-poppler_header = "poppler.h"
-have_func("poppler_page_render_selection_to_pixbuf", poppler_header)
-have_struct_member("PopplerImageMapping", "image_id", poppler_header)
-
 check_cairo
 
 add_depend_package("glib2", "glib/src", TOPDIR)
