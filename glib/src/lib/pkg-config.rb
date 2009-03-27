6f4237d7697ce52545cc122b08f57d9e5e3bd1da
Kouhei Sutou <kou@cozmixng.org>
2009-03-27 11:52:41 +0000

    * src/lib/pkg-config.rb: just LGPL 2.1. not 'or later'.

--- glib/src/lib/pkg-config.rb
+++ glib/src/lib/pkg-config.rb
@@ -9,8 +9,7 @@
 #
 # Ruby/GLib2 is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 2.1 of the License, or
-# (at your option) any later version.
+# the Free Software Foundation, either version 2.1 of the License.
 #
 # Foobar is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
