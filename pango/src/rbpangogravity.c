96b3de1f0e376f502517682804bf9e0a4676048e
Kouhei Sutou <kou@cozmixng.org>
2010-01-17 14:35:26 +0000

    2010-01-17  Hiroyuki Ito  <ZXB01226@nifty.com>
    	* src/rbpangogravity.c: fix indentation.

--- pango/src/rbpangogravity.c
+++ pango/src/rbpangogravity.c
@@ -15,7 +15,7 @@
 static VALUE
 to_rotation(VALUE self, VALUE gravity)
 {
-  return rb_float_new(pango_gravity_to_rotation(RVAL2GENUM(gravity, PANGO_TYPE_GRAVITY)));
+    return rb_float_new(pango_gravity_to_rotation(RVAL2GENUM(gravity, PANGO_TYPE_GRAVITY)));
 }
 #endif
 
@@ -24,14 +24,14 @@ void
 Init_pango_gravity(void)
 {
 #if PANGO_CHECK_VERSION(1,16,0)
-  VALUE mGravity = rb_define_module_under(mPango, "Gravity");
+    VALUE mGravity = rb_define_module_under(mPango, "Gravity");
 
-  rb_define_singleton_method(mGravity, "to_rotation", to_rotation, 1);
+    rb_define_singleton_method(mGravity, "to_rotation", to_rotation, 1);
 
-  G_DEF_CLASS(PANGO_TYPE_GRAVITY, "Gravity", mGravity);
-  G_DEF_CONSTANTS(mGravity, PANGO_TYPE_GRAVITY, "PANGO_GRAVITY_");
-  G_DEF_CLASS(PANGO_TYPE_GRAVITY_HINT, "Hint", mGravity);
-  G_DEF_CONSTANTS(mGravity, PANGO_TYPE_GRAVITY_HINT, "PANGO_GRAVITY_");
+    G_DEF_CLASS(PANGO_TYPE_GRAVITY, "Gravity", mGravity);
+    G_DEF_CONSTANTS(mGravity, PANGO_TYPE_GRAVITY, "PANGO_GRAVITY_");
+    G_DEF_CLASS(PANGO_TYPE_GRAVITY_HINT, "Hint", mGravity);
+    G_DEF_CONSTANTS(mGravity, PANGO_TYPE_GRAVITY_HINT, "PANGO_GRAVITY_");
 #endif
 }
 
