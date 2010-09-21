ac06c34356879b1580dea612df9b1206a9cbc234
Kouhei Sutou <kou@cozmixng.org>
2010-09-21 12:14:22 +0000

    * ./: make buidlable with rake-compiler.

--- gdkpixbuf/ext/gdk_pixbuf2/extconf.rb
+++ gdkpixbuf/ext/gdk_pixbuf2/extconf.rb
@@ -6,7 +6,7 @@ require 'pathname'
 
 base_dir = Pathname(__FILE__).dirname.parent.parent.expand_path
 top_dir = base_dir.parent
-mkmf_gnome2_dir = top_dir + "glib2" + 'lib'
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
 
 $LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
 
@@ -28,6 +28,9 @@ if PKGConfig.have_package('gdk-2.0')
   check_cairo
 end
 
-add_depend_package("glib2", "glib/ext/glib2", top_dir.to_s)
+build_dir = "glib/tmp/#{RUBY_PLATFORM}/glib2/#{RUBY_VERSION}"
+options = {}
+options[:target_build_dir] = build_dir if (top_dir + build_dir).exist?
+add_depend_package("glib2", "glib/ext/glib2", top_dir.to_s, options)
 create_pkg_config_file("Ruby/GdkPixbuf2", package_id)
 create_makefile(module_name)
