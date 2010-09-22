17ea0318d1b72e7ed57904d09d1a0094b435ea11
Kouhei Sutou <kou@cozmixng.org>
2010-09-22 16:18:27 +0000

    * ./: make buildable with rake-compiler.

---
+++ rsvg/ext/rsvg2/extconf.rb
@@ -0,0 +1,66 @@
+=begin
+extconf.rb for Ruby/RSVG extention library
+=end
+
+require 'pathname'
+
+base_dir = Pathname(__FILE__).dirname.parent.parent.expand_path
+top_dir = base_dir.parent
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "rsvg2"
+package_id = "librsvg-2.0"
+
+require 'mkmf-gnome2'
+
+setup_win32(module_name, base_dir)
+
+PKGConfig.have_package(package_id) or exit 1
+
+rsvg_header = "librsvg/rsvg.h"
+have_func("rsvg_set_default_dpi", rsvg_header)
+have_func("rsvg_set_default_dpi_x_y", rsvg_header)
+have_func("rsvg_handle_set_dpi", rsvg_header)
+have_func("rsvg_handle_set_dpi_x_y", rsvg_header)
+have_func("rsvg_handle_get_metadata", rsvg_header)
+have_func("rsvg_handle_free", rsvg_header)
+have_func("rsvg_handle_get_pixbuf_sub", rsvg_header)
+have_header("librsvg/rsvg-gz.h")
+have_type("RsvgDimensionData", "librsvg/rsvg.h")
+
+if have_header("librsvg/rsvg-cairo.h")
+  options = {}
+  rcairo_source_dir_names = ["rcairo"]
+  if /mingw|cygwin|mswin32/ =~ RUBY_PLATFORM
+    rcairo_source_dir_names.unshift("rcairo.win32")
+  end
+  rcairo_source_dir_names.each do |rcairo_source_dir_name|
+    rcairo_source_dir = top_dir.parent.expand_path + rcairo_source_dir_name
+    if rcairo_source_dir.exist?
+      options[:rcairo_source_dir] = rcairo_source_dir.to_s
+      break
+    end
+  end
+  check_cairo(options)
+end
+
+build_dir = "glib/tmp/#{RUBY_PLATFORM}/glib2/#{RUBY_VERSION}"
+options = {}
+options[:target_build_dir] = build_dir if (top_dir + build_dir).exist?
+add_depend_package("glib2", "glib/ext/glib2", top_dir.to_s, options)
+
+create_pkg_config_file("Ruby/RSVG", package_id, nil, "ruby-rsvg2.pc")
+
+$defs << " -DRUBY_RSVG2_COMPILATION"
+enum_type_prefix = "librsvg-enum-types"
+unless have_header("librsvg/#{enum_type_prefix}.h")
+  include_paths = PKGConfig.cflags_only_I("librsvg-2.0")
+  include_path = include_paths.split.find do |x|
+    /librsvg/.match(x)
+  end.sub(/^-I/, "")
+  headers = Dir.glob(File.join(include_path, "librsvg", "*.h"))
+  glib_mkenums(enum_type_prefix, headers, "RSVG_TYPE_", ["librsvg/rsvg.h"])
+end
+create_makefile(module_name)
--- rsvg/extconf.rb
+++ rsvg/extconf.rb
@@ -1,48 +1,48 @@
-=begin
-extconf.rb for Ruby/RSVG extention library
-=end
+#!/usr/bin/env ruby
 
-PACKAGE_NAME = "rsvg2"
-PACKAGE_ID   = "librsvg-2.0"
+require 'pathname'
+require 'mkmf'
+require 'rbconfig'
+require 'fileutils'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/rsvg/src'
+package = "rsvg2"
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+base_dir = Pathname(__FILE__).dirname.expand_path
+ext_dir = base_dir + "ext" + package
+mkmf_gnome2_dir = base_dir + 'lib'
 
-require 'mkmf-gnome2'
+ruby = File.join(RbConfig::CONFIG['bindir'],
+                 RbConfig::CONFIG['ruby_install_name'] +
+                 RbConfig::CONFIG["EXEEXT"])
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
-
-rsvg_header = "librsvg/rsvg.h"
-have_func("rsvg_set_default_dpi", rsvg_header)
-have_func("rsvg_set_default_dpi_x_y", rsvg_header)
-have_func("rsvg_handle_set_dpi", rsvg_header)
-have_func("rsvg_handle_set_dpi_x_y", rsvg_header)
-have_func("rsvg_handle_get_metadata", rsvg_header)
-have_func("rsvg_handle_free", rsvg_header)
-have_func("rsvg_handle_get_pixbuf_sub", rsvg_header)
-have_header("librsvg/rsvg-gz.h")
-have_type("RsvgDimensionData", "librsvg/rsvg.h")
-
-check_cairo and have_header("librsvg/rsvg-cairo.h")
-
-add_depend_package("glib2", "glib/src", TOPDIR)
-
-create_pkg_config_file("Ruby/RSVG", PACKAGE_ID, nil, "ruby-rsvg2.pc")
-
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_RSVG2_COMPILATION") do
-  enum_type_prefix = "librsvg-enum-types"
-  unless have_header("librsvg/#{enum_type_prefix}.h")
-    include_paths = PKGConfig.cflags_only_I("librsvg-2.0")
-    include_path = include_paths.split.find do |x|
-      /librsvg/.match(x)
-    end.sub(/^-I/, "")
-    headers = Dir.glob(File.join(include_path, "librsvg", "*.h"))
-    glib_mkenums(enum_type_prefix, headers, "RSVG_TYPE_", ["librsvg/rsvg.h"])
-  end
+Dir.chdir(ext_dir.to_s) do
+  system(ruby, "extconf.rb", *ARGV) || exit(false)
 end
 
-create_top_makefile
+create_makefile(package)
+FileUtils.mv("Makefile", "Makefile.lib")
+
+File.open("Makefile", "w") do |makefile|
+  makefile.puts(<<-EOM)
+all:
+	(cd ext/#{package} && $(MAKE))
+	$(MAKE) -f Makefile.lib
+
+install:
+	(cd ext/#{package} && $(MAKE) install)
+	$(MAKE) -f Makefile.lib install
+
+site-install:
+	(cd ext/#{package} && $(MAKE) site-install)
+	$(MAKE) -f Makefile.lib site-install
+
+clean:
+	(cd ext/#{package} && $(MAKE) clean)
+	$(MAKE) -f Makefile.lib clean
+
+distclean:
+	(cd ext/#{package} && $(MAKE) distclean)
+	$(MAKE) -f Makefile.lib distclean
+	@rm -f Makefile.lib
+EOM
+end
