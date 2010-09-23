75ba06e5b6b85f386c6338e838afa4ee22134b7d
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 04:56:38 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gtksourceview/extconf.rb
+++ gtksourceview/extconf.rb
@@ -2,26 +2,39 @@
 extconf.rb for Ruby/GtkSourceView extention library
 =end
 
-PACKAGE_NAME = "gtksourceview"
-PACKAGE_ID   = "gtksourceview-1.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gtksourceview/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gtksourceview"
+package_id = "gtksourceview-1.0"
+
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
+setup_win32(module_name, base_dir)
+
+PKGConfig.have_package(package_id) or exit(false)
+
 have_func("gtk_source_print_job_get_type")
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-make_version_header("GTKSOURCEVIEW", PACKAGE_ID)
+make_version_header("GTKSOURCEVIEW", package_id)
 
-create_pkg_config_file("Ruby/GtkSourceView", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GTKSOURCEVIEW_COMPILATION")
+create_pkg_config_file("Ruby/GtkSourceView", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GTKSOURCEVIEW_COMPILATION")
 create_top_makefile
 
