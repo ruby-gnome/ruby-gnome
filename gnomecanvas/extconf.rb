ab1aa55c40aeaa085a3970ee270d6e9ee52bdea9
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:44:19 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gnomecanvas/extconf.rb
+++ gnomecanvas/extconf.rb
@@ -2,33 +2,40 @@
 extconf.rb for Ruby/GnomeCanvas2 extention library
 =end
 
-PACKAGE_NAME = "gnomecanvas2"
-PACKAGE_ID   = "libgnomecanvas-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gnomecanvas/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gnomecanvas2"
+package_id = "libgnomecanvas-2.0"
 
 require 'mkmf-gnome2'
 
-#
-# detect GTK+ configurations
-#
+setup_win32(module_name, base_dir)
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
+PKGConfig.have_package(package_id) or exit 1
 
 have_func('gnome_canvas_set_center_scroll_region')
 have_func('gnome_canvas_path_def_get_type')
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
-add_depend_package("libart2", "libart/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
+add_depend_package("libart2", "libart/src", top_dir.to_s)
 
-make_version_header("GNOMECANVAS", PACKAGE_ID)
+make_version_header("GNOMECANVAS", package_id)
 
-create_pkg_config_file("Ruby/GnomeCanvas2", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOMECANVAS2_COMPILATION")
+create_pkg_config_file("Ruby/GnomeCanvas2", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GNOMECANVAS2_COMPILATION")
 create_top_makefile
