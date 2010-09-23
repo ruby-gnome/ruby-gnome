087644142ca68fe4ff297915bae4d6a437f21b6c
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 06:57:22 +0000

    * lib/atk.rb: support bundled Windows DLL.

--- atk/lib/atk.rb
+++ atk/lib/atk.rb
@@ -1,2 +1,12 @@
 require 'glib2'
-require 'atk.so'
+
+base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
+vendor_dir = base_dir + "vendor" + "local"
+vendor_bin_dir = vendor_dir + "bin"
+GLib.prepend_environment_path(vendor_bin_dir)
+begin
+  major, minor, micro, = RUBY_VERSION.split(/\./)
+  require "#{major}.#{minor}/atk.so"
+rescue LoadError
+  require 'atk.so'
+end
