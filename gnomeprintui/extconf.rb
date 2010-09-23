792792c3d287000e44fc0f349e2ff01c6f339d3a
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:51:51 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gnomeprintui/extconf.rb
+++ gnomeprintui/extconf.rb
@@ -2,37 +2,43 @@
 extconf.rb for Ruby/GnomePrintUI extention library
 =end
 
-require "fileutils"
+require 'pathname'
 
-PACKAGE_NAME = "gnomeprintui2"
-PACKAGE_ID   = "libgnomeprintui-2.2"
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gnomeprintui/src'
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+module_name = "gnomeprintui2"
+package_id = "libgnomeprintui-2.2"
 
 require 'mkmf-gnome2'
 
-app_name = "LIBGNOMEPRINTUI"
+setup_win32(module_name, base_dir)
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
+PKGConfig.have_package(package_id) or exit 1
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
-add_depend_package("libart2", "libart/src", TOPDIR)
-add_depend_package("gnomeprint2", "gnomeprint/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
+add_depend_package("libart2", "libart/src", top_dir.to_s)
+add_depend_package("gnomeprint2", "gnomeprint/src", top_dir.to_s)
 
 have_header("libgnomeprintui/gnome-print-config-dialog.h")
 have_header("libgnomeprintui/gnome-font-dialog.h")
 
-make_version_header(app_name, PACKAGE_ID)
+make_version_header("LIBGNOMEPRINTUI", package_id)
 
-create_pkg_config_file("Ruby/GnomePrintUI", PACKAGE_ID,
+create_pkg_config_file("Ruby/GnomePrintUI", package_id,
                        nil, "ruby-gnomeprintui2.pc")
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR,
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
                           "-DRUBY_GNOMEPRINTUI2_COMPILATION") do
   enum_type_dir = "libgnomeprintui"
   enum_type_prefix = "libgnomeprintui-enum-types"
