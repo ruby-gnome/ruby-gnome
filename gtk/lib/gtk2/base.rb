2415ac921573887f7981cb18169121747795f8b7
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 07:01:46 +0000

    * lib/gtk2/base.rb: support bundled Windows DLL.

--- gtk/lib/gtk2/base.rb
+++ gtk/lib/gtk2/base.rb
@@ -12,7 +12,18 @@
 require 'glib2'
 require 'atk'
 require 'pango'
-require 'gtk2.so'
+
+base_dir = Pathname.new(__FILE__).dirname.dirname.dirname.expand_path
+vendor_dir = base_dir + "vendor" + "local"
+vendor_bin_dir = vendor_dir + "bin"
+GLib.prepend_environment_path(vendor_bin_dir)
+begin
+  major, minor, micro, = RUBY_VERSION.split(/\./)
+  require "#{major}.#{minor}/gtk2.so"
+rescue LoadError
+  require "gtk2.so"
+end
+
 require 'gdk_pixbuf2'
 
 module Gdk
