608b65708421ee363121b6a2ebef833ccdc9fdfd
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:29:57 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gconf/extconf.rb
+++ gconf/extconf.rb
@@ -2,26 +2,36 @@
 extconf.rb for Ruby/GConf2 extention library
 =end
 
-PACKAGE_NAME = "gconf2"
-PACKAGE_ID   = "gconf-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..') 
-SRCDIR = TOPDIR + '/gconf/src'
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gconf2"
+package_id = "gconf-2.0"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
+setup_win32(module_name, base_dir)
+
+PKGConfig.have_package(package_id) or exit 1
 PKGConfig.have_package("gobject-2.0") or exit 1
 
-setup_win32(PACKAGE_NAME)
+[["glib", "glib2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-add_depend_package("glib2", "glib/src", TOPDIR)
+make_version_header("GCONF", package_id)
 
-make_version_header("GCONF", PACKAGE_ID)
-
-create_pkg_config_file("Ruby/GConf2", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GCONF2_COMPILATION")
+create_pkg_config_file("Ruby/GConf2", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GCONF2_COMPILATION")
 create_top_makefile
