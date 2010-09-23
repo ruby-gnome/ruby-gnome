32c05572351e21f9a5baf81cd49c458251f39397
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:46:42 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- libart/extconf.rb
+++ libart/extconf.rb
@@ -2,18 +2,23 @@
 extconf.rb for Ruby/Libart2 extension library
 =end
 
-PACKAGE_NAME = "libart2"
-PACKAGE_ID   = "libart-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/libart/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "libart2"
+package_id = "libart-2.0"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
+setup_win32(module_name, base_dir)
+
+PKGConfig.have_package(package_id) or exit 1
 
 $libs += " -lz "
 PKGConfig.have_package('libpng12') or have_header("png.h") or exit 1
@@ -23,9 +28,8 @@ have_library("png", "png_create_write_struct")
 have_library("jpeg", "jpeg_set_defaults")
 have_library("art_lgpl_2", "art_affine_identity") or exit 1
 
-setup_win32(PACKAGE_NAME)
-
-create_pkg_config_file("Ruby/Libart2", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_LIBART2_COMPILATION")
+create_pkg_config_file("Ruby/Libart2", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_LIBART2_COMPILATION")
 create_top_makefile
 
