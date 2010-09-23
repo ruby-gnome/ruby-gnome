89ce03279eacf55257d63e6490ea849128922208
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:53:38 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gnomevfs/extconf.rb
+++ gnomevfs/extconf.rb
@@ -2,29 +2,39 @@
 extconf.rb for Ruby/GnomeVFS extention library
 =end
 
-PACKAGE_NAME = "gnomevfs"
-PACKAGE_ID   = "gnome-vfs-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gnomevfs/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gnomevfs"
+package_id = "gnome-vfs-2.0"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
+setup_win32(module_name, base_dir)
+
+PKGConfig.have_package(package_id) or exit 1
 PKGConfig.have_package('gnome-vfs-module-2.0') or exit 1
 PKGConfig.have_package("gobject-2.0") or exit 1
 
 have_func("rb_exec_recursive")
 
-setup_win32(PACKAGE_NAME)
+[["glib", "glib2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-add_depend_package("glib2", "glib/src", TOPDIR)
+make_version_header("GNOMEVFS", package_id)
 
-make_version_header("GNOMEVFS", PACKAGE_ID)
-
-create_pkg_config_file("Ruby/GnomeVFS", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOMEVFS_COMPILATION")
+create_pkg_config_file("Ruby/GnomeVFS", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GNOMEVFS_COMPILATION")
 create_top_makefile
