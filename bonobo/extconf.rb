5bb761b343e9d597507854c6686f62dcc25565a5
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:22:06 +0000

    * extconf.rb: follow Ruby/GLib2 directory structure change.

--- bonobo/extconf.rb
+++ bonobo/extconf.rb
@@ -2,33 +2,39 @@
 extconf.rb for Ruby/Bonobo2 extension library
 =end
 
-PACKAGE_NAME = "bonobo2"
-PACKAGE_ID   = "libbonobo-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/bonobo/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "bonobo2"
+package_id = "libbonobo-2.0"
 
 require 'mkmf-gnome2'
 
-#
-# detect configurations
-#
+setup_win32(module_name, base_dir)
 
 (PKGConfig.have_package('libbonobo-2.0') and
  PKGConfig.have_package('libbonoboui-2.0')) or exit 1
-check_win32
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
 begin
   srcdir = File.dirname($0) == "." ? "." :
     File.expand_path(File.dirname($0) + "/src")
 
-  create_pkg_config_file("Ruby/Bonobo2", PACKAGE_ID)
+  create_pkg_config_file("Ruby/Bonobo2", package_id)
 
   Dir.mkdir "src" unless FileTest.exist? "src"
   Dir.chdir "src"
@@ -48,12 +54,12 @@ begin
     end
     add_obj("rbbonoboinits#{obj_ext}")
 
-	make_version_header("BONOBO", PACKAGE_ID, '.')
+    make_version_header("BONOBO", package_id, '.')
 
     #
     # create Makefiles
     #
-    create_makefile(PACKAGE_NAME, srcdir)
+    create_makefile(module_name, srcdir)
     raise Interrupt if not FileTest.exist? "Makefile"
 
     mfile = File.open("Makefile", "a")
