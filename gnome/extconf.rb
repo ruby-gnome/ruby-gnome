12f8399397a3f90672cbb439e27d9ef4859b9742
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:39:38 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gnome/extconf.rb
+++ gnome/extconf.rb
@@ -2,25 +2,36 @@
 extconf.rb for Ruby/GNOME2 extention library
 =end
 
-PACKAGE_NAME = "gnome2"
-PACKAGE_ID   = "libgnomeui-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gnome/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gnome2"
+package_id = "libgnomeui-2.0"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
+setup_win32(module_name, base_dir)
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+PKGConfig.have_package(package_id) or exit 1
 
-make_version_header("GNOME", PACKAGE_ID)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-create_pkg_config_file("Ruby/GNOME2", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOME2_COMPILATION")
+make_version_header("GNOME", package_id)
+
+create_pkg_config_file("Ruby/GNOME2", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GNOME2_COMPILATION")
 create_top_makefile
