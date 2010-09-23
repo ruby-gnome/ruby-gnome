acda7be1d2663a27091de5430765b2c92442078b
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 03:42:48 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gtkmozembed/extconf.rb
+++ gtkmozembed/extconf.rb
@@ -2,24 +2,29 @@
 extconf.rb for Ruby/GtkMozEmbed extention library
 =end
 
-PACKAGE_NAME = "gtkmozembed"
-PACKAGE_IDS = ["libxul-embedding-unstable",
+require 'pathname'
+
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
+
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gtkmozembed"
+package_ids = ["libxul-embedding-unstable",
                "libxul-embedding",
                "xulrunner-gtkmozembed",
                "firefox-gtkmozembed",
                "seamonkey-gtkmozembed",
                "mozilla-gtkmozembed"]
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gtkmozembed/src'
-
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
-
 require 'mkmf-gnome2'
 
+setup_win32(module_name, base_dir)
+
 package_id = nil
-PACKAGE_IDS.each do |v|
+package_ids.each do |v|
   if PKGConfig.exist?(v)
     package_id = v
     $stderr.puts "#{v} is found."
@@ -41,7 +46,6 @@ end
 
 PKGConfig.have_package('gtk+-2.0')
 PKGConfig.have_package(package_id)
-setup_win32(PACKAGE_NAME)
 
 mozilla_library_path = PKGConfig.libs_only_L(package_id)
 mozilla_path = mozilla_library_path.strip.sub(/^-L/, "")
@@ -55,8 +59,14 @@ else
   exit 1
 end
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
 if have_library("xpcomglue")
   have_header("gtkmozembed_glue.cpp") or exit 1
@@ -72,7 +82,7 @@ make_version_header("GTKMOZEMBED", package_id)
 
 create_pkg_config_file('Ruby/GtkMozEmbed', package_id)
 
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR,
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
                           "-DRUBY_GTKMOZEMBED_COMPILATION") do
   enum_type_prefix = "gtkmozembed-enum-types"
   include_paths = PKGConfig.cflags_only_I(package_id)
