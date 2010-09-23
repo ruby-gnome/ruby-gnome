4cb851eebfcb215132e7a8b32a294a135c26e1e6
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:59:10 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gstreamer/extconf.rb
+++ gstreamer/extconf.rb
@@ -2,21 +2,24 @@
 extconf.rb for Ruby/GStreamer extention library
 =end
 
-PACKAGE_NAME = "gst"
-PACKAGE_ID   = "gstreamer-0.10"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gstreamer/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gst"
+package_id = "gstreamer-0.10"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-PKGConfig.have_package('gstreamer-plugins-base-0.10') or exit 1
+setup_win32(module_name, base_dir)
 
-setup_win32(PACKAGE_NAME)
+PKGConfig.have_package(package_id) or exit 1
+PKGConfig.have_package('gstreamer-plugins-base-0.10') or exit 1
 
 have_header("unistd.h")
 have_header("io.h")
@@ -26,8 +29,15 @@ if have_library("gstinterfaces-0.10") and
   $CFLAGS += " -DHAVE_GST_OVERLAY"
 end
 
-add_depend_package("glib2", "glib/src", TOPDIR)
+[["glib", "glib2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-create_pkg_config_file("Ruby/GStreamer", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GST_COMPILATION")
+create_pkg_config_file("Ruby/GStreamer", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GST_COMPILATION")
 create_top_makefile
