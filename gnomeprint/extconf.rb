76af2ca7ba523db788b1b33a3d8174781436d5ee
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:49:50 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gnomeprint/extconf.rb
+++ gnomeprint/extconf.rb
@@ -1,27 +1,38 @@
 =begin
 extconf.rb for Ruby/GnomePrint extention library
 =end
-PACKAGE_NAME = "gnomeprint2"
-PACKAGE_ID   = "libgnomeprint-2.2"
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gnomeprint/src'
+require 'pathname'
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
+
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gnomeprint2"
+package_id = "libgnomeprint-2.2"
 
 require 'mkmf-gnome2'
-app_name = "LIBGNOMEPRINT"
 
-PKGConfig.have_package(PACKAGE_ID, 2, 8) or exit 1
-setup_win32(PACKAGE_NAME)
+setup_win32(module_name, base_dir)
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("libart2", "libart/src", TOPDIR)
+PKGConfig.have_package(package_id) or exit 1
 
-make_version_header(app_name, PACKAGE_ID)
+[["glib", "glib2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
+add_depend_package("libart2", "libart/src", top_dir.to_s)
 
-create_pkg_config_file("Ruby/GnomePrint", PACKAGE_ID,
+make_version_header("LIBGNOMEPRINT", package_id)
+
+create_pkg_config_file("Ruby/GnomePrint", package_id,
                        nil, "ruby-gnomeprint2.pc")
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GNOMEPRINT2_COMPILATION")
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GNOMEPRINT2_COMPILATION")
 create_top_makefile
