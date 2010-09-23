cdda9dd14ed3d478a517a353b0228f2abea6e398
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 05:23:45 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- vte/extconf.rb
+++ vte/extconf.rb
@@ -2,33 +2,42 @@
 extconf.rb for Ruby/VTE extention library
 =end
 
-PACKAGE_NAME = "vte"
-PACKAGE_ID = "vte"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/vte/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "vte"
+package_id = "vte"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID, 0, 12, 1) or exit 1
-setup_win32(PACKAGE_NAME)
+setup_win32(module_name, base_dir)
 
-check_cairo
+PKGConfig.have_package(package_id, 0, 12, 1) or exit(false)
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
-add_depend_package("atk", "atk/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"],
+ ["atk", "atk"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
 vte_headers = ["vte/vte.h"]
 have_type("VteTerminalCursorBlinkMode", vte_headers)
 
 unless have_macro("VTE_CHECK_VERSION", vte_headers)
-  make_version_header("VTE", PACKAGE_ID)
+  make_version_header("VTE", package_id)
 end
 
-create_pkg_config_file("Ruby/VTE", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_VTE_COMPILATION")
+create_pkg_config_file("Ruby/VTE", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_VTE_COMPILATION")
 create_top_makefile
