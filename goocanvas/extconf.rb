ccf5483c962103461b82552ee26df0188c81a5c6
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 02:56:06 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- goocanvas/extconf.rb
+++ goocanvas/extconf.rb
@@ -2,28 +2,51 @@
 extconf.rb for Ruby/GooCanvas extension library
 =end
 
-PACKAGE_NAME = "goocanvas"
-PACKAGE_ID   = "goocanvas"
+require 'pathname'
 
-base_dir = File.expand_path(File.dirname(__FILE__))
-TOPDIR = File.expand_path(base_dir + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = base_dir + '/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift(MKMF_GNOME2_DIR)
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "goocanvas"
+package_id = "goocanvas"
 
 require 'mkmf-gnome2'
 
-PKGConfig.have_package(PACKAGE_ID, 0, 8) or exit 1
-setup_win32(PACKAGE_NAME)
+setup_win32(module_name, base_dir)
 
-check_cairo or exit 1
+PKGConfig.have_package(package_id) or exit 1
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+options = {}
+rcairo_source_dir_names = ["rcairo"]
+if /mingw|cygwin|mswin32/ =~ RUBY_PLATFORM
+  rcairo_source_dir_names.unshift("rcairo.win32")
+end
+rcairo_source_dir_names.each do |rcairo_source_dir_name|
+  rcairo_source_dir = top_dir.parent.expand_path + rcairo_source_dir_name
+  if rcairo_source_dir.exist?
+    options[:rcairo_source_dir] = rcairo_source_dir.to_s
+    break
+  end
+end
+check_cairo(options) or exit(false)
 
-make_version_header("GOO_CANVAS", PACKAGE_NAME)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-create_pkg_config_file("Ruby/GooCanvas", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GOO_CANVAS_COMPILATION")
+
+make_version_header("GOO_CANVAS", package_id)
+
+create_pkg_config_file("Ruby/GooCanvas", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GOO_CANVAS_COMPILATION")
 create_top_makefile
