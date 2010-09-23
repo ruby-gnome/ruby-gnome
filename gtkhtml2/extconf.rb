1e8de10097d53f4352d7e16905ad538602982b0b
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 03:13:00 +0000

    * extconf.rb: support Ruby/GLib2 directory structure change.

--- gtkhtml2/extconf.rb
+++ gtkhtml2/extconf.rb
@@ -2,23 +2,23 @@
 extconf.rb for Ruby/GtkHtml2 extention library
 =end
 
-PACKAGE_NAME = "gtkhtml2"
-PACKAGE_ID   = "libgtkhtml-2.0"
+require 'pathname'
 
-TOPDIR = File.expand_path(File.dirname(__FILE__) + '/..')
-MKMF_GNOME2_DIR = TOPDIR + '/glib/src/lib'
-SRCDIR = TOPDIR + '/gtkhtml2/src'
+base_dir = Pathname(__FILE__).dirname.expand_path
+top_dir = base_dir.parent.expand_path
+mkmf_gnome2_dir = top_dir + "glib" + 'lib'
+top_build_dir = Pathname(".").parent.expand_path
 
-$LOAD_PATH.unshift MKMF_GNOME2_DIR
+$LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
+
+module_name = "gtkhtml2"
+package_id = "libgtkhtml-2.0"
 
 require 'mkmf-gnome2'
 
-#
-# detect GTK+ configurations
-#
+setup_win32(module_name, base_dir)
 
-PKGConfig.have_package(PACKAGE_ID) or exit 1
-setup_win32(PACKAGE_NAME)
+PKGConfig.have_package(package_id) or exit 1
 
 have_func('html_view_scroll_to_node')
 have_func('html_box_block_get_containing_width')
@@ -27,11 +27,18 @@ have_func('html_box_table_get_top_offset')
 have_func('html_box_table_get_bottom_offset')
 #have_func('gnome_canvas_set_center_scroll_region')
 
-add_depend_package("glib2", "glib/src", TOPDIR)
-add_depend_package("gtk2", "gtk/src", TOPDIR)
+[["glib", "glib2"],
+ ["gtk", "gtk2"]].each do |directory, library_name|
+  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{library_name}/#{RUBY_VERSION}"
+  add_depend_package(library_name, "#{directory}/ext/#{library_name}",
+                     top_dir.to_s,
+                     :top_build_dir => top_build_dir.to_s,
+                     :target_build_dir => build_dir)
+end
 
-make_version_header("GTKHTML", PACKAGE_ID)
+make_version_header("GTKHTML", package_id)
 
-create_pkg_config_file("Ruby/GtkHtml2", PACKAGE_ID)
-create_makefile_at_srcdir(PACKAGE_NAME, SRCDIR, "-DRUBY_GTKHTML2_COMPILATION")
+create_pkg_config_file("Ruby/GtkHtml2", package_id)
+create_makefile_at_srcdir(module_name, (base_dir + "src").to_s,
+                          "-DRUBY_GTKHTML2_COMPILATION")
 create_top_makefile
