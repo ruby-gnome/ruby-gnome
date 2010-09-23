55d2f2b07318bb2c546880eecb5125da3ab5a6a2
Kouhei Sutou <kou@cozmixng.org>
2010-09-23 13:34:11 +0000

    * lib/gio2.rb: support bundled Windows DLL.

--- gio/lib/gio2.rb
+++ gio/lib/gio2.rb
@@ -1,5 +1,15 @@
 require 'glib2'
-require 'gio2.so'
+
+base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
+vendor_dir = base_dir + "vendor" + "local"
+vendor_bin_dir = vendor_dir + "bin"
+GLib.prepend_environment_path(vendor_bin_dir)
+begin
+  major, minor, micro, = RUBY_VERSION.split(/\./)
+  require "#{major}.#{minor}/gio2.so"
+rescue LoadError
+  require "gio2.so"
+end
 
 class GLib::DataInputStream
   def each
