930418dbed4723aad8b8081df45f2607a6cd86ae
Kouhei Sutou <kou@cozmixng.org>
2010-10-17 12:57:03 +0000

    * ext/gtk2/rbgdkcairo.c: move GDK and cairo related code from   Ruby/GdkPixbuf2.   Patch by Andrea Dallera. Thanks!!!

--- gtk2/ext/gtk2/rbgdkcairo.c
+++ gtk2/ext/gtk2/rbgdkcairo.c
@@ -13,10 +13,12 @@
 
 #if GTK_CHECK_VERSION(2,8,0) && defined(HAVE_RB_CAIRO_H)
 #  define CAIRO_AVAILABLE 1
+#else
+#  define CAIRO_AVAILABLE 0
 #endif
 
-#ifdef CAIRO_AVAILABLE
-
+#if CAIRO_AVAILABLE
+#include <gdk/gdkcairo.h>
 #include <rb_cairo.h>
 
 #define _SELF(self) RVAL2CRCONTEXT(self)
@@ -30,13 +32,20 @@ gdkdraw_cairo_set_source_color(self, color)
     return self;
 }
 
-/* Move to gdkpixbuf/rbgdk-pixbuf.c
-void gdk_cairo_set_source_pixbuf (cairo_t      *cr,
-				  GdkPixbuf    *pixbuf,
-				  double        pixbuf_x,
-				  double        pixbuf_y);
-*/
+static VALUE
+gdkdraw_cairo_set_source_pixbuf(int argc, VALUE *argv, VALUE self)
+{
+    VALUE pixbuf, pixbuf_x, pixbuf_y;
 
+    rb_scan_args(argc, argv, "12", &pixbuf, &pixbuf_x, &pixbuf_y);
+
+    gdk_cairo_set_source_pixbuf(_SELF(self),
+                                GDK_PIXBUF(RVAL2GOBJ(pixbuf)),
+                                NIL_P(pixbuf_x) ? 0 : NUM2DBL(pixbuf_x),
+                                NIL_P(pixbuf_y) ? 0 : NUM2DBL(pixbuf_y));
+    rb_cairo_check_status(cairo_status(_SELF(self)));
+    return self;
+}
 
 #if GTK_CHECK_VERSION(2,10,0)
 static VALUE
@@ -86,6 +95,8 @@ Init_gtk_gdk_cairo()
 #if CAIRO_AVAILABLE
     rb_define_method(rb_cCairo_Context, "set_source_gdk_color",
                      gdkdraw_cairo_set_source_color, 1);
+    rb_define_method(rb_cCairo_Context, "set_source_pixbuf",
+                     gdkdraw_cairo_set_source_pixbuf, -1);
 #if GTK_CHECK_VERSION(2,10,0)
     rb_define_method(rb_cCairo_Context, "set_source_pixmap", gdkdraw_cairo_set_source_pixmap, 3);
 #endif
