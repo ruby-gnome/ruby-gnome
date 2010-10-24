4430068f62f733e726b97194f0bdc14e6e239a52
Kouhei Sutou <kou@cozmixng.org>
2010-10-24 04:42:07 +0000

    * ext/glib2/rbglib.c (rbg_rval2cstr): add missing '*'.   Reported by tmtms. Thanks!!!

--- glib2/ext/glib2/rbglib.c
+++ glib2/ext/glib2/rbglib.c
@@ -49,7 +49,7 @@ rbg_rval2cstr(VALUE *str)
     StringValue(*str);
 #ifdef HAVE_RUBY_ENCODING_H
     if (rb_enc_get(*str) != rb_utf8_encoding()) {
-        str = rb_str_export_to_enc(*str, rb_utf8_encoding());
+        *str = rb_str_export_to_enc(*str, rb_utf8_encoding());
     }
 #endif
     return RSTRING_PTR(*str);
