a259e6323af0ec7fbea8a5204d586df4dd5f9b7c
Kouhei Sutou <kou@cozmixng.org>
2011-01-29 13:25:37 +0000

    * ext/gio2/gunixsocketaddress.c (Init_gunixsocketaddress): disable   G_TYPE_UNIX_SOCKET_ADDRESS_TYPE on GIO 2.24 because it was appeared   in GIO 2.26.

--- gio2/ext/gio2/gunixsocketaddress.c
+++ gio2/ext/gio2/gunixsocketaddress.c
@@ -54,8 +54,10 @@ Init_gunixsocketaddress(G_GNUC_UNUSED VALUE glib)
 #ifdef HAVE_GIO_UNIX
         VALUE unixsocketaddress = G_DEF_CLASS(G_TYPE_UNIX_SOCKET_ADDRESS, "UnixSocketAddress", glib);
 
+#  if GLIB_CHECK_VERSION(2, 26, 0)
         G_DEF_CLASS(G_TYPE_UNIX_SOCKET_ADDRESS_TYPE, "Type", unixsocketaddress);
         G_DEF_CONSTANTS(unixsocketaddress, G_TYPE_UNIX_SOCKET_ADDRESS_TYPE, "G_UNIX_SOCKET_ADDRESS_");
+#  endif
 
         rb_define_singleton_method(unixsocketaddress, "new_abstract", unixsocketaddress_new_abstract, 1);
         rb_define_singleton_method(unixsocketaddress, "abstract_names_supported?", unixsocketaddress_abstract_names_supported, 0);
