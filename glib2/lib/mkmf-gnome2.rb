6ae5988fc613aa769b4d5ae6b754347608e6d6c5
Kouhei Sutou <kou@cozmixng.org>
2011-01-29 09:11:04 +0000

    * lib/mkmf-gnome2.rb:   - add load error check.   - remove needless rb_cairo.h check.

--- glib2/lib/mkmf-gnome2.rb
+++ glib2/lib/mkmf-gnome2.rb
@@ -343,9 +343,15 @@ def check_cairo(options={})
   $CFLAGS += " -I#{rcairo_source_dir}/ext/cairo" if rcairo_source_dir
   have_rb_cairo_h = have_header('rb_cairo.h')
   unless have_rb_cairo_h
-    cairo_gem_spec = Gem.source_index.find_name("cairo").last
-    $CFLAGS += " -I#{cairo_gem_spec.full_gem_path}/ext/cairo " if cairo_gem_spec
-    have_rb_cairo_h = have_header('rb_cairo.h')
+    begin
+      require 'rubygems'
+      cairo_gem_spec = Gem.source_index.find_name("cairo").last
+      if cairo_gem_spec
+        $CFLAGS += " -I#{cairo_gem_spec.full_gem_path}/ext/cairo"
+        have_rb_cairo_h = have_header('rb_cairo.h')
+      end
+    rescue LoadError
+    end
   end
 
   if have_rb_cairo_h
