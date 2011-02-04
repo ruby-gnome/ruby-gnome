b47154307a801a4de14573fe920dbb5f603979d3
Masaaki Aoyagi <masaakiaoyagi@users.sourceforge.net>
2011-02-04 22:01:12 +0000

    * change to ANSI C style.

--- atk/ext/atk/rbatkcomponent.c
+++ atk/ext/atk/rbatkcomponent.c
@@ -15,16 +15,13 @@
 
 /*
 static void
-focus_handler(aobj, bool)
-    AtkObject* aobj;
-    gboolean bool;
+focus_handler(AtkObject *aobj, gboolean bool)
 {
     VALUE ret = rb_funcall((VALUE)func, id_call, 2,
                            GOBJ2RVAL(aobj), CBOOL2RVAL(bool));
 }
 static VALUE
-comp_add_focus_handler(self)
-     VALUE self;
+comp_add_focus_handler(VALUE self)
 {
     VALUE func = rb_block_proc();
     G_RELATIVE(self, func);
@@ -33,8 +30,7 @@ comp_add_focus_handler(self)
 */
 
 static VALUE
-comp_contains(self, x, y, coord_type)
-    VALUE self, x, y, coord_type;
+comp_contains(VALUE self, VALUE x, VALUE y, VALUE coord_type)
 {
     return CBOOL2RVAL(atk_component_contains(_SELF(self),
                                              NUM2INT(x), NUM2INT(y),
@@ -42,8 +38,7 @@ comp_contains(self, x, y, coord_type)
 }
 
 static VALUE
-comp_get_extents(self, coord_type)
-    VALUE self, coord_type;
+comp_get_extents(VALUE self, VALUE coord_type)
 {
     gint x, y, width, height; 
     atk_component_get_extents(_SELF(self), &x, &y, &width, &height,
@@ -53,8 +48,7 @@ comp_get_extents(self, coord_type)
 
 #ifdef HAVE_ATK_COMPONENT_GET_LAYER
 static VALUE
-comp_get_layer(self)
-    VALUE self;
+comp_get_layer(VALUE self)
 {
     return GENUM2RVAL(atk_component_get_layer(_SELF(self)), ATK_TYPE_LAYER);
 }
@@ -62,16 +56,14 @@ comp_get_layer(self)
 
 #ifdef HAVE_ATK_COMPONENT_GET_MDI_ZORDER
 static VALUE
-comp_get_mdi_zorder(self)
-    VALUE self;
+comp_get_mdi_zorder(VALUE self)
 {
     return INT2NUM(atk_component_get_mdi_zorder(_SELF(self)));
 }
 
 #endif
 static VALUE
-comp_get_position(self, coord_type)
-    VALUE self, coord_type;
+comp_get_position(VALUE self, VALUE coord_type)
 {
     gint x, y;
     atk_component_get_position(_SELF(self), &x, &y,
@@ -80,8 +72,7 @@ comp_get_position(self, coord_type)
 }
 
 static VALUE
-comp_get_size(self)
-    VALUE self;
+comp_get_size(VALUE self)
 {
     gint width, height;
     atk_component_get_size(_SELF(self), &width, &height);
@@ -89,15 +80,13 @@ comp_get_size(self)
 }
 
 static VALUE
-comp_grab_focus(self)
-    VALUE self;
+comp_grab_focus(VALUE self)
 {
     return CBOOL2RVAL(atk_component_grab_focus(_SELF(self)));
 }
 
 static VALUE
-comp_ref_accessible_at_point(self, x, y, coord_type)
-    VALUE self, x, y, coord_type;
+comp_ref_accessible_at_point(VALUE self, VALUE x, VALUE y, VALUE coord_type)
 {
     return GOBJ2RVAL(atk_component_ref_accessible_at_point(
                          _SELF(self),
@@ -106,16 +95,14 @@ comp_ref_accessible_at_point(self, x, y, coord_type)
 }
 
 static VALUE
-comp_remove_focus_handler(self, handler_id)
-    VALUE self, handler_id;
+comp_remove_focus_handler(VALUE self, VALUE handler_id)
 {
     atk_component_remove_focus_handler(_SELF(self), NUM2UINT(handler_id));
     return self;
 }
 
 static VALUE
-comp_set_extents(self, x, y, width, height, coord_type)
-    VALUE self, x, y, width, height, coord_type;
+comp_set_extents(VALUE self, VALUE x, VALUE y, VALUE width, VALUE height, VALUE coord_type)
 {
     gboolean ret = atk_component_set_extents(_SELF(self),
                                              NUM2INT(x), NUM2INT(y),
@@ -126,8 +113,7 @@ comp_set_extents(self, x, y, width, height, coord_type)
 }
 
 static VALUE
-comp_set_position(self, x, y, coord_type)
-    VALUE self, x, y, coord_type;
+comp_set_position(VALUE self, VALUE x, VALUE y, VALUE coord_type)
 {
     gboolean ret = atk_component_set_position(_SELF(self),
                                               NUM2INT(x), NUM2INT(y),
@@ -137,8 +123,7 @@ comp_set_position(self, x, y, coord_type)
 }
 
 static VALUE
-comp_set_size(self, width, height)
-    VALUE self, width, height;
+comp_set_size(VALUE self, VALUE width, VALUE height)
 {
     gboolean ret = atk_component_set_size(_SELF(self),
                                           NUM2INT(width), NUM2INT(height));
@@ -148,8 +133,7 @@ comp_set_size(self, width, height)
 
 #if ATK_CHECK_VERSION(1,12,0)
 static VALUE
-comp_get_alpha(self)
-    VALUE self;
+comp_get_alpha(VALUE self)
 {
     return rb_float_new(atk_component_get_alpha(_SELF(self)));
 }
