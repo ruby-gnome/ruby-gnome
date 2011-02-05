fc708dc7c2e1d39444c6d5b7c5140e39301360cc
Masaaki Aoyagi <masaakiaoyagi@users.sourceforge.net>
2011-02-05 11:06:31 +0000

    * change to ANSI C style.

--- gtk2/ext/gtk2/rbgtkaccelmap.c
+++ gtk2/ext/gtk2/rbgtkaccelmap.c
@@ -13,16 +13,14 @@
 #define RVAL2MOD(mods) (NIL_P(mods) ? 0 : RVAL2GFLAGS(mods, GDK_TYPE_MODIFIER_TYPE))
 
 static VALUE
-accel_map_add_entry(self, path, key, mods)
-    VALUE self, path, key, mods;
+accel_map_add_entry(VALUE self, VALUE path, VALUE key, VALUE mods)
 {
     gtk_accel_map_add_entry(RVAL2CSTR(path), NUM2UINT(key), RVAL2MOD(mods));
     return self;
 }
 
 static VALUE
-accel_map_lookup_entry(self, path)
-    VALUE self, path;
+accel_map_lookup_entry(VALUE self, VALUE path)
 {
     GtkAccelKey key;
     if(gtk_accel_map_lookup_entry(RVAL2CSTR(path), &key))
@@ -32,44 +30,35 @@ accel_map_lookup_entry(self, path)
 }
 
 static VALUE
-accel_map_change_entry(self, path, key, mods, replace)
-    VALUE self, path, key, mods, replace;
+accel_map_change_entry(VALUE self, VALUE path, VALUE key, VALUE mods, VALUE replace)
 {
     return CBOOL2RVAL(gtk_accel_map_change_entry(RVAL2CSTR(path), NUM2UINT(key),
                                                  RVAL2MOD(mods), RVAL2CBOOL(replace)));
 }
 
 static VALUE
-accel_map_load(self, filename)
-    VALUE self, filename;
+accel_map_load(VALUE self, VALUE filename)
 {
     gtk_accel_map_load(RVAL2CSTR(filename));
     return self;
 }
 
 static VALUE
-accel_map_save(self, filename)
-    VALUE self, filename;
+accel_map_save(VALUE self, VALUE filename)
 {
     gtk_accel_map_save(RVAL2CSTR(filename));
     return self;
 }
 
 static VALUE
-accel_map_add_filter(self, pattern)
-    VALUE self, pattern;
+accel_map_add_filter(VALUE self, VALUE pattern)
 {
     gtk_accel_map_add_filter(RVAL2CSTR(pattern));
     return self;
 }
 
 static void
-accel_map_foreach_func(func, path, key, mods, changed)
-    gpointer func;
-    const gchar *path;
-    guint key;
-    GdkModifierType mods;
-    gboolean changed;
+accel_map_foreach_func(gpointer func, const gchar *path, guint key, GdkModifierType mods, gboolean changed)
 {
     rb_funcall((VALUE)func, id_call, 4,
                CSTR2RVAL(path), UINT2NUM(key), GFLAGS2RVAL(mods, GDK_TYPE_MODIFIER_TYPE),
@@ -77,8 +66,7 @@ accel_map_foreach_func(func, path, key, mods, changed)
 }
 
 static VALUE
-accel_map_foreach(self)
-    VALUE self;
+accel_map_foreach(VALUE self)
 {
     volatile VALUE func = rb_block_proc();
     gtk_accel_map_foreach((gpointer)func,
@@ -87,8 +75,7 @@ accel_map_foreach(self)
 }
 
 static VALUE
-accel_map_foreach_unfilterd(self)
-    VALUE self;
+accel_map_foreach_unfilterd(VALUE self)
 {
     volatile VALUE func = rb_block_proc();
     gtk_accel_map_foreach_unfiltered((gpointer)func,
@@ -107,23 +94,20 @@ void        gtk_accel_map_save_fd           (gint fd);
 
 #if GTK_CHECK_VERSION(2,4,0)
 static VALUE
-accel_map_get(self)
-    VALUE self;
+accel_map_get(VALUE self)
 {
     return GOBJ2RVAL(gtk_accel_map_get());
 }
 
 static VALUE
-accel_map_lock_path(self, accel_path)
-    VALUE self, accel_path;
+accel_map_lock_path(VALUE self, VALUE accel_path)
 {
     gtk_accel_map_lock_path(RVAL2CSTR(accel_path));
     return self;
 }
 
 static VALUE
-accel_map_unlock_path(self, accel_path)
-    VALUE self, accel_path;
+accel_map_unlock_path(VALUE self, VALUE accel_path)
 {
     gtk_accel_map_unlock_path(RVAL2CSTR(accel_path));
     return self;
