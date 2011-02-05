3343caf996e5f3d45a97a5a64d8793115fb5a0c0
Masaaki Aoyagi <masaakiaoyagi@users.sourceforge.net>
2011-02-05 11:09:19 +0000

    * change to ANSI C style.

--- gtk2/ext/gtk2/rbgtkbbox.c
+++ gtk2/ext/gtk2/rbgtkbbox.c
@@ -15,8 +15,7 @@
 #include "global.h"
 
 static VALUE
-bbox_set_child_secondary(self, child, is_secondary)
-    VALUE self, child, is_secondary;
+bbox_set_child_secondary(VALUE self, VALUE child, VALUE is_secondary)
 {
     gtk_button_box_set_child_secondary(GTK_BUTTON_BOX(RVAL2GOBJ(self)), 
                                        GTK_WIDGET(RVAL2GOBJ(child)),
@@ -25,8 +24,7 @@ bbox_set_child_secondary(self, child, is_secondary)
 }
 #if GTK_CHECK_VERSION(2,4,0)
 static VALUE
-bbox_get_child_secondary(self, child)
-    VALUE self, child;
+bbox_get_child_secondary(VALUE self, VALUE child)
 {
     return CBOOL2RVAL(gtk_button_box_get_child_secondary(GTK_BUTTON_BOX(RVAL2GOBJ(self)), 
                                                          GTK_WIDGET(RVAL2GOBJ(child))));
