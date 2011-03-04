af1a5f6e42e248f1f63fe42cdf42d296ec54a1cd
Kouhei Sutou <kou@cozmixng.org>
2011-03-04 01:43:35 +0000

    * ext/glib2/rbglib.h: 0.90.7 -> 0.90.8.

--- glib2/ext/glib2/rbglib.h
+++ glib2/ext/glib2/rbglib.h
@@ -23,7 +23,7 @@ extern "C" {
 
 #define RBGLIB_MAJOR_VERSION 0
 #define RBGLIB_MINOR_VERSION 90
-#define RBGLIB_MICRO_VERSION 8
+#define RBGLIB_MICRO_VERSION 9
 
 #ifndef RSTRING_PTR
 #  define RSTRING_PTR(s) (RSTRING(s)->ptr)
