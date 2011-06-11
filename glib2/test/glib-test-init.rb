183dbab2f7499c2a4d18a6870ef0efdb25599a57
Kouhei Sutou <kou@cozmixng.org>
2011-06-11 03:01:03 +0000

    * Rakefile, test/: use test-unit 2 gem. don't bundle it.

--- glib2/test/glib-test-init.rb
+++ glib2/test/glib-test-init.rb
@@ -1,6 +1,4 @@
 $VERBOSE = true
 
-glib_top = File.expand_path(File.join(File.dirname(__FILE__), ".."))
-
-$LOAD_PATH.unshift(File.join(glib_top, "test-unit", "lib"))
+gem 'test-unit'
 require 'test/unit'
