69e9bdbdcb9195622b0bf4bb79fb62d63a4b2027
Masaaki Aoyagi <masaakiaoyagi@users.sourceforge.net>
2011-09-13 12:08:32 +0000

    * lib/vte/deprecated.rb: - change module name to GLib::Deprecatable(TODO: move to glib). - improve warning message. - fix constant_get

--- vte/lib/vte/deprecated.rb
+++ vte/lib/vte/deprecated.rb
@@ -1,44 +1,46 @@
-module GNOMEUtils
-  @@deprecated_const = {}
+module GLib
+  module Deprecatable
+    @@deprecated_const = {}
 
-  def define_deprecated_const(deprecated_const, new_const)
-    @@deprecated_const[self] ||= {}
-    @@deprecated_const[self][deprecated_const.to_sym] = new_const
-  end
+    def define_deprecated_const(deprecated_const, new_const)
+      @@deprecated_const[self] ||= {}
+      @@deprecated_const[self][deprecated_const.to_sym] = new_const
+    end
 
-  def define_deprecated_method(deprecated_method, new_method)
-    if public_method_defined?(new_method)
-      define_method(deprecated_method) do |*args, &block|
-        warn "'#{deprecated_method}' has been deprecated. Use '#{new_method}'."
-        __send__(new_method, *args, &block)
+    def define_deprecated_method(deprecated_method, new_method)
+      if public_method_defined?(new_method)
+        define_method(deprecated_method) do |*args, &block|
+          warn "#{caller[0]}: '#{deprecated_method}' has been deprecated. Use '#{new_method}'."
+          __send__(new_method, *args, &block)
+        end
       end
     end
-  end
 
-  private
+    private
 
-  def const_missing(deprecated_const)
-    if new_const = (@@deprecated_const[self] || {})[deprecated_const.to_sym]
-      if new_const = constant_get(new_const)
-        warn "'#{[name, deprecated_const].join('::')}' has been deprecated. Use '#{new_const}'."
-        const_set(deprecated_const, new_const)
+    def const_missing(deprecated_const)
+      if new_const = (@@deprecated_const[self] || {})[deprecated_const.to_sym]
+        if new_const = constant_get(new_const)
+          warn "#{caller[0]}: '#{[name, deprecated_const].join('::')}' has been deprecated. Use '#{new_const}'."
+          const_set(deprecated_const, new_const)
+        end
       end
     end
-  end
 
-  def constant_get(const)
-    const.split('::').inject(Object){|r, c| r.const_get(c, false)} rescue nil
+    def constant_get(const)
+      const.split('::').inject(Object){|r, c| r.const_get(c)} rescue nil
+    end
   end
 end
 
 module Vte
-  extend GNOMEUtils
+  extend GLib::Deprecatable
   define_deprecated_const 'TerminalEraseBinding', 'Vte::Terminal::EraseBinding'
   define_deprecated_const 'TerminalCursorBlinkMode', 'Vte::Terminal::CursorBlinkMode'
   define_deprecated_const 'TerminalCursorShape', 'Vte::Terminal::CursorShape'
 
   class Terminal
-    extend GNOMEUtils
+    extend GLib::Deprecatable
     define_deprecated_method 'match_set_cursor_type', 'match_set_cursor'
   end
 end
