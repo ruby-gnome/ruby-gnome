9d1037a5352eb6ceb5fd77f5a2672553921fc4c8
Nikolai Weibull <now@bitwi.se>
2011-10-03 10:28:32 +0200

    Add RVAL2ATKATTRIBUTEGSLIST helper

--- atk/ext/atk/rbatkeditabletext.c
+++ atk/ext/atk/rbatkeditabletext.c
@@ -12,25 +12,97 @@
 
 #define _SELF(s) (ATK_EDITABLE_TEXT(RVAL2GOBJ(s)))
 
+struct rval2atkattributegslist_args {
+    VALUE ary;
+    long n;
+    GSList *result;
+};
+
 static VALUE
-rbatk_edit_set_run_attributes(VALUE self, VALUE attributes, VALUE start_offset, VALUE end_offset)
+rbatk_rval2atkattributegslist_body(VALUE value)
 {
     long i;
-    gboolean ret;
-    AtkAttributeSet* list = NULL;
-                                                                                
-    if (NIL_P(attributes)) return Qfalse;
+    struct rval2atkattributegslist_args *args = (struct rval2atkattributegslist_args *)value;
 
-    Check_Type(attributes, T_ARRAY);
-    for (i=0; i<RARRAY_LEN(attributes); i++) {
-        list = g_slist_append(list, RVAL2GOBJ(RARRAY_PTR(attributes)[i]));
+    for (i = 0; i < args->n; i++) {
+        VALUE ary = rb_ary_to_ary(RARRAY_PTR(args->ary)[i]);
+        AtkAttribute *attribute;
+
+        if (RARRAY_LEN(ary) != 2)
+            rb_raise(rb_eArgError, "attribute %ld should be an array of length 2", i);
+
+        attribute = g_new(AtkAttribute, 1);
+        args->result = g_slist_append(args->result, attribute);
+        attribute->name = g_strdup(RVAL2CSTR(RARRAY_PTR(ary)[0]));
+        attribute->value = g_strdup(RVAL2CSTR(RARRAY_PTR(ary)[1]));
     }
 
-    ret = CBOOL2RVAL(atk_editable_text_set_run_attributes(
-                         _SELF(self), list,
-                         NUM2INT(start_offset), NUM2INT(end_offset)));
+    return Qnil;
+}
+
+static void
+rbatk_atkattributegslist_free(GSList *list)
+{
+    GSList *p;
+
+    for (p = list; p != NULL; p = g_slist_next(p)) {
+        AtkAttribute *attribute = (AtkAttribute *)p->data;
+
+        g_free(attribute->value);
+        g_free(attribute->name);
+        g_free(attribute);
+    }
+
     g_slist_free(list);
-    return ret;
+}
+
+static VALUE
+rbatk_rval2atkattributegslist_rescue(VALUE value)
+{
+    rbatk_atkattributegslist_free(((struct rval2atkattributegslist_args *)value)->result);
+
+    rb_exc_raise(rb_errinfo());
+}
+
+static GSList *
+rbatk_rval2atkattributegslist(VALUE value)
+{
+    struct rval2atkattributegslist_args args;
+
+    args.ary = rb_ary_to_ary(value);
+    args.n = RARRAY_LEN(args.ary);
+    args.result = NULL;
+
+    rb_rescue(rbatk_rval2atkattributegslist_body, (VALUE)&args,
+              rbatk_rval2atkattributegslist_rescue, (VALUE)&args);
+
+    return args.result;
+}
+
+#define RVAL2ATKATTRIBUTEGSLIST(value) rbatk_rval2atkattributegslist(value)
+
+static VALUE
+rbatk_edit_set_run_attributes(VALUE self,
+                              VALUE attributes,
+                              VALUE rbstart_offset,
+                              VALUE rbend_offset)
+{
+    AtkEditableText *editable;
+    gint start_offset, end_offset;
+    AtkAttributeSet *list;
+    gboolean result;
+
+    if (NIL_P(attributes))
+        return Qfalse;
+
+    editable = _SELF(self);
+    start_offset = NUM2INT(rbstart_offset);
+    end_offset = NUM2INT(rbend_offset);
+    list = RVAL2ATKATTRIBUTEGSLIST(attributes);
+    result = atk_editable_text_set_run_attributes(editable, list, start_offset, end_offset);
+    rbatk_atkattributegslist_free(list);
+
+    return CBOOL2RVAL(result);
 }
 
 static VALUE
