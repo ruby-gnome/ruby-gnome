ff236183fb0803a5997b440bf2a73ac64f4a2972
Kouhei Sutou <kou@clear-code.com>
2011-10-04 22:18:09 +0900

    [glib2][test] use RbConfig.ruby if available.

--- glib2/test/test_spawn.rb
+++ glib2/test/test_spawn.rb
@@ -19,9 +19,13 @@ class TestGLibSpawn < Test::Unit::TestCase
   end
 
   def test_async_clear_environment
-    ruby = File.join(RbConfig::CONFIG['bindir'],
-                     RbConfig::CONFIG['RUBY_INSTALL_NAME'] +
-                       RbConfig::CONFIG['EXEEXT'])
+    if RbConfig.respond_to?(:ruby)
+      ruby = RbConfig.ruby
+    else
+      ruby = File.join(RbConfig::CONFIG['bindir'],
+                       RbConfig::CONFIG['RUBY_INSTALL_NAME'] +
+                         RbConfig::CONFIG['EXEEXT'])
+    end
     pid = GLib::Spawn.async(Dir.pwd, [ruby, '-e', 'exit 1 unless ENV.empty?'], [], GLib::Spawn::DO_NOT_REAP_CHILD)
     _, status = *Process.waitpid2(pid)
     assert status.success?
