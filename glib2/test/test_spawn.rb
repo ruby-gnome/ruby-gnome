e58b9f898c399d883c1e7d25b7eb7398412b7555
Kouhei Sutou <kou@clear-code.com>
2011-10-04 22:13:37 +0900

    [glib2] don't use tab for indent.

--- glib2/test/test_spawn.rb
+++ glib2/test/test_spawn.rb
@@ -11,15 +11,15 @@ class TestGLibSpawn < Test::Unit::TestCase
   end
 
   def test_async_keep_environment
-	if File.exist?('/bin/true')
-		assert_nothing_raised do
-			GLib::Spawn.async(Dir.pwd, ['/bin/true'], nil, 0)
-		end
-	end
+    if File.exist?('/bin/true')
+      assert_nothing_raised do
+        GLib::Spawn.async(Dir.pwd, ['/bin/true'], nil, 0)
+      end
+    end
   end
 
   def test_async_clear_environment
-	bin = File.join(Config::CONFIG['bindir'], Config::CONFIG['RUBY_INSTALL_NAME'])
+    bin = File.join(Config::CONFIG['bindir'], Config::CONFIG['RUBY_INSTALL_NAME'])
     pid= GLib::Spawn.async(Dir.pwd, [bin, '-e', 'exit 1 unless ENV.empty?'], [], GLib::Spawn::DO_NOT_REAP_CHILD)
     _, status = *Process.waitpid2(pid)
     assert status.success?
