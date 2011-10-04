05a09117af548babd59c81bbc990e1590a212518
Kouhei Sutou <kou@clear-code.com>
2011-10-04 22:13:57 +0900

    [glib2][test] add a missing space.

--- glib2/test/test_spawn.rb
+++ glib2/test/test_spawn.rb
@@ -20,7 +20,7 @@ class TestGLibSpawn < Test::Unit::TestCase
 
   def test_async_clear_environment
     bin = File.join(Config::CONFIG['bindir'], Config::CONFIG['RUBY_INSTALL_NAME'])
-    pid= GLib::Spawn.async(Dir.pwd, [bin, '-e', 'exit 1 unless ENV.empty?'], [], GLib::Spawn::DO_NOT_REAP_CHILD)
+    pid = GLib::Spawn.async(Dir.pwd, [bin, '-e', 'exit 1 unless ENV.empty?'], [], GLib::Spawn::DO_NOT_REAP_CHILD)
     _, status = *Process.waitpid2(pid)
     assert status.success?
   end
