85d86979482c906be02740a94cdb5c3663f7c774
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-10-23 01:44:41 +0900

    Change file header comments

--- gtk2/ext/gtk2/rbgtkselection.c
+++ gtk2/ext/gtk2/rbgtkselection.c
@@ -1,19 +1,27 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
-/************************************************
+/*
+ *  Copyright (C) 2011  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2002,2003 Masao Mutoh
+ *  Copyright (C) 1998-2000 Yukihiro Matsumoto,
+ *                          Daisuke Kanda,
+ *                          Hiroshi Igarashi
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA
+ */
 
-  rbgtkselection.c -
-
-  $Author: ggc $
-  $Date: 2007/07/13 14:27:10 $
-
-  Copyright (C) 2002,2003 Masao Mutoh
-
-  This file is devided from rbgtkwidget.c.
-  rbgtkwidget.c -
-  Copyright (C) 1998-2000 Yukihiro Matsumoto,
-                          Daisuke Kanda,
-                          Hiroshi Igarashi
-************************************************/
 #include "global.h"
 
 #define RVAL2WIDGET(w) (GTK_WIDGET(RVAL2GOBJ(w)))
