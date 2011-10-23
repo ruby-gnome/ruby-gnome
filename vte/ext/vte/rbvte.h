1cc222a2f4a5409e0ffb4891e8453cfd1cd731cc
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-10-23 13:43:15 +0900

    Move method define macros

--- vte/ext/vte/rbvte.h
+++ vte/ext/vte/rbvte.h
@@ -21,30 +21,6 @@
 #define RVAL2PFD(obj) ((PangoFontDescription*)RVAL2BOXED(self, PANGO_TYPE_FONT_DESCRIPTION))
 #define PFD2RVAL(obj) (BOXED2RVAL(obj, PANGO_TYPE_FONT_DESCRIPTION))
 
-#define RG_DEF_MODFUNC(method, argc) \
-        rb_define_module_function(RG_TARGET_NAMESPACE, #method, rg_m_ ## method, argc)
-#define RG_DEF_MODFUNC_P(method, argc) \
-        rb_define_module_function(RG_TARGET_NAMESPACE, #method"?", rg_m_ ## method ## _p, argc)
-#define RG_DEF_MODFUNC_OPERATOR(ope, func, argc) \
-        rb_define_module_function(RG_TARGET_NAMESPACE, ope, rg_m_operator_ ## func, argc)
-#define RG_DEF_SMETHOD(method, argc) \
-        rb_define_singleton_method(RG_TARGET_NAMESPACE, #method, rg_s_ ## method, argc)
-#define RG_DEF_SMETHOD_P(method, argc) \
-        rb_define_singleton_method(RG_TARGET_NAMESPACE, #method"?", rg_s_ ## method ## _p, argc)
-#define RG_DEF_SMETHOD_OPERATOR(ope, func, argc) \
-        rb_define_singleton_method(RG_TARGET_NAMESPACE, ope, rg_s_operator_ ## func, argc)
-#define RG_DEF_METHOD(method, argc) \
-        rb_define_method(RG_TARGET_NAMESPACE, #method, rg_ ## method, argc)
-#define RG_DEF_METHOD_P(method, argc) \
-        rb_define_method(RG_TARGET_NAMESPACE, #method"?", rg_ ## method ## _p, argc)
-#define RG_DEF_METHOD_BANG(method, argc) \
-        rb_define_method(RG_TARGET_NAMESPACE, #method"!", rg_ ## method ## _bang, argc)
-#define RG_DEF_METHOD_OPERATOR(ope, func, argc) \
-        rb_define_method(RG_TARGET_NAMESPACE, ope, rg_operator_ ## func, argc)
-#define RG_DEF_ATTR(attr, read, write, ex) \
-        rb_attr(RG_TARGET_NAMESPACE, rb_intern(attr), read, write, ex)
-#define RG_DEF_ALIAS(new, old) rb_define_alias(RG_TARGET_NAMESPACE, new, old)
-
 extern void Init_vte_access(VALUE mVte);
 extern void Init_vte_reaper(VALUE mVte);
 extern void Init_vte_terminal(VALUE mVte);
