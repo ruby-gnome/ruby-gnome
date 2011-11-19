95e2703e352f16d8f12f7d7ce1ece65110990e54
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-11-19 15:56:09 +0900

    Remove Gtk::Curve and Gtk::GammaCurve

--- gtk3/lib/gtk3/deprecated.rb
+++ gtk3/lib/gtk3/deprecated.rb
@@ -1,4 +1,8 @@
 module Gtk
+  extend GLib::Deprecatable
+  define_deprecated_const :Curve, :raise => "Don't use this widget anymore."
+  define_deprecated_const :GammaCurve, :raise => "Don't use this widget anymore."
+
   class BindingSet
     extend GLib::Deprecatable
     define_deprecated_method :entry_clear, :entry_remove
