94e8e143a6e15fb6709a3cb008be72f3bed48d90
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-11-29 23:59:03 +0900

    Implement Gtk::Overlay

--- gtk3/ext/gtk3/rbgtk.c
+++ gtk3/ext/gtk3/rbgtk.c
@@ -533,6 +533,7 @@ Init_gtk_gtk(void)
     Init_gtk_message_dialog(RG_TARGET_NAMESPACE);
     Init_gtk_misc(RG_TARGET_NAMESPACE);
     Init_gtk_notebook(RG_TARGET_NAMESPACE);
+    Init_gtk_overlay(RG_TARGET_NAMESPACE);
     Init_gtk_page_setup(RG_TARGET_NAMESPACE);
     Init_gtk_page_setup_unix_dialog(RG_TARGET_NAMESPACE);
     Init_gtk_paned(RG_TARGET_NAMESPACE);
