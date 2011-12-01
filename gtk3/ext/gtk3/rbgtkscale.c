04d4ea78b09b981fba273131219f76c38fb7f791
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-12-01 19:47:43 +0900

    Implement Gtk::Scale

--- gtk3/ext/gtk3/rbgtkscale.c
+++ gtk3/ext/gtk3/rbgtkscale.c
@@ -28,6 +28,26 @@
 #define _SELF(self) (GTK_SCALE(RVAL2GOBJ(self)))
 
 static VALUE
+rg_initialize(int argc, VALUE *argv, VALUE self)
+{
+    VALUE orientation, arg1, arg2, arg3;
+    GtkWidget *widget = NULL;
+
+    rb_scan_args(argc, argv, "13", &orientation, &arg1, &arg2, &arg3);
+
+   if (NIL_P(arg1) || RVAL2GTYPE(arg1) == GTK_TYPE_ADJUSTMENT) {
+        widget = gtk_scale_new(RVAL2GENUM(orientation, GTK_TYPE_ORIENTATION),
+                               NIL_P(arg1) ? NULL : GTK_ADJUSTMENT(RVAL2GOBJ(arg1)));
+    } else {
+        widget = gtk_scale_new_with_range(RVAL2GENUM(orientation, GTK_TYPE_ORIENTATION),
+                                          NUM2DBL(arg1), NUM2DBL(arg2), NUM2DBL(arg3));
+    }
+    RBGTK_INITIALIZE(self, widget);
+
+    return Qnil;
+}
+
+static VALUE
 rg_layout(VALUE self)
 {
     return GOBJ2RVAL(gtk_scale_get_layout(_SELF(self)));
@@ -41,10 +61,37 @@ rg_layout_offsets(VALUE self)
     return rb_assoc_new(INT2NUM(x), INT2NUM(y));
 }
 
+static VALUE
+rg_add_mark(int argc, VALUE *argv, VALUE self)
+{
+    VALUE value, position, markup;
+
+    rb_scan_args(argc, argv, "21", &value, &position, &markup);
+
+    gtk_scale_add_mark(_SELF(self),
+                       NUM2DBL(value),
+                       RVAL2GENUM(position, GTK_TYPE_POSITION_TYPE),
+                       RVAL2CSTR_ACCEPT_NIL(markup));
+
+    return self;
+}
+
+static VALUE
+rg_clear_marks(VALUE self)
+{
+    gtk_scale_clear_marks(_SELF(self));
+
+    return self;
+}
+
 void 
 Init_gtk_scale(VALUE mGtk)
 {
     VALUE RG_TARGET_NAMESPACE = G_DEF_CLASS(GTK_TYPE_SCALE, "Scale", mGtk);
+
+    RG_DEF_METHOD(initialize, -1);
     RG_DEF_METHOD(layout, 0);
     RG_DEF_METHOD(layout_offsets, 0);
+    RG_DEF_METHOD(add_mark, -1);
+    RG_DEF_METHOD(clear_marks, 0);
 }
