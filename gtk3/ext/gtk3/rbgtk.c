891b58db8133edddd2f8ca8089d478421a4e8414
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-12-07 23:01:37 +0900

    Implement Gtk::NumerableIcon

--- gtk3/ext/gtk3/rbgtk.c
+++ gtk3/ext/gtk3/rbgtk.c
@@ -534,6 +534,7 @@ Init_gtk_gtk(void)
     Init_gtk_message_dialog(RG_TARGET_NAMESPACE);
     Init_gtk_misc(RG_TARGET_NAMESPACE);
     Init_gtk_notebook(RG_TARGET_NAMESPACE);
+    Init_gtk_numerableicon(RG_TARGET_NAMESPACE);
     Init_gtk_orientable(RG_TARGET_NAMESPACE);
     Init_gtk_overlay(RG_TARGET_NAMESPACE);
     Init_gtk_page_setup(RG_TARGET_NAMESPACE);
