cb177f4c0b316fee3a28567ec2aa20a3d0df1517
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-12-10 13:07:49 +0900

    Implement Gtk::Accessible#widget family

--- gtk3/ext/gtk3/rbgtkaccessible.c
+++ gtk3/ext/gtk3/rbgtkaccessible.c
@@ -22,11 +22,26 @@
 #include "rbgtk3private.h"
 
 #define RG_TARGET_NAMESPACE cAccessible
+#define _SELF(self) (RVAL2GTKACCESSIBLE(self))
 
 static VALUE
 rg_connect_widget_destroyed(VALUE self)
 {
     gtk_accessible_connect_widget_destroyed(RVAL2GTKACCESSIBLE(self));
+    return self;
+}
+
+static VALUE
+rg_widget(VALUE self)
+{
+    return GOBJ2RVAL(gtk_accessible_get_widget(_SELF(self)));
+}
+
+static VALUE
+rg_set_widget(VALUE self, VALUE widget)
+{
+    gtk_accessible_set_widget(_SELF(self), RVAL2GTKWIDGET(widget));
+
     return self;
 }
 
@@ -36,4 +51,8 @@ Init_gtk_accessible(VALUE mGtk)
     VALUE RG_TARGET_NAMESPACE = G_DEF_CLASS(GTK_TYPE_ACCESSIBLE, "Accessible", mGtk);
 
     RG_DEF_METHOD(connect_widget_destroyed, 0);
+    RG_DEF_METHOD(widget, 0);
+    RG_DEF_METHOD(set_widget, 1);
+
+    G_DEF_SETTERS(RG_TARGET_NAMESPACE);
 }
