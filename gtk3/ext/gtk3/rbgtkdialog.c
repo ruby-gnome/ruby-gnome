b83069c85364b59563a1d421bb3cdeba2f37c8ac
Masaaki Aoyagi <masaaki.aoyagi@gmail.com>
2011-12-20 21:54:00 +0900

    Change Gtk::Dialog#add_button to use RVAL2GLIBID

--- gtk3/ext/gtk3/rbgtkdialog.c
+++ gtk3/ext/gtk3/rbgtkdialog.c
@@ -30,8 +30,9 @@
 static VALUE
 rg_add_button(VALUE self, VALUE button_text, VALUE response_id)
 {
+    VALUE buffer;
     return GOBJ2RVAL(gtk_dialog_add_button(_SELF(self),
-                                           RVAL2CSTR_ACCEPT_SYMBOL(button_text), 
+                                           SYMBOL_P(button_text) ? RVAL2GLIBID(button_text, buffer) : RVAL2CSTR(button_text),
                                            RVAL2GTKRESPONSETYPE(response_id)));
 }
 
