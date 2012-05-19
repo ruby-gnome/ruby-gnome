c50684749d059a643690487c6a2d02c6945152c7
Kouhei Sutou <kou@clear-code.com>
2012-05-19 19:50:52 +0900

    gtksourceview3: install required native packages automatically

--- gtksourceview3/ext/gtksourceview3/extconf.rb
+++ gtksourceview3/ext/gtksourceview3/extconf.rb
@@ -55,7 +55,11 @@ check_cairo(rcairo_options)
 
 setup_win32(module_name, base_dir)
 
-PKGConfig.have_package(package_id) or exit(false)
+unless required_pkg_config_package(package_id,
+                                   :debian => "libgtksourceview3.0-dev",
+                                   :fedora => "gtksourceview3-devel")
+  exit(false)
+end
 
 make_version_header("GTKSOURCEVIEW3", package_id, ".")
 
