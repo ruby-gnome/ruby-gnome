b2cacb0ee97229ba1086593f8c1d4ac19437561d
Kouhei Sutou <kou@clear-code.com>
2012-11-11 23:21:55 +0900

    poppler: fix wrong package name on Red Hat/Fedora
    [ruby-gnome2-devel-en] poppler, build fails, incorrect
    required_pkg_config_package?
    
    Reported by Grant Schoep. Thanks!!!

--- poppler/ext/poppler/extconf.rb
+++ poppler/ext/poppler/extconf.rb
@@ -57,7 +57,7 @@ setup_win32(module_name, base_dir)
 
 unless required_pkg_config_package(package_id,
                                    :debian => "libpoppler-glib-dev",
-                                   :redhat => "glib2-devel",
+                                   :redhat => "poppler-glib-devel",
                                    :homebrew => "poppler", # need --with-glib option
                                    :macports => "poppler")
   exit(false)
