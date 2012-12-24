3b0fc03ac926371d0908edb676db482deffe4637
Kouhei Sutou <kou@clear-code.com>
2012-12-24 16:05:31 +0900

    gi: use spaces for indent

--- gobject-introspection/ext/gobject-introspection/rb-gi-type-info.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-type-info.c
@@ -29,8 +29,8 @@ gi_type_info_get_type(void)
     static GType type = 0;
     if (type == 0) {
 	type = g_boxed_type_register_static("GITypeInfo",
-					    (GBoxedCopyFunc)g_base_info_ref,
-					    (GBoxedFreeFunc)g_base_info_unref);
+                                            (GBoxedCopyFunc)g_base_info_ref,
+                                            (GBoxedFreeFunc)g_base_info_unref);
     }
     return type;
 }
