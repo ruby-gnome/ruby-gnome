81a99d3c6451e24839a2dc81bfd57b778145db6e
Kouhei Sutou <kou@clear-code.com>
2012-12-25 21:36:08 +0900

    gi: define UnionInfo methods

--- gobject-introspection/ext/gobject-introspection/rb-gi-union-info.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-union-info.c
@@ -44,6 +44,103 @@ rg_n_fields(VALUE self)
     return INT2NUM(g_union_info_get_n_fields(info));
 }
 
+static VALUE
+rg_get_field(VALUE self, VALUE rb_n)
+{
+    GIUnionInfo *info;
+    gint n;
+
+    info = SELF(self);
+    n = NUM2INT(rb_n);
+    return GI_BASE_INFO2RVAL_WITH_UNREF(g_union_info_get_field(info, n));
+}
+
+static VALUE
+rg_n_methods(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return INT2NUM(g_union_info_get_n_methods(info));
+}
+
+static VALUE
+rg_get_method(VALUE self, VALUE rb_n_or_name)
+{
+    GIUnionInfo *info;
+    GIFunctionInfo *function_info;
+
+    info = SELF(self);
+    if (RB_TYPE_P(self, T_FIXNUM)) {
+        gint n;
+        n = NUM2INT(rb_n_or_name);
+        function_info = g_union_info_get_method(info, n);
+    } else {
+        const gchar *name;
+        name = RVAL2CSTR(rb_n_or_name);
+        function_info = g_union_info_find_method(info, name);
+    }
+
+    return GI_BASE_INFO2RVAL_WITH_UNREF(function_info);
+}
+
+static VALUE
+rg_discriminated_p(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return CBOOL2RVAL(g_union_info_is_discriminated(info));
+}
+
+static VALUE
+rg_discriminator_offset(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return INT2NUM(g_union_info_get_discriminator_offset(info));
+}
+
+static VALUE
+rg_discriminator_type(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return GI_BASE_INFO2RVAL_WITH_UNREF(g_union_info_get_discriminator_type(info));
+}
+
+static VALUE
+rg_get_discriminator(VALUE self, VALUE rb_n)
+{
+    GIUnionInfo *info;
+    gint n;
+
+    info = SELF(self);
+    n = NUM2INT(rb_n);
+    return GI_BASE_INFO2RVAL_WITH_UNREF(g_union_info_get_discriminator(info, n));
+}
+
+static VALUE
+rg_size(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return ULONG2NUM(g_union_info_get_size(info));
+}
+
+
+static VALUE
+rg_alignment(VALUE self)
+{
+    GIUnionInfo *info;
+
+    info = SELF(self);
+    return ULONG2NUM(g_union_info_get_alignment(info));
+}
+
 void
 rb_gi_union_info_init(VALUE rb_mGI, VALUE rb_cGIRegisteredTypeInfo)
 {
@@ -54,4 +151,13 @@ rb_gi_union_info_init(VALUE rb_mGI, VALUE rb_cGIRegisteredTypeInfo)
 				rb_cGIRegisteredTypeInfo);
 
     RG_DEF_METHOD(n_fields, 0);
+    RG_DEF_METHOD(get_field, 1);
+    RG_DEF_METHOD(n_methods, 0);
+    RG_DEF_METHOD(get_method, 1);
+    RG_DEF_METHOD_P(discriminated, 0);
+    RG_DEF_METHOD(discriminator_offset, 0);
+    RG_DEF_METHOD(discriminator_type, 0);
+    RG_DEF_METHOD(get_discriminator, 1);
+    RG_DEF_METHOD(size, 0);
+    RG_DEF_METHOD(alignment, 0);
 }
