17d81564f00fabd7052b6ae139dbaab1b86332a8
Kouhei Sutou <kou@clear-code.com>
2013-01-04 22:50:05 +0900

    gi: add CallableInfo#in_args, #n_in_args, #out_args and #n_out_args

--- gobject-introspection/lib/gobject-introspection/callable-info.rb
+++ gobject-introspection/lib/gobject-introspection/callable-info.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2012  Ruby-GNOME2 Project Team
+# Copyright (C) 2013  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -14,27 +14,42 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "glib2"
-
-base_dir = Pathname.new(__FILE__).dirname.dirname.dirname.expand_path
-vendor_dir = base_dir + "vendor" + "local"
-vendor_bin_dir = vendor_dir + "bin"
-GLib.prepend_environment_path(vendor_bin_dir)
-begin
-  major, minor, _ = RUBY_VERSION.split(/\./)
-  require "#{major}.#{minor}/gobject_introspection.so"
-rescue LoadError
-  require "gobject_introspection.so"
-end
+require "gobject-introspection/collection-reader"
 
 module GObjectIntrospection
-  LOG_DOMAIN = "GObjectIntrospection"
+  class CallableInfo
+    extend CollectionReader
+
+    collection_reader("args")
+
+    def in_args
+      args.find_all do |arg|
+        case arg.direction
+        when Direction::IN, Direction::INOUT
+          true
+        else
+          false
+        end
+      end
+    end
+
+    def n_in_args
+      in_args.size
+    end
+
+    def out_args
+      args.find_all do |arg|
+        case arg.direction
+        when Direction::OUT, Direction::INOUT
+          true
+        else
+          false
+        end
+      end
+    end
+
+    def n_out_args
+      out_args.size
+    end
+  end
 end
-
-GLib::Log.set_log_domain(GObjectIntrospection::LOG_DOMAIN)
-
-require "gobject-introspection/repository"
-require "gobject-introspection/callable-info"
-require "gobject-introspection/object-info"
-require "gobject-introspection/struct-info"
-require "gobject-introspection/loader"
