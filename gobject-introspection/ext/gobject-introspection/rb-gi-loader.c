b2ede3742bee34ab421eeed2edc0e75fc36b8c5e
Kouhei Sutou <kou@clear-code.com>
2013-01-13 20:57:47 +0900

    gi: add Loader.register_boxed_converter

--- gobject-introspection/ext/gobject-introspection/rb-gi-loader.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-loader.c
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2012  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2012-2013  Ruby-GNOME2 Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -21,6 +21,8 @@
 #include "rb-gobject-introspection.h"
 
 #define RG_TARGET_NAMESPACE rb_cGILoader
+
+static const gchar *boxed_converters_name = "@@boxed_converters";
 
 static VALUE
 rg_s_define_class(int argc, VALUE *argv, G_GNUC_UNUSED VALUE klass)
@@ -80,6 +82,62 @@ rg_s_define_struct(int argc, VALUE *argv, G_GNUC_UNUSED VALUE klass)
     return rb_class;
 }
 
+typedef struct {
+    GType type;
+    VALUE rb_converters;
+    VALUE rb_converter;
+} BoxedInstanec2RObjData;
+
+static void
+boxed_converter_free(gpointer user_data)
+{
+    BoxedInstanec2RObjData *data = user_data;
+    rb_ary_delete(data->rb_converters, data->rb_converter);
+    g_free(data);
+}
+
+static VALUE
+boxed_instance2robj(gpointer instance, gpointer user_data)
+{
+    BoxedInstanec2RObjData *data = user_data;
+    VALUE default_rb_instance;
+    VALUE klass;
+    gint flags = 0;
+    ID id_call;
+
+    default_rb_instance = rbgobj_make_boxed_default(instance, data->type);
+    CONST_ID(id_call, "call");
+    klass = rb_funcall(data->rb_converter, id_call, 1, default_rb_instance);
+    return rbgobj_make_boxed_raw(instance, data->type, klass, flags);
+}
+
+static VALUE
+rg_s_register_boxed_converter(VALUE klass, VALUE rb_gtype)
+{
+    RGConvertTable table;
+    BoxedInstanec2RObjData *data;
+    ID id_to_i;
+    VALUE boxed_converters;
+
+    memset(&table, 0, sizeof(RGConvertTable));
+    CONST_ID(id_to_i, "to_i");
+    table.type = NUM2UINT(rb_funcall(rb_gtype, id_to_i, 0));
+    table.klass = Qnil;
+    table.instance2robj = boxed_instance2robj;
+
+    data = g_new(BoxedInstanec2RObjData, 1);
+    data->type = table.type;
+    data->rb_converter = rb_block_proc();
+    boxed_converters = rb_cv_get(klass, boxed_converters_name);
+    rb_ary_push(boxed_converters, data->rb_converter);
+    table.user_data = data;
+    table.notify = boxed_converter_free;
+
+    rbgobj_convert_define(&table);
+
+    return Qnil;
+}
+
 void
 rb_gi_loader_init(VALUE rb_mGI)
 {
@@ -87,7 +145,10 @@ rb_gi_loader_init(VALUE rb_mGI)
 
     RG_TARGET_NAMESPACE = rb_define_class_under(rb_mGI, "Loader", rb_cObject);
 
+    rb_cv_set(RG_TARGET_NAMESPACE, boxed_converters_name, rb_ary_new());
+
     RG_DEF_SMETHOD(define_class, -1);
     RG_DEF_SMETHOD(define_interface, 3);
     RG_DEF_SMETHOD(define_struct, -1);
+    RG_DEF_SMETHOD(register_boxed_converter, 1);
 }
