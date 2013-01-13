255798e035dbb303ae0dd1d4ed11f72728ea7e88
Kouhei Sutou <kou@clear-code.com>
2013-01-13 19:48:41 +0900

    glib2: remove deprecated G_DEF_FUNDAMENTAL
    Use RG_DEF_CONVERSION instead.

--- glib2/ext/glib2/rbgcompat.h
+++ glib2/ext/glib2/rbgcompat.h
@@ -24,21 +24,6 @@
 
 G_BEGIN_DECLS
 
-typedef struct {
-    GType type;
-    VALUE (*get_superclass)(void);
-    void (*type_init_hook)(VALUE);
-    void (*rvalue2gvalue)(VALUE val, GValue *result);
-    VALUE (*gvalue2rvalue)(const GValue *);
-    void (*initialize)(VALUE, gpointer);
-    gpointer (*robj2instance)(VALUE);
-    VALUE (*instance2robj)(gpointer);
-} RGFundamental;
-
-#define G_DEF_FUNDAMENTAL(f) (rbgobj_fund_define_fundamental(f))
-
-extern void rbgobj_fund_define_fundamental(RGFundamental *fundamental);
-
 G_END_DECLS
 
 #endif
