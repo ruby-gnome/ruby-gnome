02c36171dc9298ab0554361d46cd6b2a8f1baf45
Kouhei Sutou <kou@clear-code.com>
2013-01-24 21:18:38 +0900

    1.1.9 -> 1.2.0

--- glib2/ext/glib2/rbglib.h
+++ glib2/ext/glib2/rbglib.h
@@ -34,8 +34,8 @@ extern "C" {
 #endif /* __cplusplus */
 
 #define RBGLIB_MAJOR_VERSION 1
-#define RBGLIB_MINOR_VERSION 1
-#define RBGLIB_MICRO_VERSION 9
+#define RBGLIB_MINOR_VERSION 2
+#define RBGLIB_MICRO_VERSION 0
 
 #ifndef RSTRING_PTR
 #  define RSTRING_PTR(s) (RSTRING(s)->ptr)
