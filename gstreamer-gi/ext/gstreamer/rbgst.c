0857004dbc617777b9d86a4c14a91c1caaa0bda0
Kouhei Sutou <kou@clear-code.com>
2013-02-06 21:44:38 +0900

    gstreamer-gi: add for testing GObject Introspection based binding

---
+++ gstreamer-gi/ext/gstreamer/rbgst.c
@@ -0,0 +1,77 @@
+/* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
+/*
+ *  Copyright (C) 2013  Ruby-GNOME2 Project Team
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ *  MA  02110-1301  USA
+ */
+
+#include <gst/gst.h>
+
+#include <rb-gobject-introspection.h>
+
+extern void Init_gstreamer (void);
+
+static gboolean
+rg_gst_bus_func_p(GIArgInfo *info)
+{
+    GITypeInfo type_info;
+    GIBaseInfo *interface_info;
+    const gchar *namespace;
+    const gchar *name;
+    gboolean gst_bus_func_p = FALSE;
+
+    g_arg_info_load_type(info, &type_info);
+    interface_info = g_type_info_get_interface(&type_info);
+    namespace = g_base_info_get_namespace(interface_info);
+    name = g_base_info_get_name(interface_info);
+    if (strcmp(namespace, "Gst") == 0 && strcmp(name, "BusFunc") == 0) {
+        gst_bus_func_p = TRUE;
+    }
+    g_base_info_unref(interface_info);
+
+    return gst_bus_func_p;
+}
+
+static gboolean
+rg_gst_bus_func_callback(GstBus *bus, GstMessage *message, gpointer user_data)
+{
+    RBGICallbackData *callback_data = user_data;
+    VALUE rb_keep;
+    ID id_call;
+
+    CONST_ID(id_call, "call");
+    rb_keep = rb_funcall(callback_data->rb_callback, id_call, 2,
+                         GOBJ2RVAL(bus), GOBJ2RVAL(message));
+    if (callback_data->metadata->scope_type == GI_SCOPE_TYPE_ASYNC) {
+        rb_gi_callback_data_free(callback_data);
+    }
+    return RVAL2CBOOL(rb_keep);
+}
+
+static gpointer
+rg_gst_bus_func_callback_finder(GIArgInfo *info)
+{
+    if (!rg_gst_bus_func_p(info)) {
+        return NULL;
+    }
+    return rg_gst_bus_func_callback;
+}
+
+void
+Init_gstreamer (void)
+{
+    rb_gi_callback_register_finder(rg_gst_bus_func_callback_finder);
+}
---
+++ gstreamer-gi/lib/gst.rb
@@ -0,0 +1,65 @@
+# Copyright (C) 2013  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+require "gobject-introspection"
+
+base_dir = Pathname.new(__FILE__).dirname.dirname.dirname.expand_path
+vendor_dir = base_dir + "vendor" + "local"
+vendor_bin_dir = vendor_dir + "bin"
+GLib.prepend_environment_path(vendor_bin_dir)
+
+module Gst
+  LOG_DOMAIN = "GStreamer"
+  GLib::Log.set_log_domain(LOG_DOMAIN)
+
+  class << self
+    @initialized = false
+    def init(argv=ARGV)
+      return if @initialized
+      @initialized = true
+      loader = Loader.new(self, argv)
+      loader.load("Gst")
+    end
+  end
+
+  class Loader < GObjectIntrospection::Loader
+    def initialize(base_module, init_arguments)
+      super(base_module)
+      @init_arguments = init_arguments
+    end
+
+    private
+    def pre_load(repository, namespace)
+      init = repository.find(namespace, "init")
+      argc, argv = init.invoke(1 + @init_arguments.size,
+                               [$0] + @init_arguments)
+      @init_arguments.replace(argv)
+    end
+
+    def post_load(repository, namespace)
+      require_extension
+    end
+
+    def require_extension
+      begin
+        major, minor, _ = RUBY_VERSION.split(/\./)
+        require "#{major}.#{minor}/gstreamer.so"
+      rescue LoadError
+        require "gstreamer.so"
+      end
+    end
+  end
+end
