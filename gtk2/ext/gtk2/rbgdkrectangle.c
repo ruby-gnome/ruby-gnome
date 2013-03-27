1cab1839fccba86a91f01fbbcd3ce076e11a7b70
Kouhei Sutou <kou@clear-code.com>
2013-03-27 21:32:35 +0900

    gtk2: add missing g_boxed_copy()
    GitHub: #138
    
    Reported by Takuma Nakajima. Thanks!!!

--- gtk2/ext/gtk2/rbgdkrectangle.c
+++ gtk2/ext/gtk2/rbgdkrectangle.c
@@ -37,7 +37,7 @@ rg_initialize(VALUE self, VALUE x, VALUE y, VALUE width, VALUE height)
     new.width = NUM2INT(width);
     new.height = NUM2INT(height);
 
-    G_INITIALIZE(self, &new);
+    G_INITIALIZE(self, g_boxed_copy(GDK_TYPE_RECTANGLE, &new));
     return Qnil;
 }
 
