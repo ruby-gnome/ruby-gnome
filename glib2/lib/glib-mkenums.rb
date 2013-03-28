91f81acb47391e53abb34a5ac5ccd8ec9f0978b1
Kouhei Sutou <kou@clear-code.com>
2013-03-28 22:49:56 +0900

    glib2: remove a warning
    glib2/lib/glib-mkenums.rb:32: warning: assigned but unused variable - ret

--- glib2/lib/glib-mkenums.rb
+++ glib2/lib/glib-mkenums.rb
@@ -3,7 +3,7 @@
 #
 # C language enum description generation library like as glib-mkenums tool.
 #
-# Copyright(C) 2006-2008 Ruby-GNOME2 Project.
+# Copyright(C) 2006-2013 Ruby-GNOME2 Project.
 #
 # This program is licenced under the same license of Ruby-GNOME2.
 #
@@ -29,8 +29,6 @@ module GLib
     end
 
     def parse_const_lines(const_lines)
-      ret = ""
-
       if @options[:force_flags] or /<</ =~ const_lines
         @type = "flags"
         @Type = "Flags"
