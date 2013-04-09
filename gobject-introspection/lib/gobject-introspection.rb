acbe54bfccb00444357f768e5e1dc71e2f423671
Kouhei Sutou <kou@clear-code.com>
2013-04-10 00:18:57 +0900

    Remove a needless ".dirname"

--- gobject-introspection/lib/gobject-introspection.rb
+++ gobject-introspection/lib/gobject-introspection.rb
@@ -16,7 +16,7 @@
 
 require "glib2"
 
-base_dir = Pathname.new(__FILE__).dirname.dirname.dirname.expand_path
+base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
 vendor_dir = base_dir + "vendor" + "local"
 vendor_bin_dir = vendor_dir + "bin"
 GLib.prepend_environment_path(vendor_bin_dir)
