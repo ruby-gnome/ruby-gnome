51fbd90a7efda428ca3d35cddc374560a2511229
Kouhei Sutou <kou@clear-code.com>
2013-04-12 22:43:52 +0900

    gstreamer-gi: use bundled .typelib

--- gstreamer-gi/lib/gst.rb
+++ gstreamer-gi/lib/gst.rb
@@ -20,6 +20,8 @@ base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
 vendor_dir = base_dir + "vendor" + "local"
 vendor_bin_dir = vendor_dir + "bin"
 GLib.prepend_environment_path(vendor_bin_dir)
+vendor_lib_dir = vendor_dir + "lib"
+GObjectIntrospection.prepend_environment_path(vendor_lib_dir)
 
 module Gst
   LOG_DOMAIN = "GStreamer"
