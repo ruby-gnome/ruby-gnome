bff22ada86fe58a1476fda6b50763419cac147c8
Kouhei Sutou <kou@clear-code.com>
2013-04-13 16:35:54 +0900

    pango windows: set typelib path

--- pango/lib/pango.rb
+++ pango/lib/pango.rb
@@ -21,6 +21,14 @@ base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
 vendor_dir = base_dir + "vendor" + "local"
 vendor_bin_dir = vendor_dir + "bin"
 GLib.prepend_dll_path(vendor_bin_dir)
+if vendor_dir.exist?
+  begin
+    require "gobject-introspection"
+    vendor_girepository_dir = vendor_dir + "lib" + "girepository-1.0"
+    GObjectIntrospection.prepend_typelib_path(vendor_girepository_dir)
+  rescue LoadError
+  end
+end
 begin
   major, minor, _ = RUBY_VERSION.split(/\./)
   require "#{major}.#{minor}/pango.so"
