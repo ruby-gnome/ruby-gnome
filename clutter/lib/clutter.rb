b0bdfadb815bcf3973bcdef78ba56267748618f6
Kouhei Sutou <kou@clear-code.com>
2013-04-13 16:16:35 +0900

    clutter windows: use bundled typelib

--- clutter/lib/clutter.rb
+++ clutter/lib/clutter.rb
@@ -21,6 +21,8 @@ base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
 vendor_dir = base_dir + "vendor" + "local"
 vendor_bin_dir = vendor_dir + "bin"
 GLib.prepend_dll_path(vendor_bin_dir)
+vendor_girepository_dir = vendor_dir + "lib" + "girepository-1.0"
+GObjectIntrospection.prepend_typelib_path(vendor_girepository_dir)
 
 module Clutter
   LOG_DOMAIN = "Clutter"
