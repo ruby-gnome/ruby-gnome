edc9d1794c644418e2a1e09e051187d903d8075c
Kouhei Sutou <kou@clear-code.com>
2013-06-30 23:22:00 +0900

    gdk_pixbuf2 windows: use rcairo on Windows for libpng and zlib

--- gdk_pixbuf2/lib/gdk_pixbuf2.rb
+++ gdk_pixbuf2/lib/gdk_pixbuf2.rb
@@ -4,6 +4,11 @@ base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
 vendor_dir = base_dir + "vendor" + "local"
 vendor_bin_dir = vendor_dir + "bin"
 GLib.prepend_dll_path(vendor_bin_dir)
+
+if vendor_dir.exist?
+  require "cairo"
+end
+
 begin
   major, minor, _ = RUBY_VERSION.split(/\./)
   require "#{major}.#{minor}/gdk_pixbuf2.so"
