3be5076bca8aed18471e49cc52863316e8a7bc38
Kouhei Sutou <kou@clear-code.com>
2013-09-18 22:16:52 +0900

    gstreamer: add convenience methods to Gst::ElementFactory

---
+++ gstreamer/lib/gst/element-factory.rb
@@ -0,0 +1,35 @@
+# Copyright (C) 2013  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module Gst
+  class ElementFactory
+    def long_name
+      get_metadata("long-name")
+    end
+
+    def klass
+      get_metadata("klass")
+    end
+
+    def description
+      get_metadata("description")
+    end
+
+    def author
+      get_metadata("author")
+    end
+  end
+end
