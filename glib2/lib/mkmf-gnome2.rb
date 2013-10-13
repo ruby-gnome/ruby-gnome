1bdc9b78085ad24f6d0f0c9ec5a2c59c6607313d
Kouhei Sutou <kou@clear-code.com>
2013-10-13 19:07:27 +0900

    glib2: remove needless RubyGems fallback
    We already dropped Ruby 1.8.

--- glib2/lib/mkmf-gnome2.rb
+++ glib2/lib/mkmf-gnome2.rb
@@ -3,7 +3,7 @@
 #
 # Extended mkmf for Ruby-GNOME2 and Ruby/GLib based libraries.
 #
-# Copyright(C) 2003-2011 Ruby-GNOME2 Project.
+# Copyright(C) 2003-2013 Ruby-GNOME2 Project.
 #
 # This program is licenced under the same
 # license of Ruby-GNOME2.
@@ -11,13 +11,7 @@
 
 require 'English'
 require 'mkmf'
-begin
-  require 'pkg-config'
-rescue LoadError
-  require 'rubygems'
-  gem 'pkg-config'
-  require 'pkg-config'
-end
+require 'pkg-config'
 require 'glib-mkenums'
 
 $CFLAGS += " #{ENV['CFLAGS']}" if ENV['CFLAGS']
