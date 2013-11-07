718a5cb9b13bb2b6fe0e146430a68759937c20c4
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-11-07 22:06:13 +0900

    gio2: implement tests

--- gio2/test/test_bufferedinputstream.rb
+++ gio2/test/test_bufferedinputstream.rb
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 
+class TestBufferedInputStream < Test::Unit::TestCase
+  def test_buffered_input_stream
+    assert_nothing_raised do
+      Gio::BufferedInputStream.new(Gio::FileInputStream.new)
+    end
+  end
+end
--- gio2/test/test_charsetconverter.rb
+++ gio2/test/test_charsetconverter.rb
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
 
+class TestCharsetConverter < Test::Unit::TestCase
+  def test_converter
+    assert_nothing_raised do
+      Gio::CharsetConverter.new("UTF-8", "ASCII")
+    end
+  end
+end
