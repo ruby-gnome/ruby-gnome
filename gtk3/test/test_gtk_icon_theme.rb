31905579a3d4a6c0bf83bc050fc3cefc09528da3
Kouhei Sutou <kou@clear-code.com>
2013-11-18 23:10:58 +0900

    gtk3: use face-cool instead of undo
    It seems that gnome-icon-thme on Ubuntu 12.04 LTS includes face-cool.

--- gtk3/test/test_gtk_icon_theme.rb
+++ gtk3/test/test_gtk_icon_theme.rb
@@ -8,11 +8,11 @@ class TestGtkIconTheme < Test::Unit::TestCase
   def test_choose_icon
     assert_nil(@theme.choose_icon("non-existent", 100))
 
-    icon = @theme.choose_icon("undo", 10)
+    icon = @theme.choose_icon("face-cool", 10)
     assert_not_nil(icon)
-    assert_match(/undo/, icon.filename)
+    assert_match(/face-cool/, icon.filename)
 
-    assert_not_nil(@theme.choose_icon("undo", 29, [:use_builtin, :no_svg]))
+    assert_not_nil(@theme.choose_icon("face-cool", 29, [:use_builtin, :no_svg]))
   end
 
   def test_contexts
@@ -20,7 +20,7 @@ class TestGtkIconTheme < Test::Unit::TestCase
   end
 
   def test_icons
-    assert_operator(@theme.icons, :include?, "undo")
+    assert_operator(@theme.icons, :include?, "face-cool")
     assert_operator(@theme.icons("Actions"), :include?, "remove")
   end
 end
