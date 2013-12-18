1a200740eb4154f0ba7c0f278f594ec224ef914e
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2013-12-18 21:47:51 +0900

    poppler extconf.rb: require Poppler 0.12 or later

--- poppler/ext/poppler/extconf.rb
+++ poppler/ext/poppler/extconf.rb
@@ -58,7 +58,7 @@ end
 
 setup_win32(module_name, base_dir)
 
-unless required_pkg_config_package([package_id, 0, 8, 0],
+unless required_pkg_config_package([package_id, 0, 12, 0],
                                    :debian => "libpoppler-glib-dev",
                                    :redhat => "poppler-glib-devel",
 #                                   :homebrew => "poppler", # need --with-glib option
