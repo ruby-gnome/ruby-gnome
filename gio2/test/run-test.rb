109baad7a9e8f92018453bdf85b8c4afaf429719
Kouhei Sutou <kou@clear-code.com>
2014-02-21 21:20:49 +0900

    gio2: clean up test runner

--- gio2/test/run-test.rb
+++ gio2/test/run-test.rb
@@ -1,15 +1,41 @@
 #!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2013-2014  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-base = File.dirname(File.expand_path(__FILE__))
-root = File.dirname(File.dirname(base))
-['glib2', 'gio2'].each do |lib|
-  ['ext/%s' % lib, 'lib'].each do |sub|
-    $LOAD_PATH.unshift File.join(root, lib, sub)
+ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
+ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
+
+modules = [
+  "glib2",
+  "gio2",
+]
+
+modules.each do |name|
+  base = File.join(ruby_gnome2_base, name)
+  if File.exist?("Makefile") and system("which make > /dev/null")
+    `make -C #{base.dump} > /dev/null` or exit(false)
   end
+  $LOAD_PATH.unshift(File.join(base, "ext", name))
+  $LOAD_PATH.unshift(File.join(base, "lib"))
 end
 
-require 'gio2'
-require File.join(root, 'glib2/test/glib-test-init')
+$LOAD_PATH.unshift(File.join(ruby_gnome2_base, "glib2", "test"))
+require "glib-test-init"
 
-exit Test::Unit::AutoRunner.run(true, base)
+require "gio2"
+
+exit Test::Unit::AutoRunner.run(true)
