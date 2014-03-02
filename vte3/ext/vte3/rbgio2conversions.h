8943bee139fac86e5aa3886917e76758e2d08cd3
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-03-03 00:41:06 +0900

    vte3: fix Copyright
    2011 ->
    2014
       ^

--- vte3/ext/vte3/rbgio2conversions.h
+++ vte3/ext/vte3/rbgio2conversions.h
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2011  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2014  Ruby-GNOME2 Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
