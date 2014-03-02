0fda7aa0b67d915ce8e6f8115c73789261ac8ea6
Kouhei Sutou <kou@clear-code.com>
2014-03-02 21:22:38 +0900

    gio2 test: split

--- gio2/test/gio2-test-utils/fixture.rb
+++ gio2/test/gio2-test-utils/fixture.rb
@@ -14,7 +14,11 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "test-unit"
-require "test/unit/notify"
-
-require "gio2-test-utils/fixture"
+module GioTestUtils
+  module Fixture
+    def fixture_path(*components)
+      base_dir = File.join(File.dirname(__FILE__), "..", "fixture")
+      File.expand_path(File.join(base_dir, *components))
+    end
+  end
+end
