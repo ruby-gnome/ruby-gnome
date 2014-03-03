34a2452665a0cd147b04a28a661f471d251191b6
Kouhei Sutou <kou@clear-code.com>
2014-03-03 22:14:47 +0900

    gi: fix free function for an array of UTF-8-STRING
    Because RVAL2STRV doesn't copy elements in an array. So we should free
    only container. We should not free internal elements.

--- gobject-introspection/ext/gobject-introspection/rb-gi-argument.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-argument.c
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2012-2013  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2012-2014  Ruby-GNOME2 Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -1288,7 +1288,7 @@ rb_gi_value_argument_free_array_c(GIArgument *argument,
         break;
       case GI_TYPE_TAG_UTF8:
       case GI_TYPE_TAG_FILENAME:
-        g_strfreev(argument->v_pointer);
+        g_free(argument->v_pointer);
         break;
       case GI_TYPE_TAG_ARRAY:
       case GI_TYPE_TAG_INTERFACE:
