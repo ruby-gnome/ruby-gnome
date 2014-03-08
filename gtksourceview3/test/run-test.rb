abb35fb900cdf731e1e0b97ba20b1b3908e3eb49
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-03-09 00:06:25 +0900

    gktsourceview3 test: use gtk3 and gkd3 instead of gtk2.
    gtksourceview3 depends gdk3 and gtk3.

--- gtksourceview3/test/run-test.rb
+++ gtksourceview3/test/run-test.rb
@@ -7,13 +7,14 @@ glib_base = File.join(ruby_gnome2_base, "glib2")
 atk_base = File.join(ruby_gnome2_base, "atk")
 pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
-gtk_base = File.join(ruby_gnome2_base, "gtk2")
+gdk3_base = File.join(ruby_gnome2_base, "gdk3")
+gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 gtk_source_view3_base = File.join(ruby_gnome2_base, "gtksourceview3")
 
 $LOAD_PATH.unshift(glib_base)
 require 'test/glib-test-init'
 
-[glib_base, atk_base, pango_base, gdk_pixbuf_base, gtk_base,
+[glib_base, atk_base, pango_base, gdk_pixbuf_base, gdk3_base, gtk3_base,
  gtk_source_view3_base].each do |target|
   if system("which make > /dev/null")
     `make -C #{target.dump} > /dev/null` or exit(1)
@@ -25,7 +26,7 @@ require 'test/glib-test-init'
   $LOAD_PATH.unshift(File.join(target, "lib"))
 end
 
-$LOAD_PATH.unshift(File.join(gtk_base, "test"))
+$LOAD_PATH.unshift(File.join(gtk3_base, "test"))
 require 'gtk-test-utils'
 
 require 'gtksourceview3'
