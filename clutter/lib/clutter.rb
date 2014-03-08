b558832f1e2f1ee38aae83c176f89889242f3cda
Kouhei Sutou <kou@clear-code.com>
2014-03-08 21:48:22 +0900

    clutter: use different variable name

--- clutter/lib/clutter.rb
+++ clutter/lib/clutter.rb
@@ -82,8 +82,8 @@ module Clutter
         1 + @init_arguments.size,
         [$0] + @init_arguments,
       ]
-      error, arguments = init.invoke(:arguments => arguments)
-      @init_arguments.replace(arguments)
+      error, returned_arguments = init.invoke(:arguments => arguments)
+      @init_arguments.replace(returned_arguments)
       if error.to_i <= 0
         raise InitError, "failed to initialize Clutter: #{error.name}"
       end
