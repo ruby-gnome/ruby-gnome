799880557484dcdc60d758e54a711252298c9cf6
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-09 12:57:42 +0900

    gtksourceview3 sample: suppress warnings for deprecated API
    sourcelanguagemanager.rb:13:in `<main>': 'Gtk::SourceLanguageManager' has been deprecated. Use 'GtkSource::LanguageManager'.

--- gtksourceview3/sample/sourcelanguagemanager.rb
+++ gtksourceview3/sample/sourcelanguagemanager.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/env ruby
 =begin
-  sourcelanguagesmanager.rb - Ruby/GtkSourceView sample script.
+  sourcelanguagesmanager.rb - Ruby/GtkSourceView3 sample script.
 
-  Copyright (c) 2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2006-2014  Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 
   $Id: sourcelanguagesmanager.rb,v 1.3 2007/06/03 02:11:07 mutoh Exp $
 =end
 
-require 'gtksourceview3'
+require "gtksourceview3"
 
-s = Gtk::SourceLanguageManager.new
+s = GtkSource::LanguageManager.new
 s.language_ids.each do |v|
   puts v
 end
