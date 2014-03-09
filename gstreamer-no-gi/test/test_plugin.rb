9bb68368e95f50c4afad24f7b26dc025e08de7df
Kouhei Sutou <kou@clear-code.com>
2014-03-09 18:31:57 +0900

    gstreamer-no-gi: remove deprecated code

--- gstreamer-no-gi/test/test_plugin.rb
+++
@@ -1,6 +0,0 @@
-class TestPlugin < Test::Unit::TestCase
-  def test_source
-    plugin = Gst::Registry.default.find_plugin("playback")
-    assert_equal("gst-plugins-base", plugin.source)
-  end
-end
