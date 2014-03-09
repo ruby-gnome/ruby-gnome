4adbd54ae454bc38811ba18bab12d1784aee8b82
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-09 16:13:32 +0900

    gtisourceview3 test: remove a test
    Because Gtk::SourceView#get_mark_category_background has been deprecated.

--- gtksourceview3/test/test_source_view.rb
+++ gtksourceview3/test/test_source_view.rb
@@ -1,17 +1,23 @@
 # -*- coding: utf-8 -*-
+#
+# Copyright (C) 2011-2014  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 class TestSourceView < Test::Unit::TestCase
-  def test_mark_category_background
-    view = Gtk::SourceView.new
-
-    red_rgb = [65535, 0, 0]
-    red_color = Gdk::Color.parse("red")
-    assert_not_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
-
-    view.set_mark_category_background("XXX", red_color)
-    assert_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
-
-    view.set_mark_category_background("XXX", nil)
-    assert_not_equal(red_rgb, view.get_mark_category_background("XXX").to_a)
+  def setup
+    @view = GtkSource::View.new
   end
 end
