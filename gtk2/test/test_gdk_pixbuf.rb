e9cc336777780b67cd0cc01c510754c43a1dc955
Masafumi Yokoyama <myokoym@gmail.com>
2014-03-29 10:56:01 +0900

    gtk2 test: add a test for `Gdk::Pixbuf.from_drawable`

---
+++ gtk2/test/test_gdk_pixbuf.rb
@@ -0,0 +1,35 @@
+# Copyright (C) 2014  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+class TestGdkPixbuf < Test::Unit::TestCase
+  def test_from_drawable
+    colormap = nil
+    gdk_window = Gtk::Invisible.new.window
+    src_x  = 0
+    src_y  = 0
+    width  = 0
+    height = 0
+
+    assert_nothing_raised do
+      Gdk::Pixbuf.from_drawable(colormap,
+                                gdk_window,
+                                src_x,
+                                src_y,
+                                width,
+                                height)
+    end
+  end
+end
