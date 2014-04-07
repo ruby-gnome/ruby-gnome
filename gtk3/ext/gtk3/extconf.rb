07b4ec72d504b8f62b96657504fc92b9910f3c7f
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-07 18:58:29 +0900

    gtk3: use gtk3-no-gi to test in Travis CI.
    This is workaround. Currently, gtk3 depends gdk3-no-gi.

--- gtk3/ext/gtk3/extconf.rb
+++ gtk3/ext/gtk3/extconf.rb
@@ -33,7 +33,7 @@ end
 
 have_func("rb_errinfo")
 
-["glib2", "atk", "pango", "gdk_pixbuf2", "gdk3"].each do |package|
+["glib2", "atk", "pango", "gdk_pixbuf2", "gdk3-no-gi"].each do |package|
   directory = "#{package}#{version_suffix}"
   build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{package}/#{RUBY_VERSION}"
   add_depend_package(package, "#{directory}/ext/#{package}",
