80c1a3260d882ea5ffa12aa5f46b3de5cf8b46a6
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-13 06:17:30 +0900

    glib2 OSX: add try_compiler_option_strictly
    In recent OSX, default compiler of Command Line Tools 4.0 or higher is clang.
    So, '-Wout-of-line-declaration' causes flood of warnings as follows:
    
    ```
    warning: unknown warning option '-Wunsafe-loop-optimizations'; did you
    mean
          '-Wout-of-line-declaration'? [-Wunknown-warning-option]
    ```

--- glib2/lib/mkmf-gnome2.rb
+++ glib2/lib/mkmf-gnome2.rb
@@ -22,6 +22,12 @@ def try_compiler_option(opt, &block)
   end
 end
 
+def try_compiler_option_strictly(opt, &block)
+  checking_for "#{opt} option to compiler" do
+    $CFLAGS += " #{opt}" if try_compile '', opt + " -Werror", &block
+  end
+end
+
 try_compiler_option '-Wall'
 try_compiler_option '-Waggregate-return'
 try_compiler_option '-Wcast-align'
@@ -52,7 +58,8 @@ try_compiler_option '-Wswitch-enum'
 try_compiler_option '-Wundef'
 # NOTE: Incredible amounts of false positives.
 #try_compiler_option '-Wunreachable-code'
-try_compiler_option '-Wunsafe-loop-optimizations'
+try_compiler_option_strictly '-Wout-of-line-declaration'
+try_compiler_option_strictly '-Wunsafe-loop-optimizations'
 try_compiler_option '-Wwrite-strings'
 
 if /-Wl,--no-undefined/ =~ $LDFLAGS.to_s
