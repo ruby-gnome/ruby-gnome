d10928e8b793916bfde354d2ccc2231881b09fcb
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-21 05:09:42 +0900

    gtksourceview3 test: add GtkSource::LanguageManager test

---
+++ gtksourceview3/test/test_source_language_manager.rb
@@ -0,0 +1,35 @@
+# Copyright (C) 2014  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+class TestLanguageManager < Test::Unit::TestCase
+  def setup
+    @lang = GtkSource::LanguageManager.new
+    @view = GtkSource::View.new
+  end
+
+  def test_language
+    language = @lang.get_language("ruby")
+    @view.buffer.language = language
+    assert_equal(language, @view.buffer.language)
+
+    highlight = true
+    @view.buffer.highlight_syntax = highlight
+    assert_true(@view.buffer.highlight_syntax?)
+
+    @view.buffer.highlight_matching_brackets = highlight
+    assert_true(@view.buffer.highlight_matching_brackets?)
+  end
+end
