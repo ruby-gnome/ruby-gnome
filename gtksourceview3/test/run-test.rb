6a3bb125fff277b64e33bc1dab986c856d4f2e31
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-04-21 03:48:23 +0900

    gtksourceview3 test: add load path

--- gtksourceview3/test/run-test.rb
+++ gtksourceview3/test/run-test.rb
@@ -5,8 +5,10 @@ ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
 
 glib_base = File.join(ruby_gnome2_base, "glib2")
 atk_base = File.join(ruby_gnome2_base, "atk")
+cairo_gobject_base = File.join(ruby_gnome2_base, "cairo-gobject")
 pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
+gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
 gdk3_base = File.join(ruby_gnome2_base, "gdk3")
 gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 gtk_source_view3_base = File.join(ruby_gnome2_base, "gtksourceview3")
@@ -14,7 +16,8 @@ gtk_source_view3_base = File.join(ruby_gnome2_base, "gtksourceview3")
 $LOAD_PATH.unshift(glib_base)
 require 'test/glib-test-init'
 
-[glib_base, atk_base, pango_base, gdk_pixbuf_base, gdk3_base, gtk3_base,
+[glib_base, atk_base, cairo_gobject_base, pango_base, gdk_pixbuf_base,
+ gobject_introspection_base, gdk3_base, gtk3_base,
  gtk_source_view3_base].each do |target|
   if system("which make > /dev/null")
     `make -C #{target.dump} > /dev/null` or exit(1)
