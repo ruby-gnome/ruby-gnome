c20fe1c1aef2eb7276bbe52181bcc32e4cada2dd
Kouhei Sutou <kou@clear-code.com>
2014-04-20 21:59:25 +0900

    cairo-gobject: suppress a warning
        warning: instance variable gboxed not initialized

--- cairo-gobject/ext/cairo-gobject/rb-cairo-gobject.c
+++ cairo-gobject/ext/cairo-gobject/rb-cairo-gobject.c
@@ -27,13 +27,17 @@ static gpointer                                                         \
 prefix ## _robj2instance(VALUE rb_object,                               \
                          G_GNUC_UNUSED gpointer user_data)              \
 {                                                                       \
-    VALUE rb_boxed;                                                     \
+    ID id_gboxed;                                                       \
+    VALUE rb_boxed = Qnil;                                              \
                                                                         \
-    rb_boxed = rb_iv_get(rb_object, "gboxed");                          \
+    CONST_ID(id_gboxed, "gboxed");                                      \
+    if (rb_ivar_defined(rb_object, id_gboxed)) {                        \
+        rb_boxed = rb_ivar_get(rb_object, id_gboxed);                   \
+    }                                                                   \
     if (NIL_P(rb_boxed)) {                                              \
         gpointer cr_object = RVAL2CR(rb_object);                        \
         rb_boxed = rbgobj_make_boxed_default(cr_object, gtype);         \
-        rb_iv_set(rb_object, "gboxed", rb_boxed);                       \
+        rb_ivar_set(rb_object, id_gboxed, rb_boxed);                    \
     }                                                                   \
                                                                         \
     return rbgobj_boxed_get_default(rb_boxed, gtype);                   \
