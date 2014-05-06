d62c1bf1bf1be79a5dfa3ecf25a342c6efe828d7
cosmo0920 <cosmo_originstring@yahoo.co.jp>
2014-05-06 18:27:09 +0900

    gtksourceview3: suppress previous definition warning
    `markup` and `text` are property.

--- gtksourceview3/ext/gtksourceview3/rbgtksourcegutterrenderertext.c
+++ gtksourceview3/ext/gtksourceview3/rbgtksourcegutterrenderertext.c
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2011  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2011-2014  Ruby-GNOME2 Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -31,34 +31,10 @@ rg_initialize(VALUE self)
     return Qnil;
 }
 
-static VALUE
-rg_set_markup(VALUE self, VALUE markup)
-{
-    StringValue(markup);
-    gtk_source_gutter_renderer_text_set_markup(_SELF(self),
-                                               RSTRING_PTR(markup),
-                                               RSTRING_LEN(markup));
-
-    return self;
-}
-
-static VALUE
-rg_set_text(VALUE self, VALUE text)
-{
-    StringValue(text);
-    gtk_source_gutter_renderer_text_set_text(_SELF(self),
-                                             RSTRING_PTR(text),
-                                             RSTRING_LEN(text));
-
-    return self;
-}
-
 void
 Init_gtksource_gutterrenderertext(VALUE mGtkSource)
 {
     VALUE RG_TARGET_NAMESPACE = G_DEF_CLASS(GTK_SOURCE_TYPE_GUTTER_RENDERER_TEXT, "GutterRendererText", mGtkSource);
 
     RG_DEF_METHOD(initialize, 0);
-    RG_DEF_METHOD(set_markup, 1);
-    RG_DEF_METHOD(set_text, 1);
 }
