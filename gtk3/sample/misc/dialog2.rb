0735deaeb39a6f3106a9437904390e7addaf202f
chrono <chrono@lldev.jp>
2014-09-18 13:11:28 +0900

    gtk3 sample: make a sample script gtk3 ready
    gtk3/sample/misc/dialog2.rb:
    
    * modify method calls with a deprecated argument style
    * modify deprecated constant names
    * modify deprecated method names
    * remove Gtk::Dialog::NO_SEPARATOR constant because it is obsolete

--- gtk3/sample/misc/dialog2.rb
+++ gtk3/sample/misc/dialog2.rb
@@ -11,20 +11,21 @@
 require 'gtk3'
 
 window = Gtk::Window.new("Gtk::Dialog sample2")
-button = Gtk::Button.new("Create Dialog")
+button = Gtk::Button.new(:label => "Create Dialog")
 button.signal_connect("clicked") do 
-  dialog = Gtk::Dialog.new("Gtk::Dialog Sample 2", window, 
-                           Gtk::Dialog::MODAL|Gtk::Dialog::NO_SEPARATOR,
-                           [Gtk::Stock::OK, Gtk::Dialog::RESPONSE_OK],
-                           [Gtk::Stock::CANCEL, Gtk::Dialog::RESPONSE_CANCEL])
-  dialog.vbox.add(Gtk::Label.new("Gtk::Dialog Sample 2"))
+  dialog = Gtk::Dialog.new(:title =>"Gtk::Dialog Sample 2",
+                           :parent => window,
+                           :flags => Gtk::Dialog::Flags::MODAL,
+                           :buttons => [[Gtk::Stock::OK, Gtk::ResponseType::OK],
+                           [Gtk::Stock::CANCEL, Gtk::ResponseType::CANCEL]])
+  dialog.child.add(Gtk::Label.new("Gtk::Dialog Sample 2"))
   dialog.set_default_size(300, 300)
-  dialog.vbox.show_all
+  dialog.child.show_all
   result = dialog.run
   case result
-  when Gtk::Dialog::RESPONSE_OK
+  when Gtk::ResponseType::OK
     p "OK"
-  when Gtk::Dialog::RESPONSE_CANCEL
+  when Gtk::ResponseType::CANCEL
     p "Cancel"
   end
   dialog.destroy
