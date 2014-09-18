5eb0fc2383816aa5fa5489b6a9462e3124daf308
Masafumi Yokoyama <myokoym@gmail.com>
2014-09-19 00:30:54 +0900

    gtk3 sample: cleanup

--- gtk3/sample/misc/threads.rb
+++ gtk3/sample/misc/threads.rb
@@ -8,39 +8,39 @@
   $Id: threads.rb,v 1.5 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-require 'gtk3'
-require 'thread'
+require "gtk3"
+require "thread"
 
 label = Gtk::Label.new
 
-Thread.new {
-  (0...1000).each { |cnt|
+Thread.new do
+  (0...1000).each do |cnt|
     p "A:" + cnt.to_s
     label.label = "A:" + cnt.to_s
     sleep(2)
-  }
-}
+  end
+end
 
 start_button = Gtk::Button.new("start")
 stop_button = Gtk::Button.new("stop")
 
 start_button.signal_connect("clicked") do
-  start_button.sensitive=false
-  @th = Thread.new {
-    (0...10).each { |cnt|
+  start_button.sensitive = false
+  @th = Thread.new do
+    (0...10).each do |cnt|
       p "B:" + cnt.to_s
       label.label = "B:" + cnt.to_s
       sleep(2)
-    }
+    end
     @th = nil
     start_button.sensitive = true
     start_button.grab_focus
     stop_button.sensitive = false
-  }
+  end
   stop_button.sensitive = true
   stop_button.grab_focus
 end
-  
+
 stop_button.signal_connect("clicked") do
   if @th
     @th.kill
@@ -54,8 +54,8 @@ end
 
 stop_button.sensitive = false
 
-box = Gtk::VBox.new.
-  set_size_request(100,100)
+box = Gtk::VBox.new
+box.set_size_request(100, 100)
 
 box << label << start_button << stop_button
 
@@ -63,10 +63,8 @@ win = Gtk::Window.new << box
 
 win.show_all.signal_connect("delete_event") do
   p "Exiting..."
-  Gtk.main_quit 
-  Thread.list.each {|t| t.kill }
+  Gtk.main_quit
+  Thread.list.each {|t| t.kill}
 end
 
 Gtk.main
-
-
