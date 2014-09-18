8c29b00ee8c722218c1e775b976c5e0c22e27935
Masafumi Yokoyama <myokoym@gmail.com>
2014-09-18 23:59:33 +0900

    gtk3 sample: fix if statement style

--- gtk3/sample/misc/drawing.rb
+++ gtk3/sample/misc/drawing.rb
@@ -65,12 +65,14 @@ class A < Canvas
                         @last.x, @last.y, ev.x, ev.y)
 
       x1, x2 = if (@last.x < ev.x)
-               then [@last.x, ev.x]
-               else [ev.x,    @last.x]
+                 [@last.x, ev.x]
+               else
+                 [ev.x,    @last.x]
                end
       y1, y2 = if (@last.y < ev.y)
-               then [@last.y, ev.y]
-               else [ev.y,    @last.y]
+                 [@last.y, ev.y]
+               else
+                 [ev.y,    @last.y]
                end
       widget.queue_draw_area(x1, y1, x2 - x1 + 1, y2 - y1 + 1)
     end
