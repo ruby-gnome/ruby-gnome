f164ca5c4de4d18889145e3a4bb2af9d5cb3efaa
Kouhei Sutou <kou@clear-code.com>
2014-10-13 14:03:14 +0900

    cairo-gobject: fix wrong package name on Debian

--- cairo-gobject/ext/cairo-gobject/extconf.rb
+++ cairo-gobject/ext/cairo-gobject/extconf.rb
@@ -74,7 +74,7 @@ end
 setup_win32(module_name, base_dir)
 
 unless required_pkg_config_package(package_id,
-                                   :debian => "libcairo-gobject2",
+                                   :debian => "libcairo2-dev",
                                    :fedora => "cairo-gobject-devel")
   exit(false)
 end
