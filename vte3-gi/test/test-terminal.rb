54c52ea998552360e6e6b98840a44932f2bd59ec
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2014-10-22 22:57:10 +0900

    vte3-gi test: add rewrap_on_resize test
    rewrap_on_resize has been provided since vte 0.36.

--- vte3-gi/test/test-terminal.rb
+++ vte3-gi/test/test-terminal.rb
@@ -33,4 +33,10 @@ class TestTerminal < Test::Unit::TestCase
       @terminal.fork_command(flags, nil, nil)
     end
   end
+
+  def test_rewrap_on_resize
+    omit_if_not_respond(@terminal, "rewrap_on_resize?")
+    @terminal.rewrap_on_resize = false
+    assert_false(@terminal.rewrap_on_resize?)
+  end
 end
