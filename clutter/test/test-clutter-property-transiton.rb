a21326891d2a133c51a96cdfcb5e125619ee0f3d
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2014-11-07 00:00:58 +0900

    clutter test: fix test class name
    naming rule: ClutterXXXTest

--- clutter/test/test-clutter-property-transiton.rb
+++ clutter/test/test-clutter-property-transiton.rb
@@ -14,7 +14,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-class ClutterPropertyTransition < Test::Unit::TestCase
+class ClutterPropertyTransitionTest < Test::Unit::TestCase
   include ClutterTestUtils
 
   def setup
