4d9bbd191370ef794edb696c7ed34f8bed124e5b
Kouhei Sutou <kou@clear-code.com>
2014-11-24 16:57:22 +0900

    gtk3-gi test: use options style

--- gtk3-gi/test/test_gtk_image.rb
+++ gtk3-gi/test/test_gtk_image.rb
@@ -2,7 +2,12 @@ class TestGtkImage < Test::Unit::TestCase
   include GtkTestUtils
 
   def test_stock
-    image = Gtk::Image.new(Gtk::Stock::GO_FORWARD, :dialog)
-    assert_equal(Gtk::Stock::GO_FORWARD.to_s, image.stock)
+    image = Gtk::Image.new(:stock => Gtk::Stock::GO_FORWARD,
+                           :size => :dialog)
+    assert_equal([
+                   Gtk::Stock::GO_FORWARD.to_s,
+                   Gtk::IconSize::DIALOG,
+                 ],
+                 image.stock)
   end
 end
