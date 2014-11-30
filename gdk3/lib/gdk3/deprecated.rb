06b146e650fa797e5afd5e4a14a3a102af021d2c
Masafumi Yokoyama <myokoym@gmail.com>
2014-12-01 00:36:53 +0900

    gdk3: use define_deprecated_const for Gdk::Window::Hints

--- gdk3/lib/gdk3/deprecated.rb
+++ gdk3/lib/gdk3/deprecated.rb
@@ -136,11 +136,7 @@ module Gdk
     define_deprecated_const :AttributesType, "Gdk::WindowAttributesType"
     define_deprecated_const :Edge, "Gdk::WindowEdge"
     define_deprecated_const :FilterReturn, "Gdk::FilterReturn"
-
-    class Hints
-      extend GLib::Deprecatable
-      define_deprecated_flags "Gdk::WindowHints"
-    end
+    define_deprecated_const :Hints, "Gdk::WindowHints"
 
     class ModifierType
       extend GLib::Deprecatable
