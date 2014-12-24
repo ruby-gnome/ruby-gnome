e1888771217afb7da5d7b22522f1837eaded3dfb
Kouhei Sutou <kou@clear-code.com>
2014-12-24 23:56:42 +0900

    gtk3: remove "-gi"

--- gtk3/test/run-test.rb
+++ gtk3/test/run-test.rb
@@ -27,7 +27,7 @@ gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
 gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
 gio2_base = File.join(ruby_gnome2_base, "gio2")
 gdk3_base = File.join(ruby_gnome2_base, "gdk3")
-gtk3_gi_base = File.join(ruby_gnome2_base, "gtk3-gi")
+gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 
 [
   [glib_base, "glib2"],
@@ -38,7 +38,7 @@ gtk3_gi_base = File.join(ruby_gnome2_base, "gtk3-gi")
   [gobject_introspection_base, "gobject-introspection"],
   [gio2_base, "gio2"],
   [gdk3_base, "gdk3"],
-  [gtk3_gi_base, "gtk3-gi"]
+  [gtk3_base, "gtk3"]
 ].each do |target, module_name|
   if File.exist?(File.join(target, "Makefile"))
     if system("which make > /dev/null")
@@ -52,9 +52,9 @@ end
 $LOAD_PATH.unshift(File.join(glib_base, "test"))
 require 'glib-test-init'
 
-$LOAD_PATH.unshift(File.join(gtk3_gi_base, "test"))
+$LOAD_PATH.unshift(File.join(gtk3_base, "test"))
 require 'gtk-test-utils'
 
-require 'gtk3-gi'
+require 'gtk3'
 
 exit Test::Unit::AutoRunner.run(true)
