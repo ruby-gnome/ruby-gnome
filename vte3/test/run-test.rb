e0217d5c1305d24d24af83fa672e940b3e041a54
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-02 07:56:50 +0900

    vte3 test: add load path for gio2

--- vte3/test/run-test.rb
+++ vte3/test/run-test.rb
@@ -25,6 +25,7 @@ pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
 cairo_gobject_base = File.join(ruby_gnome2_base, "cairo-gobject")
 gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
+gio2_base = File.join(ruby_gnome2_base, "gio2")
 gdk3_base = File.join(ruby_gnome2_base, "gdk3")
 gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 vte3_base = File.join(ruby_gnome2_base, "vte3")
@@ -36,6 +37,7 @@ modules = [
   [cairo_gobject_base, "cairo-gobject"],
   [gdk_pixbuf_base, "gdk_pixbuf2"],
   [gobject_introspection_base, "gobject-introspection"],
+  [gio2_base, "gio2"],
   [gdk3_base, "gdk3"],
   [gtk3_base, "gtk3"],
   [vte3_base, "vte3"],
