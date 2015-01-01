1f5674e5c6dda18e4b2cd192873ccbb7ee9a5d8e
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-01 23:26:04 +0900

    gtksourceview3 test: Add load path for Gio2

--- gtksourceview3/test/run-test.rb
+++ gtksourceview3/test/run-test.rb
@@ -9,6 +9,7 @@ cairo_gobject_base = File.join(ruby_gnome2_base, "cairo-gobject")
 pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
 gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
+gio2_base = File.join(ruby_gnome2_base, "gio2")
 gdk3_base = File.join(ruby_gnome2_base, "gdk3")
 gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 gtk_source_view3_base = File.join(ruby_gnome2_base, "gtksourceview3")
@@ -23,6 +24,7 @@ dependencies = [
   pango_base,
   gdk_pixbuf_base,
   gobject_introspection_base,
+  gio2_base,
   gdk3_base,
   gtk3_base,
   gtk_source_view3_base,
