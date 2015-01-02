701d5f9d335210f8d1cce99610ff38f8062d7c1c
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-02 11:15:25 +0900

    gtk3 test: Add for Gtk::EventController tests
    But now, Gtk::EventController#handle_event does not work.... :<

---
+++ gtk3/test/test_gtk_event_controller.rb
@@ -0,0 +1,39 @@
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+class TestGtkEventController < Test::Unit::TestCase
+  include GtkTestUtils
+
+  def setup
+    @gesture = Gtk::GestureSingle.new
+  end
+
+  def test_propergation_phase
+    @gesture.propagation_phase = :capture
+    assert_equal(Gtk::PropagationPhase::CAPTURE, @gesture.propagation_phase)
+  end
+
+  def test_handle_event
+    touch_update_event = Gdk::EventTouch.new(:touch_update)
+    assert_true(@gesture.handle_event(touch_update_event))
+  end
+
+  class TestEnum < self
+    def test_enum_propergation_phase
+      assert_const_defined(Gtk::PropagationPhase, :BUBBLE)
+    end
+  end
+end
