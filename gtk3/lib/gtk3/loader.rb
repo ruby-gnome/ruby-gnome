0c03946441421ade73d8d02de90e83b8aa8cec0f
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-01-07 23:08:10 +0900

    gtk3: Implement Gtk::StyleProperties#get_properties method
    Or, more sophisticated method is required here? :<

--- gtk3/lib/gtk3/loader.rb
+++ gtk3/lib/gtk3/loader.rb
@@ -68,6 +68,7 @@ module Gtk
       require "gtk3/search-bar"
       require "gtk3/spin-button"
       require "gtk3/stack"
+      require "gtk3/style-properties"
       require "gtk3/text-buffer"
       require "gtk3/tree-iter"
       require "gtk3/tree-selection"
---
+++ gtk3/lib/gtk3/style-properties.rb
@@ -0,0 +1,29 @@
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module Gtk
+  class StyleProperties
+    alias_method :get_property_raw, :get_property
+    def get_property(key, state)
+      found, property = get_property_raw(key, state)
+      if found
+        property.value
+      else
+        nil
+      end
+    end
+  end
+end
