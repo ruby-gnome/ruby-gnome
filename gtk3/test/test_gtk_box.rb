af34a7e8fb692b0c187082370b799eb69715d36b
Kouhei Sutou <kou@clear-code.com>
2015-01-11 16:08:34 +0900

    gtk3: Box.new accept nil as spacing

--- gtk3/test/test_gtk_box.rb
+++ gtk3/test/test_gtk_box.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2014  Ruby-GNOME2 Project Team
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -14,27 +14,15 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-module Gtk
-  class Box
-    alias_method :initialize_raw, :initialize
-    def initialize(orientation, spacing=0)
-      initialize_raw(orientation, spacing || 0)
-    end
+class TestGtkBox < Test::Unit::TestCase
+  include GtkTestUtils
 
-    alias_method :pack_start_raw, :pack_start
-    def pack_start(child, options={})
-      expand  = options[:expand]  || false
-      fill    = options[:fill]    || false
-      padding = options[:padding] || 0
-      pack_start_raw(child, expand, fill, padding)
-    end
-    
-    alias_method :pack_end_raw, :pack_end
-    def pack_end(child, options={})
-      expand  = options[:expand]  || false
-      fill    = options[:fill]    || false
-      padding = options[:padding] || 0
-      pack_end_raw(child, expand, fill, padding)
+  sub_test_case ".new" do
+    sub_test_case "spacing" do
+      def test_nil
+        box = Gtk::Box.new(:vertical, nil)
+        assert_equal(0, box.spacing)
+      end
     end
   end
 end
