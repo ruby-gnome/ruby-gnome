07bc9627157565ecec114380339551c30cb1f1b8
Kouhei Sutou <kou@clear-code.com>
2015-01-11 20:29:55 +0900

    gtk3 test: group Builder#add_from_string

--- gtk3/test/test_gtk_builder.rb
+++ gtk3/test/test_gtk_builder.rb
@@ -19,12 +19,14 @@ class TestGtkBuilder < Test::Unit::TestCase
     assert_kind_of(Gtk::Dialog, builder["dialog1"])
   end
 
-  def test_add_from_string
+  sub_test_case "#add_from_string" do
+  def test_ascii_only
     builder = Gtk::Builder.new
     assert_nothing_raised do
       builder.add_from_string(ui_definition)
     end
     assert_kind_of(Gtk::Box, builder["vbox1"])
+  end
   end
 
   def test_add_with_file
