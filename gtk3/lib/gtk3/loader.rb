78cd0f62bfed95a7a1449f8f7fd8eda409446b4f
Kouhei Sutou <kou@clear-code.com>
2015-02-22 14:57:37 +0900

    gtk3: make Dialog#run returns response type enum value

--- gtk3/lib/gtk3/loader.rb
+++ gtk3/lib/gtk3/loader.rb
@@ -75,6 +75,7 @@ module Gtk
       require "gtk3/builder"
       require "gtk3/container"
       require "gtk3/css-provider"
+      require "gtk3/dialog"
       require "gtk3/icon-theme"
       require "gtk3/image"
       require "gtk3/label"
