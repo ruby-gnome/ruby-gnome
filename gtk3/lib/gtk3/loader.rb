851b4ec93a2241a45aa12075d270fef7e42da4fe
Kouhei Sutou <kou@clear-code.com>
2015-03-09 22:51:46 +0900

    gtk3: support Hash style constructor for Gtk::ToolButton
    GitHub: fix #320
    
    Reported by cedlemo. Thanks!!!

--- gtk3/lib/gtk3/loader.rb
+++ gtk3/lib/gtk3/loader.rb
@@ -90,6 +90,7 @@ module Gtk
       require "gtk3/style-context"
       require "gtk3/style-properties"
       require "gtk3/text-buffer"
+      require "gtk3/tool-button"
       require "gtk3/tree-iter"
       require "gtk3/tree-model"
       require "gtk3/tree-selection"
