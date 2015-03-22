ec96b7afed89d7aa2f7d0614f9a0db0873661072
Kouhei Sutou <kou@clear-code.com>
2015-03-22 11:57:19 +0900

    gtk3: fix wrong enum object name

--- gtk3/lib/gtk3/deprecated.rb
+++ gtk3/lib/gtk3/deprecated.rb
@@ -473,7 +473,7 @@ module Gtk
 
   class IconTheme
     extend GLib::Deprecatable
-    define_deprecated_flags :LookupFlags, 'LOOKUP'
+    define_deprecated_flags :IconLookupFlags, 'LOOKUP'
   end
 
   class IconView
