07387f437fbaf922b6b84f438814198168a6612a
cedlemo <cedlemo@gmx.com>
2015-03-22 19:38:20 +0100

    update to gtk3 api  and update copyright in expander.rb

--- gtk3/sample/misc/expander.rb
+++ gtk3/sample/misc/expander.rb
@@ -2,13 +2,11 @@
 =begin
   expander.rb - Ruby/GTK2 sample script.
 
-  Copyright (c) 2004-2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2004-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-
-  $Id: expander.rb,v 1.3 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-require 'gtk3'
+require "gtk3"
 
 expander = Gtk::Expander.new("Expander Sample")
 expander.expanded = true
@@ -21,7 +19,10 @@ end
 button.set_size_request(200, 200)
 expander.add(button)
 
-Gtk::Window.new.add(expander).show_all.signal_connect("destroy"){Gtk.main_quit}
+win = Gtk::Window.new
+win.add(expander)
+win.show_all
+win.signal_connect("destroy"){Gtk.main_quit}
 
 Gtk.main
 
