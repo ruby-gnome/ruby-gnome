04623a959d86b43c71192277ae8591ca3a056e81
cedlemo <cedlemo@gmx.com>
2015-03-22 19:31:49 +0100

    really update copyright in filechosser.rb

--- gtk3/sample/misc/filechooser.rb
+++ gtk3/sample/misc/filechooser.rb
@@ -2,7 +2,7 @@
 =begin
   filechooser.rb - Ruby/GTK2 sample script.
 
-  Copyright (c) 2004-2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2004-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 =end
 
