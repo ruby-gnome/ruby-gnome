2c40b803e91cd9c1da883a9052c872d2c85e06e2
cedlemo <cedlemo@gmx.com>
2015-04-04 18:56:10 +0200

    update to gtk3 api  and update copyright in toolbar.rb

--- gtk3/sample/misc/toolbar.rb
+++ gtk3/sample/misc/toolbar.rb
@@ -1,54 +1,63 @@
 #!/usr/bin/env ruby
 =begin
-  toolbar.rb - Ruby/GTK2 sample script.
+  toolbar.rb - Ruby/GTK3 sample script.
 
-  Copyright (c) 2002-2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-
-  $Id: toolbar.rb,v 1.11 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-require 'gtk3'
+require "gtk3"
 
+test_xpm_file = "#{File.expand_path(File.dirname(__FILE__))}/test.xpm"
 window = Gtk::Window.new("Gtk::Toolbar sample")
 
 window.signal_connect("destroy") do
   Gtk.main_quit
 end
+window.set_default_size 600, 600
 
 toolbar = Gtk::Toolbar.new
 
-toolbar.append("Horizontal", "Horizontal toolbar layout", "Toolbar/Horizontal",
-  Gtk::Image.new("test.xpm")) do
-  toolbar.orientation = Gtk::ORIENTATION_HORIZONTAL
-end
-toolbar.append("Vertical", "Vertical toolbar layout", "Toolbar/Vertical",
-  Gtk::Image.new("test.xpm")) do
-  toolbar.orientation = Gtk::ORIENTATION_VERTICAL
-end
-toolbar.append_space
-toolbar.append("Icons", "Only show toolbar icons", "Toolbar/IconsOnly",
-  Gtk::Image.new("test.xpm")) do
-  toolbar.toolbar_style = Gtk::Toolbar::ICONS
-end
-toolbar.append("Text", "Only show toolbar text", "Toolbar/TextOnly",
-  Gtk::Image.new("test.xpm")) do
-  toolbar.toolbar_style = Gtk::Toolbar::TEXT
-end
-toolbar.append("Both", "Show toolbar icons and text", "Toolbar/Both",
-  Gtk::Image.new("test.xpm")) do
-  toolbar.toolbar_style = Gtk::Toolbar::BOTH
-end
-toolbar.append_space
-toolbar.append("Enable", "Enable tooltips",nil,
-  Gtk::Image.new("test.xpm")) do
-  toolbar.tooltips = true
-end
-toolbar.append("Disable", "Disable tooltips",nil,
-  Gtk::Image.new("test.xpm")) do
-  toolbar.tooltips = false
+button_horizontal = Gtk::ToolButton.new(:icon_widget => Gtk::Image.new(:file => test_xpm_file),
+                                        :label => "Horizontal toolbar layout")
+button_horizontal.tooltip_text = "Toolbar/Horizontal"
+button_horizontal.signal_connect "clicked" do 
+  toolbar.orientation = :horizontal
 end
 
+button_vertical = Gtk::ToolButton.new(:icon_widget => Gtk::Image.new(:file => test_xpm_file),
+                                      :label => "Vertical toolbar layout")
+button_vertical.tooltip_text = "Toolbar/Vertical"
+button_vertical.signal_connect "clicked" do 
+  toolbar.orientation = :vertical
+end
+
+button_icons_only = Gtk::ToolButton.new(:icon_widget => Gtk::Image.new(:file => test_xpm_file),
+                                        :label => "Only show toolbar icons")
+button_icons_only.tooltip_text = "Toolbar/IconsOnly"
+button_icons_only.signal_connect "clicked" do
+  toolbar.style = Gtk::ToolbarStyle::ICONS
+end
+
+button_text_only =  Gtk::ToolButton.new(:icon_widget => Gtk::Image.new(:file => test_xpm_file),
+                                        :label => "Only show toolbar text")
+button_text_only.tooltip_text = "Toolbar/TextOnly"
+button_text_only.signal_connect "clicked" do
+  toolbar.style = Gtk::ToolbarStyle::TEXT
+end
+
+button_both = Gtk::ToolButton.new(:icon_widget => Gtk::Image.new(:file => test_xpm_file),
+                                  :label => "Show toolbar icons and text")
+button_both.tooltip_text = "Toolbar/Both"
+button_both.signal_connect "clicked" do 
+  toolbar. style = Gtk::ToolbarStyle::BOTH
+end
+
+toolbar.insert(button_both, 0)
+toolbar.insert(button_text_only, 0)
+toolbar.insert(button_icons_only, 0)
+toolbar.insert(button_vertical, 0)
+toolbar.insert(button_horizontal, 0)
 window.add(toolbar)
 window.show_all
 
