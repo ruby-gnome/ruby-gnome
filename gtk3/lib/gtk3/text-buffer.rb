bf9221b83af23ecb78eae2580571eab90a4bf155
Kouhei Sutou <kou@clear-code.com>
2015-04-05 13:16:23 +0900

    gtk3: fix variable name

--- gtk3/lib/gtk3/text-buffer.rb
+++ gtk3/lib/gtk3/text-buffer.rb
@@ -78,7 +78,7 @@ module Gtk
         when TextChildAnchor
           insert_text_child_anchor_raw(iter, target)
         else
-          insert_raw(iter, text, text.bytesize)
+          insert_raw(iter, target, target.bytesize)
         end
       end
 
