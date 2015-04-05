1ef1395179cb155d86e707d017bcd4922956268d
Kouhei Sutou <kou@clear-code.com>
2015-04-05 12:50:16 +0900

    gtk3 test: add a test for Gtk::TreePath#indices

--- gtk3/test/test_gtk_tree_path.rb
+++ gtk3/test/test_gtk_tree_path.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2010-2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 class TestGtkTreePath < Test::Unit::TestCase
   include GtkTestUtils
 
@@ -11,6 +27,12 @@ class TestGtkTreePath < Test::Unit::TestCase
     assert_equal(tree_path("0"), tree_path("0"))
     assert_not_equal(tree_path("0"), tree_path("1"))
     assert_not_equal(tree_path("0"), "0")
+  end
+
+  def test_indices
+    indices, depth = tree_path("2").indices
+    assert_equal([[2], 1],
+                 [indices, depth])
   end
 
   private
