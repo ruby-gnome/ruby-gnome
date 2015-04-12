5511884db8afc896d2a5585935fce2a411c34b59
Kouhei Sutou <kou@clear-code.com>
2015-04-12 14:08:01 +0900

    gtksourceview3: sink newly created floating object

--- gtksourceview3/lib/gtksourceview3/loader.rb
+++ gtksourceview3/lib/gtksourceview3/loader.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2014  Ruby-GNOME2 Project Team
+# Copyright (C) 2014-2015  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -33,6 +33,12 @@ module GtkSource
 
     def require_libraries
       require "gtksourceview3/deprecated"
+    end
+
+    def initialize_post(object)
+      super
+      return unless object.is_a?(GLib::Object)
+      self.class.reference_gobject(object, :sink => true)
     end
   end
 end
