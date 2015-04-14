fe9ce830eca44e87ca4926476341b4260a115e31
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2015-04-15 08:47:39 +0900

    clutter: add a missing version module

---
+++ clutter/lib/clutter/version.rb
@@ -0,0 +1,28 @@
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module Clutter
+  module Version
+    STRING = [MAJOR, MINOR, MICRO].join(".")
+
+    class << self
+      def or_later?(major, minor, micro=nil)
+        micro ||= 0
+        Clutter.check_version?(major, minor, micro)
+      end
+    end
+  end
+end
