6e0b12fd26c03a024ca359f5bd8259d25d0ae287
Kouhei Sutou <kou@clear-code.com>
2015-04-19 12:13:14 +0900

    gtk3: CentOS 7 has gtk3-devel

--- gtk3/ext/gtk3/extconf.rb
+++ gtk3/ext/gtk3/extconf.rb
@@ -65,7 +65,7 @@ setup_windows(module_name, base_dir)
 
 unless required_pkg_config_package(package_id,
                                    :debian => "libgtk-3-dev",
-                                   :fedora => "gtk3-devel",
+                                   :redhat => "gtk3-devel",
                                    :homebrew => "gtk+3",
                                    :macports => "gtk3")
   exit(false)
