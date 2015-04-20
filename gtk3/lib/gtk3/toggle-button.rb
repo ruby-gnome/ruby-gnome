3b4a1f24f6f1c92dad7b4a70d44347dab7ee5f45
cedlemo <cedlemo@gmx.com>
2015-04-20 18:53:44 +0200

    use hash argument for Gtk::ToggleButton#new

---
+++ gtk3/lib/gtk3/toggle-button.rb
@@ -0,0 +1,33 @@
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+module Gtk
+  class ToggleButton
+    alias_method :initialize_raw, :initialize
+    def initialize(options={})
+      label    = options[:label]
+      mnemonic = options[:mnemonic]
+      if options.empty?
+        initialize_raw
+      elsif label
+        mnemonic ? initialize_new_with_mnemonic(label) :
+                   initialize_new_with_label(label)
+      else
+        raise ArgumentError, "Invalid arguments."
+      end
+    end
+  end
+end
