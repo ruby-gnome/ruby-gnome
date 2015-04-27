9f623493c104cc1da1d562d79c1890f1b05359ca
Kouhei Sutou <kou@clear-code.com>
2015-04-27 20:36:59 +0900

    gdk3: define Gdk::Window#size

--- gdk3/lib/gdk3/deprecated.rb
+++ gdk3/lib/gdk3/deprecated.rb
@@ -176,6 +176,9 @@ module Gdk
     define_deprecated_method :shape_combine_mask, :warn => "Don't use this method."
     define_deprecated_method :input_shape_combine_mask, :warn => "Don't use this method."
     define_deprecated_method :set_back_pixmap, :warn => "Don't use this method."
+    define_deprecated_method :size, :warn => "Use #width and #height instead." do |_self|
+      [_self.width, _self.height]
+    end
   end
 
   class WindowAttr
