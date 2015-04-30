5571ad1ce22f5c0acd8b64f02fb6ff90598d4a8b
Kouhei Sutou <kou@clear-code.com>
2015-04-30 19:51:32 +0900

    gtk3 test: add tests for Gtk::Widget#drag_dest_set

--- gtk3/test/test-gtk-widget.rb
+++ gtk3/test/test-gtk-widget.rb
@@ -21,4 +21,26 @@ class TestGtkWidget < Test::Unit::TestCase
       @widget.override_background_color(:normal, nil)
     end
   end
+
+  sub_test_case "#drag_dest_set" do
+    test "targets - TargetEntry" do
+      target = "text/uri-list"
+      info = 0
+      @widget.drag_dest_set(:all,
+                            [Gtk::TargetEntry.new(target, 0, info)],
+                            :copy)
+      list = @widget.drag_dest_get_target_list
+      assert_equal(info, list.find(target))
+    end
+
+    test "targets - Array" do
+      target = "text/uri-list"
+      info = 0
+      @widget.drag_dest_set(:all,
+                            [[target, 0, info]],
+                            :copy)
+      list = @widget.drag_dest_get_target_list
+      assert_equal(info, list.find(target))
+    end
+  end
 end
