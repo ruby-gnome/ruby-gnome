abab291faa5cd291d0906e40c9393983ecdcd403
cedlemo <cedlemo@gmx.com>
2015-05-03 18:52:42 +0200

    update to gtk3 api  and update copyright in button.rb

--- gtk3/sample/misc/button.rb
+++ gtk3/sample/misc/button.rb
@@ -2,10 +2,8 @@
 =begin
   button.rb - Ruby/GTK sample script.
 
-  Copyright (c) 2002-2006 Ruby-GNOME2 Project Team 
+  Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-
-  $Id: button.rb,v 1.10 2006/06/17 13:18:12 mutoh Exp $
 =end
 
 require "gtk3"
@@ -19,18 +17,18 @@ end
 box1 = Gtk::Box.new(:vertical, 0)
 window.add(box1)
 
-table = Gtk::Table.new(3, 3, false)
-table.set_row_spacings(5)
-table.set_column_spacings(5)
-table.set_border_width(10)
-box1.pack_start(table, :expand => true, :fill => true, :padding => 0)
+grid = Gtk::Grid.new
+grid.set_row_spacing(5)
+grid.set_column_spacing(5)
+grid.set_border_width(10)
+box1.pack_start(grid, :expand => true, :fill => true, :padding => 0)
 
 button = []
 0.upto(8) do |i|
-  button.push Gtk::Button.new(:label => "button"+(i+1).to_s)
+  button.push Gtk::Button.new(:label => "button" + (i + 1).to_s)
 end
 0.upto(8) do |i|
-  button[i].signal_connect("clicked") do |w|
+  button[i].signal_connect("clicked") do
     p [i]
     j = (i + 1) % 9
     if button[j].visible?
@@ -41,15 +39,16 @@ end
   end
   button[i].show
 end
-table.attach(button[0], 0, 1, 0, 1, nil, nil, 0, 0)
-table.attach(button[1], 1, 2, 1, 2, nil, nil, 0, 0)
-table.attach(button[2], 2, 3, 2, 3, nil, nil, 0, 0)
-table.attach(button[3], 0, 1, 2, 3, nil, nil, 0, 0)
-table.attach(button[4], 2, 3, 0, 1, nil, nil, 0, 0)
-table.attach(button[5], 1, 2, 2, 3, nil, nil, 0, 0)
-table.attach(button[6], 1, 2, 0, 1, nil, nil, 0, 0)
-table.attach(button[7], 2, 3, 1, 2, nil, nil, 0, 0)
-table.attach(button[8], 0, 1, 1, 2, nil, nil, 0, 0)
+
+grid.attach(button[0], 0, 0, 1, 1)
+grid.attach(button[1], 1, 1, 1, 1)
+grid.attach(button[2], 2, 2, 1, 1)
+grid.attach(button[3], 0, 2, 1, 1)
+grid.attach(button[4], 2, 0, 1, 1)
+grid.attach(button[5], 1, 1, 1, 1)
+grid.attach(button[6], 1, 0, 1, 1)
+grid.attach(button[7], 2, 1, 1, 1)
+grid.attach(button[8], 0, 1, 1, 1)
 
 separator = Gtk::Separator.new(:vertical)
 box1.pack_start(separator, :expand => false, :fill => true, :padding => 0)
