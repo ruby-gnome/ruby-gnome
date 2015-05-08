cf9a147afd52d918766eef350b3b6c9f18850dfc
cedlemo <cedlemo@gmx.com>
2015-05-08 14:21:21 +0200

    add a new sample illustrating the use of Gtk::Application

---
+++ gtk3/sample/misc/application.rb
@@ -0,0 +1,31 @@
+#!/usr/bin/env ruby
+=begin
+  application.rb - Ruby/GTK version of the example-1.c at
+  https://developer.gnome.org/gtk3/stable/gtk-getting-started.html
+  Copyright (c) 2015  Ruby-GNOME2 Project Team
+  This program is licenced under the same licence as Ruby-GNOME2.
+=end
+
+require "gtk3"
+myapp = Gtk::Application.new("org.gtk.example", 0)
+
+myapp.signal_connect "activate" do |app|
+  win = Gtk::ApplicationWindow.new(app)
+  win.set_title("window")
+  win.set_default_size 200, 200
+
+  button_box = Gtk::ButtonBox.new(:horizontal)
+  win.add(button_box)
+
+  button = Gtk::Button.new(:label => "Hello World")
+  button.signal_connect "clicked" do
+    puts "Hello World"
+    win.destroy
+  end
+
+  button_box.add(button)
+
+  win.show_all
+end
+
+myapp.run
