aae36bc9ffc6fe2e6210e87bccdb191157f4c185
cedlemo <cedlemo@gmx.com>
2015-05-15 18:40:52 +0200

    Update to Gtk3 api and update copyright in misc_button.rb

--- gtk3/sample/misc/misc_button.rb
+++ gtk3/sample/misc/misc_button.rb
@@ -1,35 +1,32 @@
 #!/usr/bin/env ruby
 =begin
   misc_button.rb - Ruby/GTK sample script.
-                                                                                
-  Copyright (c) 2004-2006 Ruby-GNOME2 Project Team
+
+  Copyright (c) 2004-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-                                                                                
-  $Id: misc_button.rb,v 1.2 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-require 'gtk3'
+require "gtk3"
 
 box = Gtk::Box.new(:vertical)
 
-#Gtk::ColorButton
+# Gtk::ColorButton
 colorbutton = Gtk::ColorButton.new
 colorbutton.signal_connect("color-set") do
-  p colorbutton.color.to_a
+  p colorbutton.color.to_s
 end
 box.add(colorbutton)
 
-
-#Gtk::FontButton
+# Gtk::FontButton
 fontbutton = Gtk::FontButton.new
 fontbutton.signal_connect("font-set") do
   p fontbutton.font_name
 end
 box.add(fontbutton)
 
-#Gtk::FileChooserButton
-filebutton = Gtk::FileChooserButton.new("Gtk::FileChooserButton", 
-                                    Gtk::FileChooser::Action::OPEN)
+# Gtk::FileChooserButton
+filebutton = Gtk::FileChooserButton.new("Gtk::FileChooserButton",
+                                    Gtk::FileChooserAction::OPEN)
 filebutton.filename = GLib.home_dir
 filebutton.signal_connect("current-folder-changed") do |w, e|
   p filebutton.filename
@@ -37,13 +34,17 @@ end
 
 box.add(filebutton)
 
-#Quit
+# Quit
 quitbutton = Gtk::Button.new(:label => "Quit")
 quitbutton.signal_connect("clicked") do
   Gtk.main_quit
 end
 box.add(quitbutton)
 
-Gtk::Window.new.add(box).set_default_size(200, 100).show_all
+win = Gtk::Window.new
+win.add(box)
+win.set_default_size(200, 100)
+win.show_all
+win.signal_connect("delete-event") { Gtk.main_quit }
 
 Gtk.main
