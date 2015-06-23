5a9b0ead32060dba3ee86a362ff4cd37f754e7f1
cedlemo <cedlemo@gmx.com>
2015-06-23 18:52:59 +0200

    Improve test for Gtk::Image#new

--- gtk3/test/test-gtk-image.rb
+++ gtk3/test/test-gtk-image.rb
@@ -1,35 +1,50 @@
 class TestGtkImage < Test::Unit::TestCase
   include GtkTestUtils
 
-  def test_stock
-    image = Gtk::Image.new(:stock => Gtk::Stock::GO_FORWARD,
-                           :size => :dialog)
-    assert_equal([
-                   Gtk::Stock::GO_FORWARD.to_s,
-                   Gtk::IconSize::DIALOG,
-                 ],
-                 image.stock)
-  end
+  sub_test_case ".new" do
+    test "stock" do
+      image = Gtk::Image.new(:stock => Gtk::Stock::GO_FORWARD,
+                             :size => :dialog)
+      assert_equal([
+                     Gtk::Stock::GO_FORWARD.to_s,
+                     Gtk::IconSize::DIALOG,
+                   ],
+                   image.stock)
+    end
 
-  def test_icon_name
-    icon_name = "dialog-password"
-    image = Gtk::Image.new(:icon_name => icon_name,
-                           :size => Gtk::IconSize::DIALOG)
-    assert_equal([
-                   icon_name,
-                   Gtk::IconSize::DIALOG,
-                 ],
-                 image.icon_name)
-  end
+    test "icon_name" do
+      icon_name = "dialog-password"
+      image = Gtk::Image.new(:icon_name => icon_name,
+                             :size => Gtk::IconSize::DIALOG)
+      assert_equal([
+                     icon_name,
+                     Gtk::IconSize::DIALOG,
+                   ],
+                   image.icon_name)
+    end
 
-  def test_icon
-    icon = Gio::ThemedIcon.new("gtk-open")
-    image = Gtk::Image.new(:icon => icon,
-                           :size => :dialog)
-    assert_equal([
-                   icon,
-                   Gtk::IconSize::DIALOG,
-                 ],
-                 image.icon)
+    test "icon" do
+      icon = Gio::ThemedIcon.new("gtk-open")
+      image = Gtk::Image.new(:icon => icon,
+                             :size => :dialog)
+      assert_equal([
+                     icon,
+                     Gtk::IconSize::DIALOG,
+                   ],
+                   image.icon)
+    end
+    test "resource" do
+      resource = Gio::Resource.load(fixture_path("image.gresource"))
+      Gio::Resources.register(resource)
+      begin
+        resource_path = "/org/ruby/gnome/gnome-logo-icon.png"
+        image = Gtk::Image.new( :resource => resource_path,
+                                :size => :dialog)
+        assert_equal( resource_path,
+                      image.resource)
+      ensure
+        Gio::Resources.unregister(resource)
+      end
+    end
   end
 end
