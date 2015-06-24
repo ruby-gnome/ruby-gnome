8412c3a57c6d4856b72c3ae32a18124838131dae
Kouhei Sutou <kou@clear-code.com>
2015-06-24 21:29:20 +0900

    gtk3 sample: GTK+ functions must be called from main thread
    GDK's thread documentation says:
    
    > GTK+, however, is not thread safe. You should only use GTK+ and GDK
    > from the thread gtk_init() and gtk_main() were called on. This is
    > usually referred to as the "main thread".
    
    https://developer.gnome.org/gdk3/stable/gdk3-Threads.html#gdk3-Threads.description

--- gtk3/sample/misc/threads.rb
+++ gtk3/sample/misc/threads.rb
@@ -9,12 +9,50 @@
 require "gtk3"
 require "thread"
 
+class GtkJobQueue
+  def initialize
+    @queue = Queue.new
+    @worker_id = nil
+  end
+
+  def push(&job)
+    @queue << job
+    if @worker_id.nil?
+      start_worker
+    end
+  end
+
+  def stop
+    return if @worker_id.nil?
+    GLib::Source.remove(@worker_id)
+    @worker_id = nil
+  end
+
+  private
+  def start_worker
+    @worker_id = GLib::Idle.add do
+      job = @queue.pop
+      job.call
+      if @queue.empty?
+        @worker_id = nil
+        GLib::Source::REMOVE
+      else
+        GLib::Source::CONTINUE
+      end
+    end
+  end
+end
+
+job_queue = GtkJobQueue.new
+
 label = Gtk::Label.new
 
 Thread.new do
   (0...1000).each do |cnt|
     p "A:" + cnt.to_s
-    label.label = "A:" + cnt.to_s
+    job_queue.push do
+      label.label = "A:" + cnt.to_s
+    end
     sleep(2)
   end
 end
@@ -27,13 +65,17 @@ start_button.signal_connect("clicked") do
   @th = Thread.new do
     (0...10).each do |cnt|
       p "B:" + cnt.to_s
-      label.label = "B:" + cnt.to_s
+      job_queue.push do
+        label.label = "B:" + cnt.to_s
+      end
       sleep(2)
     end
     @th = nil
-    start_button.sensitive = true
-    start_button.grab_focus
-    stop_button.sensitive = false
+    job_queue.push do
+      start_button.sensitive = true
+      start_button.grab_focus
+      stop_button.sensitive = false
+    end
   end
   stop_button.sensitive = true
   stop_button.grab_focus
@@ -61,6 +103,7 @@ win = Gtk::Window.new << box
 
 win.show_all.signal_connect("delete_event") do
   p "Exiting..."
+  job_queue.stop
   Gtk.main_quit
   Thread.list.each { |t| t.kill if t != Thread.main }
 end
