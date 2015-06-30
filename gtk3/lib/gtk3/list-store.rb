ef411b70be158b1dbee2b283a411ab4aba2ca79f
cedlemo <cedlemo@gmx.com>
2015-06-30 18:50:27 +0200

    Remove trailing whitespace

--- gtk3/lib/gtk3/list-store.rb
+++ gtk3/lib/gtk3/list-store.rb
@@ -49,14 +49,14 @@ module Gtk
       setup_iter(iter)
       iter
     end
-  
+
     alias_method :prepend_raw, :prepend
     def prepend
       iter = prepend_raw
       setup_iter(iter)
       iter
     end
-    
+
     alias_method :insert_raw, :insert
     def insert(index)
       iter = insert_raw(index)
