839b2a5cddac3b40c9017ae0e87b788956ed14f0
cedlemo <cedlemo@gmx.com>
2015-07-03 18:56:55 +0200

    Update Copyright and fix style issue in tree_combo.rb

--- gtk3/sample/misc/tree_combo.rb
+++ gtk3/sample/misc/tree_combo.rb
@@ -1,15 +1,13 @@
 =begin
-  tree_combo.rb - Gtk::TreeView and Gtk::CellRendererCombo 
+  tree_combo.rb - Gtk::TreeView and Gtk::CellRendererCombo
   sample script.
 
-  Copyright (C) 2006 Masao Mutoh
+  Copyright (C) 2006-2015 Masao Mutoh
+  Copyright (c) 2006-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-
-  $Id: tree_combo.rb,v 1.3 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-
-require 'gtk3'
+require "gtk3"
 
 T_APPLICATION       = 0
 T_COMBO_TEXT_COLUMN = 1
@@ -20,43 +18,41 @@ T_COMBO_TEXT        = 5
 
 # The COMPUTER combo model
 combo_model = Gtk::ListStore.new(String)
-  
-["Ruby", "Perl", "Python", "Java"].each do |v|
+
+%w(Ruby Perl Python Java).each do |v|
   iter = combo_model.append
   iter[0] = v
 end
 
-
 # The table model.
 model = Gtk::ListStore.new(String,         # T_APPLICATION
-			   Integer,        # T_COMBO_TEXT_COLUMN
-			   Gtk::ListStore, # T_COMBO_MODEL
-			   TrueClass,      # T_COMBO_HAS_ENTRY
-			   TrueClass,      # T_COMBO_EDITABLE
-			   String)         # T_COMBO_TEXT
+                           Integer,        # T_COMBO_TEXT_COLUMN
+                           Gtk::ListStore, # T_COMBO_MODEL
+                           TrueClass,      # T_COMBO_HAS_ENTRY
+                           TrueClass,      # T_COMBO_EDITABLE
+                           String)         # T_COMBO_TEXT
 
 view = Gtk::TreeView.new(model)
 
 # 1st column(Text)
 trenderer = Gtk::CellRendererText.new
 tcol = Gtk::TreeViewColumn.new("Application", trenderer,
-			       :text => T_APPLICATION)
+                               :text => T_APPLICATION)
 view.append_column(tcol)
 
 # 2nd column(Combo)
 crenderer = Gtk::CellRendererCombo.new
-crenderer.signal_connect("edited") do |renderer, path, text|
+crenderer.signal_connect("edited") do |_renderer, path, text|
   model.get_iter(path)[T_COMBO_TEXT] = text
 end
 
 ccol = Gtk::TreeViewColumn.new("Language", crenderer,
-			       :text_column => T_COMBO_TEXT_COLUMN, 
-			       :model => T_COMBO_MODEL, 
-			       :has_entry => T_COMBO_HAS_ENTRY,
-			       :editable => T_COMBO_EDITABLE,
-			       :text => T_COMBO_TEXT)
+                               :text_column => T_COMBO_TEXT_COLUMN,
+                               :model => T_COMBO_MODEL,
+                               :has_entry => T_COMBO_HAS_ENTRY,
+                               :editable => T_COMBO_EDITABLE,
+                               :text => T_COMBO_TEXT)
 view.append_column(ccol)
-
 
 # Create rows.
 (1..2).each do |v|
@@ -69,11 +65,9 @@ view.append_column(ccol)
 end
 
 win = Gtk::Window.new
-win.signal_connect("delete_event"){
-  Gtk.main_quit
-}
+win.signal_connect("delete_event") { Gtk.main_quit }
 
-win.add(view).show_all
+win.add(view)
+win.show_all
 
 Gtk.main
-
