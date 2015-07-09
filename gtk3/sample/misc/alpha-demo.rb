641153eca7c32647b6fa66559b0089a44c81daa6
cedlemo <cedlemo@gmx.com>
2015-07-09 18:41:22 +0200

    Improve function that handle screen change

--- gtk3/sample/misc/alpha-demo.rb
+++ gtk3/sample/misc/alpha-demo.rb
@@ -29,7 +29,7 @@ class AlphaDemo < Gtk::Window
     end
     set_double_buffered(false)
 
-    signal_connect("draw") do |widget, event|
+    signal_connect("draw") do |widget, _event|
       cr = widget.window.create_cairo_context
 
       rgba = [1.0, 1.0, 1.0]
@@ -47,20 +47,20 @@ class AlphaDemo < Gtk::Window
       cr.fill_preserve
       cr.stroke
     end
-    signal_connect("screen-changed") do |widget, old_screen|
-      screen_changed(widget, old_screen)
+    signal_connect("screen-changed") do |widget, _old_screen|
+      screen_changed(widget)
     end
 
     screen_changed(self)
   end
 
-  def screen_changed(widget,old_screen=nil)
-    visual = screen.rgba_visual
+  def screen_changed(widget)
+    visual = widget.screen.rgba_visual
     if visual && screen.composited?
       set_visual(visual)
       @supports_alpha = true
     else
-      set_visual(screen.system_visual)
+      set_visual(widget.screen.system_visual)
       @supports_alpha = false
     end
   end
