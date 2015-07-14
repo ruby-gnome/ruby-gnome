3eb4e831e76edce7baa5f535b1a66f285171e7a1
cedlemo <cedlemo@gmx.com>
2015-07-14 15:29:21 +0200

    Update to Gtk3 api, update Copyright and fix style issue in to_drawable.rb

--- gtk3/sample/misc/to_drawable.rb
+++ gtk3/sample/misc/to_drawable.rb
@@ -2,41 +2,33 @@
 =begin
   to_drawable.rb - Gdk::Pixbuf(GDK methods) sample script.
 
-  Copyright (c) 2002-2006 Ruby-GNOME2 Project Team 
+  Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
-
-  $Id: to_drawable.rb,v 1.6 2006/06/17 13:18:12 mutoh Exp $
 =end
 
-require 'gtk3'
+require "gtk3"
 
-filename = ARGV[0]
-unless filename
-  puts "ruby #{$0} filename"
-  exit(1)
-end
+filename = "#{File.expand_path(File.dirname(__FILE__))}/gnome-logo-icon.png"
+
+window = Gtk::Window.new("Drawable sample")
+window.set_border_width(10)
 
 pixbuf = Gdk::Pixbuf.new(filename)
 
-w = Gtk::Window.new("Drawable sample")
-w.realize
-w.signal_connect('delete-event') do
-  Gtk.main_quit
+drawing_area = Gtk::DrawingArea.new
+window.add(drawing_area)
+
+drawing_area.set_size_request(pixbuf.width + 40,
+                              pixbuf.height + 40)
+drawing_area.signal_connect("draw") do |_widget, cr|
+  cr.set_source_rgb(1,1,1)
+  cr.paint
+  cr.set_source_pixbuf(pixbuf, 20, 20)
+  cr.paint
+  false
 end
 
-d = Gtk::DrawingArea.new
-gc = Gdk::GC.new(w.window)
-d.signal_connect('draw') do |w, e|
-  unless e.count > 0
-    width = w.allocation.width
-    height = w.allocation.height
-    pixbuf = pixbuf.scale(width, height)
-    d.window.draw_pixbuf(gc, pixbuf, 0, 0, 0, 0,
-			 width, height, Gdk::RGB::DITHER_NORMAL, 0, 0)
-  end
-  true
-end
-w.add(d)
-w.show_all
+window.show_all
+window.signal_connect("delete-event") { Gtk.main_quit }
 
 Gtk.main
