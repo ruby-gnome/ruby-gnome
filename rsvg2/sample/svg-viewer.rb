144ea22603e262ca636fbe6c3f8aa198b53f2c14
Robert A. Heiler <shevegen@gmail.com>
2015-08-24 21:28:37 +0200

    Update svg-viewer.rb

--- rsvg2/sample/svg-viewer.rb
+++ rsvg2/sample/svg-viewer.rb
@@ -27,11 +27,11 @@ window = Gtk::Window.new
 window.set_default_size(width, height)
 area = Gtk::DrawingArea.new
 
-window.signal_connect("destroy") do
+window.signal_connect(:destroy) do
   Gtk.main_quit
 end
 
-area.signal_connect("expose_event") do |widget, event|
+area.signal_connect(:expose_event) do |widget, event|
   context = widget.window.create_cairo_context
   window_width, window_height = widget.window.size
   context.scale(window_width.to_f / width, window_height.to_f / height)
