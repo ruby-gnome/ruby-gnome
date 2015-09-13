d09a5b97bb4c6ec5d500c7e4fae946cc45ead522
cedlemo <cedlemo@gmx.com>
2015-09-13 09:38:13 +0200

    Revert underscore usage for path return value

--- gtk3/sample/misc/tooltips.rb
+++ gtk3/sample/misc/tooltips.rb
@@ -18,14 +18,14 @@ end
 def treeview_query_tooltip(treeview, keyboard_tip, x, y, tooltip)
   if keyboard_tip
     # Keyboard mode
-    path, _ = treeview.cursor
+    path, = treeview.cursor
     if not path
       return false
     end
   else
     bin_x, bin_y = treeview.convert_widget_to_bin_window_coords(x, y)
     # Mouse mode
-    path, _ = treeview.get_path_at_pos(bin_x, bin_y)
+    path, = treeview.get_path_at_pos(bin_x, bin_y)
     if not path
       return false
     end
