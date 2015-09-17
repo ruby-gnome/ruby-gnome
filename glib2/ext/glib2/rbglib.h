c1c9e29f82db772dc4b0b2dfbd1730820dfcec66
Kouhei Sutou <kou@clear-code.com>
2015-09-17 21:00:22 +0900

    Bump version

--- glib2/ext/glib2/rbglib.h
+++ glib2/ext/glib2/rbglib.h
@@ -36,7 +36,7 @@ extern "C" {
 
 #define RBGLIB_MAJOR_VERSION 3
 #define RBGLIB_MINOR_VERSION 0
-#define RBGLIB_MICRO_VERSION 3
+#define RBGLIB_MICRO_VERSION 4
 
 #ifndef RSTRING_PTR
 #  define RSTRING_PTR(s) (RSTRING(s)->ptr)
