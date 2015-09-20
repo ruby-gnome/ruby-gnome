2cea321e1435d338244adc947bbb19a09d1e8a57
cedlemo <cedlemo@gmx.com>
2015-09-20 18:54:22 +0200

    Implement test for Gdk::Pixbuf::Version.or_later? method

---
+++ gdk_pixbuf2/test/run-test.rb
@@ -0,0 +1,45 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2013  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
+ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
+
+glib_base = File.join(ruby_gnome2_base, "glib2")
+gdk_pixbuf2_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
+
+modules = [
+  [glib_base, "glib2"],
+  [gdk_pixbuf2_base, "gdk_pixbuf2"]
+]
+modules.each do |target, module_name|
+  if system("which make > /dev/null")
+    `make -C #{target.dump} > /dev/null` or exit(false)
+  end
+  $LOAD_PATH.unshift(File.join(target, "ext", module_name))
+  $LOAD_PATH.unshift(File.join(target, "lib"))
+end
+
+$LOAD_PATH.unshift(File.join(glib_base, "test"))
+require "glib-test-init"
+
+$LOAD_PATH.unshift(File.join(gdk_pixbuf2_base, "test"))
+require "gdk_pixbuf2-test-utils"
+
+require "gdk3"
+
+exit Test::Unit::AutoRunner.run(true)
---
+++ gdk_pixbuf2/test/test-version.rb
@@ -0,0 +1,47 @@
+# Copyright (C) 2015  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+class TestGdkPixbufVersion < Test::Unit::TestCase
+  include GdkPixbufTestUtils
+
+  test "STRING" do
+    major = Gdk::Pixbuf::Version::MAJOR
+    minor = Gdk::Pixbuf::Version::MINOR
+    micro = Gdk::Pixbuf::Version::MICRO
+    assert_equal([major, minor, micro].join("."),
+                 Gdk::Pixbuf::Version::STRING)
+  end
+
+  sub_test_case("#or_later?") do
+    test "same" do
+      assert_true(Gdk::Pixbuf::Version.or_later?(Gdk::Pixbuf::Version::MAJOR,
+                                             Gdk::Pixbuf::Version::MINOR,
+                                             Gdk::Pixbuf::Version::MICRO))
+    end
+
+    test "later" do
+      assert_true(Gdk::Pixbuf::Version.or_later?(Gdk::Pixbuf::Version::MAJOR,
+                                         Gdk::Pixbuf::Version::MINOR - 1,
+                                         Gdk::Pixbuf::Version::MICRO))
+    end
+
+    test "earlier" do
+      assert_false(Gdk::Pixbuf::Version.or_later?(Gdk::Pixbuf::Version::MAJOR,
+                                          Gdk::Pixbuf::Version::MINOR + 1,
+                                          Gdk::Pixbuf::Version::MICRO))
+    end
+  end
+end
