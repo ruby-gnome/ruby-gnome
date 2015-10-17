5a03cfae8230af6f434e53b5a365af395e863c4e
cedlemo <cedlemo@gmx.com>
2015-10-17 10:41:35 +0200

    add a test module and implement the use of test_mod.rb

---
+++ gtk3/sample/gtk-demo/test_mod.rb
@@ -0,0 +1,14 @@
+# Copyright (c) 2008-2015 Ruby-GNOME2 Project Team
+# This program is licenced under the same licence as Ruby-GNOME2.
+#
+=begin
+= test demo 
+
+Demonstrates the demo interface.
+=end
+module TestModDemo
+
+  def self.run_demo
+    puts "ok"
+  end
+end
