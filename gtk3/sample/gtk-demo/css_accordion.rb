927c3eea1aa12f80eca7f5038644541b621a7dbc
cedlemo <cedlemo@gmx.com>
2015-11-17 19:03:27 +0100

    Update css_accordion demo

--- gtk3/sample/gtk-demo/css_accordion.rb
+++ gtk3/sample/gtk-demo/css_accordion.rb
@@ -1,75 +1,53 @@
-# This sample code is a port of gtk3/demos/gtk-demo/css_accordion.c. The
-# CSS files used in this sample code are copied from gtk3/demos/gtk-demo.
-# They are licensed under the terms of the GNU Lesser General Public
-# License, version 2.1 or (at your option) later.
+# Copyright (c) 2015 Ruby-GNOME2 Project Team
+# This program is licenced under the same licence as Ruby-GNOME2.
 #
-# Copyright (C) 2013-2015  Ruby-GNOME2 Project Team
-#
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 =begin
 = CSS Theming/CSS Accordion
 
 A simple accordion demo written using CSS transitions and multiple backgrounds
 =end
+def apply_style(widget, provider)
+  widget.children.each do |child|
+    style_context = child.style_context
+    style_context.add_provider(provider, Gtk::StyleProvider::PRIORITY_USER)
+    apply_style(child, provider) if child.respond_to?(:children)
+  end
+end
 
-require "common"
+module CssAccordionDemo
 
-module Demo
-  class CssAccordion < BasicWindow
-    def initialize
-      super("CSS Accordion")
-      set_default_size(600, 300)
+  def self.run_demo(main_window)
+    window = Gtk::Window.new(:toplevel)
+    window.screen = main_window.screen
 
-      container = Gtk::Box.new(:horizontal, 0)
-      container.set_halign(:center)
-      container.set_valign(:center)
-      add(container)
+    window.set_title("CSS Accordion")
+    window.set_default_size(600, 300)
 
-      child = Gtk::Button.new(:label => "This")
+    container = Gtk::Box.new(:horizontal, 0)
+    container.set_halign(:center)
+    container.set_valign(:center)
+
+    window.add(container)
+
+    %w(This Is A CSS Accordion :-).each do |label|
+      child = Gtk::Button.new(:label => label)
       container.add(child)
-
-      child = Gtk::Button.new(:label => "Is")
-      container.add(child)
-
-      child = Gtk::Button.new(:label => "A")
-      container.add(child)
-
-      child = Gtk::Button.new(:label => "CSS")
-      container.add(child)
-
-      child = Gtk::Button.new(:label => "Accordion")
-      container.add(child)
-
-      child = Gtk::Button.new(:label => ":-)")
-      container.add(child)
-
-      provider = Gtk::CssProvider.new
-      Dir.chdir(__dir__) do
-        provider.load(:data => File.read("css_accordion.css"))
-      end
-
-      apply_css(self, provider)
     end
 
-    def apply_css(widget, provider)
-      widget.style_context.add_provider(provider, GLib::MAXUINT)
-      if widget.is_a?(Gtk::Container)
-        widget.each_all do |child|
-          apply_css(child, provider)
-        end
-      end
+    provider = Gtk::CssProvider.new
+    provider.load_from_resource("/css_accordion/css_accordion.css")
+
+    style_context = window.style_context
+    style_context.add_provider(provider, Gtk::StyleProvider::PRIORITY_USER)
+
+    apply_style(window, provider)
+
+    if !window.visible?
+      window.show_all
+    else
+      window.destroy
     end
+  
+    window
   end
 end
