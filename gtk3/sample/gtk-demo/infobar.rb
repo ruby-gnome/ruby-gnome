e24b0bebf289d6c780a5467d58043a6c46636404
cedlemo <cedlemo@gmx.com>
2015-12-08 15:23:31 +0100

    Simplify infobar.rb

--- gtk3/sample/gtk-demo/infobar.rb
+++ gtk3/sample/gtk-demo/infobar.rb
@@ -2,14 +2,14 @@
 # This program is licenced under the same licence as Ruby-GNOME2.
 #
 =begin
-=  Info Bars  
- 
- Info bar widgets are used to report important messages to the user. 
+=  Info Bars
+
+ Info bar widgets are used to report important messages to the user.
 =end
 module InfobarDemo
   def self.run_demo(main_window)
     actions = Gtk::Box.new(:horizontal, 0)
-    
+
     window = Gtk::Window.new(:toplevel)
     window.screen = main_window.screen
     window.set_title("Info Bars")
@@ -18,63 +18,20 @@ module InfobarDemo
     vbox = Gtk::Box.new(:vertical, 0)
     window.add(vbox)
 
-    bar = Gtk::InfoBar.new
-    vbox.pack_start(bar,
-                    :expand => false,
-                    :fill => false,
-                    :padding => 0)
-
-    bar.set_message_type(:info)
-    
-    label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::INFO")
-    label.set_line_wrap(true)
-    label.set_xalign(0)
-    
-    bar.content_area.pack_start(label,
-                                :expand => false,
-                                :fill => false,
-                                :padding => 0)
-
-    button = Gtk::ToggleButton.new(:label => "Message")
-    button.bind_property("active", bar, "visible", :bidirectional)
-    button.set_active(false)
-    actions.add(button)
-
-    bar = Gtk::InfoBar.new
-    vbox.pack_start(bar,
-                    :expand => false,
-                    :fill => false,
-                    :padding => 0)
-
-    bar.set_message_type(:warning)
-    
-    label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::WARNING")
-    label.set_line_wrap(true)
-    label.set_xalign(0)
-    
-    bar.content_area.pack_start(label,
-                                :expand => false,
-                                :fill => false,
-                                :padding => 0)
-
-    button = Gtk::ToggleButton.new(:label => "Warning")
-    button.bind_property("active", bar, "visible", :bidirectional)
-    actions.add(button)
-    button.set_active(false)
+    generate_simple_infobar_and_button("info", vbox, actions)
+    generate_simple_infobar_and_button("warning", vbox, actions)
 
     bar = Gtk::InfoBar.new
     bar.add_button("_OK", Gtk::ResponseType::OK)
     bar.set_show_close_button(true)
     bar.signal_connect "response" do |info_bar, response_id|
-      if response_id == Gtk::ResponseType::CLOSE
-        info_bar.hide
-      end
+      info_bar.hide if response_id == Gtk::ResponseType::CLOSE
       dialog = Gtk::MessageDialog.new(:parent => info_bar.toplevel,
                                       :flags => [:modal, :destroy_with_parent],
                                       :type => :info,
                                       :buttons => :ok,
                                       :message => "You clicked a button on an info bar")
-      dialog.secondary_text = "Your response has id #{response_id.to_s}"
+      dialog.secondary_text = "Your response has id #{response_id}"
       dialog.signal_connect("response") { |widget| widget.destroy }
       dialog.show_all
     end
@@ -88,67 +45,19 @@ module InfobarDemo
     label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::QUESTION")
     label.set_line_wrap(true)
     label.set_xalign(0)
-    
-    bar.content_area.pack_start(label,
-                                :expand => false,
-                                :fill => false,
-                                :padding => 0)
 
+    bar.content_area.pack_start(label, :expand => false, :fill => false, :padding => 0)
 
     button = Gtk::ToggleButton.new(:label => "Question")
     button.bind_property("active", bar, "visible", :bidirectional)
     actions.add(button)
     button.set_active(false)
 
-    bar = Gtk::InfoBar.new
-    vbox.pack_start(bar,
-                    :expand => false,
-                    :fill => false,
-                    :padding => 0)
-
-    bar.set_message_type(:error)
-    
-    label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::ERROR")
-    label.set_line_wrap(true)
-    label.set_xalign(0)
-    
-    bar.content_area.pack_start(label,
-                                :expand => false,
-                                :fill => false,
-                                :padding => 0)
-
-    button = Gtk::ToggleButton.new(:label => "Error")
-    button.bind_property("active", bar, "visible", :bidirectional)
-    actions.add(button)
-    button.set_active(false)
-
-    bar = Gtk::InfoBar.new
-    vbox.pack_start(bar,
-                    :expand => false,
-                    :fill => false,
-                    :padding => 0)
-
-    bar.set_message_type(:other)
-    
-    label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::OTHER")
-    label.set_line_wrap(true)
-    label.set_xalign(0)
-    
-    bar.content_area.pack_start(label,
-                                :expand => false,
-                                :fill => false,
-                                :padding => 0)
-
-    button = Gtk::ToggleButton.new(:label => "Other")
-    button.bind_property("active", bar, "visible", :bidirectional)
-    button.active = false
-    actions.add(button)
+    generate_simple_infobar_and_button("error", vbox, actions)
+    generate_simple_infobar_and_button("other", vbox, actions)
 
     frame = Gtk::Frame.new("Info bars")
-    vbox.pack_start(frame,
-                    :expand => false,
-                    :fill => false,
-                    :padding => 8)
+    vbox.pack_start(frame, :expand => false, :fill => false, :padding => 8)
 
     vbox2 = Gtk::Box.new(:vertical, 8)
     vbox2.set_border_width(8)
@@ -158,7 +67,7 @@ module InfobarDemo
     vbox2.pack_start(label, :expand => false, :fill => false, :padding => 0)
     actions.show_all
     vbox2.pack_start(actions, :expand => false, :fill => false, :padding => 0)
-    
+
     if !window.visible?
       window.show_all
     else
@@ -168,6 +77,18 @@ module InfobarDemo
   end
 
   def self.generate_simple_infobar_and_button(message_type, bar_parent, button_parent)
+    bar = Gtk::InfoBar.new
+    bar_parent.pack_start(bar, :expand => false, :fill => false, :padding => 0)
+    bar.set_message_type(message_type.to_sym)
 
+    label = Gtk::Label.new("This is an info bar with message type Gtk::MessageType::#{message_type.upcase}")
+    label.set_line_wrap(true)
+    label.set_xalign(0)
+
+    bar.content_area.pack_start(label, :expand => false, :fill => false, :padding => 0)
+
+    button = Gtk::ToggleButton.new(:label => "Message")
+    button.bind_property("active", bar, "visible", :bidirectional)
+    button_parent.add(button)
   end
 end
