fd541d175ded5f7b506dde795cae138889b8570c
Kouhei Sutou <kou@clear-code.com>
2016-01-07 23:21:40 +0900

    gio2: use dynamic callback

--- gio2/ext/gio2/rb-gio2.c
+++ gio2/ext/gio2/rb-gio2.c
@@ -22,49 +22,6 @@
 
 #define RG_TARGET_NAMESPACE rb_mGio
 
-static gboolean
-name_equal(GIArgInfo *info, const gchar *target_name)
-{
-    GITypeInfo type_info;
-    GIBaseInfo *interface_info;
-    const gchar *namespace;
-    const gchar *name;
-    gboolean equal_name_p = FALSE;
-
-    g_arg_info_load_type(info, &type_info);
-    interface_info = g_type_info_get_interface(&type_info);
-    namespace = g_base_info_get_namespace(interface_info);
-    name = g_base_info_get_name(interface_info);
-    if (strcmp(namespace, "Gio") == 0 && strcmp(name, target_name) == 0) {
-        equal_name_p = TRUE;
-    }
-    g_base_info_unref(interface_info);
-
-    return equal_name_p;
-}
-
-static void
-rb_gio2_async_ready_callback_callback(GObject *source_object,
-                                      GAsyncResult *result,
-                                      gpointer user_data)
-{
-    RBGICallbackData *callback_data = user_data;
-    ID id_call;
-
-    CONST_ID(id_call, "call");
-    rb_funcall(callback_data->rb_callback, id_call, 2,
-               GOBJ2RVAL(source_object), GOBJ2RVAL(result));
-}
-
-static gpointer
-rb_gio2_async_ready_callback_finder(GIArgInfo *info)
-{
-    if (!name_equal(info, "AsyncReadyCallback")) {
-        return NULL;
-    }
-    return rb_gio2_async_ready_callback_callback;
-}
-
 void
 Init_gio2 (void)
 {
@@ -74,6 +31,4 @@ Init_gio2 (void)
 
     rb_gio2_init_application(RG_TARGET_NAMESPACE);
     rb_gio2_init_pollable_source(RG_TARGET_NAMESPACE);
-
-    rb_gi_callback_register_finder(rb_gio2_async_ready_callback_finder);
 }
