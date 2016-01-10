52634c3fb2f7e69993dc7b97719d054dbfea8fc7
cedlemo <cedlemo@gmx.com>
2016-01-10 19:13:57 +0100

    adapt test to new form of GLib::Regex.new

--- glib2/test/test_regex.rb
+++ glib2/test/test_regex.rb
@@ -27,34 +27,34 @@ class TestRegex < Test::Unit::TestCase
   end
 
   def test_regex_new
-    a_regex = GLib::Regex.new("to??", 0, 0)
+    a_regex = GLib::Regex.new("to??", :compile_options => 0, :match_options => 0)
     assert_instance_of(GLib::Regex, a_regex)
   end
 
   def test_regex_pattern
-    a_regex = GLib::Regex.new("to??", 0, 0)
+    a_regex = GLib::Regex.new("to??", :compile_options => 0, :match_options => 0)
     assert_equal(a_regex.pattern, "to??")
   end
 
   def test_regex_compile_flags
     flags = GLib::RegexCompileFlags::CASELESS
-    a_regex = GLib::Regex.new("to??", flags, 0)
+    a_regex = GLib::Regex.new("to??", :compile_options => flags, :match_options => 0)
     assert_equal(a_regex.compile_flags, flags) 
   end
 
   def test_regex_match_flags
     flags = GLib::RegexMatchFlags::PARTIAL_HARD
-    a_regex = GLib::Regex.new("to??", 0, flags)
+    a_regex = GLib::Regex.new("to??", :compile_options => 0, :match_options => flags)
     assert_equal(a_regex.match_flags, flags) 
   end
 
   sub_test_case "max_backref" do
     test "none" do
-      a_regex = GLib::Regex.new("to?o", 0, 0)
+      a_regex = GLib::Regex.new("to?o", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.max_backref, 0)
     end
     test "one" do
-      a_regex = GLib::Regex.new("(to(?)o)\\g1", 0, 0)
+      a_regex = GLib::Regex.new("(to(?)o)\\g1", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.max_backref, 1)
     end
   end
@@ -72,56 +72,56 @@ class TestRegex < Test::Unit::TestCase
 
   sub_test_case "capture_count" do
     test "none" do
-      a_regex = GLib::Regex.new("to?o", 0, 0)
+      a_regex = GLib::Regex.new("to?o", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.capture_count, 0)
     end
     test "one" do
-      a_regex = GLib::Regex.new("(to(\?)o)", 0, 0)
+      a_regex = GLib::Regex.new("(to(\?)o)", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.capture_count, 1)
     end
     test "three" do
-      a_regex = GLib::Regex.new("((red|white) (king|queen))", 0, 0)
+      a_regex = GLib::Regex.new("((red|white) (king|queen))", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.capture_count, 3)
     end
   end
 
   sub_test_case "has_cr_or_lf" do
     test "none" do
-      a_regex = GLib::Regex.new("to?o", 0, 0)
+      a_regex = GLib::Regex.new("to?o", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.has_cr_or_lf, false)
     end
     test "both" do
-      a_regex = GLib::Regex.new("to\no", 0, 0)
+      a_regex = GLib::Regex.new("to\no", :compile_options => 0, :match_options => 0)
       assert(a_regex.has_cr_or_lf)
     end
     test "cr" do
-      a_regex = GLib::Regex.new("to\rtiti", 0, 0)
+      a_regex = GLib::Regex.new("to\rtiti", :compile_options => 0, :match_options => 0)
       assert(a_regex.has_cr_or_lf)
     end
   end
   
   sub_test_case "max_lookbehind" do
     test "none" do
-      a_regex = GLib::Regex.new("to?o", 0, 0)
+      a_regex = GLib::Regex.new("to?o", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.max_lookbehind, 0)
     end
     test "three" do
-      a_regex = GLib::Regex.new("(?<!foo)bar", 0, 0)
+      a_regex = GLib::Regex.new("(?<!foo)bar", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.max_lookbehind, 3)
     end
   end
 
   sub_test_case "string_number" do
     test "none" do
-      a_regex = GLib::Regex.new("too", 0, 0)
+      a_regex = GLib::Regex.new("too", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.string_number("a_name"), -1)
     end
     test "one" do
-      a_regex = GLib::Regex.new("(?<a_name>foo)bar", 0, 0)
+      a_regex = GLib::Regex.new("(?<a_name>foo)bar", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.string_number("a_name"), 1)
     end
     test "two" do
-      a_regex = GLib::Regex.new("(?<another_name>foo)(?<a_name>bar)", 0, 0)
+      a_regex = GLib::Regex.new("(?<another_name>foo)(?<a_name>bar)", :compile_options => 0, :match_options => 0)
       assert_equal(a_regex.string_number("a_name"), 2)
     end
   end
@@ -135,21 +135,21 @@ class TestRegex < Test::Unit::TestCase
   end
 
   def test_regex_split
-    a_regex = GLib::Regex.new("\s", 0, 0)
+    a_regex = GLib::Regex.new("\s", :compile_options => 0, :match_options => 0)
     string_to_split = "a bc"
     splited_strings = a_regex.split(string_to_split, 0)
     assert_equal(splited_strings, ["a", "bc"])
   end
 
   def test_regex_split_full
-    a_regex = GLib::Regex.new("\s", 0, 0)
+    a_regex = GLib::Regex.new("\s", :compile_options => 0, :match_options => 0)
     string_to_split = "a bc"
     splited_strings = a_regex.split_full(string_to_split, 0, 0, 0)
     assert_equal(splited_strings, ["a", "bc"])
   end
 
   def test_regex_replace
-    a_regex = GLib::Regex.new("\s", 0, 0)
+    a_regex = GLib::Regex.new("\s", :compile_options => 0, :match_options => 0)
     string_to_modify = "a bc"
     replacement ="_"
     modified_string = a_regex.replace(string_to_modify, 0, replacement, 0)
@@ -157,7 +157,7 @@ class TestRegex < Test::Unit::TestCase
   end
 
   def test_regex_replace_literal
-    a_regex = GLib::Regex.new("\s", 0, 0)
+    a_regex = GLib::Regex.new("\s", :compile_options => 0, :match_options => 0)
     string_to_modify = "a bc"
     replacement ="_"
     modified_string = a_regex.replace_literal(string_to_modify, 0, replacement, 0)
@@ -165,7 +165,7 @@ class TestRegex < Test::Unit::TestCase
   end
   
   def test_regex_replace_eval
-    a_regex = GLib::Regex.new("1|2|3|4", 0, 0)
+    a_regex = GLib::Regex.new("1|2|3|4", :compile_options => 0, :match_options => 0)
     replacement_strings = { "1" => "one",
                             "2" => "two",
                             "3" => "three",
@@ -211,7 +211,7 @@ class TestRegex < Test::Unit::TestCase
   end
 
   sub_test_case "match" do
-    a_regex = GLib::Regex.new("[A-Z]+", 0, 0)
+    a_regex = GLib::Regex.new("[A-Z]+", :compile_options => 0, :match_options => 0)
     
     test "no match" do
       assert_equal(nil, a_regex.match("abc def", 0))
@@ -222,13 +222,13 @@ class TestRegex < Test::Unit::TestCase
     end
   
     test "match longer" do
-      a_regex = GLib::Regex.new("<.*>", 0, 0)
+      a_regex = GLib::Regex.new("<.*>", :compile_options => 0, :match_options => 0)
       # TODO test matching "<a> <b> <c>" against the pattern "<.*>;" you would obtain one matche: "<a> <b> <c>"
     end
 end
 
   sub_test_case "match_full" do
-    a_regex = GLib::Regex.new("[A-Z]+", 0, 0)
+    a_regex = GLib::Regex.new("[A-Z]+", :compile_options => 0, :match_options => 0)
     
     test "no match" do
       assert_equal(nil, a_regex.match_full("abc def", 0, 0))
@@ -239,13 +239,13 @@ end
     end
     
     test "match longer" do
-      a_regex = GLib::Regex.new("<.*>", 0, 0)
+      a_regex = GLib::Regex.new("<.*>", :compile_options => 0, :match_options => 0)
       # TODO test matching "<a> <b> <c>" against the pattern "<.*>;" you would obtain one matche: "<a> <b> <c>"
     end
   end
 
   sub_test_case "match_all" do
-    a_regex = GLib::Regex.new("[A-Z]+", 0, 0)
+    a_regex = GLib::Regex.new("[A-Z]+", :compile_options => 0, :match_options => 0)
     
     test "no match" do
       assert_equal(nil, a_regex.match_all("abc def", 0))
@@ -256,13 +256,13 @@ end
     end
     
     test "match all" do
-      a_regex = GLib::Regex.new("<.*>", 0, 0)
+      a_regex = GLib::Regex.new("<.*>", :compile_options => 0, :match_options => 0)
       # TODO test matching "<a> <b> <c>" against the pattern "<.*>;" you would obtain three matches: "<a> <b> <c>", "<a> <b>" and "<a>".
     end
   end
   
   sub_test_case "match_all_full" do
-    a_regex = GLib::Regex.new("[A-Z]+", 0, 0)
+    a_regex = GLib::Regex.new("[A-Z]+", :compile_options => 0, :match_options => 0)
     
     test "no match" do
       assert_equal(nil, a_regex.match_all_full("abc def", 0, 0))
@@ -273,7 +273,7 @@ end
     end
 
     test "match all" do
-      a_regex = GLib::Regex.new("<.*>", 0, 0)
+      a_regex = GLib::Regex.new("<.*>", :compile_options => 0, :match_options => 0)
       # TODO test matching "<a> <b> <c>" against the pattern "<.*>;" you would obtain three matches: "<a> <b> <c>", "<a> <b>" and "<a>".
     end
   end
