f98d0b0137ea09a151e5217a9e11a49ecfb44559
cedlemo <cedlemo@gmx.com>
2016-01-11 18:40:51 +0100

    Implement GLib::RegexCompileFlags

--- glib2/ext/glib2/rbglib_regex.c
+++ glib2/ext/glib2/rbglib_regex.c
@@ -75,4 +75,5 @@ Init_glib_regex(void)
     RG_DEF_METHOD(initialize, -1);
 
     G_DEF_CLASS(G_TYPE_REGEX_MATCH_FLAGS, "RegexMatchFlags", mGLib);
+    G_DEF_CLASS(G_TYPE_REGEX_COMPILE_FLAGS, "RegexMatchFlags", mGLib);
 }
