45acde97d529c0e9f999fb0a507e0286d43fb670
Kouhei Sutou <kou@clear-code.com>
2016-01-14 23:38:00 +0900

    glib2 test: remove needless prefix

--- glib2/test/test-regex.rb
+++ glib2/test/test-regex.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2015  Ruby-GNOME2 Project Team
+# Copyright (C) 2015-2016  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -15,28 +15,28 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 class TestRegex < Test::Unit::TestCase
-  def test_regex_enum_match_flags
+  def test_enum_match_flags
     assert_const_defined(GLib, :RegexMatchFlags)
     assert_kind_of(GLib::RegexMatchFlags, GLib::RegexMatchFlags::PARTIAL_HARD)
   end
 
-  def test_regex_enum_compile_flags
+  def test_enum_compile_flags
     assert_const_defined(GLib, :RegexCompileFlags)
     assert_kind_of(GLib::RegexCompileFlags, GLib::RegexCompileFlags::CASELESS)
   end
 
-  def test_regex_pattern
+  def test_pattern
     a_regex = GLib::Regex.new("to??", :compile_options => 0, :match_options => 0)
     assert_equal(a_regex.pattern, "to??")
   end
 
-  def test_regex_compile_flags
+  def test_compile_flags
     flags = GLib::RegexCompileFlags::CASELESS
     a_regex = GLib::Regex.new("to??", :compile_options => flags, :match_options => 0)
     assert_equal(a_regex.compile_flags, flags)
   end
 
-  def test_regex_match_flags
+  def test_match_flags
     flags = GLib::RegexMatchFlags::PARTIAL_HARD
     a_regex = GLib::Regex.new("to??", :compile_options => 0, :match_options => flags)
     assert_equal(a_regex.match_flags, flags)
