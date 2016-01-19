86395c68d51558e23afdbd9aee01e75c766618db
Kouhei Sutou <kou@clear-code.com>
2016-01-20 00:23:44 +0900

    glib2 test: use setup

--- glib2/test/test-regex.rb
+++ glib2/test/test-regex.rb
@@ -73,33 +73,35 @@ class TestRegex < Test::Unit::TestCase
   end
 
   sub_test_case "match" do
-    a_regex = GLib::Regex.new("[A-Z]+")
+    setup do
+      @regex = GLib::Regex.new("[A-Z]+")
+    end
 
     test "no match no options" do
-      match_info = a_regex.match("abc def")
+      match_info = @regex.match("abc def")
       assert_equal(match_info.string, "abc def")
-      assert_equal(match_info.regex.pattern, a_regex.pattern)
+      assert_equal(match_info.regex.pattern, @regex.pattern)
       assert_equal(match_info.matches?, false)
     end
 
     test "matched no options" do
-      match_info = a_regex.match("abc DEF")
+      match_info = @regex.match("abc DEF")
       assert_equal(match_info.string, "abc DEF")
-      assert_equal(match_info.regex.pattern, a_regex.pattern)
+      assert_equal(match_info.regex.pattern, @regex.pattern)
       assert_equal(match_info.matches?, true)
     end
 
     test "no match and start position option" do
-      match_info = a_regex.match("abc def", :start_position => 4)
+      match_info = @regex.match("abc def", :start_position => 4)
       assert_equal(match_info.string, "abc def")
-      assert_equal(match_info.regex.pattern, a_regex.pattern)
+      assert_equal(match_info.regex.pattern, @regex.pattern)
       assert_equal(match_info.matches?, false)
     end
 
     test "matched  and start position option" do
-      match_info = a_regex.match("abc DEF", :start_position => 4)
+      match_info = @regex.match("abc DEF", :start_position => 4)
       assert_equal(match_info.string, "abc DEF")
-      assert_equal(match_info.regex.pattern, a_regex.pattern)
+      assert_equal(match_info.regex.pattern, @regex.pattern)
       assert_equal(match_info.matches?, true)
     end
   end
