7ca332b45beff342e0e68f018c381d2dd5b37306
Kouhei Sutou <kou@clear-code.com>
2016-01-26 00:01:10 +0900

    gi: suppress a warning

--- gobject-introspection/ext/gobject-introspection/rb-gi-function-info.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-function-info.c
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2012-2015  Ruby-GNOME2 Project Team
+ *  Copyright (C) 2012-2016  Ruby-GNOME2 Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -855,7 +855,7 @@ ffi_closure_callback(G_GNUC_UNUSED ffi_cif *cif,
                      void *data)
 {
     RBGICallback *callback = data;
-    RBGICallbackData *callback_data;
+    RBGICallbackData *callback_data = NULL;
     GArray *in_args;
     GArray *out_args;
     GPtrArray *args_metadata;
