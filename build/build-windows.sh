9969fb4d1f3787e3dd1cc90b8433f7279dfdbe21
Kouhei Sutou <kou@clear-code.com>
2016-04-02 00:30:40 +0900

    windows: update Rubies

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -3,8 +3,8 @@
 export LANG=C
 export DEBIAN_FRONTEND=noninteractive
 
-ruby_versions="2.0.0-p647 2.1.7 2.2.3"
-export RUBY_CC_VERSION="2.0.0:2.1.7:2.2.3"
+ruby_versions="2.1.10 2.2.4 2.3.0"
+export RUBY_CC_VERSION="2.1.10:2.2.4:2.3.0"
 
 N_CPUS=$(grep '^processor' /proc/cpuinfo | wc -l)
 export MAKE_N_JOBS=${N_CPUS}
