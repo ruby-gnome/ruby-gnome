70caee9118d4499bdad8f1750b97d11e6936f60b
cedlemo <cedlemo@gmx.com>
2016-05-22 18:46:12 +0200

    Add skeleton for Gnumeric bindings

---
+++ gnm/README.md
@@ -0,0 +1,25 @@
+# Ruby/GNM
+
+Ruby/GNM is a Ruby binding of Gnumeric.
+
+## Requirements
+
+* Ruby/GOffice in
+  [Ruby-GNOME2](http://ruby-gnome2.sourceforge.jp/)
+* [GNM](http://www.gnumeric.org/)
+
+## Install
+
+    gem install gnm
+
+## License
+
+Copyright (c) 2016 Ruby-GNOME2 Project Team
+
+This program is free software. You can distribute/modify this program
+under the terms of the GNU LESSER GENERAL PUBLIC LICENSE Version 2.1.
+
+## Project Websites
+
+*   http://ruby-gnome2.sourceforge.jp/
+*   https://github.com/ruby-gnome2/ruby-gnome2
---
+++ gnm/test/run-test.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2016  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
+ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
+
+gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
+goffice_base = File.join(ruby_gnome2_base, "goffice")
+
+modules = [
+  [gobject_introspection_base, "gobject-introspection"],
+  [goffice_base, "goffice"]
+]
+
+modules.each do |target, module_name|
+  makefile = File.join(target, "Makefile")
+  if File.exist?(makefile) and system("which make > /dev/null")
+    `make -C #{target.dump} > /dev/null` or exit(false)
+  end
+  $LOAD_PATH.unshift(File.join(target, "ext", module_name))
+  $LOAD_PATH.unshift(File.join(target, "lib"))
+end
+
+$LOAD_PATH.unshift(File.join(gsf_base, "test"))
+require "gnm-test-utils"
+
+require "gnm"
+
+exit Test::Unit::AutoRunner.run(true, File.join(gsf_base, "test"))
