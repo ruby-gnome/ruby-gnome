54b54b01ad56d4cdbc81896e30248dd1aadea9ea
Kouhei Sutou <kou@clear-code.com>
2016-06-05 22:02:07 +0900

    gdk_pixbuf2 test: add missing gi dependency

--- gdk_pixbuf2/test/run-test.rb
+++ gdk_pixbuf2/test/run-test.rb
@@ -20,11 +20,13 @@ ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
 ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
 
 glib_base = File.join(ruby_gnome2_base, "glib2")
+gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
 gdk_pixbuf2_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
 
 modules = [
   [glib_base, "glib2"],
-  [gdk_pixbuf2_base, "gdk_pixbuf2"]
+  [gobject_introspection_base, "gobject-introspection"],
+  [gdk_pixbuf2_base, "gdk_pixbuf2"],
 ]
 modules.each do |target, module_name|
   makefile = File.join(target, "Makefile")
