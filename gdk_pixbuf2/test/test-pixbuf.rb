4b146cf0693fb8e2c282f6976e1117575a800912
Kouhei Sutou <kou@clear-code.com>
2016-06-05 23:30:12 +0900

    gdk_pixbuf2 test: simplify

--- gdk_pixbuf2/test/test-pixbuf.rb
+++ gdk_pixbuf2/test/test-pixbuf.rb
@@ -18,51 +18,8 @@ class TestPixbuf < Test::Unit::TestCase
   include GdkPixbufTestUtils
 
   sub_test_case(".new") do
-    test "basic hash form" do
-      colorspace =  GdkPixbuf::Colorspace::RGB
-      width = 100
-      height = 100
-      has_alpha = true
-      bits_per_sample = 8
-      pixbuf = GdkPixbuf::Pixbuf.new(:colorspace => colorspace,
-                             :has_alpha => has_alpha,
-                             :width => width,
-                             :height => height,
-                             :bits_per_sample => bits_per_sample)
-      assert_equal(colorspace, pixbuf.colorspace)
-      assert_equal(width, pixbuf.width)
-      assert_equal(height, pixbuf.height)
-      assert_equal(has_alpha, pixbuf.has_alpha?)
-      assert_equal(bits_per_sample, pixbuf.bits_per_sample)
-    end
-
-    test "basic legacy form" do
-      colorspace =  GdkPixbuf::Colorspace::RGB
-      width = 100
-      height = 100
-      has_alpha = true
-      bits_per_sample = 8
-      pixbuf = GdkPixbuf::Pixbuf.new(colorspace, has_alpha, bits_per_sample,
-                                     width, height)
-      assert_equal(colorspace, pixbuf.colorspace)
-      assert_equal(width, pixbuf.width)
-      assert_equal(height, pixbuf.height)
-      assert_equal(has_alpha, pixbuf.has_alpha?)
-      assert_equal(bits_per_sample, pixbuf.bits_per_sample)
-    end
-
-    test "from_file hash form" do
-      filename = fixture_path("gnome-logo-icon.png")
-      pixbuf = GdkPixbuf::Pixbuf.new(:file => filename)
-      assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
-    end
-
-    test "from_file legacy form" do
-      pixbuf = GdkPixbuf::Pixbuf.new(fixture_path("gnome-logo-icon.png"))
-      assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
-    end
-
-    r_xpm = [
+    def r_xpm
+      [
        "10 10 3 1",
        "   c None",
        ".  c #FE0B0B",
@@ -76,16 +33,69 @@ class TestPixbuf < Test::Unit::TestCase
        "+..++..+++",
        "+..++...++",
        "+..+++...+",
-       "+..++++..+"]
-
-    test "from_xpm legacy form" do
-      pixbuf = GdkPixbuf::Pixbuf.new(r_xpm)
-      assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+        "+..++++..+",
+      ]
     end
 
-    test "from_xpm hash form" do
-      pixbuf = GdkPixbuf::Pixbuf.new(:xpm => r_xpm)
-      assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+    sub_test_case("legacy form") do
+      test "basic" do
+        colorspace =  GdkPixbuf::Colorspace::RGB
+        width = 100
+        height = 100
+        has_alpha = true
+        bits_per_sample = 8
+        pixbuf = GdkPixbuf::Pixbuf.new(colorspace,
+                                       has_alpha,
+                                       bits_per_sample,
+                                       width,
+                                       height)
+        assert_equal(colorspace, pixbuf.colorspace)
+        assert_equal(width, pixbuf.width)
+        assert_equal(height, pixbuf.height)
+        assert_equal(has_alpha, pixbuf.has_alpha?)
+        assert_equal(bits_per_sample, pixbuf.bits_per_sample)
+      end
+
+      test "from_file" do
+        pixbuf = GdkPixbuf::Pixbuf.new(fixture_path("gnome-logo-icon.png"))
+        assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+      end
+
+      test "from_xpm" do
+        pixbuf = GdkPixbuf::Pixbuf.new(r_xpm)
+        assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+      end
+    end
+
+    sub_test_case("Hash form") do
+      test "basic" do
+        colorspace =  GdkPixbuf::Colorspace::RGB
+        width = 100
+        height = 100
+        has_alpha = true
+        bits_per_sample = 8
+        pixbuf = GdkPixbuf::Pixbuf.new(:colorspace => colorspace,
+                                       :has_alpha => has_alpha,
+                                       :width => width,
+                                       :height => height,
+                                       :bits_per_sample => bits_per_sample)
+        assert_equal(colorspace, pixbuf.colorspace)
+        assert_equal(width, pixbuf.width)
+        assert_equal(height, pixbuf.height)
+        assert_equal(has_alpha, pixbuf.has_alpha?)
+        assert_equal(bits_per_sample, pixbuf.bits_per_sample)
+      end
+
+      test "from_file" do
+        filename = fixture_path("gnome-logo-icon.png")
+        pixbuf = GdkPixbuf::Pixbuf.new(:file => filename)
+        assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+      end
+
+      test "from_xpm" do
+        pixbuf = GdkPixbuf::Pixbuf.new(:xpm => r_xpm)
+        assert_equal(GdkPixbuf::Colorspace::RGB, pixbuf.colorspace)
+      end
     end
   end
 end
