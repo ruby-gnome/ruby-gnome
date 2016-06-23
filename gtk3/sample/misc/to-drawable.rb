17bed170b0fd9ac290622aaaefbba256786152a4
cedlemo <cedlemo@gmx.com>
2016-06-23 21:23:03 +0200

    Use GdkPixbuf form in to-drawable.rb comments

--- gtk3/sample/misc/to-drawable.rb
+++ gtk3/sample/misc/to-drawable.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 =begin
-  to_drawable.rb - Gdk::Pixbuf(GDK methods) sample script.
+  to_drawable.rb - GdkPixbuf::Pixbuf(GDK methods) sample script.
 
   Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
