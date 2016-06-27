424b65e9372392336f0efaa87e65b2dfec6c2aa5
cedlemo <cedlemo@gmx.com>
2016-06-27 19:36:08 +0200

    Update copyright date

--- gdk_pixbuf2/sample/flip.rb
+++ gdk_pixbuf2/sample/flip.rb
@@ -2,7 +2,7 @@
 =begin
   flip.rb - Ruby/GdkPixbuf sample script.
 
-  Copyright (c) 2005,2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2005-2016 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 
   $Id: flip.rb,v 1.3 2006/06/17 14:38:08 mutoh Exp $
