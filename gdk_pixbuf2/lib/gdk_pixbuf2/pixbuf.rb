c6a97d7d69011c3baf8f7fdd0ce3ffde8adfe20e
Kouhei Sutou <kou@clear-code.com>
2016-08-14 22:29:46 +0900

    gdk_pixbuf2 windows: support loading from file

--- gdk_pixbuf2/lib/gdk_pixbuf2/pixbuf.rb
+++ gdk_pixbuf2/lib/gdk_pixbuf2/pixbuf.rb
@@ -157,6 +157,19 @@ module GdkPixbuf
     end
     private :initialize_with_hash
 
+    if private_method_defined?(:initialize_new_from_file_utf8)
+      alias_method :initialize_new_from_file,
+                   :initialize_new_from_file_utf8
+    end
+    if private_method_defined?(:initialize_new_from_file_at_size_utf8)
+      alias_method :initialize_new_from_file_at_size,
+                   :initialize_new_from_file_at_size_utf8
+    end
+    if private_method_defined?(:initialize_new_from_file_at_scale_utf8)
+      alias_method :initialize_new_from_file_at_scale,
+                   :initialize_new_from_file_at_scale_utf8
+    end
+
     def dup
       copy
     end
