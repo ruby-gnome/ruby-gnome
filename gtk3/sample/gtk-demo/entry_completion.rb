37bf1a2f1674658e262a4cebdbcd3dcfc3473ab5
cedlemo <cedlemo@gmx.com>
2016-08-19 15:03:02 +0200

    Use the class from instead of module in entry_completion demo

--- gtk3/sample/gtk-demo/entry_completion.rb
+++ gtk3/sample/gtk-demo/entry_completion.rb
@@ -7,19 +7,20 @@
 GtkEntryCompletion provides a mechanism for adding support for
 completion in GtkEntry.
 =end
-module EntryCompletionDemo
-  def self.run_demo(main_window)
-    window = Gtk::Window.new(:toplevel)
-    window.screen = main_window.screen
-    window.set_title("Entry Completion")
-    window.set_resizable(true)
+class EntryCompletionDemo
+  def initialize(main_window)
+    @window = Gtk::Window.new(:toplevel)
+    @window.screen = main_window.screen
+    @window.title = "Entry Completion"
+    @window.resizable = true
 
     vbox = Gtk::Box.new(:vertical, 5)
-    window.add(vbox)
-    vbox.set_border_width(5)
+    @window.add(vbox)
+    vbox.border_width = 5
 
     label = Gtk::Label.new
-    label.set_markup("Completion demo, try writing <b>total</b> or <b>gnome</b> for example.")
+    markup = "Completion demo, try writing <b>total</b> or <b>gnome</b> for example."
+    label.markup = markup
     vbox.pack_start(label, :expand => false, :fill => false, :padding => 0)
 
     entry = Gtk::Entry.new
@@ -28,18 +29,22 @@ module EntryCompletionDemo
     completion = Gtk::EntryCompletion.new
     entry.completion = completion
 
-    completion.set_model(create_completion_model)
-    completion.set_text_column(0)
-
-    if !window.visible?
-      window.show_all
-    else
-      window.destroy
-    end
-    window
+    completion.model = create_completion_model
+    completion.text_column = 0
   end
 
-  def self.create_completion_model
+  def run
+    if !@window.visible?
+      @window.show_all
+    else
+      @window.destroy
+    end
+    @window
+  end
+
+  private
+
+  def create_completion_model
     store = Gtk::ListStore.new(String)
     %w(GNOME total totally).each do |word|
       iter = store.append
