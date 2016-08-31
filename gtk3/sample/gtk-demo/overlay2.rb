c0524f2b692961ff8e98ba229c4347775f9023b8
cedlemo <cedlemo@gmx.com>
2016-08-31 19:17:26 +0200

    Use the class from instead of module in overlay2 demo

--- gtk3/sample/gtk-demo/overlay2.rb
+++ gtk3/sample/gtk-demo/overlay2.rb
@@ -7,69 +7,83 @@
 Another example of an overlay with some decorative
 and some interactive controls.
 =end
-module Overlay2Demo
-  def self.run_demo(_main_window)
-    window = Gtk::Window.new(:toplevel)
-    window.set_default_size(500, 510)
-    window.set_title("Decorative Overlay")
+class Overlay2Demo
+  def initialize(_main_window)
+    @window = Gtk::Window.new(:toplevel)
+    @window.set_default_size(500, 510)
+    @window.title = "Decorative Overlay"
 
-    overlay = Gtk::Overlay.new
+    @overlay = Gtk::Overlay.new
 
     sw = Gtk::ScrolledWindow.new
     sw.set_policy(:automatic, :automatic)
 
-    text = Gtk::TextView.new
-    buffer = text.buffer
+    initialize_textview
+
+    @window.add(@overlay)
+    @overlay.add(sw)
+    sw.add(@text)
+
+    generate_image_in_overlay("/overlay2/decor1.png", :start)
+    generate_image_in_overlay("/overlay2/decor2.png", :end)
+
+    generate_adjustment
+    generate_scale_in_overlay
+
+    @overlay.show_all
+  end
+
+  def run
+    if !@window.visible?
+      @window.show_all
+    else
+      @window.destroy
+    end
+    @window
+  end
+
+  private
+
+  def generate_image_in_overlay(resource, position)
+    image = Gtk::Image.new(:resource => resource)
+    @overlay.add_overlay(image)
+    @overlay.set_overlay_pass_through(image, true)
+    image.halign = position
+    image.valign = position
+  end
+
+  def generate_adjustment
+    @adjustment = Gtk::Adjustment.new(0, 0, 100, 1, 1, 0)
+    @adjustment.signal_connect "value-changed" do |widget|
+      value = widget.value
+      @text.set_left_margin(value)
+      @tag.set_property("pixels-above-lines", value)
+    end
+    @adjustment.value = 100
+  end
+
+  def generate_scale_in_overlay
+    scale = Gtk::Scale.new(:horizontal, @adjustment)
+    scale.draw_value = false
+    scale.set_size_request(120, -1)
+    scale.margin_start = 20
+    scale.margin_end = 20
+    scale.margin_bottom = 20
+    @overlay.add_overlay(scale)
+    scale.halign = :start
+    scale.valign = :end
+    scale.tooltip_text = "Margin"
+  end
+
+  def initialize_textview
+    @text = Gtk::TextView.new
+    buffer = @text.buffer
     buffer.text = "Dear Diary..."
 
-    tag = buffer.create_tag("top-margin", "pixels-above-lines" => 0)
+    @tag = buffer.create_tag("top-margin", "pixels-above-lines" => 0)
 
     word_end = buffer.start_iter
     word_end.forward_word_end
-    buffer.apply_tag(tag, buffer.start_iter, word_end)
-
-    window.add(overlay)
-    overlay.add(sw)
-    sw.add(text)
-
-    image = Gtk::Image.new(:resource => "/overlay2/decor1.png")
-    overlay.add_overlay(image)
-    overlay.set_overlay_pass_through(image, true)
-    image.set_halign(:start)
-    image.set_valign(:start)
-
-    image = Gtk::Image.new(:resource => "/overlay2/decor2.png")
-    overlay.add_overlay(image)
-    overlay.set_overlay_pass_through(image, true)
-    image.set_halign(:end)
-    image.set_valign(:end)
-
-    adjustment = Gtk::Adjustment.new(0, 0, 100, 1, 1, 0)
-    adjustment.signal_connect "value-changed" do |widget|
-      value = widget.value
-      text.set_left_margin(value)
-      tag.set_property("pixels-above-lines", value)
-    end
-
-    scale = Gtk::Scale.new(:horizontal, adjustment)
-    scale.set_draw_value(false)
-    scale.set_size_request(120, -1)
-    scale.set_margin_start(20)
-    scale.set_margin_end(20)
-    scale.set_margin_bottom(20)
-    overlay.add_overlay(scale)
-    scale.set_halign(:start)
-    scale.set_valign(:end)
-    scale.set_tooltip_text("Margin")
-
-    adjustment.value = 100
-    overlay.show_all
-
-    if !window.visible?
-      window.show_all
-    else
-      window.destroy
-    end
-    window
+    buffer.apply_tag(@tag, buffer.start_iter, word_end)
   end
 end
