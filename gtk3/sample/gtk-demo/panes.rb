227381974548865622ed29ccc6b6a7e3535d1a17
cedlemo <cedlemo@gmx.com>
2016-09-03 11:11:07 +0200

    Use the class from instead of module in panes demo

--- gtk3/sample/gtk-demo/panes.rb
+++ gtk3/sample/gtk-demo/panes.rb
@@ -14,27 +14,27 @@
  widget, and allows you to adjust the options for each side of
  each widget.
 =end
-module PanesDemo
-  def self.run_demo(main_window)
-    window = Gtk::Window.new(:toplevel)
-    window.screen = main_window.screen
-    window.title = "Paned Widgets"
-    window.border_width = 0
+class PanesDemo
+  def initialize(main_window)
+    @window = Gtk::Window.new(:toplevel)
+    @window.screen = main_window.screen
+    @window.title = "Paned Widgets"
+    @window.border_width = 0
 
     vbox = Gtk::Box.new(:vertical, 0)
-    window.add(vbox)
+    @window.add(vbox)
 
     vpaned = Gtk::Paned.new(:vertical)
     vbox.pack_start(vpaned, :expand => true, :fill => true, :padding => 0)
     vpaned.border_width = 5
 
-    hpaned = Gtk::Paned.new(:horizontal)
-    vpaned.add1(hpaned)
+    @hpaned = Gtk::Paned.new(:horizontal)
+    vpaned.add1(@hpaned)
 
     frame = Gtk::Frame.new
     frame.shadow_type = :in
     frame.set_size_request(60, 60)
-    hpaned.add1(frame)
+    @hpaned.add1(frame)
 
     button = Gtk::Button.new(:label => "_Hi there", :use_underline => true)
     frame.add(button)
@@ -42,7 +42,7 @@ module PanesDemo
     frame = Gtk::Frame.new
     frame.shadow_type = :in
     frame.set_size_request(80, 60)
-    hpaned.add2(frame)
+    @hpaned.add2(frame)
 
     frame = Gtk::Frame.new
     frame.shadow_type = :in
@@ -50,88 +50,76 @@ module PanesDemo
     vpaned.add2(frame)
 
     # Now create toggle buttons to control sizing
-    buttons = create_pane_options(hpaned,
-                                  "Horizontal",
+    buttons = create_pane_options("Horizontal",
                                   "Left",
                                   "Right")
     vbox.pack_start(buttons, :expand => false, :fill => false, :padding => 0)
-    buttons = create_pane_options(hpaned,
-                                  "Vertical",
+    buttons = create_pane_options("Vertical",
                                   "Top",
                                   "Bottom")
     vbox.pack_start(buttons, :expand => false, :fill => false, :padding => 0)
     vbox.show_all
-
-    if !window.visible?
-      window.show_all
-    else
-      window.destroy
-    end
-    window
   end
 
-  def self.create_pane_options(paned, frame_label, label1, label2)
-    child1 = paned.child1
-    child2 = paned.child2
+  def run
+    if !@window.visible?
+      @window.show_all
+    else
+      @window.destroy
+    end
+    @window
+  end
 
+  private
+
+  def create_pane_options(frame_label, label1, label2)
+    child1 = @hpaned.child1
+    child2 = @hpaned.child2
     frame = Gtk::Frame.new(frame_label)
     frame.border_width = 4
 
-    table = Gtk::Grid.new
-    frame.add(table)
+    @table = Gtk::Grid.new
+    frame.add(@table)
 
     label = Gtk::Label.new(label1)
-    table.attach(label, 0, 0, 1, 1)
+    @table.attach(label, 0, 0, 1, 1)
 
-    check_button = Gtk::CheckButton.new("_Resize")
-    check_button.use_underline = true
-    table.attach(check_button, 0, 1, 1, 1)
-    check_button.signal_connect "toggled" do
-      resize = paned.child_get_property(child1, "resize")
-      shrink = paned.child_get_property(child1, "shrink")
-
-      paned.remove(child1)
-      paned.pack1(child1, :resize => !resize, :shrink => shrink)
-    end
-
-    check_button = Gtk::CheckButton.new("_Shrink")
-    check_button.use_underline = true
-    table.attach(check_button, 0, 2, 1, 1)
-    check_button.active = true
-    check_button.signal_connect "toggled" do
-      resize = paned.child_get_property(child1, "resize")
-      shrink = paned.child_get_property(child1, "shrink")
-
-      paned.remove(child1)
-      paned.pack1(child1, :resize => resize, :shrink => !shrink)
-    end
+    check_button(:resize, false, child1, 0, 1)
+    check_button(:shrink, true, child1, 0, 2)
 
     label = Gtk::Label.new(label2)
-    table.attach(label, 1, 0, 1, 1)
+    @table.attach(label, 1, 0, 1, 1)
 
-    check_button = Gtk::CheckButton.new("_Resize")
-    check_button.use_underline = true
-    table.attach(check_button, 1, 1, 1, 1)
-    check_button.active = true
-    check_button.signal_connect "toggled" do
-      resize = paned.child_get_property(child2, "resize")
-      shrink = paned.child_get_property(child2, "shrink")
-
-      paned.remove(child2)
-      paned.pack2(child2, :resize => !resize, :shrink => shrink)
-    end
-
-    check_button = Gtk::CheckButton.new("_Shrink")
-    check_button.use_underline = true
-    table.attach(check_button, 1, 2, 1, 1)
-    check_button.active = true
-    check_button.signal_connect "toggled" do
-      resize = paned.child_get_property(child2, "resize")
-      shrink = paned.child_get_property(child2, "shrink")
-
-      paned.remove(child2)
-      paned.pack2(child2, :resize => resize, :shrink => !shrink)
-    end
+    check_button(:resize, true, child2, 1, 1)
+    check_button(:shrink, true, child2, 1, 2)
     frame
+  end
+
+  def check_button(type, active, child, xposition, yposition)
+    is_resize = (type == :resize)
+    label = is_resize ? "_Resize" : "_Shrink"
+    check_button = Gtk::CheckButton.new(label)
+    check_button.use_underline = true
+    check_button.active = active
+    @table.attach(check_button, xposition, yposition, 1, 1)
+    check_button.signal_connect "toggled" do
+      is_child1 = (@hpaned.child1 == child)
+
+      resize = @hpaned.child_get_property(child, "resize")
+      shrink = @hpaned.child_get_property(child, "shrink")
+
+      if is_resize
+        resize = !resize
+      else
+        shrink = !shrink
+      end
+
+      @hpaned.remove(child)
+      if is_child1
+        @hpaned.pack1(child, :resize => resize, :shrink => shrink)
+      else
+        @hpaned.pack2(child, :resize => resize, :shrink => shrink)
+      end
+    end
   end
 end
