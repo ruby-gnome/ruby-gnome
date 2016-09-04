2051081697b59fc0467b40f4eb45785f179a3829
cedlemo <cedlemo@gmx.com>
2016-09-04 18:36:51 +0200

    Clean code and fix style issues

--- gtk3/sample/gtk-demo/pickers.rb
+++ gtk3/sample/gtk-demo/pickers.rb
@@ -11,54 +11,36 @@ class PickersDemo
   def initialize(main_window)
     @window = Gtk::Window.new(:toplevel)
     @window.screen = main_window.screen
-    @window.set_title("Pickers")
-    @window.set_border_width(10)
+    @window.title = "Pickers"
+    @window.border_width = 10
 
-    table = Gtk::Grid.new
-    table.set_row_spacing(3)
-    table.set_column_spacing(10)
-    @window.add(table)
+    initialize_grid
 
-    label = Gtk::Label.new("Color:")
-    label.set_halign(:start)
-    label.set_valign(:center)
-    label.set_hexpand(true)
+    label = generate_label("Color:")
     picker = Gtk::ColorButton.new
-    table.attach(label, 0, 0, 1, 1)
-    table.attach(picker, 1, 0, 1, 1)
+    @table.attach(label, 0, 0, 1, 1)
+    @table.attach(picker, 1, 0, 1, 1)
 
-    label = Gtk::Label.new("Font:")
-    label.set_halign(:start)
-    label.set_valign(:center)
-    label.set_hexpand(true)
+    label = generate_label("Font:")
     picker = Gtk::FontButton.new
-    table.attach(label, 0, 1, 1, 1)
-    table.attach(picker, 1, 1, 1, 1)
+    @table.attach(label, 0, 1, 1, 1)
+    @table.attach(picker, 1, 1, 1, 1)
 
-    label = Gtk::Label.new("File:")
-    label.set_halign(:start)
-    label.set_valign(:center)
-    label.set_hexpand(true)
+    label = generate_label("File:")
     picker = Gtk::FileChooserButton.new("Pick a file", :open)
-    table.attach(label, 0, 2, 1, 1)
-    table.attach(picker, 1, 2, 1, 1)
+    @table.attach(label, 0, 2, 1, 1)
+    @table.attach(picker, 1, 2, 1, 1)
 
-    label = Gtk::Label.new("Folder:")
-    label.set_halign(:start)
-    label.set_valign(:center)
-    label.set_hexpand(true)
+    label = generate_label("Folder:")
     picker = Gtk::FileChooserButton.new("Pick a folder", :select_folder)
-    table.attach(label, 0, 3, 1, 1)
-    table.attach(picker, 1, 3, 1, 1)
+    @table.attach(label, 0, 3, 1, 1)
+    @table.attach(picker, 1, 3, 1, 1)
 
-    label = Gtk::Label.new("Mail:")
-    label.set_halign(:start)
-    label.set_valign(:center)
-    label.set_hexpand(true)
+    label = generate_label("Mail:")
     picker = Gtk::AppChooserButton.new("x-scheme-handler/mailto")
     picker.set_show_dialog_item(true)
-    table.attach(label, 0, 4, 1, 1)
-    table.attach(picker, 1, 4, 1, 1)
+    @table.attach(label, 0, 4, 1, 1)
+    @table.attach(picker, 1, 4, 1, 1)
   end
 
   def run
@@ -68,5 +50,22 @@ class PickersDemo
       @window.destroy
     end
     @window
+  end
+
+  private
+
+  def initialize_grid
+    @table = Gtk::Grid.new
+    @table.row_spacing = 3
+    @table.column_spacing = 10
+    @window.add(@table)
+  end
+
+  def generate_label(label)
+    label = Gtk::Label.new(label)
+    label.set_halign(:start)
+    label.set_valign(:center)
+    label.set_hexpand(true)
+    label
   end
 end
