0661021636149a5914c34485e04aef2fe40f5af8
cedlemo <cedlemo@gmx.com>
2016-12-25 19:59:23 +0100

    test-settings.rb : ensure that the keyfile.ini created is removed after test

--- gio2/test/test-settings.rb
+++ gio2/test/test-settings.rb
@@ -63,13 +63,16 @@ class TestSettings < Test::Unit::TestCase
     keyfile = "#{ENV["GSETTINGS_SCHEMA_DIR"]}/keyfile.ini"
     backend = Gio::keyfile_settings_backend_new(keyfile, "/", "keyfile_settings")
     settings = Gio::Settings.new("jp.ruby-gnome2.test.value", :backend => backend)
+
     settings.reset("string")
     assert_equal("default-string", settings["string"])
     settings["string"] = "new-string"
     assert_equal("new-string", settings["string"])
+
     keyfile_content_ref = "[jp/ruby-gnome2/test/value]\nstring='new-string'\n"
     keyfile_content = File.read(keyfile)
     assert_equal(keyfile_content_ref, keyfile_content)
+    FileUtils.rm_f(keyfile)
   end
 
   private
