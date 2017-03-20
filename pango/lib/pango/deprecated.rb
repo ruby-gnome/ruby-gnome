7c13942f9e90fce4df0900bce203c239111e6132
Kouhei Sutou <kou@clear-code.com>
2017-03-20 15:49:57 +0900

    pango: keep backward compatibility

--- pango/lib/pango/deprecated.rb
+++ pango/lib/pango/deprecated.rb
@@ -15,37 +15,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module Pango
-  class Loader < GObjectIntrospection::Loader
+  extend GLib::Deprecatable
 
-    private
+  define_deprecated_const :CairoFontMap, "Pango::Cairo::FontMap"
 
-    def pre_load(repository, namespace)
-      @pending_attribute_infos = []
-    end
+  class Context
+    extend GLib::Deprecatable
 
-    def post_load(repository, namespace)
-      @pending_attribute_infos.each do |info|
-        define_struct(info, :parent => Attribute)
-      end
-      require_libraries
-    end
-
-    def load_struct_info(info)
-      case info.name
-      when /Class\z/
-        super
-      when/\AAttr[A-Z]/
-        @pending_attribute_infos << info
-      else
-        super
-      end
-    end
-
-    def require_libraries
-      require "pango/attr-language"
-      require "pango/version"
-
-      require "pango/deprecated"
-    end
+    define_deprecated_enums :Direction, "DIRECTION"
   end
 end
