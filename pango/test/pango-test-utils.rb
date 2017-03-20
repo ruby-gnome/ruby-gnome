683b59eda836c30d28c9b4a8461e0e68247058f3
Kouhei Sutou <kou@clear-code.com>
2017-03-20 15:44:22 +0900

    pango test: use Pango::Version

--- pango/test/pango-test-utils.rb
+++ pango/test/pango-test-utils.rb
@@ -2,7 +2,7 @@ module PangoTestUtils
  private
  def only_pango_version(major, minor, micro=nil)
    micro ||= 0
-   unless Pango.check_version?(major, minor, micro)
+   unless Pango::Version.or_later?(major, minor, micro)
      omit("Require Pango >= #{major}.#{minor}.#{micro}")
    end
  end
