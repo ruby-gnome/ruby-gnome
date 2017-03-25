e6dfad0017db8673ee3263cebafe71b156892cfa
cedlemo <cedlemo@gmx.com>
2017-03-25 11:01:23 +0100

    Keep backward compatibility with Pango::Rectangle#new

--- pango/lib/pango/rectangle.rb
+++ pango/lib/pango/rectangle.rb
@@ -15,38 +15,14 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module Pango
-  class Loader < GObjectIntrospection::Loader
-
-    private
-
-    def pre_load(repository, namespace)
-      @pending_attribute_infos = []
-    end
-
-    def post_load(repository, namespace)
-      @pending_attribute_infos.each do |info|
-        define_struct(info, :parent => Attribute)
-      end
-      require_libraries
-    end
-
-    def load_struct_info(info)
-      case info.name
-      when /Class\z/
-        super
-      when/\AAttr[A-Z]/
-        @pending_attribute_infos << info
-      else
-        super
-      end
-    end
-
-    def require_libraries
-      require "pango/attr-language"
-      require "pango/rectangle"
-      require "pango/version"
-
-      require "pango/deprecated"
+  class Rectangle
+    alias_method :initialize_raw, :initialize
+    def initialize(x, y, width, height)
+      initialize_raw
+      self.x = x
+      self.y = y
+      self.width = width
+      self.height = height
     end
   end
 end
