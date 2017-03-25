1b06c0666ef29dd87de876c01bd7eae5aff6b258
cedlemo <cedlemo@gmx.com>
2017-03-25 11:12:32 +0100

    Update copyright date

--- pango/test/test-color.rb
+++ pango/test/test-color.rb
@@ -1,6 +1,4 @@
-# -*- coding: utf-8 -*-
-#
-# Copyright (C) 2013  Ruby-GNOME2 Project Team
+# Copyright (C) 2013-2017 Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
