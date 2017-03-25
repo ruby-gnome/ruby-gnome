d958dd7ba0564c1ef15cfff3f492153f53a19091
cedlemo <cedlemo@gmx.com>
2017-03-25 11:09:30 +0100

    Backward compatibility for Pango::Color#new

--- pango/lib/pango/color.rb
+++ pango/lib/pango/color.rb
@@ -15,38 +15,13 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 module Pango
-  class Loader < GObjectIntrospection::Loader
-
-    private
-
-    def pre_load(repository, namespace)
-      @pending_attribute_infos = []
-    end
-
-    def post_load(repository, namespace)
-      @pending_attribute_infos.each do |info|
-        define_struct(info, :parent => Attribute)
-      end
-      require_libraries
-    end
-
-    def load_struct_info(info)
-      case info.name
-      when /Class\z/
-        super
-      when/\AAttr[A-Z]/
-        @pending_attribute_infos << info
-      else
-        super
-      end
-    end
-
-    def require_libraries
-      require "pango/attr-language"
-      require "pango/color"
-      require "pango/version"
-
-      require "pango/deprecated"
+  class Color
+    alias_method :initialize_raw, :initialize
+    def initialize(red, green, blue)
+      initialize_raw
+      self.red = red
+      self.green = green
+      self.blue = blue
     end
   end
 end
