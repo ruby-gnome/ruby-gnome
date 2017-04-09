a3011def47e81d926182261ba5d7f4cc5e328873
Kouhei Sutou <kou@clear-code.com>
2017-04-09 22:07:14 +0900

    pango test: work on LANG=C

--- pango/test/test-language.rb
+++ pango/test/test-language.rb
@@ -2,7 +2,8 @@ class TestLanguage < Test::Unit::TestCase
   include PangoTestUtils
 
   def test_default
-    assert_match(/\A[a-z]{2}-[a-z]{2}\z/, Pango::Language.default.to_s)
+    assert_match(/\A(?:[a-z]{2}-[a-z]{2}|(?:ic))\z/,
+                 Pango::Language.default.to_s)
   end
 
   def test_sample_string
