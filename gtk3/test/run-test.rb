f05d8b8a84c6a4d68f33bc76c2c8c75a8c645c15
Kouhei Sutou <kou@clear-code.com>
2017-04-25 21:57:06 +0900

    gtk3 test: reorder

--- gtk3/test/run-test.rb
+++ gtk3/test/run-test.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (C) 2013-2015  Ruby-GNOME2 Project Team
+# Copyright (C) 2013-2017  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -20,22 +20,22 @@ ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
 ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
 
 glib_base = File.join(ruby_gnome2_base, "glib2")
+gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
 atk_base = File.join(ruby_gnome2_base, "atk")
 cairo_gobject_base = File.join(ruby_gnome2_base, "cairo-gobject")
 pango_base = File.join(ruby_gnome2_base, "pango")
 gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
-gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
 gio2_base = File.join(ruby_gnome2_base, "gio2")
 gdk3_base = File.join(ruby_gnome2_base, "gdk3")
 gtk3_base = File.join(ruby_gnome2_base, "gtk3")
 
 [
   [glib_base, "glib2"],
+  [gobject_introspection_base, "gobject-introspection"],
   [atk_base, "atk"],
   [cairo_gobject_base, "cairo-gobject"],
   [pango_base, "pango"],
   [gdk_pixbuf_base, "gdk_pixbuf2"],
-  [gobject_introspection_base, "gobject-introspection"],
   [gio2_base, "gio2"],
   [gdk3_base, "gdk3"],
   [gtk3_base, "gtk3"]
