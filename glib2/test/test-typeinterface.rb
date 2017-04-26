d4272e16d028a0bcc53f13c964fcb2ae95b4379e
Kouhei Sutou <kou@clear-code.com>
2017-04-26 14:46:22 +0900

    glib2 test: remove needless require

--- glib2/test/test-typeinterface.rb
+++ glib2/test/test-typeinterface.rb
@@ -14,9 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require 'test/unit'
-require 'glib2'
-
 class TestTypeInterface < Test::Unit::TestCase
   def test_require_instantiatable_superclass
     bad_class = Class.new
