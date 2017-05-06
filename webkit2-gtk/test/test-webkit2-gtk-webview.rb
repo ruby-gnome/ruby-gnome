80067bea40940f75078543947d5038e7ee989f89
Matijs van Zuijlen <matijs@matijs.net>
2017-05-06 13:00:14 +0200

    Implement constructor with options hash for WebView

--- webkit2-gtk/test/test-webkit2-gtk-webview.rb
+++ webkit2-gtk/test/test-webkit2-gtk-webview.rb
@@ -15,47 +15,82 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 class TestWebKit2GtkWebView < Test::Unit::TestCase
-  def setup
-    @view = WebKit2Gtk::WebView.new
-    @http_server = WEBrick::HTTPServer.new(:Port => 0)
-    @http_server.mount_proc("/") do |request, response|
-      response.body = "Hello"
-    end
-    @http_server_thread = Thread.new do
-      @http_server.start
-    end
-  end
-
-  def teardown
-    @http_server.shutdown
-    @http_server_thread.join
-  end
-
-  def http_url
-    port = @http_server[:Port]
-    "http://127.0.0.1:#{port}/"
-  end
-
-  test "#load_uri" do
-    loaded = false
-
-    loop = GLib::MainLoop.new
-    timeout_id = GLib::Timeout.add(5000) do
-      timeout_id = nil
-      loop.quit
-      GLib::Source::REMOVE
-    end
-    @view.signal_connect("load-changed") do |view, event|
-      case event
-      when WebKit2Gtk::LoadEvent::FINISHED
-        GLib::Source.remove(timeout_id) if timeout_id
-        loaded = true
-        loop.quit
+  sub_test_case("#load_uri") do
+    def setup
+      @view = WebKit2Gtk::WebView.new
+      @http_server = WEBrick::HTTPServer.new(:Port => 0)
+      @http_server.mount_proc("/") do |request, response|
+        response.body = "Hello"
+      end
+      @http_server_thread = Thread.new do
+        @http_server.start
       end
     end
-    @view.load_uri(http_url)
-    loop.run
 
-    assert_true(loaded)
+    def teardown
+      @http_server.shutdown
+      @http_server_thread.join
+    end
+
+    def http_url
+      port = @http_server[:Port]
+      "http://127.0.0.1:#{port}/"
+    end
+
+    test "#load_uri" do
+      loaded = false
+
+      loop = GLib::MainLoop.new
+      timeout_id = GLib::Timeout.add(5000) do
+        timeout_id = nil
+        loop.quit
+        GLib::Source::REMOVE
+      end
+      @view.signal_connect("load-changed") do |view, event|
+        case event
+        when WebKit2Gtk::LoadEvent::FINISHED
+          GLib::Source.remove(timeout_id) if timeout_id
+          loaded = true
+          loop.quit
+        end
+      end
+      @view.load_uri(http_url)
+      loop.run
+
+      assert_true(loaded)
+    end
+  end
+
+  sub_test_case(".new") do
+    sub_test_case("Hash form") do
+      test "with context" do
+        context = WebKit2Gtk::WebContext.new
+        webview = WebKit2Gtk::WebView.new context: context
+        assert_equal(context, webview.context)
+        assert_nil(webview.user_content_manager)
+      end
+
+      test "with settings" do
+        settings = WebKit2Gtk::Settings.new
+        webview = WebKit2Gtk::WebView.new settings: settings
+        assert_equal(settings, webview.settings)
+        assert_nil(webview.user_content_manager)
+      end
+
+      test "with user content manager" do
+        manager = WebKit2Gtk::UserContentManager.new
+        webview = WebKit2Gtk::WebView.new user_content_manager: manager
+        assert_equal(manager, webview.user_content_manager)
+      end
+    end
+  end
+
+  sub_test_case("#new_with_related_view") do
+    test "create related view" do
+      settings = WebKit2Gtk::Settings.new
+      related = WebKit2Gtk::WebView.new settings: settings
+      webview = related.new_with_related_view
+      assert_equal(settings, webview.settings)
+    end
   end
 end
