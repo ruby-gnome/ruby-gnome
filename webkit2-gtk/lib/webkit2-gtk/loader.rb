e99afccd4e84249d9197ecdcf5d10815f111b386
Kouhei Sutou <kou@clear-code.com>
2017-05-07 01:05:00 +0900

    webkit2-gtk: split loader

--- webkit2-gtk/lib/webkit2-gtk/loader.rb
+++ webkit2-gtk/lib/webkit2-gtk/loader.rb
@@ -14,35 +14,41 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "gobject-introspection"
-require "gtk3"
-
-base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
-vendor_dir = base_dir + "vendor" + "local"
-vendor_bin_dir = vendor_dir + "bin"
-GLib.prepend_dll_path(vendor_bin_dir)
-
-require "webkit2-gtk/loader"
-
 module WebKit2Gtk
-  class << self
-    def const_missing(name)
-      init
-      if const_defined?(name)
-        const_get(name)
+  class Loader < GObjectIntrospection::Loader
+    NAMESPACE = "WebKit2"
+
+    def load
+      super(NAMESPACE)
+    end
+
+    private
+    def pre_load(repository, namespace)
+      define_version_module
+    end
+
+    def post_load(repository, namespace)
+      require_libraries
+    end
+
+    def define_version_module
+      @version_module = Module.new
+      @base_module.const_set("Version", @version_module)
+    end
+
+    def require_libraries
+      require "webkit2-gtk/version" if @version_module.const_defined?(:MAJOR)
+
+      require "webkit2-gtk/web-view"
+    end
+
+    def load_constant_info(info)
+      case info.name
+      when /_VERSION\z/
+        @version_module.const_set($PREMATCH, info.value)
       else
         super
       end
-    end
-
-    def init
-      class << self
-        remove_method(:init)
-        remove_method(:const_missing)
-      end
-      Gtk.init if Gtk.respond_to?(:init)
-      loader = Loader.new(self)
-      loader.load
     end
   end
 end
