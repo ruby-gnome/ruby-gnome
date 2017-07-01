531ea7a256d30c864af993a8f8047d808cc10381
cedlemo <cedlemo@gmx.com>
2017-07-01 19:32:15 +0200

    Update to new api of GLib::Bytes

--- gtk3/lib/gtk3/text-buffer.rb
+++ gtk3/lib/gtk3/text-buffer.rb
@@ -135,7 +135,7 @@ module Gtk
         when TextChildAnchor
           insert_text_child_anchor_raw(iter, target)
         else
-          insert_raw(iter, target, target.bytesize)
+          insert_raw(iter, target, target.to_s.length)
         end
       end
 
