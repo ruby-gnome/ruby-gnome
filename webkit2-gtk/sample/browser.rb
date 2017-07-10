8cf2350336875b549724d20474fc65e8455ea477
Kouhei Sutou <kou@clear-code.com>
2017-07-10 09:56:17 +0900

    webkit2-gtk sample: support showing the specified URI

--- webkit2-gtk/sample/browser.rb
+++ webkit2-gtk/sample/browser.rb
@@ -14,15 +14,33 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
+require "uri"
 require "webkit2-gtk"
 
+uri = URI.parse(ARGV.shift || "https://webkitgtk.org/")
+
 window = Gtk::Window.new
+window.set_default_size(800, 600)
 window.signal_connect("destroy") do
   Gtk.main_quit
 end
 
 view = WebKit2Gtk::WebView.new
-view.load_uri("http://webkitgtk.org/")
+view.signal_connect("load-changed") do |_, load_event|
+  p [:load_changed, view.uri, load_event]
+end
+view.signal_connect("load-failed") do |_, _, failed_uri, error|
+  message = "failed to load URI: #{failed_uri}: "
+  message << "#{error.class}(#{error.code}): #{error.message}"
+  puts(message)
+  true
+end
+
+if uri.class == URI::Generic
+  view.load_html(File.read(uri.path), uri.to_s)
+else
+  view.load_uri(uri.to_s)
+end
 
 window.add(view)
 window.show_all
