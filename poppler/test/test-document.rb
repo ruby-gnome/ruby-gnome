e97970291ed06ae0b87d1f11e59dfb9d0f1f82bb
cedlemo <cedlemo@gmx.com>
2017-07-29 11:05:18 +0200

    Use the raw api in order to make it work (to be improved)

--- poppler/test/test-document.rb
+++ poppler/test/test-document.rb
@@ -4,12 +4,12 @@ class TestDocument < Test::Unit::TestCase
     FileUtils.rm_f(saved_pdf)
 
     document = Poppler::Document.new(form_pdf)
-    find_first_text_field(document).text = "XXX"
+    find_first_text_field(document).text_set_text("XXX")
     assert(document.save(saved_pdf))
     assert(File.exist?(saved_pdf))
 
     reread_document = Poppler::Document.new(saved_pdf)
-    assert_equal("XXX", find_first_text_field(reread_document).text)
+    assert_equal("XXX", find_first_text_field(reread_document).text_get_text)
   end
 
   def test_save_a_copy
@@ -18,13 +18,13 @@ class TestDocument < Test::Unit::TestCase
 
     document = Poppler::Document.new(form_pdf)
     first_text_field = find_first_text_field(document)
-    default_text = first_text_field.text
-    first_text_field.text = "XXX"
+    default_text = first_text_field.text_get_text
+    first_text_field.text_set_text("XXX")
     assert(document.save_a_copy(copied_pdf))
     assert(File.exist?(copied_pdf))
 
     reread_document = Poppler::Document.new(copied_pdf)
-    assert_equal(default_text, find_first_text_field(reread_document).text)
+    assert_equal(default_text, find_first_text_field(reread_document).text_get_text)
   end
 
   private
@@ -32,7 +32,7 @@ class TestDocument < Test::Unit::TestCase
     document.each do |page|
       page.form_field_mapping.each do |mapping|
         field = mapping.field
-        return field if field.is_a?(Poppler::TextField)
+        return field if field.field_type == Poppler::FormFieldType::TEXT
       end
     end
   end
