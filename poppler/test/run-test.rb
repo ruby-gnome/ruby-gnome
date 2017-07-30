9d72a8c5579e0bdec9c2fd53dc31d0e3bc77b148
Kouhei Sutou <kou@clear-code.com>
2017-07-31 01:03:47 +0900

    poppler test: add missing GI dependency

--- poppler/test/run-test.rb
+++ poppler/test/run-test.rb
@@ -24,14 +24,19 @@ have_make = system("which make > /dev/null")
 glib_dir = File.expand_path(File.join(base_dir, "..", "glib2"))
 gdk_pixbuf_dir = File.expand_path(File.join(base_dir, "..", "gdk_pixbuf2"))
 pango_dir = File.join(File.join(base_dir, ".." , "pango"))
+gobject_introspection_dir =
+  File.join(File.join(base_dir, ".." , "gobject-introspection"))
 
 $LOAD_PATH.unshift(File.join(glib_dir, "test"))
 require 'glib-test-init'
 
-[[gdk_pixbuf_dir, "gdk_pixbuf2"],
- [pango_dir, "pango"],
- [glib_dir, "glib2"],
- [base_dir, "poppler"]].each do |dir, module_name|
+[
+  [glib_dir, "glib2"],
+  [gdk_pixbuf_dir, "gdk_pixbuf2"],
+  [pango_dir, "pango"],
+  [gobject_introspection_dir, "gobject-introspection"],
+  [base_dir, "poppler"],
+].each do |dir, module_name|
   if File.exist?(File.join(dir, "Makefile"))
     if have_make
       system("cd #{dir.dump} && make > /dev/null") or exit(false)
