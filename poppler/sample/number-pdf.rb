5d4ebc08dd475b12f4f2770981c5e2febfb53ca9
cedlemo <cedlemo@gmx.com>
2017-08-08 10:54:43 +0200

    Update sample to new api

--- poppler/sample/number-pdf.rb
+++ poppler/sample/number-pdf.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
-require 'poppler'
+require "poppler"
+require "pango"
 
 input = ARGV.first
 output = input.sub(/\.pdf$/, "-numbered.pdf")
@@ -21,7 +22,7 @@ Cairo::PDFSurface.new(output, *first_page.size) do |surface|
       layout = context.create_pango_layout
       layout.text = i.to_s
       layout.width = width * Pango::SCALE
-      layout.alignment = Pango::Layout::ALIGN_CENTER
+      layout.alignment = :center
       layout.font_description = Pango::FontDescription.new("Sans 288")
       context.move_to(0, (height - layout.pixel_size[1]) / 2.0)
       context.set_source_rgba(0.1, 0.1, 0.1, 0.5)
