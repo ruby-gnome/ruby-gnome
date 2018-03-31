d264c419008abace65313cf61df08a635aa76580
Kouhei Sutou <kou@clear-code.com>
2018-03-31 22:18:37 +0900

    windows: add Ruby 2.5 support
    GitHub: fix #1148
    
    Reported by Andy Meneely. Thanks!!!

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -3,8 +3,8 @@
 export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-ruby_versions="2.1.10 2.2.4 2.3.1 2.4.0"
-export RUBY_CC_VERSION="2.1.10:2.2.4:2.3.1:2.4.0"
+ruby_versions="2.1.10 2.2.4 2.3.1 2.4.0 2.5.0"
+export RUBY_CC_VERSION="2.1.10:2.2.4:2.3.1:2.4.0:2.5.0"
 
 N_CPUS=$(grep '^processor' /proc/cpuinfo | wc -l)
 export MAKE_N_JOBS=${N_CPUS}
