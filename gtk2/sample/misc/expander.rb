c39072d4e4a378b562b89f890819c2c64f2764b4
Robert A. Heiler <shevegen@gmail.com>
2018-04-02 16:01:09 +0200

    Update expander.rb

--- gtk2/sample/misc/expander.rb
+++ gtk2/sample/misc/expander.rb
@@ -2,7 +2,7 @@
 =begin
   expander.rb - Ruby/GTK2 sample script.
 
-  Copyright (c) 2004-2006 Ruby-GNOME2 Project Team
+  Copyright (c) 2004-2018 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 
   $Id: expander.rb,v 1.3 2006/06/17 13:18:12 mutoh Exp $
@@ -14,14 +14,14 @@ expander = Gtk::Expander.new("Expander Sample")
 expander.expanded = true
 
 button = Gtk::Button.new("Hide this button.")
-button.signal_connect("clicked") do
+button.signal_connect(:clicked) do
   expander.expanded = false
 end
 
 button.set_size_request(200, 200)
 expander.add(button)
 
-Gtk::Window.new.add(expander).show_all.signal_connect("destroy"){Gtk.main_quit}
+Gtk::Window.new.add(expander).show_all.signal_connect(:destroy){ Gtk.main_quit }
 
 Gtk.main
 
