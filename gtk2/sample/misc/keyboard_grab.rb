22260b3f89636ea421caf990349b6cb8337fc50d
Robert A. Heiler <shevegen@gmail.com>
2018-04-02 14:39:19 +0200

    Update keyboard_grab.rb
    Just using the shorter Symbol here.

--- gtk2/sample/misc/keyboard_grab.rb
+++ gtk2/sample/misc/keyboard_grab.rb
@@ -15,15 +15,15 @@ window = Gtk::Window.new("Keyboard Grab/Ungrab sample")
 button1 = Gtk::Button.new("Grab Keyboard!")
 button2 = Gtk::Button.new("Ungrab Keyboard!")
 
-button1.signal_connect('clicked') do
+button1.signal_connect(:clicked) do
   Gdk.keyboard_grab(window.window, true, Gdk::Event::CURRENT_TIME)
 end
 
-button2.signal_connect('clicked') do
+button2.signal_connect(:clicked) do
   Gdk.keyboard_ungrab(Gdk::Event::CURRENT_TIME)
 end
 
 window.add(Gtk::VBox.new.add(button1).add(button2))
-window.set_default_size(200,100).show_all.signal_connect("destroy"){Gtk.main_quit}
+window.set_default_size(200,100).show_all.signal_connect(:destroy){Gtk.main_quit}
 
 Gtk.main
