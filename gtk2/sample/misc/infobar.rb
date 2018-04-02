bf29d304ad5916bbb82698f307eeb2d3e13463ba
Robert A. Heiler <shevegen@gmail.com>
2018-04-02 16:02:28 +0200

    Update infobar.rb

--- gtk2/sample/misc/infobar.rb
+++ gtk2/sample/misc/infobar.rb
@@ -4,19 +4,19 @@ require 'gtk2'
 
 label=Gtk::Label.new.show
 
-info=Gtk::InfoBar.new
-info.no_show_all=true
+info = Gtk::InfoBar.new
+info.no_show_all = true
 info.content_area.add(label)
 info.add_button(Gtk::Stock::OK, Gtk::ResponseType::OK)
 info.signal_connect('response'){|info, response| info.hide}
 
-w=Gtk::Window.new
+w = Gtk::Window.new
 w.add(info)
 w.show_all
-w.signal_connect("delete_event"){Gtk.main_quit}
+w.signal_connect(:delete_event){ Gtk.main_quit }
 
 GLib::Timeout.add_seconds(1){
-  label.text='ERROR'
+  label.text = 'ERROR'
   info.message_type=Gtk::MessageType::ERROR
   info.show
   false
