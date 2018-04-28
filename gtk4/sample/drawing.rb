3283cb9e1697e78396765400562941c99a7f628f
cedlemo <cedlemo@gmx.com>
2018-04-28 19:40:23 +0200

    drawing demo : finish drag and button press signals

--- gtk4/sample/drawing.rb
+++ gtk4/sample/drawing.rb
@@ -22,7 +22,6 @@ require_relative "utils"
 
 require_gtk4
 
-surface = nil
 
 def clear_surface(surface)
   cr = Cairo::Context.new(surface)
@@ -31,84 +30,93 @@ def clear_surface(surface)
   cr.destroy
 end
 
+# Draw a rectangle on the surface at the given position
 def draw_brush(widget, surface, x, y)
   cr = Cairo::Context.new(surface)
   cr.rectangle(x - 3, y - 3, 6, 6)
   cr.fill
-  cr.destroy
   widget.queue_draw_area(x - 3, y - 3, 6, 6)
+end
+
+def generate_surface_from_widget(widget)
+  surface = nil
+  if widget.window
+    surface = widget.window.create_similar_surface(Cairo::CONTENT_COLOR,
+                                                   widget.allocated_width,
+                                                   widget.allocated_height)
+  end
+  surface
 end
 
 myapp = Gtk::Application.new "org.gtk.example", :flags_none
 
 myapp.signal_connect "activate" do |app|
-  win = Gtk::ApplicationWindow.new app
-  win.set_title "Drawing Area"
+  win = Gtk::ApplicationWindow.new(app)
+  win.set_title("Drawing Area")
 
-  win.signal_connect "delete-event" do
+  surface = nil
+
+  win.signal_connect "destroy" do
+    surface.destroy if surface
     win.destroy
   end
 
+
   frame = Gtk::Frame.new
   frame.shadow_type = Gtk::ShadowType::IN
-  win.add frame
+  win.add(frame)
 
   drawing_area = Gtk::DrawingArea.new
   # Set a minimum size
-  drawing_area.set_size_request 100, 100
-  frame.add drawing_area
+  drawing_area.set_size_request(100, 100)
+  frame.add(drawing_area)
 
-  # Signals used to handle the backing surface
-  drawing_area.set_draw_func do |_da, cr|
-    puts _da.class
-    puts cr.class
-    puts surface.class
-    cr.set_source(surface, 0, 0) if surface
+  drawing_area.set_draw_func do |da, cr|
+    if surface.nil?
+      surface = generate_surface_from_widget(da)
+      ## Initialize the surface to white
+      clear_surface(surface)
+    end
+    # Redraw the screen from the surface. Note that the draw
+    # callback receives a ready-to-be-used cairo_t that is already
+    # clipped to only draw the exposed areas of the widget
+    cr.set_source(surface, 0, 0)
     cr.paint
-    false
   end
 
-  drawing_area.signal_connect_after "size-allocate" do |da, alloc, baseline, clip|
-#  drawing_area.signal_connect "configure-event" do |da, _ev|
-
-    if da.window
-      surface.destroy if surface
-      surface = da.window.create_similar_surface(Cairo::CONTENT_COLOR,
-                                                 da.allocated_width,
-                                                 da.allocated_height)
-    end
+  drawing_area.signal_connect_after "size-allocate" do |da, alloc|
+    surface.destroy if surface
+    surface = generate_surface_from_widget(da)
     # Initialize the surface to white
     clear_surface(surface) if surface
-#
-#    # the configure event have been handled, no need for further
-#    # processing
-#    true
   end
 
-  # Event signals
-#  drawing_area.signal_connect "motion-notify-event" do |da, ev|
-#    return false unless surface
-#      coords = ev.coords
-#
-#    if ((ev.state & Gdk::EventMask::BUTTON_PRESS_MASK.to_i) != 0)
-#      draw_brush(da, surface, coords[0], coords[1])
-#    end
-#  end
+  drag = Gtk::GestureDrag.new(drawing_area)
+  drag.button = Gdk::BUTTON_PRIMARY
 
-  drawing_area.signal_connect "button-press-event" do |da, ev|
-    return false unless surface
+  start_x = 0.0
+  start_y = 0.0
 
-    if (ev.button == Gdk::BUTTON_PRIMARY)
-      coords = ev.coords
-      draw_brush(da, surface, coords[0], coords[1])
-    elsif (ev.button == Gdk::BUTTON_SECONDARY)
-      clear_surface(surface)
-      da.queue_draw
-    end
-
-    true
+  drag.signal_connect "drag-begin" do |gesture, x, y|
+    start_x = x
+    start_y = y
+    draw_brush(drawing_area, surface, x, y)
   end
 
+  drag.signal_connect "drag-update" do |gesture, x, y|
+    draw_brush(drawing_area, surface, start_x + x, start_y + y)
+  end
+
+  drag.signal_connect "drag-end" do |gesture, x, y|
+    draw_brush(drawing_area, surface, start_x + x, start_y + y)
+  end
+
+  press = Gtk::GestureMultiPress.new(drawing_area)
+  press.button = Gdk::BUTTON_SECONDARY
+  press.signal_connect "pressed" do |gesture, n_press, x, y|
+    clear_surface(surface)
+    drawing_area.queue_draw
+  end
   win.show
 end
 
