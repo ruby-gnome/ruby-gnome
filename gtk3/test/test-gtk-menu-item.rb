651f0a351a759b77ac4d04967d31f60e98c9c9b9
cedlemo <cedlemo@gmx.com>
2018-04-29 09:31:50 +0200

    Gtk3 : update test of Gtk::MenuItem test with hash as parameter

--- gtk3/test/test-gtk-menu-item.rb
+++ gtk3/test/test-gtk-menu-item.rb
@@ -1,13 +1,21 @@
 class TestGtkMenuItem < Test::Unit::TestCase
   include GtkTestUtils
 
-  def test_new_with_label
-    item = Gtk::MenuItem.new("Label")
-    assert_equal("Label", item.label)
-  end
+  sub_test_case ".new" do
+    test "without arguments" do
+      item = Gtk::MenuItem.new()
+      assert_equal("", item.label)
+    end
 
-  def test_new_with_nil_label
-    item = Gtk::MenuItem.new(nil)
-    assert_equal("", item.label)
+    test "with label" do
+      item = Gtk::MenuItem.new(:label => "Label")
+      assert_equal("Label", item.label)
+    end
+
+    test "with mnemonic" do
+      item = Gtk::MenuItem.new(:label => "Label", :use_underline => true)
+      assert_equal("Label", item.label)
+      assert(item.use_underline?)
+    end
   end
 end
