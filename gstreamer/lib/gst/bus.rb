ce3987415aa49940060149ef1f8767881837b18f
Kouhei Sutou <kou@clear-code.com>
2018-05-26 13:21:02 +0900

    gstreamer: make Gst::Bus#poll arguments omittable

--- gstreamer/lib/gst/bus.rb
+++ gstreamer/lib/gst/bus.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2013  Ruby-GNOME2 Project Team
+# Copyright (C) 2013-2018  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -36,5 +36,35 @@ module Gst
       set_sync_handler(&@sync_handler)
     end
     private :set_sync_handler
+
+    alias_method :poll_raw, :poll
+    def poll(*args)
+      events = nil
+      timeout = nil
+
+      n_args = args.size
+      case n_args
+      when 0
+      when 1
+        case args[0]
+        when Hash
+          options = args[0]
+          events = options[:events]
+          timeout = options[:timeout]
+        when Integer
+          timeout = args[0]
+        else
+          events = args[0]
+        end
+      when 2
+        events, timeout = args
+      else
+        mesasge = "wrong number of arguments "
+        message << "(given #{n_args}, expected 0..2)"
+        raise ArgumentError, message
+      end
+      poll_raw(events || :any,
+               timeout || CLOCK_TIME_NONE)
+    end
   end
 end
