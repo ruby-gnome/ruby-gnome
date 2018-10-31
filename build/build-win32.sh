9d55018935bfe3f58d499e19bfbf22b6b4d2c605
Kouhei Sutou <kou@clear-code.com>
2018-10-31 15:08:39 +0900

    windows: remove virtual machines for building fat gems

--- build/build-win32.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export BUILD_HOST="i686-w64-mingw32"
-
-/vagrant/build-windows.sh
--- build/build-win64.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-export BUILD_HOST="x86_64-w64-mingw32"
-
-export RCAIRO_WINDOWS_64=yes
-export RUBY_GNOME2_BUILD_ARCHITECTURE=x64
-
-/vagrant/build-windows.sh
