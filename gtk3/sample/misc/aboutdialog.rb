72ae43a09ad473d6aff793e3b6dc1e62755e593b
kojix2 <2xijok@gmail.com>
2018-11-21 20:00:51 +0900

    update

--- gtk3/sample/misc/aboutdialog.rb
+++ gtk3/sample/misc/aboutdialog.rb
@@ -2,9 +2,10 @@
 =begin
   aboutdialog.rb - Ruby/GTK sample script.
 
-  Copyright (c) 2005-2015 Ruby-GNOME2 Project Team
+  Copyright (c) 2005-2018 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 =end
+
 require "gtk3"
 
 unless Gtk::Version.or_later?(3, 4, 2)
@@ -13,18 +14,18 @@ unless Gtk::Version.or_later?(3, 4, 2)
 end
 
 a = Gtk::AboutDialog.new
-a.artists   = ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"]
-a.authors = ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"]
-a.comments  = "This is a sample script for Gtk::AboutDialog"
-a.copyright = "Copyright (C) 2005 Ruby-GNOME2 Project"
-a.documenters = ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"]
-a.license   = "This program is licenced under the same licence as Ruby-GNOME2."
-a.logo = GdkPixbuf::Pixbuf.new(:file => File.join(__dir__, "gnome-logo-icon.png"))
-a.program_name = "Gtk::AboutDialog sample"
+a.artists            = ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"]
+a.authors            = ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"]
+a.comments           = "This is a sample script for Gtk::AboutDialog"
+a.copyright          = "Copyright (C) 2005 Ruby-GNOME2 Project"
+a.documenters        = ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"]
+a.license            = "This program is licenced under the same licence as Ruby-GNOME2."
+a.logo               = GdkPixbuf::Pixbuf.new(:file => File.join(__dir__, "gnome-logo-icon.png"))
+a.program_name       = "Gtk::AboutDialog sample"
 a.translator_credits = "Translator 1\nTranslator 2\n"
-a.version   = "1.0.0"
-a.website   = "https://ruby-gnome2.osdn.jp"
-a.website_label = "Ruby-GNOME2 Project Website"
+a.version            = "1.0.0"
+a.website            = "https://ruby-gnome2.osdn.jp"
+a.website_label      = "Ruby-GNOME2 Project Website"
 
 a.signal_connect("activate-link") do |_widget, uri|
   p _widget.class
