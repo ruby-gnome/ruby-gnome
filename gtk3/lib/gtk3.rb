0ae7747baed521d8fb383d8469f9a461faa800ff
Kouhei Sutou <kou@clear-code.com>
2018-11-22 16:34:33 +0900

    gtk3 windows: remove needless rsvg2 gem dependency
    GitHub: fix #1261
    
    Reported by noanoa07. Thanks!!!

--- gtk3/lib/gtk3.rb
+++ gtk3/lib/gtk3.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2006-2015  Ruby-GNOME2 Project Team
+# Copyright (C) 2006-2018  Ruby-GNOME2 Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -18,10 +18,6 @@ require "gobject-introspection"
 require "atk"
 require "gdk3"
 require "gio2"
-
-if /cygwin|mingw|mswin/ === RUBY_PLATFORM
-  require "rsvg2"
-end
 
 require "gtk3/loader"
 
