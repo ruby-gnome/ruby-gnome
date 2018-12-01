72ffcca81ecc77bf21d489b173a90ba0eb618262
Kouhei Sutou <kou@clear-code.com>
2018-12-01 09:11:06 +0900

    gio2: stop to support fat gem

--- gio2/gio2.gemspec
+++ gio2/gio2.gemspec
@@ -43,7 +43,6 @@ Gem::Specification.new do |s|
   s.files += Dir.glob("ext/#{s.name}/*.{c,h,def,rb}")
   s.files += Dir.glob("test/**/*")
 
-  s.add_runtime_dependency("glib2", "= #{s.version}")
   s.add_runtime_dependency("gobject-introspection", "= #{s.version}")
 
   s.metadata["msys2_mingw_dependencies"] = "glib-networking"
--- gio2/lib/gio2.rb
+++ gio2/lib/gio2.rb
@@ -15,14 +15,6 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 require "gobject-introspection"
-require "glib2"
-
-base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
-vendor_dir = base_dir + "vendor" + "local"
-vendor_bin_dir = vendor_dir + "bin"
-GLib.prepend_dll_path(vendor_bin_dir)
-vendor_girepository_dir = vendor_dir + "lib" + "girepository-1.0"
-GObjectIntrospection.prepend_typelib_path(vendor_girepository_dir)
 
 require "gio2/loader"
 
