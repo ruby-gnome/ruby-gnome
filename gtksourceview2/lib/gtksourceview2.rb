e4e4ed0d489d35e501c72fe1b93f9e829f71f9d2
Kouhei Sutou <kou@clear-code.com>
2018-12-01 09:27:37 +0900

    gtksourecview2: stop to support fat gem

--- gtksourceview2/lib/gtksourceview2.rb
+++ gtksourceview2/lib/gtksourceview2.rb
@@ -5,15 +5,6 @@
   This program is licensed under the same licence as Ruby-GNOME2.
 =end
 
-require 'gtk2'
+require "gtk2"
 
-base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
-vendor_dir = base_dir + "vendor" + "local"
-vendor_bin_dir = vendor_dir + "bin"
-GLib.prepend_dll_path(vendor_bin_dir)
-begin
-  major, minor, = RUBY_VERSION.split(/\./)
-  require "#{major}.#{minor}/gtksourceview2.so"
-rescue LoadError
-  require "gtksourceview2.so"
-end
+require "gtksourceview2.so"
