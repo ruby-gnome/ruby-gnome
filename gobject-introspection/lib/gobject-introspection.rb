c31f1e445b3af0fc15d6d2c36c90ccd0ad52f820
Kouhei Sutou <kou@clear-code.com>
2018-12-01 09:17:38 +0900

    gobject-introspection: stop to support fat gem

--- gobject-introspection/lib/gobject-introspection.rb
+++ gobject-introspection/lib/gobject-introspection.rb
@@ -31,19 +31,7 @@ module GObjectIntrospection
   end
 end
 
-base_dir = Pathname.new(__FILE__).dirname.dirname.expand_path
-vendor_dir = base_dir + "vendor" + "local"
-vendor_bin_dir = vendor_dir + "bin"
-GLib.prepend_dll_path(vendor_bin_dir)
-begin
-  major, minor, _ = RUBY_VERSION.split(/\./)
-  require "#{major}.#{minor}/gobject_introspection.so"
-rescue LoadError
-  require "gobject_introspection.so"
-end
-
-vendor_girepository_dir = vendor_dir + "lib" + "girepository-1.0"
-GObjectIntrospection.prepend_typelib_path(vendor_girepository_dir)
+require "gobject_introspection.so"
 
 module GObjectIntrospection
   LOG_DOMAIN = "GObjectIntrospection"
