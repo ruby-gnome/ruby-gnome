75d007a04d7dcd89beacdb0c2541b94469831e0b
Kouhei Sutou <kou@clear-code.com>
2019-03-10 05:14:58 +0900

    gvlc: update extconf.rb based on gtk3's one

--- gvlc/ext/gvlc/extconf.rb
+++ gvlc/ext/gvlc/extconf.rb
@@ -1,8 +1,22 @@
-=begin
-extconf.rb for Ruby/VLC extention library
-=end
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2012-2019  Ruby-GNOME2 Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require 'pathname'
+require "pathname"
 
 base_dir = Pathname(__FILE__).dirname.parent.parent.expand_path
 top_dir = base_dir.parent
@@ -13,7 +27,7 @@ version_suffix = ""
 unless mkmf_gnome2_dir.exist?
   if /(-\d+\.\d+\.\d+)(?:\.\d+)?\z/ =~ base_dir.basename.to_s
     version_suffix = $1
-    mkmf_gnome2_dir = top_dir + "glib2#{version_suffix}" + 'lib'
+    mkmf_gnome2_dir = top_dir + "glib2#{version_suffix}" + "lib"
   end
 end
 
@@ -22,17 +36,13 @@ $LOAD_PATH.unshift(mkmf_gnome2_dir.to_s)
 module_name = "gvlc"
 package_id = "libvlc"
 
-begin
-  require 'mkmf-gnome2'
-rescue LoadError
-  require 'rubygems'
-  gem 'glib2'
-  require 'mkmf-gnome2'
-end
+require "mkmf-gnome2"
 
 ["glib2"].each do |package|
   directory = "#{package}#{version_suffix}"
-  build_dir = "#{directory}/tmp/#{RUBY_PLATFORM}/#{package}/#{RUBY_VERSION}"
+  build_base_path = "#{directory}/tmp/#{RUBY_PLATFORM}"
+  package_library_name = package.gsub(/-/, "_")
+  build_dir = "#{build_base_path}/#{package_library_name}/#{RUBY_VERSION}"
   add_depend_package(package, "#{directory}/ext/#{package}",
                      top_dir.to_s,
                      :top_build_dir => top_build_dir.to_s,
@@ -42,17 +52,20 @@ end
 unless required_pkg_config_package([package_id, 2, 0, 0],
                                    :alt_linux => "libvlc-devel",
                                    :debian => "libvlc-dev",
-                                   :msys2 => "vlc")
+                                   :msys2 => "vlc",
+                                   :homebrew => "vlc")
   exit(false)
 end
 
-unless required_pkg_config_package("gobject-2.0")
+unless PKGConfig.have_package("gobject-2.0")
   exit(false)
 end
 
 create_pkg_config_file("Ruby/VLC", package_id)
+
 $defs << "-DRUBY_VLC_COMPILATION"
 create_makefile(module_name)
+
 pkg_config_dir = with_config("pkg-config-dir")
 if pkg_config_dir.is_a?(String)
   File.open("Makefile", "ab") do |makefile|
@@ -60,4 +73,3 @@ if pkg_config_dir.is_a?(String)
     makefile.puts("pkgconfigdir=#{pkg_config_dir}")
   end
 end
-
