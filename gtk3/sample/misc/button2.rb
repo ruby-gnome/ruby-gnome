f5e6d6fa147fc01748f7ea86f0ee3d3b0a6ce16b
kojix2 <2xijok@gmail.com>
2020-02-11 20:43:50 +0900

    gtk3: fix button2 example

--- gtk3/sample/misc/button2.rb
+++ gtk3/sample/misc/button2.rb
@@ -2,8 +2,8 @@
 =begin
   button2.rb - Ruby/GTK2 sample script.
 
-  Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2002-2020 Ruby-GNOME2 Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 =end
 
 require "gtk3"
@@ -12,7 +12,7 @@ window = Gtk::Window.new("Gtk::Button sample")
 
 button1 = Gtk::Button.new(:mnemonic => "_HELLO")
 button2 = Gtk::Button.new(:label => "_HELLO")
-button3 = Gtk::Button.new(:stock_id => Gtk::Stock::QUIT)  
+button3 = Gtk::Button.new(:stock_id => Gtk::Stock::QUIT)
 
 button3.signal_connect("clicked"){ Gtk.main_quit }
 
@@ -22,7 +22,7 @@ box.add(button2)
 box.add(button3)
 
 window.add(box)
-window.set_default_size(100,100)
+window.set_default_size(100, 100)
 window.show_all
 
 Gtk.main
