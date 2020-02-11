19ec5c41726dcd72fb2c91c5c4f47465988e462d
kojix2 <2xijok@gmail.com>
2020-02-12 05:59:11 +0900

    gtk3: fix aboutdialog example (#1317)

--- gtk3/sample/misc/aboutdialog.rb
+++ gtk3/sample/misc/aboutdialog.rb
@@ -2,8 +2,8 @@
 =begin
   aboutdialog.rb - Ruby/GTK sample script.
 
-  Copyright (c) 2005-2018 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2005-2020 Ruby-GNOME Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 =end
 
 require "gtk3"
@@ -17,15 +17,15 @@ a = Gtk::AboutDialog.new
 a.artists            = ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"]
 a.authors            = ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"]
 a.comments           = "This is a sample script for Gtk::AboutDialog"
-a.copyright          = "Copyright (C) 2005 Ruby-GNOME2 Project"
+a.copyright          = "Copyright (C) 2020 Ruby-GNOME Project"
 a.documenters        = ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"]
-a.license            = "This program is licenced under the same licence as Ruby-GNOME2."
+a.license            = "This program is licenced under the same licence as Ruby-GNOME."
 a.logo               = GdkPixbuf::Pixbuf.new(:file => File.join(__dir__, "gnome-logo-icon.png"))
 a.program_name       = "Gtk::AboutDialog sample"
 a.translator_credits = "Translator 1\nTranslator 2\n"
 a.version            = "1.0.0"
 a.website            = "https://ruby-gnome2.osdn.jp"
-a.website_label      = "Ruby-GNOME2 Project Website"
+a.website_label      = "Ruby-GNOME Project Website"
 
 a.signal_connect("activate-link") do |_widget, uri|
   p _widget.class
