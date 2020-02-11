99ae2abb1f55734c10790e62de1fba6ccd8693bc
kojix2 <2xijok@gmail.com>
2020-02-12 06:15:08 +0900

    gtk3: fix builder example (#1319)

--- gtk3/sample/misc/builder.rb
+++ gtk3/sample/misc/builder.rb
@@ -5,8 +5,10 @@
   Copyright (c) 2015 Ruby-GNOME2 Project Team
   This program is licenced under the same licence as Ruby-GNOME2.
 =end
+
 require "gtk3"
-ui_file = "#{File.expand_path(File.dirname(__FILE__))}/builder.ui"
+
+ui_file = File.expand_path("builder.ui", __dir__)
 builder = Gtk::Builder.new
 builder.add_from_file(ui_file)
 
