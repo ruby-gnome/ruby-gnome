78a0dd4b38398fdcaf3f60e619e6aa03238e06f8
Sutou Kouhei <kou@clear-code.com>
2020-02-12 06:05:18 +0900

    Fix indent

--- gtk3/sample/misc/aboutdialog2.rb
+++ gtk3/sample/misc/aboutdialog2.rb
@@ -12,20 +12,21 @@ unless Gtk::Version.or_later?(3, 4, 2)
   exit
 end
 
-about_dialog = Gtk::AboutDialog.show(nil,
-		      "artists" => ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"],
-		      "authors" => ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"],
-		      "comments" => "This is a sample script for Gtk::AboutDialog",
-		      "copyright" => "Copyright (C) 2005-2020 Ruby-GNOME Project",
-		      "documenters" => ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"],
-		      "license" => "This program is licenced under the same licence as Ruby-GNOME.",
-		      "logo_icon_name" => "gtk-home",
-		      "program_name" => "Gtk::AboutDialog sample",
-		      "translator_credits" => "Translator 1 <no1@foo.bar.com>\nTranslator 2 <no2@foo.bar.com>\n",
-		      "version" => "1.0.0",
-		      "website" => "https://ruby-gnome2.osdn.jp",
-		      "website_label" => "Ruby-GNOME Project Website"
-		      )
+about_dialog = Gtk::AboutDialog.show(
+  nil,
+  "artists" => ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"],
+  "authors" => ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"],
+  "comments" => "This is a sample script for Gtk::AboutDialog",
+  "copyright" => "Copyright (C) 2005-2020 Ruby-GNOME Project",
+  "documenters" => ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"],
+  "license" => "This program is licenced under the same licence as Ruby-GNOME.",
+  "logo_icon_name" => "gtk-home",
+  "program_name" => "Gtk::AboutDialog sample",
+  "translator_credits" => "Translator 1 <no1@foo.bar.com>\nTranslator 2 <no2@foo.bar.com>\n",
+  "version" => "1.0.0",
+  "website" => "https://ruby-gnome2.osdn.jp",
+  "website_label" => "Ruby-GNOME Project Website"
+)
 
 about_dialog.signal_connect('delete_event') do
   Gtk.main_quit
