dfc6aa8b44d7febc660f0260c633ee3fb9072738
kojix2 <2xijok@gmail.com>
2020-02-11 20:16:07 +0900

    gtk3: fix aboutdialog2 example

--- gtk3/sample/misc/aboutdialog2.rb
+++ gtk3/sample/misc/aboutdialog2.rb
@@ -2,8 +2,8 @@
 =begin
   aboutdialog2.rb - Ruby/GTK sample script.
 
-  Copyright (c) 2005-2015 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2005-2020 Ruby-GNOME Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 =end
 require "gtk3"
 
@@ -12,19 +12,23 @@ unless Gtk::Version.or_later?(3, 4, 2)
   exit
 end
 
-Gtk::AboutDialog.show(nil,
+about_dialog = Gtk::AboutDialog.show(nil,
 		      "artists" => ["Artist 1 <no1@foo.bar.com>", "Artist 2 <no2@foo.bar.com>"],
 		      "authors" => ["Author 1 <no1@foo.bar.com>", "Author 2 <no2@foo.bar.com>"],
 		      "comments" => "This is a sample script for Gtk::AboutDialog",
-		      "copyright" => "Copyright (C) 2005 Ruby-GNOME2 Project",
+		      "copyright" => "Copyright (C) 2020 Ruby-GNOME Project",
 		      "documenters" => ["Documenter 1 <no1@foo.bar.com>", "Documenter 2 <no2@foo.bar.com>"],
-		      "license" => "This program is licenced under the same licence as Ruby-GNOME2.",
+		      "license" => "This program is licenced under the same licence as Ruby-GNOME.",
 		      "logo_icon_name" => "gtk-home",
 		      "program_name" => "Gtk::AboutDialog sample",
 		      "translator_credits" => "Translator 1 <no1@foo.bar.com>\nTranslator 2 <no2@foo.bar.com>\n",
 		      "version" => "1.0.0",
 		      "website" => "https://ruby-gnome2.osdn.jp",
-		      "website_label" => "Ruby-GNOME2 Project Website"
+		      "website_label" => "Ruby-GNOME Project Website"
 		      )
+
+about_dialog.signal_connect('delete_event') do
+  Gtk.main_quit
+end
 
 Gtk.main
