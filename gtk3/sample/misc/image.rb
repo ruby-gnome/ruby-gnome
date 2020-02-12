eac999681f15ccca691797da0e2f8d152e62797d
kojix2 <2xijok@gmail.com>
2020-02-12 23:10:09 +0900

    gtk3: improve image example

--- gtk3/sample/misc/image.rb
+++ gtk3/sample/misc/image.rb
@@ -2,42 +2,28 @@
 =begin
   image.rb - Ruby/GTK sample script.
 
-  Copyright (c) 2002-2015 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2002-2020 Ruby-GNOME Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 =end
 
 require "gtk3"
 
 window = Gtk::Window.new("Image")
-window.signal_connect("destroy") do
-  Gtk.main_quit
-end
-window.border_width = 0
-
-box1 = Gtk::Box.new(:vertical, 10)
-box1.border_width = 10
+window.signal_connect("destroy") { Gtk.main_quit }
+window.border_width = 10
 
 button = Gtk::Button.new
-box1.add(button)
 
-label = Gtk::Label.new("Gtk::Image\ntest")
+label = Gtk::Label.new("Gtk::Image")
 image = Gtk::Image.new(:file => "test.xpm")
 
-box2 = Gtk::Box.new(:horizontal, 5)
-box2.add(image)
-box2.add(label)
+box = Gtk::Box.new(:horizontal, 5)
+box.add(image)
+box.add(label)
 
-button.add(box2)
+button.add(box)
 
-box1.add(Gtk::Separator.new(:horizontal))
-
-button = Gtk::Button.new(:label => "close")
-button.signal_connect("clicked") do
-  Gtk.main_quit
-end
-box1.add(button)
-
-window.add(box1)
+window.add(button)
 window.show_all
 
 Gtk.main
