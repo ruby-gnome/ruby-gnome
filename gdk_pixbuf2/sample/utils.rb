9fe9c6149b0bceb11709a64132a555ddcca3ecc7
kojix2 <2xijok@gmail.com>
2020-02-16 14:18:49 +0900

    gdk_pixbuf2: fix utils example

--- gdk_pixbuf2/sample/utils.rb
+++ gdk_pixbuf2/sample/utils.rb
@@ -2,13 +2,13 @@
 =begin
   utils.rb - Ruby/GdkPixbuf sample script.
 
-  Copyright (c) 2002-2016 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2002-2020 Ruby-GNOME Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 
   $Id: utils.rb,v 1.4 2006/06/17 14:38:08 mutoh Exp $
 =end
 
-require 'gtk2'
+require 'gtk3'
 
 filename = ARGV[0]
 unless filename
@@ -18,7 +18,7 @@ end
 
 src =  GdkPixbuf::Pixbuf.new(:file => filename)
 
-vbox = Gtk::VBox.new
+vbox = Gtk::Box.new(:vertical)
 
 dst = src.add_alpha(true, 0, 0, 0)
 vbox.pack_start(Gtk::Image.new(dst))
@@ -28,14 +28,15 @@ dst = GdkPixbuf::Pixbuf.new(:colorspace => :rgb,
                             :bits_per_sample => 8, 
                             :width => src.width + 20,
                             :height => src.height + 30)
+dst.fill!(0xff000099) # Clears a pixbuf to the given RGBA value
 src.copy_area(0, 0, src.width / 2, src.height / 2, dst, 10, 20)
-vbox.pack_start(Gtk::Image.new(dst))
+vbox.pack_start(Gtk::Image.new(:pixbuf => dst))
 
 dst = src.saturate_and_pixelate(0.3, true)
-vbox.pack_start(Gtk::Image.new(dst))
+vbox.pack_start(Gtk::Image.new(:pixbuf => dst))
 
-dst = src.fill!(0xff000099) #RGBA
-vbox.pack_start(Gtk::Image.new(dst))
+dst = src.fill!(0xff000099)
+vbox.pack_start(Gtk::Image.new(:pixbuf => dst))
 
 w = Gtk::Window.new.add(vbox)
 w.signal_connect('delete-event') do
