2375116260d4ebe28e2079b7df38a90e3fcdc9b9
kojix2 <2xijok@gmail.com>
2020-02-18 00:14:01 +0900

    gtk3: improve stock example

--- gtk3/sample/misc/stock.rb
+++ gtk3/sample/misc/stock.rb
@@ -2,8 +2,8 @@
   stock.rb - Gtk::Stock sample script.
 
   Copyright (C) 2001-2006 Masao Mutoh
-  Copyright (c) 2001-2015 Ruby-GNOME2 Project Team
-  This program is licenced under the same licence as Ruby-GNOME2.
+  Copyright (c) 2001-2020 Ruby-GNOME Project Team
+  This program is licenced under the same licence as Ruby-GNOME.
 =end
 
 require "gtk3"
@@ -11,26 +11,22 @@ require "gtk3"
 window = Gtk::Window.new("Gtk::Stock sample")
 window.signal_connect("destroy") { Gtk.main_quit }
 
-stocks = Gtk::Stock.constants.sort
+stocks = Gtk::Stock.constants.sort.cycle
 
 image = Gtk::Image.new(:stock => Gtk::Stock::OK, :size => :dialog)
 label = Gtk::Label.new("Gtk::Stock::OK")
 button = Gtk::Button.new(:label => "Click!")
-cnt = 0
+
 button.signal_connect("clicked") do
-  stock_name = "Gtk::Stock::#{stocks[cnt]}"
+  stock = stocks.next
+  stock_name = "Gtk::Stock::#{stock}"
   label.set_text(stock_name)
   image.set_stock(eval(stock_name))
-  if cnt < stocks.size - 1
-    cnt += 1
-  else
-    cnt = 0
-  end
 end
 
 box = Gtk::Box.new(:vertical, 0)
-box.add(image)
-box.add(label)
+box.add(image, :expand => true)
+box.add(label, :expand => true)
 box.add(button)
 window.add(box)
 window.set_default_size(200, 200)
