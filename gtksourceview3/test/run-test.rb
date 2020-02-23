07ff4c42443465f482718bb05565e3047b290860
kojix2 <2xijok@gmail.com>
2020-02-23 12:56:44 +0900

    gtksourceview3: ruby_gnome2_base -> ruby_gnome_base

--- gtksourceview3/test/run-test.rb
+++ gtksourceview3/test/run-test.rb
@@ -1,18 +1,18 @@
 #!/usr/bin/env ruby
 
-ruby_gnome2_base = File.join(File.dirname(__FILE__), "..", "..")
-ruby_gnome2_base = File.expand_path(ruby_gnome2_base)
+ruby_gnome_base = File.join(File.dirname(__FILE__), "..", "..")
+ruby_gnome_base = File.expand_path(ruby_gnome_base)
 
-glib_base = File.join(ruby_gnome2_base, "glib2")
-atk_base = File.join(ruby_gnome2_base, "atk")
-cairo_gobject_base = File.join(ruby_gnome2_base, "cairo-gobject")
-pango_base = File.join(ruby_gnome2_base, "pango")
-gdk_pixbuf_base = File.join(ruby_gnome2_base, "gdk_pixbuf2")
-gobject_introspection_base = File.join(ruby_gnome2_base, "gobject-introspection")
-gio2_base = File.join(ruby_gnome2_base, "gio2")
-gdk3_base = File.join(ruby_gnome2_base, "gdk3")
-gtk3_base = File.join(ruby_gnome2_base, "gtk3")
-gtk_source_view3_base = File.join(ruby_gnome2_base, "gtksourceview3")
+glib_base = File.join(ruby_gnome_base, "glib2")
+atk_base = File.join(ruby_gnome_base, "atk")
+cairo_gobject_base = File.join(ruby_gnome_base, "cairo-gobject")
+pango_base = File.join(ruby_gnome_base, "pango")
+gdk_pixbuf_base = File.join(ruby_gnome_base, "gdk_pixbuf2")
+gobject_introspection_base = File.join(ruby_gnome_base, "gobject-introspection")
+gio2_base = File.join(ruby_gnome_base, "gio2")
+gdk3_base = File.join(ruby_gnome_base, "gdk3")
+gtk3_base = File.join(ruby_gnome_base, "gtk3")
+gtk_source_view3_base = File.join(ruby_gnome_base, "gtksourceview3")
 
 $LOAD_PATH.unshift(glib_base)
 require 'test/glib-test-init'
