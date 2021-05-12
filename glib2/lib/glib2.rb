dadf0d37b7099e89c8cf10f7ef95442753b13a29
Sutou Kouhei <kou@clear-code.com>
2021-05-12 23:01:56 +0900

    glib2: fix a typo

--- glib2/lib/glib2.rb
+++ glib2/lib/glib2.rb
@@ -145,7 +145,7 @@ module GLib
             return
           end
           return unless signal_.class != self
-          signal_handler_attach(signal, name.to_s) do |instance, *args|
+          signal_handler_attach(signal_, name.to_s) do |instance, *args|
             instance.__send__(signal_name, *args)
           end
         when /\A#{Regexp.escape(VIRTUAL_FUNCTION_IMPLEMENTATION_PREFIX)}/o
