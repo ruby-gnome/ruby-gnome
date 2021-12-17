98412cb10f0d2d080fd5b980536164d7c55c999d
Sutou Kouhei <kou@clear-code.com>
2021-12-17 14:10:44 +0900

    gi: add support for output buffer with input only size

--- gobject-introspection/ext/gobject-introspection/rb-gi-arguments-out.c
+++ gobject-introspection/ext/gobject-introspection/rb-gi-arguments-out.c
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2012-2019  Ruby-GNOME Project Team
+ *  Copyright (C) 2012-2021  Ruby-GNOME Project Team
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
@@ -307,7 +307,12 @@ rb_gi_arguments_out_init_arg_array_c(RBGIArguments *args,
                          rb_gi_transfer_to_string(metadata->transfer));
             }
             argument->v_pointer = RSTRING_PTR(metadata->rb_arg);
-            length_metadata->out_arg->v_size = RSTRING_LEN(metadata->rb_arg);
+            if (length_metadata->in_arg) {
+                length_metadata->in_arg->v_size = RSTRING_LEN(metadata->rb_arg);
+            }
+            if (length_metadata->out_arg) {
+                length_metadata->out_arg->v_size = RSTRING_LEN(metadata->rb_arg);
+            }
         } else {
             argument->v_pointer = ALLOC(guint8 *);
             metadata->free_func = rb_gi_arguments_out_free_array_c_uint8;
