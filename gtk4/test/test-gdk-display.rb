fb0f1af2373b328264cc95b97552c632c7bd9fd9
Sutou Kouhei <kou@clear-code.com>
2022-07-18 10:10:20 +0900

    gtk4 test gdk-display: update API to get device

--- gtk4/test/test-gdk-display.rb
+++ gtk4/test/test-gdk-display.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2015  Ruby-GNOME2 Project Team
+# Copyright (C) 2015-2022  Ruby-GNOME Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -19,7 +19,9 @@ class TestGdkDisplay < Test::Unit::TestCase
 
   test "device_is_grabbed?" do
     display = Gdk::Display.default
-    device = display.devices.first
-    assert_false(display.device_is_grabbed?(device))
+    device = display.default_seat.pointer
+    assert do
+      not display.device_is_grabbed?(device)
+    end
   end
 end
