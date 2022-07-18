707b1def3b17c379e6a9c9b0affb43dcd5c7881a
Sutou Kouhei <kou@clear-code.com>
2022-07-18 10:29:47 +0900

    gtk4 test about-dialog: update the default

--- gtk4/test/test-gtk-about-dialog.rb
+++ gtk4/test/test-gtk-about-dialog.rb
@@ -1,3 +1,19 @@
+# Copyright (C) 2008-2022  Ruby-GNOME Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
 class TestGtkAboutDialog < Test::Unit::TestCase
   include GtkTestUtils
 
@@ -6,7 +22,7 @@ class TestGtkAboutDialog < Test::Unit::TestCase
   end
 
   def test_program_name
-    assert_equal(File.basename($0), @dialog.program_name)
+    assert_nil(@dialog.program_name)
     @dialog.program_name = "my-program"
     assert_equal("my-program", @dialog.program_name)
   end
