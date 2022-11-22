4caff6d71d0eaca025a9900b4031f002426b736e
Sutou Kouhei <kou@cozmixng.org>
2022-11-23 07:52:01 +0900

    Fix copyright year

--- gtk4/sample/examples/window_default.rb
+++ gtk4/sample/examples/window_default.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 #
-# Copyright (C) 2013-2022  Ruby-GNOME Project Team
+# Copyright (C) 2022  Ruby-GNOME Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
