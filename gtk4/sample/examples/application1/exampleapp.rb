11e72400802bd6302293bb454aa8cb1ab49a5a5a
ToshioCP <ToshioCP@users.noreply.github.com>
2023-02-24 06:10:59 +0900

    gtk4: add sample/examples/application1 (#1549)
    The programs corresponds to the ones in the GTK repository.
    
    - `exampleapp.rb` is almost same as the one under `gtk3` direcotry in
      the Ruby-GNOME repository.
    - It doesn't use `utils.rb` because of the installation. It uses
      `require "gtk4"` instead.
    - `README.md` explains the installation and the desktop file. Its
      original file is `README` in the application1 directory in the original
      GTK repository. But some explanation is added for Ruby version.
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

---
+++ gtk4/sample/examples/application1/exampleapp.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+#
+# Copyright (C) 2023  Ruby-GNOME Project Team
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+# Example from:
+# * https://gitlab.gnome.org/GNOME/gtk/-/blob/main/examples/application1/exampleapp.c
+# * https://gitlab.gnome.org/GNOME/gtk/-/blob/main/examples/application1/exampleappwin.c#
+# License: LGPL2.1-or-later
+
+require "gtk4"
+
+class ExampleAppWindow < Gtk::ApplicationWindow
+  def open(file)
+  end
+end
+
+class ExampleApp < Gtk::Application
+  def initialize
+    super("org.gtk.exampleapp", :handles_open)
+
+    signal_connect "activate" do |application|
+      window = ExampleAppWindow.new(application)
+      window.present
+    end
+    signal_connect "open" do |application, files, hin|
+      window = application.windows[0] || ExampleAppWindow.new(application)
+      files.each do |file|
+        window.open(file)
+      end
+      window.present
+    end
+  end
+end
+
+app = ExampleApp.new
+app.run
