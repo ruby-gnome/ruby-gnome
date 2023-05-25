2a8d1e04cf9f863b9e9927ca060deed7f4adfd00
Sutou Kouhei <kou@clear-code.com>
2023-05-26 05:33:19 +0900

    glib2 signal: add support for write barrier

--- glib2/ext/glib2/rbgobj_signal.c
+++ glib2/ext/glib2/rbgobj_signal.c
@@ -37,7 +37,9 @@ static const rb_data_type_t rg_glib_signal_type = {
     },
     NULL,
     NULL,
-    RUBY_TYPED_FREE_IMMEDIATELY | RUBY_TYPED_FROZEN_SHAREABLE,
+    RUBY_TYPED_FREE_IMMEDIATELY |
+    RUBY_TYPED_WB_PROTECTED |
+    RUBY_TYPED_FROZEN_SHAREABLE,
 };
 
 static VALUE
