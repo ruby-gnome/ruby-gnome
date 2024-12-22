f838f5c3b3a17c29d443199520dc899b9e5b4e77
Eric Cunningham <Beagle4321_2000@yahoo.com>
2024-12-22 00:44:43 -0800

    gtksourceview5 sample: use gtksourceview5 (#1636)
    This example was listed under gtksourceview5, but it used
    gtksourceview4. I updated it to use gtk4/gtksourceview5.
    
    ---------
    
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- gtksourceview5/sample/test.rb
+++ gtksourceview5/sample/test.rb
@@ -1,35 +1,43 @@
 #!/usr/bin/env ruby
 #
-# test.rb - Ruby/GtkSourceView4 sample script.
+# test.rb - Ruby/GtkSourceView5 sample script.
 #
-# Copyright (c) 2006-2020 Ruby-GNOME Project Team
+# Copyright (c) 2006-2024 Ruby-GNOME Project Team
 # This program is licenced under the same licence as Ruby-GNOME.
 #
 # $Id: test.rb,v 1.4 2007/06/03 02:11:07 mutoh Exp $
 
-require 'gtksourceview4'
 
-window = Gtk::Window.new
-window.signal_connect('delete-event') { Gtk.main_quit }
+require 'gtksourceview5'
 
-view = GtkSource::View.new
-window.add(Gtk::ScrolledWindow.new.add(view))
-view.show_line_numbers = true
-view.insert_spaces_instead_of_tabs = true
-view.indent_width = 2
-view.show_right_margin = true
-view.right_margin_position = 80
 
-lang = GtkSource::LanguageManager.new.get_language('ruby')
-view.buffer.language = lang
-view.buffer.highlight_syntax = true
-view.buffer.highlight_matching_brackets = true
-view.buffer.text = File.read(__FILE__)
-provider = Gtk::CssProvider.new
-provider.load(data: 'textview { font-family: Monospace; font-size: 8pt; }')
-view.style_context.add_provider(provider)
+app = Gtk::Application.new("io.github.ruby-gnome.gtksourceview5.sample")
 
-window.set_default_size(480, 480)
-window.show_all
+app.signal_connect("activate") do
+  window = Gtk::ApplicationWindow.new(app)
+  
+  view = GtkSource::View.new
+  scrolled_window = Gtk::ScrolledWindow.new
+  scrolled_window.child = view
+  window.child = scrolled_window
+  view.show_line_numbers = true
+  view.insert_spaces_instead_of_tabs = true
+  view.indent_width = 2
+  view.show_right_margin = true
+  view.right_margin_position = 80
+  
+  lang = GtkSource::LanguageManager.new.get_language('ruby')
+  view.buffer.language = lang
+  view.buffer.highlight_syntax = true
+  view.buffer.highlight_matching_brackets = true
+  view.buffer.text = File.read(__FILE__)
+  provider = Gtk::CssProvider.new
+  provider.load(data: 'textview { font-family: Monospace; font-size: 8pt; }')
+  view.style_context.add_provider(provider)
+  
+  window.set_default_size(480, 480)
+  window.present
+end
 
-Gtk.main
+app.run
+
