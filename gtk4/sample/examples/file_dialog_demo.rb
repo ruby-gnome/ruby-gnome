ade0e8462514c312dd2337731814f663495a29dc
Eric Cunningham <Beagle4321_2000@yahoo.com>
2024-12-25 14:45:06 -0800

    Update gtk4/sample/examples/file_dialog_demo.rb
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- gtk4/sample/examples/file_dialog_demo.rb
+++ gtk4/sample/examples/file_dialog_demo.rb
@@ -28,7 +28,7 @@ require "gtk4"
 
 def on_activate(app)
   win = Gtk::ApplicationWindow.new(app)
-  dialog = Gtk::FileDialog.new()
+  dialog = Gtk::FileDialog.new
   dialog.modal = true
   dialog.set_initial_file(Gio::File.new_for_path(File.expand_path(__FILE__))) 
   dialog.set_initial_folder(Gio::File.new_for_path(Dir.pwd))
