8fecb4d8742399a56e406fe552a665e59a0af96a
ppibburr <tulnor33@gmail.com>
2025-01-06 19:32:12 -0500

    Update glib2/ext/glib2/rbgobj_typeinstance.c
    Co-authored-by: Sutou Kouhei <kou@cozmixng.org>

--- glib2/ext/glib2/rbgobj_typeinstance.c
+++ glib2/ext/glib2/rbgobj_typeinstance.c
@@ -114,8 +114,6 @@ rg_inspect(VALUE self)
 {
     rg_glib_instantiatable_holder *holder;
     TypedData_Get_Struct(self, rg_glib_instantiatable_holder, &rg_glib_instantiatable_type, holder);
-
-   
     return rb_sprintf("#<%" PRIsVALUE ":%p ptr=%p>",
                       rb_obj_class(self), (void *)self, holder->instance);
  
