c14bf27626e26de124c7d1438f63e3c6a7a66780
ppibburr <tulnor33@gmail.com>
2025-01-10 01:01:22 -0500

    Update rbglib-variant.c

--- glib2/ext/glib2/rbglib-variant.c
+++ glib2/ext/glib2/rbglib-variant.c
@@ -360,7 +360,8 @@ Init_glib_variant(void)
     RG_DEF_METHOD(initialize, -1);
     RG_DEF_METHOD(value, 0);
     RG_DEF_METHOD(type, 0);
-
+    RG_DEF_METHOD(to_s, -1);
+    
     G_DEF_ERROR(G_VARIANT_PARSE_ERROR,
                 "VariantParseError",
                 rbg_mGLib(),
