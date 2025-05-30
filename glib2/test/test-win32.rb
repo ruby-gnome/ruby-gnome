31b2961c55bf43ecd66e078f471fc3013e0ae982
Sutou Kouhei <kou@clear-code.com>
2025-05-30 10:32:54 +0900

    glib2 test: omit GLib::Win32.get_package_installation_directory_of_module
    We can't detect suitable expected on GitHub Actions by
    `RbConfig.ruby`.

--- glib2/test/test-win32.rb
+++ glib2/test/test-win32.rb
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2021  Ruby-GNOME Project Team
+# Copyright (C) 2015-2025  Ruby-GNOME Project Team
 #
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public
@@ -19,6 +19,9 @@ class TestGLibWin32 < Test::Unit::TestCase
 
   def test_get_package_install_directory_of_module
     only_windows
+    if ENV["GITHUB_ACTIONS"] == "true"
+      omit("We can't detect suitable expected on GitHub Actions")
+    end
 
     expected = Pathname(RbConfig.ruby).parent.parent.to_s
     actual = GLib::Win32.get_package_installation_directory_of_module
