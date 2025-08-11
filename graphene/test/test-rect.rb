16a6bc4c5c404dc902147bfeaa402fff84cd3d0b
Sutou Kouhei <kou@clear-code.com>
2025-08-11 18:32:45 +0900

    graphene test: remove needless require

--- graphene/test/test-rect.rb
+++ graphene/test/test-rect.rb
@@ -14,8 +14,6 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-require "stringio"
-
 class TestRect < Test::Unit::TestCase
   sub_test_case("#initialize") do
     test("nothing") do
