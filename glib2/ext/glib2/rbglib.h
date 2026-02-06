aeae70ec63c1cec420d43c85be194bbb2f06b16f
Sutou Kouhei <kou@clear-code.com>
2026-02-06 11:57:25 +0900

    Bump version

--- glib2/ext/glib2/rbglib.h
+++ glib2/ext/glib2/rbglib.h
@@ -1,6 +1,6 @@
 /* -*- c-file-style: "ruby"; indent-tabs-mode: nil -*- */
 /*
- *  Copyright (C) 2002-2025  Ruby-GNOME Project Team
+ *  Copyright (C) 2002-2026  Ruby-GNOME Project Team
  *  Copyright (C) 2002,2003  Masahiro Sakai
  *
  *  This library is free software; you can redistribute it and/or
@@ -33,7 +33,7 @@ G_BEGIN_DECLS
 
 #define RBGLIB_MAJOR_VERSION 4
 #define RBGLIB_MINOR_VERSION 3
-#define RBGLIB_MICRO_VERSION 5
+#define RBGLIB_MICRO_VERSION 6
 
 /* For Ruby < 3.0 */
 #ifndef RB_LL2NUM
