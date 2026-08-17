7d672803735043475e7cb46e5751348d11e68977
Sutou Kouhei <kou@clear-code.com>
2026-08-17 09:27:00 +0900

    glib2: use GXml for `GLib::InterfaceInfo#get_constant` test

--- dockerfiles/run-test.sh
+++ dockerfiles/run-test.sh
@@ -48,7 +48,7 @@ MAKEFLAGS="-j$(nproc)" rake -f /ruby-gnome/Rakefile gem:install
 if [ "${RUBY_GNOME_TEST_ENABLE:-yes}" != "no" ]; then
   if [ "${RUBY_GNOME_GLIB2_GIREPOSITORY_ENABLE:-no}" = "yes" ] && \
        apt --version; then
-    sudo apt install -y -V gir1.2-glib-2.0-dev
+    sudo apt install -y -V gir1.2-glib-2.0-dev gir1.2-gxml-0.20
   fi
   export RUBY_GNOME_BUILD_DIR="${PWD}"
   if type dbus-run-session > /dev/null 2>&1 &&
